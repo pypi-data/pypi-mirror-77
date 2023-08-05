7eaf193e6d59f079ae28dbbb6c8c9885f594e104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:17 +0000

    Add package raylab/0.12.2/raylab-0.12.2-py3-none-any.whl

---
+++ packages/raylab/raylab-0.12.2-py3-none-any.whl/raylab/policy/losses/__init__.py
@@ -0,0 +1,33 @@
+"""Modularized losses/procedures for RL algorithms."""
+from .cdq_learning import ClippedDoubleQLearning
+from .cdq_learning import SoftCDQLearning
+from .isfv_iteration import ISFittedVIteration
+from .isfv_iteration import ISSoftVIteration
+from .mage import MAGE
+from .maximum_entropy import MaximumEntropyDual
+from .mle import MaximumLikelihood
+from .mle import ModelEnsembleMLE
+from .policy_gradient import ActionDPG
+from .policy_gradient import DeterministicPolicyGradient
+from .policy_gradient import ReparameterizedSoftPG
+from .svg import OneStepSoftSVG
+from .svg import OneStepSVG
+from .svg import TrajectorySVG
+
+
+__all__ = [
+    "ClippedDoubleQLearning",
+    "ActionDPG",
+    "DeterministicPolicyGradient",
+    "ISFittedVIteration",
+    "ISSoftVIteration",
+    "MaximumEntropyDual",
+    "MAGE",
+    "MaximumLikelihood",
+    "ModelEnsembleMLE",
+    "OneStepSVG",
+    "OneStepSoftSVG",
+    "ReparameterizedSoftPG",
+    "SoftCDQLearning",
+    "TrajectorySVG",
+]
