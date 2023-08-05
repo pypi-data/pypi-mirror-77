e9967535ec0884210956913da1f4163ae48d98f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package pyramid-sawing/1.1.3/pyramid_sawing-1.1.3-py3-none-any.whl

---
+++ packages/pyramid-sawing/pyramid_sawing-1.1.3-py3-none-any.whl/pyramid_sawing/main.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+import logging
+from logging.config import dictConfig
+
+import yaml
+from pyramid.settings import asbool
+
+from .filters import EnvironFilter
+from .utils import local_settings
+
+__all__ = (
+    'includeme',
+    )
+
+PROJECT = 'pyramid_sawing'
+PREFIX = PROJECT
+
+
+class TransitLogger:
+    """A Pyramid tween that logs in transit requests."""
+    settings_prefix = '.'.join([PREFIX, 'transit_logging'])
+
+    def __init__(self, handler, registry):
+        self.handler = handler
+        global_settings = registry.settings
+        settings = local_settings(global_settings, self.settings_prefix)
+        logger_name = settings.get('logger_name', 'transit_logger')
+        self.logger = logging.getLogger(logger_name)
+        # Ensure the logger has the EnvironFilter added.
+        self.logger.addFilter(EnvironFilter())
+
+    def __call__(self, request):
+        request.response = self.handler(request)
+        self.logger.info(' ')
+        return request.response
+
+
+def includeme(config):
+    """Pyramid pluggable and discoverable function."""
+    global_settings = config.registry.settings
+    settings = local_settings(global_settings, PREFIX)
+
+    try:
+        file = settings['file']
+    except KeyError:
+        raise KeyError("Must supply '{}.file' configuration value "
+                       "in order to configure logging via '{}'."
+                       .format(PREFIX, PROJECT))
+
+    with open(file, 'r') as f:
+        logging_config = yaml.load(f)
+
+    dictConfig(logging_config)
+
+    # Enable transit logging?
+    if asbool(settings.get('transit_logging.enabled?', False)):
+        config.add_tween('pyramid_sawing.main.TransitLogger')
---
+++ packages/pyramid-sawing/pyramid_sawing-1.1.3-py3-none-any.whl/pyramid_sawing/tests.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+import os
+import sys
+import io
+import logging
+import socket
+import unittest
+from tempfile import mkstemp
+from wsgiref.util import request_uri
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from pyramid import testing
+from pyramid.request import Request
+
+
+class UtilsTests(unittest.TestCase):
+
+    def test_local_settings(self):
+        prefix = 'xyz'
+        settings = {
+            '{}.foo'.format(prefix): 'bar',
+            '{}.bar'.format(prefix): 'foo',
+            'oof': 'arb',
+            }
+        expected = {
+            'foo': 'bar',
+            'bar': 'foo',
+            }
+        # Test the utility...
+        from .utils import local_settings
+        self.assertEqual(expected, local_settings(settings, prefix))
+
+
+CONFIG = """\
+version: 1
+
+formatters:
+  generic:
+    format    : '%(levelname)-5.5s;.;%(message)s'
+  contextualized:
+    # filters : [context]
+    format    : '%(levelname)-5.5s;.;%(hostname)s;.;%(message)s'
+  apache_style:
+    # filters : [environ]
+    format    : '%(REMOTE_ADDR)s - %(REMOTE_USER)s [asctime] "%(REQUEST_METHOD)s %(REQUEST_URI)s %(HTTP_VERSION)s" %(status)s %(bytes)s "%(HTTP_REFERER)s" "%(HTTP_USER_AGENT)s"'
+    datefmt   : '%d/%b/%Y:%H:%M:%S'
+filters:
+  context:
+    ()        : pyramid_sawing.filters.ContextFilter
+  environ:
+    ()        : pyramid_sawing.filters.EnvironFilter
+handlers:
+  logio:
+    class     : logging.StreamHandler
+    level     : INFO
+    formatter : generic
+    stream    : 'ext://pyramid_sawing.tests.logio'
+  logio_with_context:
+    class     : logging.StreamHandler
+    level     : INFO
+    formatter : contextualized
+    filters   : [context]
+    stream    : 'ext://pyramid_sawing.tests.logio'
+  logio_for_transit:
+    class     : logging.StreamHandler
+    formatter : apache_style
+    stream    : 'ext://pyramid_sawing.tests.logio'
+  console:
+    class     : logging.StreamHandler
+    formatter : apache_style
+    filters   : [context, environ]
+    stream    : 'ext://sys.stdout'
+loggers:
+  pyramid_sawing.tests:
+    level     : INFO
+    handlers  : [logio_with_context]
+    propagate : 0
+  my_transit_logger:
+    handlers  : [logio_for_transit]
+    propagate : 0
+root:
+  level       : NOTSET
+  handlers    : [logio]
+"""
+
+if sys.version_info < (3,):
+    logio = io.BytesIO()
+else:
+    logio = io.StringIO()
+
+
+class IncludePluginTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # Put the config somewhere.
+        self.logging_config_filepath = mkstemp()[1]
+        with open(self.logging_config_filepath, 'w') as f:
+            f.write(CONFIG)
+        # Settings...
+        self.settings = {
+            'pyramid_sawing.file': self.logging_config_filepath,
+            }
+        self.addCleanup(os.remove, self.logging_config_filepath)
+
+        global logio
+        self.logio_position = logio.tell()
+
+    def test_logging(self):
+        """Test that the includeme function loads the logging config."""
+        from .main import includeme
+        with testing.testConfig(settings=self.settings) as config:
+            includeme(config)
+
+        # Set up the loggers
+        root = logging.getLogger()
+        local = logging.getLogger('pyramid_sawing.tests')
+
+        # Try logging...
+        root_log_msg = 'O⋮o'
+        root.error(root_log_msg)
+        local_log_msg = '>,<'
+        local.info(local_log_msg)
+
+        global logio
+        logio.seek(self.logio_position)
+        log_lines = logio.readlines()
+        self.assertEqual(len(log_lines), 2)
+        log_lines = [l.rstrip('\n') for l in log_lines]
+
+        # Check the root message...
+        parsed_root_msg = log_lines[0].split(';.;')
+        self.assertEqual(parsed_root_msg, ['ERROR', root_log_msg])
+        parsed_local_msg = log_lines[1].split(';.;')
+        self.assertEqual(parsed_local_msg,
+                         ['INFO ', socket.gethostname(), local_log_msg])
+
+
+class TransitLoggingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # Put the config somewhere.
+        self.logging_config_filepath = mkstemp()[1]
+        with open(self.logging_config_filepath, 'w') as f:
+            f.write(CONFIG)
+        # Settings...
+        self.settings = {
+            'pyramid_sawing.file': self.logging_config_filepath,
+            'pyramid_sawing.transit_logging.logger_name': 'my_transit_logger',
+            }
+        self.addCleanup(os.remove, self.logging_config_filepath)
+
+        global logio
+        self.logio_position = logio.tell()
+
+    @property
+    def target(self):
+        from .main import TransitLogger
+        return TransitLogger
+
+    def make_one(self, handler=None, registry=None):
+        if handler is None:
+            handler = mock.Mock()
+        if registry is None:
+            registry = mock.Mock()
+            registry.settings = self.settings
+        return self.target(handler, registry)
+
+    def test_logging_a_request(self):
+        request = Request.blank('/foo')
+        request.environ.update({
+            'HTTP_VERSION': '1.1',
+            'REMOTE_ADDR': '127.0.0.1',
+            })
+        response = mock.Mock()
+        response.status = '404 Not Found'
+        response.headers = {'content-length': 0}
+        handler = mock.Mock(return_value=response)
+        config_kwargs = {'request': request, 'settings': self.settings}
+        with testing.testConfig(**config_kwargs) as config:
+            request.registry = config.registry
+            tween = self.make_one(handler=handler, registry=config.registry)
+            tween(request)
+
+        global logio
+        logio.seek(self.logio_position)
+        log_lines = logio.readlines()
+        self.assertEqual(len(log_lines), 1)
+        log_lines = [l.rstrip('\n') for l in log_lines]
+
+        log_line = log_lines[0]
+        self.assertEqual(
+            log_line,
+            '127.0.0.1 - - [asctime] "GET http://localhost:80/foo 1.1" 404 Not Found 0 "-" "-"')
