1e39093f0444026596a3e79ed9228cf0f3ccc590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:30 +0000

    Add package anymath/0.0.0/anymath-0.0.0.tar.gz

---
+++ packages/anymath/anymath-0.0.0.tar.gz/anymath-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anymath',
+      version='0.0.0',
+      description='working with any number system',
+      packages=['anymath'],
+      author_email='riabovnick080@yandex.ru',
+      zip_safe=False)
