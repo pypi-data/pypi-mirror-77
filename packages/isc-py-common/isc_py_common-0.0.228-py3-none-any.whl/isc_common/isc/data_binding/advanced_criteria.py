199fd0c7d3312c71e4d95d7a04e3e3e1c56c652d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package isc-py-common/0.0.228/isc_py_common-0.0.228-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.228-py3-none-any.whl/isc_common/isc/data_binding/advanced_criteria.py
@@ -0,0 +1,22 @@
+from isc_common.isc.data_binding.criterion import Criterion
+
+
+class AdvancedCriteria(Criterion):
+    _constructor = None
+
+    def __init__(self, **kwargs):
+        lst = kwargs.get('lst')
+        if isinstance(lst, list):
+            for l in lst:
+                for k, v in l.items():
+                    if isinstance(v, list):
+                        setattr(self, k, [AdvancedCriteria(**criterion) if criterion.get('_constructor') == 'AdvancedCriteria' else Criterion(**criterion) for criterion in v])
+                    else:
+                        setattr(self, k, v() if callable(v) else v)
+
+        else:
+            for k, v in kwargs.items():
+                if isinstance(v, list):
+                    setattr(self, k, [AdvancedCriteria(**criterion) if criterion.get('_constructor') == 'AdvancedCriteria' else Criterion(**criterion) for criterion in v])
+                else:
+                    setattr(self, k, v() if callable(v) else v)
