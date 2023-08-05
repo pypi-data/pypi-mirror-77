6e13de4a1b840d024215a7bc5e9b6b4283271d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package imagesearch/0.2.2/imagesearch-0.2.2-py3-none-any.whl

---
+++ packages/imagesearch/imagesearch-0.2.2-py3-none-any.whl/imagesearch/__init__.py
@@ -0,0 +1,9 @@
+"""
+Returns the similiarity between a reference image and a set of other images.
+"""
+__version__ = "0.2.2"
+
+from imagesearch.fingerprint import Algorithm, ImageFingerprint
+from imagesearch import exceptions
+from imagesearch.compare import ImageDiff
+from imagesearch.dupe import Dupe
