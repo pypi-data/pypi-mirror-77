6ecbd5bb01424dd81c02aa655a9c754e57e02c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:37 +0000

    Add package cooperative-dataclasses/0.2.4/cooperative_dataclasses-0.2.4.tar.gz

---
+++ packages/cooperative-dataclasses/cooperative_dataclasses-0.2.4.tar.gz/cooperative_dataclasses-0.2.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cooperative_dataclasses']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cooperative-dataclasses',
+    'version': '0.2.4',
+    'description': 'Cooperative dataclasses.',
+    'long_description': '=======================\nCooperative Dataclasses\n=======================\n\n.. role:: bash(code)\n    :language: bash\n\n.. role:: python(code)\n   :language: python\n\nThis repository provides a version of dataclasses that can be used in\ncooperative inheritance.\n',
+    'author': 'Neil Girdhar',
+    'author_email': 'mistersheik@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/NeilGirdhar/cooperative_dataclasses',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
