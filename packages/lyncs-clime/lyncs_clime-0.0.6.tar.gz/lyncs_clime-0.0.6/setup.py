4750d72fb867538967431631534314aba442c4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:21 +0000

    Add package lyncs-clime/0.0.6/lyncs_clime-0.0.6.tar.gz

---
+++ packages/lyncs-clime/lyncs_clime-0.0.6.tar.gz/lyncs_clime-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from lyncs_setuptools import setup, CMakeExtension
+
+setup(
+    "lyncs_clime",
+    ext_modules=[CMakeExtension("lyncs_clime.clime", ".")],
+    data_files=[("test", ["test/conf.unity"])],
+    install_requires=["lyncs-cppyy",],
+    keywords=["Lyncs", "c-lime", "Lattice QCD",],
+    entry_points={"console_scripts": ["lyncs_lime_content = lyncs_clime:reader.main",]},
+)
