e3d2034247aadb8d252306d16bafbf249880c3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package docassemble-wrappers/0.0.6/docassemble_wrappers-0.0.6.tar.gz

---
+++ packages/docassemble-wrappers/docassemble_wrappers-0.0.6.tar.gz/docassemble_wrappers-0.0.6/docassemble_wrappers/validator_collection_br.py
@@ -0,0 +1,40 @@
+from docassemble.base.util import validation_error
+from validator_collection_br import validators_br
+
+
+def validate_cpf(value):
+    # Docassemble only performs validation if the field is filled
+    if not value:
+        return True
+    
+    try:
+        value = validators_br.cpf(value)
+    except Exception as e:
+        validation_error(str(e))
+    return True
+
+
+def validate_cnpj(value):
+    # Docassemble only performs validation if the field is filled
+    if not value:
+        return True
+    
+    try:
+        value = validators_br.cnpj(value)
+    except Exception as e:
+        validation_error(str(e))
+
+    return True
+
+
+def validate_person_full_name(value):
+    # # Docassemble only performs validation if the field is filled
+    # if not value:
+    #     return True
+
+    try:
+        value = validators_br.person_full_name(value)
+    except Exception as e:
+        validation_error(str(e))
+
+    return True
