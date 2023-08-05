16c47d5c628cea0083882cc32bd07e97131e9841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package newts/0.0.1/newts-0.0.1.tar.gz

---
+++ packages/newts/newts-0.0.1.tar.gz/newts-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "newts",
+    version = "0.0.1",
+    author = "Benjamin Montet",
+    author_email = "b.montet@unsw.edu.au",
+    description = ("Center for NEWTS"),
+    license = "MIT",
+    url = "https://github.com/benmontet/newts",
+    packages=['newts']
+)
