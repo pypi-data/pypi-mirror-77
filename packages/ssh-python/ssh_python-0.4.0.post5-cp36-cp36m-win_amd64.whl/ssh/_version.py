0bd188e71290f733bbc33ad0a73e84fee3f92b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:11 +0000

    Add package ssh-python/0.4.0.post5/ssh_python-0.4.0.post5-cp36-cp36m-win_amd64.whl

---
+++ packages/ssh-python/ssh_python-0.4.0.post5-cp36-cp36m-win_amd64.whl/ssh/_version.py
@@ -0,0 +1,10 @@
+
+import json
+
+version_json = '''
+{"date": "2020-08-19T18:10:33.423449", "dirty": false, "error": null, "full-revisionid": "1747e32ba3ca6a51b70108f9aae3bc2291b09f61", "version": "0.4.0.post5"}'''  # END VERSION_JSON
+
+
+def get_versions():
+    return json.loads(version_json)
+
