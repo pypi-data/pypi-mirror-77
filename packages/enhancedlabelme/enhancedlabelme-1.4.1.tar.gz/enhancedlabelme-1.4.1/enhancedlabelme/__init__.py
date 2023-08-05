e91b11fd423f2b2351403503c22899176df0fe7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:43 +0000

    Add package enhancedlabelme/1.4.1/enhancedlabelme-1.4.1.tar.gz

---
+++ packages/enhancedlabelme/enhancedlabelme-1.4.1.tar.gz/enhancedlabelme-1.4.1/enhancedlabelme/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+import logging
+import sys
+
+from qtpy import QT_VERSION
+
+
+__appname__ = 'Enhanced LabelMe'
+
+# Semantic Versioning 2.0.0: https://semver.org/
+# 1. MAJOR version when you make incompatible API changes;
+# 2. MINOR version when you add functionality in a backwards-compatible manner;
+# 3. PATCH version when you make backwards-compatible bug fixes.
+__version__ = '1.4.1'
+
+QT4 = QT_VERSION[0] == '4'
+QT5 = QT_VERSION[0] == '5'
+del QT_VERSION
+
+PY2 = sys.version[0] == '2'
+PY3 = sys.version[0] == '3'
+del sys
+
+from enhancedlabelme.label_file import LabelFile
+from enhancedlabelme import utils
