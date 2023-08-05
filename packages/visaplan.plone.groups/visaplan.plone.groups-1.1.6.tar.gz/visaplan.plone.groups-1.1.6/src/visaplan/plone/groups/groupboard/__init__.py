3582452501b19721646220faf97cc0df8c1e2e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package visaplan.plone.groups/1.1.6/visaplan.plone.groups-1.1.6.tar.gz

---
+++ packages/visaplan.plone.groups/visaplan.plone.groups-1.1.6.tar.gz/visaplan.plone.groups-1.1.6/src/visaplan/plone/groups/groupboard/__init__.py
@@ -0,0 +1,9 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+from __future__ import print_function
+
+__author__="enrico"
+__date__ ="$29.04.2013 15:44:16$"
+
+if __name__ == "__main__":
+    print("Hello World")
