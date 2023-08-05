3f3e615d9b17bd8452d72f6a5627546e9afdfc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:06 +0000

    Add package panviva/1.0.2/panviva-1.0.2.tar.gz

---
+++ packages/panviva/panviva-1.0.2.tar.gz/panviva-1.0.2/panviva/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from panviva.api.operations_api import OperationsApi
+from panviva.api.resources_api import ResourcesApi
