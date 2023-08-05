56cee13a380ea1287a384d88dae72db77eeebd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package binney/0.0.4/binney-0.0.4.tar.gz

---
+++ packages/binney/binney-0.0.4.tar.gz/binney-0.0.4/src/binney/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "binney"
+__summary__ = "binney: logistic regression tool-kit"
+__uri__ = "https://github.com/ihmeuw-msca/flipper"
+
+__version__ = "0.0.4"
+
+__author__ = "IHME Math Sciences"
+__email__ = "ihme.math.sciences@gmail.com"
+
+__license__ = "GNU GPLv3"
+__copyright__ = f"Copyright 2020 {__author__}"
