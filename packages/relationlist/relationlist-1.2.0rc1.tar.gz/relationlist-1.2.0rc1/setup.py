9749babe013eb132d6d1be97f7f69a79a5e02e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package relationlist/1.2.0rc1/relationlist-1.2.0rc1.tar.gz

---
+++ packages/relationlist/relationlist-1.2.0rc1.tar.gz/relationlist-1.2.0rc1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+setuptools.setup(
+    name = 'relationlist',
+    version = '1.2.0rc1',
+    author = 'wyz23x2',
+    author_email = 'wyz23x2@163.com',
+    description = 'A module that contains a class that supports relations.',
+    long_description = '''See `the official documentation <https://relationlist.readthedocs.io>`_\ .''',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
+    
