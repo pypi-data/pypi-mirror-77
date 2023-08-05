e148c20ea66596128789afbe6d24a1f210876353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:36 +0000

    Add package haveibeenpywned/0.2.91/haveibeenpywned-0.2.91.tar.gz

---
+++ packages/haveibeenpywned/haveibeenpywned-0.2.91.tar.gz/haveibeenpywned-0.2.91/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.2.91"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="haveibeenpywned",
+    version=VERSION,
+    author="Sam Neal",
+    author_email="sam.w.neal@gmail.com",
+    description="A simple python wrapper for the www.haveibeenpwned.com API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scv-m/haveibeenpywned",
+    download_url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["ratelimit", "requests",],
+)
