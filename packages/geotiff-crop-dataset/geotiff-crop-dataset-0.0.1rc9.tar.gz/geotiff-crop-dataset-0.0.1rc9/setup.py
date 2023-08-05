4992a8850c7907c7ab14b78feae79e6fbe4d21de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:28 +0000

    Add package geotiff-crop-dataset/0.0.1rc9/geotiff-crop-dataset-0.0.1rc9.tar.gz

---
+++ packages/geotiff-crop-dataset/geotiff-crop-dataset-0.0.1rc9.tar.gz/geotiff-crop-dataset-0.0.1rc9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='geotiff-crop-dataset',
+    url='https://github.com/tayden/pytorch_crop_dataset',
+    author='Taylor Denouden',
+    author_email='taylor.denouden@hakai.org',
+    description='A Pytorch dataset that crops rasterio-readable image files to small sections on the fly.',
+    install_requires=[
+        'numpy',
+        'rasterio'
+    ],
+    license='MIT',
+    packages=['geotiff_crop_dataset'],
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+)
