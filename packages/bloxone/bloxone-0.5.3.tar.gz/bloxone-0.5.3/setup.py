e7109755585f1a3df2d5de4c376ebc64d5dbc730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:42 +0000

    Add package bloxone/0.5.3/bloxone-0.5.3.tar.gz

---
+++ packages/bloxone/bloxone-0.5.3.tar.gz/bloxone-0.5.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from bloxone import __version__
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bloxone", 
+    version=__version__,
+    author="Chris Marrison",
+    author_email="chris@infoblox.com",
+    description="BloxOne API Wrapper Module",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # url="https://github.com/pypa/sampleproject",
+    packages=['bloxone'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[ 'requests' ],
+    # data_files=['CHANGELOG.rst','README.rst','LICENSE'],
+    # include_package_data=True
+) 
