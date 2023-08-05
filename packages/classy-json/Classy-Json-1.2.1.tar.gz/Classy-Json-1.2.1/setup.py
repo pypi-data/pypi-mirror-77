dfb4b8501e6b228a03a66b893fdd3314034312d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:40 +0000

    Add package classy-json/1.2.1/Classy-Json-1.2.1.tar.gz

---
+++ packages/classy-json/Classy-Json-1.2.1.tar.gz/Classy-Json-1.2.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Classy-Json',
+    version='1.2.1',
+    author='Iapetus-11',
+    description='An attempt to recreate the way json behaves in Javascript, but in Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Iapetus-11/Classy-Json',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
