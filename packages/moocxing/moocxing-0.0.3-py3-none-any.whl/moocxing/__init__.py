1cd9a8d8991418bdcaa3ab6935e249f4ec5cf6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:21 +0000

    Add package moocxing/0.0.3/moocxing-0.0.3-py3-none-any.whl

---
+++ packages/moocxing/moocxing-0.0.3-py3-none-any.whl/moocxing/__init__.py
@@ -0,0 +1,1 @@
+from .moocxing import MOOCXING
---
+++ packages/moocxing/moocxing-0.0.3-py3-none-any.whl/moocxing/package/__init__.py
@@ -0,0 +1,8 @@
+print("**********************************************")
+print("* M O O C X I N G 人 工 智 能 学 习 专 用 库 *")
+print("*           V1.0  2020/05/14                 *")
+print("**********************************************")
+
+from mcpi.minecraft import Minecraft
+__all__ = ['MXMqtt', 'MXMedia', 'MXSpeech',
+           "MXNLP", "MXSerial"]
