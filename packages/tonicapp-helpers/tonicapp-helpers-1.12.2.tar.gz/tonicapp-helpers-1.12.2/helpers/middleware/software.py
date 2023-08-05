7630e02d4c01dcae7e9062d65fb261e1d4ca0753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package tonicapp-helpers/1.12.2/tonicapp-helpers-1.12.2.tar.gz

---
+++ packages/tonicapp-helpers/tonicapp-helpers-1.12.2.tar.gz/tonicapp-helpers-1.12.2/helpers/middleware/software.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseBadRequest
+
+
+class SoftwareTypeMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        app_type = request.META.get('HTTP_SOFTWARE_TYPE')
+        if not app_type:
+            return HttpResponseBadRequest(
+                'Add Software-Type to the header. Example: Software-Type: Web or Software-Type: Application'
+            )
+        if app_type not in ['Application', 'Web']:
+            return HttpResponseBadRequest(
+                'Incorrect Software-Type, the available options are: Application or Web.'
+            )
+
+        request.software_type = 'mobile'
+
+        if 'Web' in app_type:
+            request.software_type = 'web'
+
+        response = self.get_response(request)
+
+        return response
