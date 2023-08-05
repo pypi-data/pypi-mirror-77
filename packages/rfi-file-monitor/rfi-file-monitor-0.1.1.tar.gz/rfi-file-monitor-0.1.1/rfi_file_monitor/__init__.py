c32fb53d05cd718484b8cf5bf05280664a1a0d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package rfi-file-monitor/0.1.1/rfi-file-monitor-0.1.1.tar.gz

---
+++ packages/rfi-file-monitor/rfi-file-monitor-0.1.1.tar.gz/rfi-file-monitor-0.1.1/rfi_file_monitor/__init__.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+from .application import Application
+
+__version__ = "0.1.1"
+
+# main entrypoint
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('Started')
+    app = Application()
+    rv = app.run(sys.argv)
+    logging.info('Finished')
+    sys.exit(rv)
