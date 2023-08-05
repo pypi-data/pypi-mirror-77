9c1507a99d14ada0f03bfdd466004331bf31847a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:56 +0000

    Add package pysjtu/0.3.1/pysjtu-0.3.1-py3-none-any.whl

---
+++ packages/pysjtu/pysjtu-0.3.1-py3-none-any.whl/pysjtu/client/api/__init__.py
@@ -0,0 +1,6 @@
+from .course import CourseLibMixin
+from .exam import ExamMixin
+from .gpa import GPAMixin
+from .schedule import ScheduleMixin
+from .score import ScoreMixin
+from .selection import SelectionMixin
