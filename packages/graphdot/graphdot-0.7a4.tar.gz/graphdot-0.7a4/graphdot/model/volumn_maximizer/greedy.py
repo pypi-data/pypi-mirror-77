6c1211b04d7d7da5953d0103225ccfe5cf17c72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package graphdot/0.7a4/graphdot-0.7a4.tar.gz

---
+++ packages/graphdot/graphdot-0.7a4.tar.gz/graphdot-0.7a4/graphdot/model/volumn_maximizer/greedy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# class GreedyVolumeMaximizer:
+#     def __init__(self):
+#         pass
+
+#     def __call__(self, X, kernel, )
+
+def greedy_volume_maximizer(X, kernel, batch_size=100, down_select=10):
+    for 
