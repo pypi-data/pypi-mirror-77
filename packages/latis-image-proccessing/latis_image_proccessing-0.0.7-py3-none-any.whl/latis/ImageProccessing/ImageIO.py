7f414d687cc41b699083cdc5eb5f9ee5f6d08365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package latis-image-proccessing/0.0.7/latis_image_proccessing-0.0.7-py3-none-any.whl

---
+++ packages/latis-image-proccessing/latis_image_proccessing-0.0.7-py3-none-any.whl/latis/ImageProccessing/ImageIO.py
@@ -0,0 +1,54 @@
+from . import Image
+import numpy as np
+
+import pydicom
+from pydicom.encaps import encapsulate
+from pydicom.uid import JPEG2000, RLELossless, ImplicitVRLittleEndian
+
+
+
+
+class ImageIO:
+
+    # private
+    def ensure_even(self, stream):
+        # Very important for some viewers
+        if len(stream) % 2:
+            return stream + b"\x00"
+        return stream
+
+    def loadImage(self, file):
+        ds = pydicom.dcmread(file)
+        pixel_array = np.uint16(ds.pixel_array)
+        cols = ds.Columns
+        rows = ds.Rows
+        return Image(pixel_array, cols, rows, ds)
+
+    def buildFile(self, image, saveName):
+        # TODO : edit height and width elements
+        dataset = ImageIO.buildDataSetImplicitVRLittleEndian(image)
+        dataset.save_as(saveName)
+        return saveName
+
+    def buildDataSetJPEG2000(self, image):
+        from io import BytesIO
+        from PIL import Image as PImage
+        dataset = image.dataset
+        pixels = image.pixelData
+        frame_data = []
+        with BytesIO() as output:
+            image = PImage.fromarray(pixels)
+            image.save(output, format="JPEG2000")
+            frame_data.append(output.getvalue())
+        dataset.PixelData = encapsulate(frame_data)
+        dataset.file_meta.TransferSyntaxUID = JPEG2000
+        dataset.is_implicit_VR = False
+        return dataset
+
+    def buildDataSetImplicitVRLittleEndian(self, image):
+        dataset = image.dataset
+        pixels = image.pixelData
+        dataset.PixelData = pixels.tobytes()
+        dataset.file_meta.TransferSyntaxUID = ImplicitVRLittleEndian
+        dataset.is_implicit_VR = True
+        return dataset
