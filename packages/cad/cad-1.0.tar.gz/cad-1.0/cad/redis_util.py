b71829e55c4d9535a7914a2126cc5414ec4f131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:59 +0000

    Add package cad/1.0/cad-1.0.tar.gz

---
+++ packages/cad/cad-1.0.tar.gz/cad-1.0/cad/redis_util.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 _*-  
+"""
+@author  : Lin Luo / Bruce Liu
+@time    : 2020/1/3 11:01
+@contact : 15869300264@163.com
+"""
+
+from redis import ConnectionPool, Redis
+
+
+class RedisUtil(object):
+    def __init__(self, host: str = 'localhost', port: int = 6379, db: int = 0, password: str = '', url_str: str = None,
+                 **kwargs):
+        if url_str is not None:
+            hosts = url_str.split(':')
+            host = hosts[0]
+            ports = hosts[1].split('/')
+            port = ports[0]
+            db = ports[1]
+        self._pool = ConnectionPool(host=host, port=port, db=db, password=password, **kwargs)
+        self._redis = Redis(connection_pool=self._pool)
+
+    @property
+    def redis(self):
+        return self._redis
+
+    @property
+    def pool(self):
+        return self._pool
---
+++ packages/cad/cad-1.0.tar.gz/cad-1.0/tests/test_app_config_util.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 _*-  
+"""
+@license : Copyright(C), WAYZ
+@author  : Bruce Liu
+@time    : 2020/1/22 16:25
+@contact : bruce.liu@wayz.ai
+"""
+from unittest import TestCase
+
+from cad.app_config_util import AppConfigUtil
+
+ak = '9a508ca66ee679bc'
+sk = 'e201737ee515c7b8'
+
+
+class TestAppConfigUtil(TestCase):
+    def test_get_dict_success(self):
+        """
+
+        :return:
+        """
+        instance = AppConfigUtil(2)
+        res = instance.check(ak, sk)
+        self.assertEqual(res, True)
+        res = instance.check(ak, sk, auth_code='234')
+        self.assertEqual(res, True)
+        res = instance.check(ak, '123')
+        self.assertEqual(res, False)
+        res = instance.check(ak, None)
+        self.assertEqual(res, True)
