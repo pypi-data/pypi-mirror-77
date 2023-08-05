7861c63bd7c8f78f998fcc0de21a9ce2cb8bfe54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package ncbi-datasets-pylib/6.4.0/ncbi-datasets-pylib-6.4.0.tar.gz

---
+++ packages/ncbi-datasets-pylib/ncbi-datasets-pylib-6.4.0.tar.gz/ncbi-datasets-pylib-6.4.0/src/ncbi/datasets/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ncbi.datasets.api.assembly_dataset_descriptors_api import AssemblyDatasetDescriptorsApi
+from ncbi.datasets.api.download_api import DownloadApi
+from ncbi.datasets.api.gene_dataset_descriptors_api import GeneDatasetDescriptorsApi
+from ncbi.datasets.api.tax_tree_api import TaxTreeApi
+from ncbi.datasets.api.virus_dataset_api import VirusDatasetApi
+from ncbi.datasets.api.virus_download_api import VirusDownloadApi
