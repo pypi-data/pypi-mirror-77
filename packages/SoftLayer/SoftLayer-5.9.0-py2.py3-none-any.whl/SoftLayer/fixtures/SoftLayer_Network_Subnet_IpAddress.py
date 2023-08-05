dbefbee46072d0bb0ef0c3e54b41677b3d451316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package SoftLayer/5.9.0/SoftLayer-5.9.0-py2.py3-none-any.whl

---
+++ packages/SoftLayer/SoftLayer-5.9.0-py2.py3-none-any.whl/SoftLayer/fixtures/SoftLayer_Network_Subnet_IpAddress.py
@@ -0,0 +1,11 @@
+getByIpAddress = {
+    'id': 12345,
+    'ipAddress': '10.0.1.37',
+    'isBroadcast': False,
+    'isGateway': False,
+    'isNetwork': False,
+    'isReserved': False,
+    'subnetId': 5678,
+}
+
+editObject = True
