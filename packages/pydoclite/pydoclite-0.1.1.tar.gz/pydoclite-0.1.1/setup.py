7b1e53dc24efa920410cb612ce1a0dae34666090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:36 +0000

    Add package pydoclite/0.1.1/pydoclite-0.1.1.tar.gz

---
+++ packages/pydoclite/pydoclite-0.1.1.tar.gz/pydoclite-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os 
+from platform import system
+
+if system()=="Linux":
+    libpath="linux/docliteshared.so"
+elif system()=="Darwin":
+    libpath = "darwin/docliteshared.so"
+else:
+    raise ValueError("os platform not supported yet")
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name="pydoclite",
+    packages=find_packages(),
+    version="0.1.1",
+    description="pydoclite is the python library interface for doclite",
+    author="abrahamakerele38@gmail.com",
+    license="MIT",
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    package_data={"pydoclite": [libpath]},
+    include_package_data=True,
+    test_suite="tests",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
