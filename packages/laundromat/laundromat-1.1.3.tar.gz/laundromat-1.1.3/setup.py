2a460a931a51b0325f0be80e0538ec2839a55497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:47 +0000

    Add package laundromat/1.1.3/laundromat-1.1.3.tar.gz

---
+++ packages/laundromat/laundromat-1.1.3.tar.gz/laundromat-1.1.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+
+
+setup(
+    name='laundromat',
+    version='1.1.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    author="Marius Dioli @ NAV IKT and Petter Sunde Nymark @ NAV IKT",
+    description="Scrub personal information from text with Laundromat.",
+    project_description="",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    package_data={'laundromat': ['spacy/data/*.csv']}
+
+)
