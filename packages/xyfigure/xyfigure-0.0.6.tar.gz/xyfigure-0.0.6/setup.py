fbc86ffcb6812fdd2571f99d4d9d7245c7f13e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package xyfigure/0.0.6/xyfigure-0.0.6.tar.gz

---
+++ packages/xyfigure/xyfigure-0.0.6.tar.gz/xyfigure-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xyfigure",
+    version="0.0.6",
+    author="Chad B. Hovey",
+    author_email="chovey@sandia.gov",
+    description="A library for xy plotting and processing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sandialabs/sibl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
