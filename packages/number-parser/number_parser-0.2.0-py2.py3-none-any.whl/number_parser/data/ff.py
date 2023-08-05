c6819aaa9575befbab6eca0b33a762a49ad56a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:47:10 +0000

    Add package number-parser/0.2.0/number_parser-0.2.0-py2.py3-none-any.whl

---
+++ packages/number-parser/number_parser-0.2.0-py2.py3-none-any.whl/number_parser/data/ff.py
@@ -0,0 +1,31 @@
+info = {
+    "UNIT_NUMBERS": {
+        "ɓolum": 0,
+        "gooto": 1,
+        "goʼo": 1,
+        "ɗiɗi": 2,
+        "ɗiɗo": 2,
+        "tati": 3,
+        "tato": 3,
+        "nawi": 4,
+        "nawo": 4,
+        "jowi": 5,
+        "njowo": 5,
+        "jeegomo": 6,
+        "jeegoʼo": 6,
+        "jeeɗiɗi": 7,
+        "jeeɗiɗo": 7,
+        "jeetati": 8,
+        "jeetato": 8,
+        "jeenawi": 9,
+        "jeenawo": 9
+    },
+    "DIRECT_NUMBERS": {},
+    "TENS": {
+        "noogas": 20
+    },
+    "HUNDREDS": {},
+    "BIG_POWERS_OF_TEN": {},
+    "SKIP_TOKENS": [],
+    "USE_LONG_SCALE": False
+}
---
+++ packages/number-parser/number_parser-0.2.0-py2.py3-none-any.whl/number_parser/data/vi.py
@@ -0,0 +1,24 @@
+info = {
+    "UNIT_NUMBERS": {
+        "không": 0,
+        "một": 1,
+        "hai": 2,
+        "ba": 3,
+        "bốn": 4,
+        "năm": 5,
+        "sáu": 6,
+        "bảy": 7,
+        "tám": 8,
+        "chín": 9
+    },
+    "DIRECT_NUMBERS": {},
+    "TENS": {},
+    "HUNDREDS": {},
+    "BIG_POWERS_OF_TEN": {
+        "trăm": 100,
+        "nghìn": 1000,
+        "triệu": 1000000,
+        "tỷ": 1000000000
+    },
+    "SKIP_TOKENS": []
+}
