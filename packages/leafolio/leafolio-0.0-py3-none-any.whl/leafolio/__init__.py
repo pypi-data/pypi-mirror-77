dd6983c1432f5af7297ab92eed1eb366efc465ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:34 +0000

    Add package leafolio/0.0/leafolio-0.0-py3-none-any.whl

---
+++ packages/leafolio/leafolio-0.0-py3-none-any.whl/leafolio/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-module-docstring
+__author__ = "M S Wang"
+__copyright__ = "Copyright 2020, M S Wang"
+__date__ = "2020/08/20"
+__license__ = "GPLv3"
+__version__ = "0.0"
+__url__ = "https://github.com/MikeSWang/Leafolio/"
