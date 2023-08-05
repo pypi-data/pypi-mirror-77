1da6884689e67177975cfe55970bdcc9b8a5af42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:30 +0000

    Add package benatools/0.0.13/benatools-0.0.13.tar.gz

---
+++ packages/benatools/benatools-0.0.13.tar.gz/benatools-0.0.13/benatools/torch/efficient_net.py
@@ -0,0 +1,10 @@
+from efficientnet_pytorch import EfficientNet
+
+def create_efn(b, dim=128, weights='imagenet', include_top=False):
+        version = 'efficientnet-b'+str(b)
+        if weights is None:
+                model = EfficientNet.from_name(version)
+        else:
+                model = EfficientNet.from_pretrained(version)
+
+        return model
