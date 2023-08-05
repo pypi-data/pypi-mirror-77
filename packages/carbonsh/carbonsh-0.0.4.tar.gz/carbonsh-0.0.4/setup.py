e2b3c5b7e00f6c7947b894686b56210181b8cfa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package carbonsh/0.0.4/carbonsh-0.0.4.tar.gz

---
+++ packages/carbonsh/carbonsh-0.0.4.tar.gz/carbonsh-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.readlines()
+
+setup(
+    name='carbonsh',
+    version='0.0.4',
+    packages=['carbonsh'],
+    url='https://github.com/MrMarble/carbonsh',
+    license='GPL-3.0-or-later',
+    author='MrMarble',
+    author_email='',
+    description='carbon.now.sh python module',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    tests_require=['pytest>=6.0.1<7'],
+    python_requires=">=3.6<4",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
