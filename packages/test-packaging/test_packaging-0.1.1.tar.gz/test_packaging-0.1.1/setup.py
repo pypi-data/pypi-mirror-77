784af1f639a2e795933933d22a67d5523848aac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:57 +0000

    Add package test-packaging/0.1.1/test_packaging-0.1.1.tar.gz

---
+++ packages/test-packaging/test_packaging-0.1.1.tar.gz/test_packaging-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_packaging',
+      version='0.1.1',
+      description='test of packaging',
+      url='http://github.com/hector-mao-net/test_packaging',
+      author='Hector Mao',
+      author_email='hectormao1025@gmail.com',
+      license='MIT',
+      packages=['test_packaging'],
+      zip_safe=False)
