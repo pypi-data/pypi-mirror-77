361359e60ba5ebf1e027563eb68b66234babbe14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package pedantic/1.0.5/pedantic-1.0.5.tar.gz

---
+++ packages/pedantic/pedantic-1.0.5.tar.gz/pedantic-1.0.5/pedantic/class_decorators.py
@@ -0,0 +1,45 @@
+from typing import Callable, Any
+
+# local file imports
+from pedantic.method_decorators import pedantic, pedantic_require_docstring, trace, timer
+
+
+def for_all_methods(decorator: Callable) -> Callable:
+    """
+    From: https://stackoverflow.com/questions/6307761/how-to-decorate-all-functions-of-a-class-without-typing-it-over-and-over-for-eac/6307868#6307868
+    Example:
+    >>> @for_all_methods(pedantic)
+    >>> class C(object):
+    >>>     def m1(self): pass
+    >>>     def m2(self, x): pass
+    """
+    def decorate(cls: Any) -> Any:
+        for attr in cls.__dict__:
+            if callable(getattr(cls, attr)):
+                # if 'is_class_decorator' in inspect.getfullargspec(decorator).annotations: DOESNT WORK HERE
+                try:
+                    setattr(cls, attr, decorator(getattr(cls, attr), is_class_decorator=True))
+                except TypeError:
+                    setattr(cls, attr, decorator(getattr(cls, attr)))
+        return cls
+    return decorate
+
+
+def pedantic_class(cls: Any) -> Callable:
+    """Shortcut for @for_all_methods(pedantic) """
+    return for_all_methods(decorator=pedantic)(cls=cls)
+
+
+def pedantic_class_require_docstring(cls: Any) -> Callable:
+    """Shortcut for @for_all_methods(pedantic_require_docstring) """
+    return for_all_methods(decorator=pedantic_require_docstring)(cls=cls)
+
+
+def trace_class(cls: Any) -> Callable:
+    """Shortcut for @for_all_methods(trace) """
+    return for_all_methods(decorator=trace)(cls=cls)
+
+
+def timer_class(cls: Any) -> Callable:
+    """Shortcut for @for_all_methods(timer) """
+    return for_all_methods(decorator=timer)(cls=cls)
