f8e52b566d13d91679290d3895cda046e176af32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:46 +0000

    Add package cs-models/0.0.101.dev59/cs-models-0.0.101.dev59.tar.gz

---
+++ packages/cs-models/cs-models-0.0.101.dev59.tar.gz/cs-models-0.0.101.dev59/src/cs_models/resources/LatestPipelineProduct/schemas.py
@@ -0,0 +1,27 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+    pre_load,
+)
+from ...utils.utils import pre_load_date_fields
+
+
+class LatestPipelineProductResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    product_name = fields.String(validate=not_blank, required=True)
+    latest_catalyst_date = fields.DateTime()
+    updated_at = fields.DateTime()
+
+    @pre_load
+    def convert_string_to_datetime(self, in_data):
+        date_fields = ['date']
+
+        in_data = pre_load_date_fields(
+            in_data,
+            date_fields,
+            date_format='%Y%m%dT%H%M%S',
+        )
+        return in_data
