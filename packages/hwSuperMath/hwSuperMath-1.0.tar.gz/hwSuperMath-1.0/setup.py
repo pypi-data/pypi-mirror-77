e1b0151c8978202fba0983b518716c562bd0a59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package hwSuperMath/1.0/hwSuperMath-1.0.tar.gz

---
+++ packages/hwSuperMath/hwSuperMath-1.0.tar.gz/hwSuperMath-1.0/setup.py
@@ -0,0 +1,11 @@
+#encoding=utf-8
+from distutils.core import setup
+
+setup(
+    name='hwSuperMath',  # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，里面只有数学方法，用于测试哦',  #描述
+    author='gaoqi', # 作者
+    author_email='gaoqi110@163.com',
+    py_modules=['hwSuperMath.demo1','hwSuperMath.demo2'] # 要发布的模块
+)
