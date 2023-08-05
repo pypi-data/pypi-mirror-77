b854d36e26cbb7e8e441fbea28948c6d31b673fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:31 +0000

    Add package jupyter-kernel-gateway/2.4.3/jupyter_kernel_gateway-2.4.3-py2.py3-none-any.whl

---
+++ packages/jupyter-kernel-gateway/jupyter_kernel_gateway-2.4.3-py2.py3-none-any.whl/kernel_gateway/_version.py
@@ -0,0 +1,8 @@
+"""kernel_gateway version info"""
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (2, 4, 3)
+
+__version__ = '.'.join(map(str, version_info))
