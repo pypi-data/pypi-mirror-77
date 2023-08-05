4b9dbc55ad2332a3e4b53523abb37be36a9a57fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:05 +0000

    Add package distributions-nisarg/0.1/distributions_nisarg-0.1.tar.gz

---
+++ packages/distributions-nisarg/distributions_nisarg-0.1.tar.gz/distributions_nisarg-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_nisarg',
+      version='0.1',
+      description='Gaussian distributions',
+      Author = 'Nisarg Nikhil',
+      packages=['distributions_nisarg'],
+      zip_safe=False)
