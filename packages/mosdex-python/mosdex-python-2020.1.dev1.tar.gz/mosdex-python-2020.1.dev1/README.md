253fc68b788c93e7791b3b5f65dad6e32bd047d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:10 +0000

    Add package mosdex-python/2020.1.dev1/mosdex-python-2020.1.dev1.tar.gz

---
+++ packages/mosdex-python/mosdex-python-2020.1.dev1.tar.gz/mosdex-python-2020.1.dev1/README.md
@@ -0,0 +1,9 @@
+# mosdex-python
+
+Package is in `mosdex-python/mosdex`
+
+A sailco example is in the `samples` directory.
+
+The experimental version of MOSDEX Schema and some example files are in `mosdex-python/data`
+
+PDFs of the output are in `mosdex-python/data`
---
+++ packages/mosdex-python/mosdex-python-2020.1.dev1.tar.gz/mosdex-python-2020.1.dev1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mosdex-python',
+    version='2020.1-dev.1',
+    packages=find_packages(exclude="tests"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/coin-modeling-dev/mosdex-python/archive/2020.1.dev1.tar.gz',
+    author='Alan King',
+    author_email='kingaj@us.ibm.com',
+    description='Package for running demo of MOSDEX modular optimization problem data standard',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pandas', 'json', 'pprint', 'jsonschema', 'records']
+)
