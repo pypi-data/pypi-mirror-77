8d99c7d21b54c76b5ab1dc834bdb099518842e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package pytest-resource-path/1.2.1/pytest-resource-path-1.2.1.tar.gz

---
+++ packages/pytest-resource-path/pytest-resource-path-1.2.1.tar.gz/pytest-resource-path-1.2.1/pytest_resource_path/exceptions.py
@@ -0,0 +1,14 @@
+"""This module implements exceptions for this package."""
+
+__all__ = ["Error", "LogicError"]
+
+
+class Error(Exception):
+    """
+    Base class for exceptions in this module.
+    @see https://docs.python.org/3/tutorial/errors.html#user-defined-exceptions
+    """
+
+
+class LogicError(Error):
+    """This Error indicates programing miss."""
