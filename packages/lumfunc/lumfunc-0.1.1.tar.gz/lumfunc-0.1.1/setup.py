dec15883395967e9fb8fbb4506fbd78f2f663d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:05 +0000

    Add package lumfunc/0.1.1/lumfunc-0.1.1.tar.gz

---
+++ packages/lumfunc/lumfunc-0.1.1.tar.gz/lumfunc-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='lumfunc',  
+     version='0.1.1',
+     author="Manasvee Saraf",
+     author_email="saraf.manasvee@gmail.com",
+     description="Galaxian Luminosity Function Constructor package using the 1/Vmax estimator and Schechter model.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/manasveesaraf/LuminosityFunction",
+     packages=['lumfunc'],
+     install_requires=['scipy', 'matplotlib', 'numpy', 'astropy' ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
+ 
