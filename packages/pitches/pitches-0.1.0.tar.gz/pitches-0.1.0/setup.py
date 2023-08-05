7548814643c4c74cc40ad86383016bca581525b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package pitches/0.1.0/pitches-0.1.0.tar.gz

---
+++ packages/pitches/pitches-0.1.0.tar.gz/pitches-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pitches',
+    version='0.1.0',    
+    description='Convert between scientific pitch notation and frequency',
+    url='https://github.com/kapteynpeter/pitches',
+    author='Peter Kapteyn',
+    author_email='kapteynpeter@gmail.com',
+    license='MIT',
+    packages=['pitches'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ],
+)
