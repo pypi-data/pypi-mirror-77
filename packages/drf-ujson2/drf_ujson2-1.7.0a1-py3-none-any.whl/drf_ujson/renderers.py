c1e10d814cdfb6c0328fb2f4d2f3eaceb759895d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package drf-ujson2/1.7.0a1/drf_ujson2-1.7.0a1-py3-none-any.whl

---
+++ packages/drf-ujson2/drf_ujson2-1.7.0a1-py3-none-any.whl/drf_ujson/renderers.py
@@ -0,0 +1,59 @@
+from typing import Union, Optional, Mapping, Any
+
+from rest_framework.renderers import JSONRenderer
+import ujson
+
+
+__all__ = ["UJSONRenderer"]
+
+
+class UJSONRenderer(JSONRenderer):
+    """
+    Renderer which serializes to JSON.
+    Applies JSON's backslash-u character escaping for non-ascii characters.
+    Uses the blazing-fast ujson library for serialization.
+    """
+
+    # Controls whether forward slashes (/) are escaped.
+    escape_forward_slashes: bool = False
+    # Used to enable special encoding of "unsafe" HTML characters into safer
+    # Unicode sequences.
+    encode_html_chars: bool = False
+
+    # TODO: typing
+    translate_func = None
+
+    def render(
+        self,
+        data: Union[dict, None],
+        accepted_media_type: Optional[str] = None,
+        renderer_context: Mapping[str, Any] = None,
+    ) -> bytes:
+
+        if data is None:
+            return bytes()
+
+        if self.translate_func is not None:
+            data = self.translate_func(data)
+
+        accepted_media_type = accepted_media_type or ""
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context)
+
+        ret = ujson.dumps(
+            data,
+            ensure_ascii=self.ensure_ascii,
+            escape_forward_slashes=self.escape_forward_slashes,
+            encode_html_chars=self.encode_html_chars,
+            indent=indent or 0,
+        )
+
+        # force return value to unicode
+        if isinstance(ret, str):
+            # We always fully escape \u2028 and \u2029 to ensure we output JSON
+            # that is a strict javascript subset. If bytes were returned
+            # by json.dumps() then we don't have these characters in any case.
+            # See: http://timelessrepo.com/json-isnt-a-javascript-subset
+            ret = ret.replace("\u2028", "\\u2028").replace("\u2029", "\\u2029")
+            return bytes(ret.encode("utf-8"))
+        return ret
