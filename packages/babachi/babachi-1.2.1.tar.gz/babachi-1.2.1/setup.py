93d1146c32e8ecfbc80ff2bb487b71ac721f3119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package babachi/1.2.1/babachi-1.2.1.tar.gz

---
+++ packages/babachi/babachi-1.2.1.tar.gz/babachi-1.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from package import __version__
+from os.path import join, dirname
+
+setup(
+    name='babachi',
+    version=__version__,
+    packages=find_packages(),
+    package_data={'package': ['tests/*.tsv']},
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    entry_points={
+        'console_scripts': [
+            'babachi = package.BADEstimation:segmentation_start',
+        ],
+    },
+    author="Sergey Abramov, Alexandr Boytsov",
+    author_email='aswq22013@gmail.com',
+    install_requires=[
+        'docopt>=0.6.2',
+        'numpy>=1.18.0',
+        'schema>=0.7.2',
+        'contextlib2>=0.5.5',
+        'pandas>=1.0.4',
+        'matplotlib>=3.2.1',
+        'seaborn>=0.10.1'
+    ],
+    python_requires='>=3.6',
+    url="https://github.com/autosome-ru/BABACHI",
+)
