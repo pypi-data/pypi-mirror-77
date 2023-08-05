a0b44bf9a7a00869473ec4db639dac04b41d2d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package ftpvl-beta/0.2.1/ftpvl-beta-0.2.1.tar.gz

---
+++ packages/ftpvl-beta/ftpvl-beta-0.2.1.tar.gz/ftpvl-beta-0.2.1/README.md
@@ -0,0 +1,66 @@
+# FPGA Tool Performance Visualization Library (FTPVL)
+![Python application](https://github.com/TypingKoala/FPGA-Tool-Performance-Visualization-Library/workflows/Python%20application/badge.svg)
+
+<img src="https://raw.githubusercontent.com/SymbiFlow/FPGA-Tool-Performance-Visualization-Library/master/docs/images/styled_viz.png" alt="Single Table Visualization" width="100%">
+
+FTPVL is a library for simplifying the data collection and visualization process
+for Symbiflow development. Although it was made with Symbiflow in mind, it is
+highly extensible for future integration with other software.
+
+## Example Usage
+Take a look at the notebooks below to demonstrate the functionality of FTPVL.
+
+1. [Using `HydraFetcher` and Processors](https://colab.research.google.com/drive/1BIQ-iulDFpzcve7lGJPwLePJ5ETBJ6Ut?usp=sharing)
+2. [Styling tables with `SingleTableVisualizer`](https://colab.research.google.com/drive/1u3EnmIYnTBk-LXZhqNHt_h4aMuq-_cWq?usp=sharing)
+3. [Comparing two Evaluations using the internal dataframe](https://colab.research.google.com/drive/1I7InmA6210vIIwdQ7TGHE6aF_WwIm1dM?usp=sharing)
+4. [Filtering and Aggregating an Evaluation](https://colab.research.google.com/drive/1DDwlQFS81RGLL-q8DsgICF-HOC5ir6oS?usp=sharing)
+5. [Comparing multiple Evaluations](https://colab.research.google.com/drive/1kSF3bEjG_c6bLh9PLus9GPk5aLrtYhly?usp=sharing)
+
+## Documentation
+Extensive documentation, including a *Getting Started* guide, is available on
+[ReadTheDocs](https://ftpvl.readthedocs.io).
+
+Documentation of this library is generated in the `docs/` folder by reading
+the docstrings from the source code.
+The website is generated using [Sphinx](https://www.sphinx-doc.org/en/master/)
+using the [Read the Docs theme](https://github.com/readthedocs/sphinx_rtd_theme).
+
+```bash
+pip install -r requirements.txt
+cd docs
+make html
+```
+
+## Dependencies
+* `pandas`: for data management and processing ([website](https://pandas.pydata.org/))
+* `seaborn`: for colormap generation ([website](https://seaborn.pydata.org/))
+* `jinja2`: for visualization generation ([website](https://jinja.palletsprojects.com/))
+* `scipy`: for support of built-in aggregators([website](https://www.scipy.org/))
+
+### Development Dependencies
+* `requests-mock`: for mocking request object for testing fetchers ([website](https://requests-mock.readthedocs.io/en/latest/))
+* `pylint`: for linting ([website](https://www.pylint.org/))
+* `pytest`: testrunner ([website](https://docs.pytest.org/en/stable/))
+* `coverage`: for coverage testing ([website](https://coverage.readthedocs.io/))
+* `sphinx`: for documentation generation ([website](https://www.sphinx-doc.org/en/master/))
+* `sphinx-rtd-theme`: for documentation generation (theme) ([website](https://github.com/readthedocs/sphinx_rtd_theme))
+
+## Changes
+### 0.2.1
+* Add support for fetching using the absolute evaluation number instead of relative
+* Add support for handling legacy Icebreaker processors
+
+### 0.2.0
+* Added evaluation concatenation and `CompareToFirst` processors for relative comparisons between evaluations.
+* Fixed `HydraFetcher` issues when fetching an older evaluation that contains more than one build artifact.
+
+### 0.1.6
+* Added support for filter and aggregator processors, fixes [#9](https://github.com/SymbiFlow/FPGA-Tool-Performance-Visualization-Library/issues/9)
+
+### 0.1.5
+* Added support for custom projects and jobsets in HydraFetcher.
+
+### 0.1.4
+* Added RelativeDiff processor.
+* Updated some internal docstrings
+
---
+++ packages/ftpvl-beta/ftpvl-beta-0.2.1.tar.gz/ftpvl-beta-0.2.1/ftpvl/fetchers.py
@@ -0,0 +1,336 @@
+""" Fetchers are responsible for ingesting and standardizing data for future processing. """
+from datetime import datetime
+import json
+from typing import Any, Dict, List
+
+import pandas as pd
+import requests
+
+import ftpvl.helpers as Helpers
+from ftpvl.evaluation import Evaluation
+
+
+class Fetcher:
+    """
+    This is a superclass for all fetchers.
+
+    Fetchers allow the user to retrieve test data from a data source and output
+    as an Evaluation for use by other tools in the library.
+    """
+
+    def __init__(self):
+        self._abs_eval_id = None
+
+    def _download(self) -> Any:
+        """
+        Retrieves evaluation data over the internet and returns the data.
+        """
+        raise NotImplementedError
+
+    def _preprocess(self, data: Any) -> pd.DataFrame:
+        """
+        Process the data and return the resulting dataframe.
+        """
+        raise NotImplementedError
+
+    def get_evaluation(self) -> Evaluation:
+        """
+        Returns an Evaluation that represents the fetched data.
+        """
+        data = self._download()
+        preprocessed_df = self._preprocess(data)
+        return Evaluation(preprocessed_df, eval_id=self._abs_eval_id)
+
+
+class HydraFetcher(Fetcher):
+    """
+    Represents a downloader and preprocessor of test results from
+    `hydra.vtr.tools`.
+
+    Parameters
+    ----------
+    project : str
+        The project name to use when fetching from Hydra
+    jobset : str
+        The jobset name to use when fetching from Hydra
+    eval_num : int, optional
+        An integer that specifies the evaluation to download. Functionality
+        differs depending on whether `absolute_eval_num` is True, by default 0
+    absolute_eval_num : bool, optional
+        Flag that specifies if the eval_num is an absolute identifier instead of
+        a relative identifier. If True, the fetcher will find an evaluation
+        with the exact ID in `eval_num`. If False, `eval_num` should be a
+        non-negative integer with `0` being the latest evaluation and `1` being
+        the second latest evaluation, etc. By default False.
+    mapping : dict, optional
+        A dictionary mapping input column names to output
+        column names, if needed for remapping, by default None
+    hydra_clock_names : list, optional
+        An optional ordered list of strings used in finding
+        the actual frequency for each build result, by default None
+    """
+
+    def __init__(
+        self,
+        project: str,
+        jobset: str,
+        eval_num: int = 0,
+        absolute_eval_num: bool = False,
+        mapping: dict = None,
+        hydra_clock_names: list = None
+    ) -> None:
+        super().__init__() # inits self._abs_eval_id
+        self.project = project
+        self.jobset = jobset
+        self.eval_num = eval_num
+        self.absolute_eval_num = absolute_eval_num
+        self.mapping = mapping
+        self.hydra_clock_names = hydra_clock_names
+
+    def _get_builds(self, eval_num: int, params: str = "") -> List[int]:
+        """
+        Recursive function that returns a list of build numbers given an eval_num
+        and whether it is an absolute eval num.
+
+        Parameters
+        ----------
+        eval_num : int
+            An integer that specifies the evaluation to download. Functionality
+            differs depending on whether `absolute_eval_num` is True
+        params : str
+            A string of query parameters used when fetching the evaluations.
+            Most commonly used for pagination. By default, "".
+
+        Returns
+        -------
+        List[int]
+            A list of builds that correspond with the eval_num
+
+        Raises
+        ------
+        ConnectionError
+            Raised if the HTTP request to get the evaluations fails.
+        IndexError
+            Raised if the relative eval_num is not valid.
+        ValueError
+            Raised if the eval_num has no associated builds.
+        """
+        resp = requests.get(
+            f"https://hydra.vtr.tools/jobset/{self.project}/{self.jobset}/evals{params}",
+            headers={"Content-Type": "application/json"},
+        )
+        if resp.status_code != 200:
+            raise ConnectionError("Unable to get evals from server.")
+        evals_json = resp.json()
+
+        if self.absolute_eval_num:
+            self._abs_eval_id = eval_num # set absolute id for eval
+            for eval_data in evals_json["evals"]:
+                if eval_data["id"] == eval_num:
+                    return eval_data["builds"]
+            if "next" in evals_json:
+                return self._get_builds(
+                    eval_num,
+                    evals_json["next"] # query param for next page
+                )
+            else: # if couldn't find ID and there is no next page
+                raise ValueError(f"Unable to find absolute eval_num {eval_num}")
+
+        else: # if not absolute eval number
+            if eval_num >= len(evals_json["evals"]):
+                # check if there is a next page
+                if "next" in evals_json:
+                    return self._get_builds(
+                        eval_num - len(evals_json["evals"]),
+                        evals_json["next"] # query param for next page
+                    )
+                else:
+                    raise IndexError(f"Unable to find relative eval_num {self.eval_num}")
+            self._abs_eval_id = evals_json["evals"][eval_num]["id"] # set absolute id for eval
+            return evals_json["evals"][eval_num]["builds"]
+
+    def _download(self) -> List[Dict]:
+        """
+        Fetches data from Hydra, returning a list of decoded meta.json dicts
+        corresponding to the builds of the eval_num evaluation.
+
+        Returns
+        -------
+        List[Dict]
+            A list of dictionaries that represent the decoded meta.json files
+            of each test result.
+
+        Raises
+        ------
+        ConnectionError
+            Raised if `hydra.vtr.tools` returns a non-200 status code when
+            fetching evals.
+
+        IndexError
+            Raised if the specified eval_num is invalid due to it being too
+            large.
+
+        ValueError
+            Raised if all builds in a given eval failed.
+        """
+        # get build numbers from eval_num
+        build_nums = self._get_builds(self.eval_num)
+
+        # fetch build info and download 'meta.json'
+        data = []
+        for build_num in build_nums:
+            # get build info
+            resp = requests.get(
+                f"https://hydra.vtr.tools/build/{build_num}",
+                headers={"Content-Type": "application/json"},
+            )
+            if resp.status_code != 200:
+                raise Exception(f"Unable to get build {build_num}, got status code {resp.status_code}.")
+            
+            decoded = None
+            try:
+                decoded = resp.json()
+            except json.decoder.JSONDecodeError as err:
+                raise Exception(f"Unable to decode build {build_num} JSON file, {str(err)}")
+                
+            # check if build was successful
+            if decoded.get("buildstatus") != 0:
+                print(f"Warning: Build {build_num} failed with non-zero exit. Skipping...")
+                continue
+
+            # check if meta.json exists
+            meta_json_id = None
+            for product_id, product_desc in decoded.get("buildproducts", {}).items():
+                if product_desc.get("name", "") == "meta.json":
+                    meta_json_id = product_id
+            
+            if meta_json_id is None:
+                print(f"Warning: Build {build_num} does not contain meta.json file. Skipping...")
+                continue
+            
+            # download meta.json
+            resp = requests.get(
+                f"https://hydra.vtr.tools/build/{build_num}/download/{meta_json_id}/meta.json",
+                headers={"Content-Type": "application/json"},
+            )
+            if resp.status_code != 200:
+                print(
+                    "Warning:",
+                    f"Unable to get build {build_num} meta.json file.",
+                )
+                continue
+            try:
+                data.append(resp.json())
+            except json.decoder.JSONDecodeError:
+                print("Warning:", f"Unable to decode build {build_num}")
+
+        if len(data) == 0:
+            raise ValueError(f"Unable to get any successful builds from eval_num {self.eval_num}.")
+
+        return data
+
+    def _check_legacy_icebreaker(self, row):
+        """
+        Returns True if row is from a test on an Icebreaker board before Jul 31,
+        2020.
+
+        This is useful because these legacy tests recorded frequency in MHz
+        instead of Hz, while all other boards record in Hz. This flag can be
+        used to check if the units need to be changed.
+
+        Parameters
+        ----------
+        row : dict
+            a dictionary that is the result of decoding a meta.json file
+
+        Returns
+        -------
+        bool
+            Returns true if row is icebreaker board before Aug 1, 2020. False
+            otherwise.
+        """
+        date = None
+        board = None
+        try:
+            date = row["date"] # format: 2020-07-17T22:12:41
+            board = row["board"]
+            timestamp = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+            return timestamp < datetime(2020, 7, 31) and board == "icebreaker"
+        except KeyError:
+            print("Warning: Unable to find date and board in meta.json, required for supporting legacy Icebreaker.")
+            return False # Assume not legacy icebreaker
+
+    def _preprocess(self, data: List[Dict]) -> pd.DataFrame:
+        """
+        Using data from _download(), processes and standardizes the data and
+        returns a Pandas DataFrame.
+        """
+        flattened_data = [Helpers.flatten(x) for x in data]
+
+        processed_data = []
+        for row in flattened_data:
+            legacy_icestorm = self._check_legacy_icebreaker(row)
+            processed_row = {}
+            if self.mapping is None:
+                processed_row = row
+            else:
+                for in_col_name, out_col_name in self.mapping.items():
+                    processed_row[out_col_name] = row[in_col_name]
+
+            actual_freq = Helpers.get_actual_freq(row, self.hydra_clock_names)
+            if actual_freq:
+                if legacy_icestorm:
+                    # freq in MHz, no change needed
+                    processed_row["freq"] = actual_freq
+                else:
+                    processed_row["freq"] = actual_freq / 1_000_000 # convert hz to mhz
+            processed_row.update(Helpers.get_versions(row))
+            processed_data.append(processed_row)
+
+        return pd.DataFrame(processed_data).dropna(axis=1, how="all")
+    
+    def get_evaluation(self) -> Evaluation:
+        return super().get_evaluation()
+
+
+class JSONFetcher(Fetcher):
+    """
+    Represents a loader and preprocessor of test results from a JSON file.
+
+    Parameters
+    ----------
+    path : str
+        A string file path pointing to the dataframe JSON file.
+
+    mapping : dict, optional
+        An optional dictionary mapping input column names to output
+        column names., by default None
+    """
+
+    def __init__(self, path: str, mapping: dict = None) -> None:
+
+        super().__init__()
+        self.path = path
+        self.mapping = mapping
+
+    def _download(self) -> str:
+        """
+        Returns the path of the JSON file.
+        """
+        return self.path
+
+    def _preprocess(self, path: str) -> pd.DataFrame:
+        """
+        Given the path, load the data in pandas, process the data, and return
+        the resulting dataframe.
+        """
+        loaded_df = pd.read_json(path)
+        if self.mapping is None:
+            return loaded_df
+        else:
+            return loaded_df.filter(items=self.mapping.keys()).rename(
+                columns=self.mapping
+            )
+
+    def get_evaluation(self) -> Evaluation:
+        return super().get_evaluation()
