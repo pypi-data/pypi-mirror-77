9a1a33618d6733378df74f5f431401245fa955cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package grok-client/0.2/grok_client-0.2.tar.gz

---
+++ packages/grok-client/grok_client-0.2.tar.gz/grok_client-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='grok_client',
+    version='0.2',
+    packages=['grok_client'],
+    url='',
+    license='',
+    author='jarbasAI',
+    install_requires=["pexpect"],
+    author_email='jarbasai@mailfence.com',
+    description=''
+)
