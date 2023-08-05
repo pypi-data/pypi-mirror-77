e76d9c660b9f022b06e760553443774007615b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package datrics/0.0.1/datrics-0.0.1.tar.gz

---
+++ packages/datrics/datrics-0.0.1.tar.gz/datrics-0.0.1/datrics/controllers/projects_controller.py
@@ -0,0 +1,27 @@
+import requests
+
+from datrics.services.storage_service import StorageService
+from datrics.services.show_data_service import ShowDataService
+from datrics.services.show_message import show_error, show_message
+from datrics.controllers.auth_controller import AuthController
+from datrics.constants.links import GET_PROJECTS_URL
+
+
+class ProjectsController:
+
+    def __init__(self):
+        self.messages = []
+
+    def get_projects(self):
+        token = StorageService.get_token()
+        params = {
+            'token': token
+        }
+        response = requests.get(url=GET_PROJECTS_URL, params=params)
+
+        if response.status_code == requests.codes.ok:
+            projects = response.json()['projects']
+            ShowDataService.show_projects(projects)
+        elif response.status_code == requests.codes.unauthorized:
+            AuthController().logout()
+            show_error("Please log in to get projects")
