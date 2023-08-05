7433b06518a21920bec8a203ecf3b0fafdf7e878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package cadCAD-diagram/0.0.1.2/cadCAD_diagram-0.0.1.2.tar.gz

---
+++ packages/cadCAD-diagram/cadCAD_diagram-0.0.1.2.tar.gz/cadCAD_diagram-0.0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+long_description = """
+Automated cadCAD system models diagram through GraphViz
+"""
+
+setup(name='cadCAD_diagram',
+      version='0.0.1.2',
+      description="Diagram tool for cadCAD",
+      long_description=long_description,
+      url='https://github.com/danlessa/cadCAD_diagram',
+      author='Danilo Lessa Bernardineli',
+      author_email='danilo@block.science, danilo.lessa@gmail.com',
+      packages=find_packages(),
+      install_requires=['graphviz', 'cadCAD']
+)
