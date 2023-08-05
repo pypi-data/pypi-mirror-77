135a7b876bad2bb2ac4f4b3ca8438cc2a08ea2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:18 +0000

    Add package fancy-descriptor/0.1.0a4/fancy_descriptor-0.1.0a4-py3-none-any.whl

---
+++ packages/fancy-descriptor/fancy_descriptor-0.1.0a4-py3-none-any.whl/fancy_descriptors/tests/unit/method_descriptor_factories/test_method_descriptor_factory_base.py
@@ -0,0 +1,6 @@
+from fancy_descriptors.tests.unit.method_descriptor_factories.fake_method_descriptor_factory import FakeMethodDescriptorFactory
+
+
+class TestMethodDescriptorFactoryBase:
+    def test_singleton(self):
+        assert FakeMethodDescriptorFactory() is FakeMethodDescriptorFactory()
