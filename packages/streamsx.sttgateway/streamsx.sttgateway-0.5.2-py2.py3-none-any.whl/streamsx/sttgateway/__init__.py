e5d254693398a68714c6eb06062e4aa8b2b0e937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:42 +0000

    Add package streamsx.sttgateway/0.5.2/streamsx.sttgateway-0.5.2-py2.py3-none-any.whl

---
+++ packages/streamsx.sttgateway/streamsx.sttgateway-0.5.2-py2.py3-none-any.whl/streamsx/sttgateway/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2020
+
+"""
+"""
+
+
+__version__='0.5.2'
+
+__all__ = [ 'WatsonSTT', 'configure_connection', 'download_toolkit']
+from streamsx.sttgateway._sttgateway import WatsonSTT, configure_connection, download_toolkit
+
