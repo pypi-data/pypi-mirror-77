134e0f0fdf38c9b324453898043dff2f4021a5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package sktmls/2020.8.19/sktmls-2020.8.19.tar.gz

---
+++ packages/sktmls/sktmls-2020.8.19.tar.gz/sktmls-2020.8.19/sktmls/config/ye.py
@@ -0,0 +1,7 @@
+MLS_API_URL = {
+    "dev": "https://ab-internal.dev.sktmls.com",
+    "stg": "https://ab-internal.stg.sktmls.com",
+    "prd": "https://ab-internal.sktmls.com",
+}
+
+HDFS_OPTIONS = ""
