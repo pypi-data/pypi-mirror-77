e11c00eed35e973d5de6cc5b7d351ab66d921f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package invenio-drafts-resources/0.2.2/invenio_drafts_resources-0.2.2-py2.py3-none-any.whl

---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.2.2-py2.py3-none-any.whl/invenio_drafts_resources/resources/draft.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Drafts Resources module to create REST APIs."""
+
+from flask import g
+from flask_resources import CollectionResource, SingletonResource
+from flask_resources.context import resource_requestctx
+
+from ..services import RecordDraftService
+from .draft_config import DraftActionResourceConfig, DraftResourceConfig, \
+    DraftVersionResourceConfig
+
+
+class DraftResource(SingletonResource):
+    """Draft resource."""
+
+    default_config = DraftResourceConfig
+
+    def __init__(self, service=None, *args, **kwargs):
+        """Constructor."""
+        super(DraftResource, self).__init__(*args, **kwargs)
+        self.service = service or RecordDraftService()
+
+    def read(self):
+        """Read an item."""
+        identity = g.identity
+        id_ = resource_requestctx.route["pid_value"]
+
+        return self.service.read_draft(id_, identity), 200
+
+    def create(self):
+        """Create an item."""
+        data = resource_requestctx.request_content
+        identity = g.identity
+        id_ = resource_requestctx.route["pid_value"]
+
+        return self.service.edit(id_, data, identity), 201
+
+    def update(self):
+        """Update an item."""
+        # TODO: IMPLEMENT ME!
+        return self.service.update(), 200
+
+    def delete(self):
+        """Delete an item."""
+        # TODO: IMPLEMENT ME!
+        return self.service.delete(), 200
+
+
+class DraftVersionResource(CollectionResource):
+    """Draft version resource."""
+
+    default_config = DraftVersionResourceConfig
+
+    def __init__(self, service=None, *args, **kwargs):
+        """Constructor."""
+        super(DraftVersionResource, self).__init__(*args, **kwargs)
+        self.service = service or RecordDraftService()
+
+    def search(self):
+        """Perform a search over the items."""
+        # TODO: IMPLEMENT ME!
+        return self.service.search()
+
+    def create(self):
+        """Create an item."""
+        identity = g.identity
+        id_ = resource_requestctx.route["pid_value"]
+
+        return self.service.new_version(id_, identity), 201
+
+
+class DraftActionResource(SingletonResource):
+    """Draft action resource."""
+
+    default_config = DraftActionResourceConfig
+
+    def __init__(self, service=None, *args, **kwargs):
+        """Constructor."""
+        super(DraftActionResource, self).__init__(*args, **kwargs)
+        self.service = service or RecordDraftService()
+
+    def create(self):
+        """Any POST business logic."""
+        # FIXME: Implement as CMD patter loaded from config
+        if resource_requestctx.route["action"] == "publish":
+            identity = g.identity
+            id_ = resource_requestctx.route["pid_value"]
+            return self.service.publish(id_, identity), 202
+        return {}, 202
