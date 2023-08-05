888cc8848e96580880538375bbf274a0b20dba50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:06 +0000

    Add package chess-gif/0.0.1/chess_gif-0.0.1-py3-none-any.whl

---
+++ packages/chess-gif/chess_gif-0.0.1-py3-none-any.whl/chess_gif/create_frame.py
@@ -0,0 +1,168 @@
+from typing import Iterable,List
+from PIL import Image , ImageDraw
+from pprint import pprint
+import os 
+from pyvips import Image as VipsImage
+from chess import Board
+
+
+class Chess_Image:
+    """
+    Creates Single Image frames for as for rendering the GIFs
+    as per requirements.
+
+    Parameters
+    ----------
+    colors : tuple
+        Specify white and black square color in the format 
+        ``(white_color ,black_color)``.
+
+    side : int, optional
+        Side of **single square** of the chess_board in **pixels**. 
+        Defaults to ``70``.
+
+    piece_theme: str, optional.
+        Choose one of the available piece-themes mentioned in the notes below. 
+        Defaults to ``merida``.       
+
+    h_margin : `int`, optional
+        Black horizontal margin around the chess_board to be rendered in the GIF. 
+        Default ``0``.
+
+    v_margin : `int`, optional
+        Black vertical margin around the chess_board to be rendered in the GIF. 
+        Default ``0``.
+
+    """
+
+    def __init__(
+            self, colors:tuple,
+            side:int = 70,
+            piece_theme:str = 'merida',
+            h_margin:int = 0,
+            v_margin:int = 0
+     ):
+
+        # Changing the directory to read data
+        cwd = os.getcwd()
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(script_dir)
+        
+        self.white_color , self.black_color = colors
+
+        # Validating side of the square
+        assert isinstance(side,int),'side should be an int.'
+        assert side > 10  , 'side should be greater than 10 pixels.'
+        #: side of one square on the chessboard in pixels
+        self.side = side
+
+        # Validating piece theme
+        assert isinstance( piece_theme ,str ),'piece_theme should be a string.'
+        valid_piece_themes =  os.listdir('data/piece/')   # Get all the available piece_themes
+        piece_theme_stmt = f'{piece_theme} is not a valid piece_theme. Choose one of the following piece themes:\n{valid_piece_themes} .'
+        assert piece_theme in valid_piece_themes, piece_theme_stmt
+        self.piece_theme = piece_theme
+        # Load the pieces 
+        self.pieces = Chess_Pieces(piece_theme, round(side) )
+
+        #Set horizontal and vertical margin
+        self.horizantal_margin = h_margin
+        self.vertical_margin = v_margin
+
+
+        self.create_initial_board()
+
+        os.chdir(cwd)
+
+
+    def create_initial_board(self):
+        '''
+        Returns a PIL image object of the base chess board without pieces.
+        '''
+
+        #: Size of the 8x8 square chess-board
+        self.board_size =  8 * self.side
+        # self.vertical_margin = round( 0.15 * self.board_size  )
+        # self.horizantal_margin = round(0.02 * self.board_size)
+
+        # (Width , Height)
+        self.img_size = ( 
+                self.board_size + 2 * self.horizantal_margin,
+                self.board_size + 2 * self.vertical_margin
+             )
+        
+        # Atrribute which will have the base image of the board
+        self.board_img = Image.new( "RGB", self.img_size  )
+        ImageDrawer = ImageDraw.Draw(self.board_img)
+
+        for i in range(8):
+            y1 = self.vertical_margin + self.side * i
+            y2 = y1 + self.side
+            for j in range(8):
+                x1 = self.horizantal_margin + self.side * j
+                x2 =  x1 + self.side
+                if (i+j) % 2 == 0:
+                    color = self.white_color
+                else:
+                    color = self.black_color
+                ImageDrawer.rectangle( (x1,y1,x2,y2), fill =color, outline =color)
+
+        # self.board_img.save('Images/result.png') # To be removed 
+        return self.board_img
+
+
+
+    def create_position(self,current_position:Board):
+        '''
+        Renders passed current_postion on to the chess board.
+        '''
+        board_list = list( map( lambda s:s.split(),str(current_position).split('\n')) )
+        base_img = self.board_img.copy()
+        for r in range(8):
+            for c in range(8):
+                piece = board_list[r][c]
+                if  piece!= '.':
+                    piece_img = self.pieces.piece_imgs[piece]
+                    base_img.paste( piece_img, self.get_location(r,c), mask=piece_img )
+        # base_img.save('Images/board_pieces.png') #To be removed
+        return base_img
+
+    def get_location(self,row:int,col:int)->tuple:
+            x = self.horizantal_margin + col*self.side
+            y = self.vertical_margin + row*self.side
+            return x,y
+
+
+class Chess_Pieces:
+    """
+    Loads the Chess_Pieces from memory in PIL objects.
+    """
+    def __init__(self,piece_theme:str , size:int = 70 ):
+
+
+        piece_dir = os.path.join( 'data','piece', piece_theme )
+
+        #: Maps pieces to their corresponding .svg filenames
+        self.pieces_map ={
+            'r':'bR' , 'q':'bQ' , 'n':'bN' , 'k' : 'bK' , 'p':'bP', 'b':'bB',
+            'R':'wR' , 'Q':'wQ' , 'N':'wN' , 'K' : 'wK' , 'P':'wP', 'B':'wB'
+        }
+
+        # Reads the available piece theme's .svg images and save them as .png of appropraite size
+        for piece in self.pieces_map:
+            piece_path = os.path.join(piece_dir,  self.pieces_map[piece]+'.svg')
+            image = VipsImage.thumbnail(piece_path, size , height=size)
+            image.write_to_file(f"Images/{ self.pieces_map[piece]}.png")
+        
+        
+        #: Dictionary where pieces are the keys and corresponding PIL Images are the values
+        self.piece_imgs = dict()
+        for piece in  self.pieces_map:
+            self.piece_imgs[piece] = Image.open(f"Images/{ self.pieces_map[piece]}.png")
+
+    
+if __name__ == "__main__":
+    obj = Chess_Image( ('#ffe0b3','#802b00') , side ='2', piece_theme='dog' )
+    obj.create_position(Board())
+
+        
---
+++ packages/chess-gif/chess_gif-0.0.1-py3-none-any.whl/chess_gif/gif_maker.py
@@ -0,0 +1,187 @@
+from chess_gif.create_frame import Chess_Image
+from chess import pgn as pgn_reader
+from chess import flip_vertical,Board
+from typing import Iterable
+from pprint import pprint
+from pygifsicle import optimize as optimize_gif
+from imageio import mimwrite
+import os
+import errno
+from pkg_resources import resource_string
+
+class Gifmaker:
+    '''
+    Class for converting a chess ``PGN`` file to a ``GIF``.
+
+    Parameters
+    ----------
+    pgn_file_path : str
+        Path to the source PGN file to be convereted to GIF.
+
+    Other Parameters
+    ----------------
+    colors : `tuple`, optional
+        colors for white and black squares.. Default ``( '#9e3725','#ffe0b3' )``.
+    piece_theme : `str`, optional
+        Choose one of the available piece_themes.
+        Default ``merida``.
+    side : `int`, optional
+        Size of the side of a single chess-square in pixels. Default ``70``.
+    h_margin : `int`, optional
+        Black horizontal margin around the chess_board to be rendered in the GIF. 
+        Default ``0``.
+    v_margin : `int`, optional
+        Black vertical margin around the chess_board to be rendered in the GIF. 
+        Default ``0``.
+
+
+    Example
+    --------
+    >>> from chess_gif.gif_maker import Gifmaker
+    >>> obj = Gifmaker('game.pgn')
+    >>> obj.make_gif('chess_game.gif)
+
+    | ``chess_game.gif`` will be created in the current working directory.
+
+    .. image:: ../docs/chess_game.gif
+
+    |
+    | The resolution of the images is adjustable by changing the side keyword argument.
+
+
+    Note
+    ----
+        Following are the available piece-themes:
+
+        **{ 'alpha', 'california' , 'cardinal' , 'cburnett' , 'chess7' , 'chessnut' , 'companion' , 'dubrovny' , 'fantasy' , 'fresca' , 'gioco' , 'icpieces' , 'kosal' , 'leipzig' , 'letter' , 'libra' , 'maestro' , 'merida' , 'mono' , 'pirouetti' , 'pixel' , 'reillycraig' , 'riohacha' , 'shapes' , 'spatial' , 'staunty' , 'tatiana' }**.
+        
+        These are the publicly available themes taken from **lichess.org's** amazing `repository-lila <https://github.com/ornicar/lila>`_ .
+
+    '''
+
+    yellow_green =  ( '#ffffdd','#86a666')
+    brown_off_white = ('#f0d9b5', '#b58863')
+    blue_white = ('#dee3e6','#8ca2ad')
+
+    def __init__(self, 
+                pgn_file_path:str, 
+                **kwargs:dict ):
+        """Constructor method
+        """
+
+        # Set all the kwargs to default values if not provided
+        kwargs.setdefault('colors', ('#dee3e6','#8ca2ad') )
+        kwargs.setdefault('piece_theme','merida')
+        kwargs.setdefault('side',70)
+        kwargs.setdefault('h_margin',0)
+        kwargs.setdefault('v_margin',0)
+
+        pprint(kwargs)
+        
+        self.kwargs = kwargs
+
+        # Check if file exists
+        if not os.path.isfile(pgn_file_path):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), pgn_file_path) 
+
+        self.pgn_file_path = pgn_file_path
+
+
+    def make_gif( self, gif_file_path:str = 'chess.gif' ):
+        '''
+        Makes the gif of the loaded pgn at the specified destination file path.
+
+        Parameters
+        ----------
+        gif_file_path : str
+            Destination directory to store the gif file.
+
+        '''
+        
+        pgn_file = open(self.pgn_file_path)
+
+        chess_game = pgn_reader.read_game(pgn_file)
+
+        # Stores the headers in the pgn
+        self.header_info = chess_game.headers
+
+        # self.white_player = self.header_info['White']
+        # self.black_player = self.header_info['Black']
+        # self.BlackElo = self.header_info['BlackElo']
+        # self.WhiteElo = self.header_info['WhiteElo']
+
+        # print('White Player:',self.white_player)
+        # print('Black Player:',self.black_player,'\n')
+
+        # print( 'WhiteElo:', self.WhiteElo )
+        # print( 'BlackElo:', self.BlackElo )
+
+        # get string notation of the game
+        # print(str( chess_game.game()).replace('\n','') )
+
+        #get the start position
+        # print(chess_game.board() )
+
+        self.white_timeline = []
+        self.black_timeline = []
+        self.board_states = [Board()]
+
+        main_line_itr = chess_game.mainline()
+        for node in main_line_itr:
+            if node.parent is not None:
+                current_move = node.san()
+                time_left = node.clock()
+
+                current_board = node.board()
+                self.board_states.append(current_board)
+                
+                if current_board.turn:
+                    self.white_timeline.append(time_left)
+                else:
+                    self.black_timeline.append(time_left)
+
+                # White's perspective
+                # print('\n',current_board)  
+
+                # # Black's perspective
+                # print( '\n',current_board.transform( flip_vertical ))
+        
+        obj = Chess_Image(
+                        colors = self.kwargs['colors'],
+                        side = self.kwargs['side'], 
+                        piece_theme = self.kwargs['piece_theme'],
+                        h_margin=self.kwargs['h_margin'],
+                        v_margin=self.kwargs['v_margin']
+            )
+        
+
+        frames =  list( map(lambda x:obj.create_position(x) , self.board_states ) )
+
+        mimwrite( 
+            gif_file_path,
+            frames, duration = 1,
+            subrectangles = True ,
+            palettesize = 256 # default = 256
+        )
+        
+        optimize_gif(gif_file_path)
+
+        # print(self.white_timeline)
+        # print(self.black_timeline)
+
+
+if __name__ == "__main__":
+
+    # pgn_file_path = 'data/pgn/MikelOjda_vs_iDontTalkiDoIt_2020.03.19.pgn'
+    # pgn_file_path = 'data\\pgn\\lichess_pgn_2020.08.03_Mrunank_vs_prem_singh_bhati.gKZs5DjG.pgn'
+    pgn_file_path = r'data\pgn\lichess_pgn_2020.08.01_Mrunank_vs_Blindtakes34.E2og9BtO.pgn'
+
+    # yellow_green = (  '#ffffdd','#86a666')
+    # brown_off_white = ('#f0d9b5', '#b58863')
+    # blue_white = ('#dee3e6','#8ca2ad')
+
+    obj = Gifmaker(pgn_file_path, colors=Gifmaker.blue_white, piece_theme = 'merida' )
+    obj.make_gif('hello.gif')
+
+
+
