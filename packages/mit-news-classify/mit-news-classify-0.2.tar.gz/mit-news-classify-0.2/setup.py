f4a0249093a85f327a75bcbb92be0a65ecd8f1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package mit-news-classify/0.2/mit-news-classify-0.2.tar.gz

---
+++ packages/mit-news-classify/mit-news-classify-0.2.tar.gz/mit-news-classify-0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mit-news-classify", # Replace with your own username
+    version="0.2",
+    author="Arun Wongprommoon",
+    author_email="arunwpm@mit.edu",
+    description="A news classification tool developed for Improve the News, a project by Max Tegmark",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rayaburong/mit-news-classify",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tensorflow',
+        'sklearn',
+        'gensim',
+        'transformers',
+        'torch',
+        'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
