34efde9b03aaa6a607df8a98275a0c07fdb7548c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package napoleontoolbox/2.6.27/napoleontoolbox-2.6.27.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.6.27.tar.gz/napoleontoolbox-2.6.27/napoleontoolbox/napoleon_config_tools/__init__.py
@@ -0,0 +1,13 @@
+__author__ = "hugo.inzirillo"
+
+from napoleontoolbox.napoleon_config_tools.parser import target, config, getter
+
+__all__ = ['target', 'config', 'getter']
+
+
+
+
+
+
+if __name__ == '__main__':
+    end = True
