1fd1b93849ee78370f6c9458950891a827544224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:56 +0000

    Add package sherlockpipe/0.10.0/sherlockpipe-0.10.0.tar.gz

---
+++ packages/sherlockpipe/sherlockpipe-0.10.0.tar.gz/sherlockpipe-0.10.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sherlockpipe", # Replace with your own username
+    version="0.10.0",
+    author="F.J. Pozuelos & M. Dévora",
+    author_email="fjpozuelos@uliege.be",
+    description="Search for Hints of Exoplanets fRom Lightcurves Of spaCe based seeKers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/franpoz/SHERLOCK",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=["numpy",
+                        "cython",
+                        "pandas",
+                        "lightkurve",
+                        "transitleastsquares",
+                        "requests",
+                        "eleanor",
+                        "wotan",
+                        "matplotlib"
+    ]
+)
