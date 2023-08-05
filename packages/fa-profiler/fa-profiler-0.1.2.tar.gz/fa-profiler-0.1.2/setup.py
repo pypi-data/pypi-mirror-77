0970e23a8a20f6ca7990631887c4d1ce0eb0d85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package fa-profiler/0.1.2/fa-profiler-0.1.2.tar.gz

---
+++ packages/fa-profiler/fa-profiler-0.1.2.tar.gz/fa-profiler-0.1.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+setup.py - Build using tox
+"""
+
+import re
+import os
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+CURDIR = dirname(abspath(__file__))
+REQUIREMENTS = ['robotframework >= 3.0']
+with open(join(CURDIR, 'src', 'FormulatedAutomation', 'version.py')) as f:
+    VERSION = re.search("^VERSION = '(.*)'", f.read()).group(1)
+with open(join(CURDIR, 'README.md'), encoding='utf-8') as f:
+    DESCRIPTION = f.read()
+print(dir(os.listdir(CURDIR)))
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.8
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+
+setup(
+    name='fa-profiler',
+    version=VERSION,
+    description='Robot Framework System Profiler',
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author='Formulated Automation',
+    author_email='hello@formulatedautomation.com',
+    url='https://github.com/FormulatedAutomation/Profiler',
+    license='Apache License 2.0',
+    keywords='robotframework testing testautomation profiler',
+    platforms='any',
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS,
+    package_dir={'': 'src'},
+    packages=['FormulatedAutomation', 'FormulatedAutomation.Profiler']
+)
---
+++ packages/fa-profiler/fa-profiler-0.1.2.tar.gz/fa-profiler-0.1.2/src/FormulatedAutomation/Profiler/system_win.py
@@ -0,0 +1,97 @@
+"""
+System profiling module for windows
+"""
+
+import winreg
+from .system_base import SystemBase
+
+
+class SystemWin(SystemBase):
+    """Library for generating a profile report on the current system
+
+    """
+
+    def get_profile(self):
+        profile = super().get_profile()
+        profile['windows'] = {
+            'programs': self.get_programs(),
+        }
+        return profile
+
+    def get_programs(self):
+        return {
+            'uninstall_list': self.__get_uninstall_list(),
+            'office_info': self.__get_office_info(),
+        }
+
+    def _dump_program_list_from_hive(self, hive, flag):
+        """ Dump the registry listed programs"""
+        try:
+            a_reg = winreg.ConnectRegistry(None, hive)
+            a_key = winreg.OpenKey(
+                a_reg,
+                r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                0, winreg.KEY_READ | flag)
+
+            count_subkey = winreg.QueryInfoKey(a_key)[0]
+
+            software_list = []
+
+            for i in range(count_subkey):
+                software = {}
+                try:
+                    asubkey_name = winreg.EnumKey(a_key, i)
+                    asubkey = winreg.OpenKey(a_key, asubkey_name)
+                    software['name'] = winreg.QueryValueEx(
+                        asubkey, "DisplayName")[0]
+                    software['data_source'] = "Uninstall List"
+
+                    try:
+                        software['version'] = winreg.QueryValueEx(
+                            asubkey, "DisplayVersion")[0]
+                    except EnvironmentError:
+                        software['version'] = 'undefined'
+                    try:
+                        software['publisher'] = winreg.QueryValueEx(
+                            asubkey, "Publisher")[0]
+                    except EnvironmentError:
+                        software['publisher'] = 'undefined'
+                    software_list.append(software)
+                except EnvironmentError:
+                    continue
+            return sorted(software_list, key=lambda p: p['name'])
+        except FileNotFoundError:
+            return []
+
+
+    def __get_office_info(self):
+        """ Get more detailed Office info than you get from the Uninstall
+        list """
+        # TODO: @mdp better exception handling for all the things
+        try:
+            a_reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+            a_key = winreg.OpenKey(
+                a_reg,
+                r"SOFTWARE\Microsoft\Office\ClickToRun\Configuration",
+                0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
+            office_details = {
+                'name': 'Office 365',
+                'data_source': 'Registry Lookup',
+            }
+            office_details['version'] = winreg.QueryValueEx(
+                a_key, "VersionToReport")[0]
+            office_details['account'] = winreg.QueryValueEx(
+                a_key, "O365HomePremRetail.EmailAddress")[0]
+            return office_details
+        except FileNotFoundError: # Handle registry access or missing errors
+            return {}
+
+    def __get_uninstall_list(self):
+        return(
+            self._dump_program_list_from_hive(
+                winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_32KEY)
+            + self._dump_program_list_from_hive(
+                winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_64KEY)
+            + self._dump_program_list_from_hive(
+                winreg.HKEY_CURRENT_USER, 0)
+        )
