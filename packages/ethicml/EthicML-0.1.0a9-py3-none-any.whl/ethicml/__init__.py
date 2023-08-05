91035be6a4a3d0e7659cabd517788f68cc1fd031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package ethicml/0.1.0a9/EthicML-0.1.0a9-py3-none-any.whl

---
+++ packages/ethicml/EthicML-0.1.0a9-py3-none-any.whl/ethicml/__init__.py
@@ -0,0 +1,2 @@
+"""EthicML."""
+from . import algorithms, common, data, evaluators, metrics, preprocessing, utility, visualisation
---
+++ packages/ethicml/EthicML-0.1.0a9-py3-none-any.whl/ethicml/evaluators/__init__.py
@@ -0,0 +1,6 @@
+"""This module contains evaluators which apply algorithms over datasets and obtain metrics."""
+
+from .cross_validator import *
+from .evaluate_models import *
+from .parallelism import *
+from .per_sensitive_attribute import *
