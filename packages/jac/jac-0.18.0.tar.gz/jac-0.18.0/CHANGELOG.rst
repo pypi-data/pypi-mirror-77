43793874840d02552d3f48ee331e36c8c8363868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:58 +0000

    Add package jac/0.18.0/jac-0.18.0.tar.gz

---
+++ packages/jac/jac-0.18.0.tar.gz/jac-0.18.0/CHANGELOG.rst
@@ -0,0 +1,19 @@
+CHANGELOG
+---------
+
+0.17.2
+``````
+
+- Make sure that flask contrib script always push an app context.
+
+0.17.1
+``````
+
+- Fix error when the blueprint cannot be found (i.e. flask debug toolbar)
+
+0.17.0
+``````
+
+- Fix app factory initialization
+- Change how tests and linters run
+- Add compress command to `jac.contrib.flask`
---
+++ packages/jac/jac-0.18.0.tar.gz/jac-0.18.0/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='jac',
+    author='Jayson Reis',
+    author_email='santosdosreis@gmail.com',
+    version='0.18.0',
+    packages=find_packages(exclude=('tests*', )),
+    install_requires=open('requirements.txt').readlines(),
+    description='A Jinja extension (compatible with Flask and other frameworks) '
+                'to compile and/or compress your assets.',
+    long_description='{0}\n\n{1}'.format(read('README.rst'), read('CHANGELOG.rst')),
+    url='https://github.com/jaysonsantos/jinja-assets-compressor',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
