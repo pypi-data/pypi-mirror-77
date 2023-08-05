934c201b9425715ad18d9e837c11ed50bf292315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:57 +0000

    Add package wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue/0.4.0/wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue-0.4.0.tar.gz

---
+++ packages/wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue/wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue-0.4.0.tar.gz/wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue-0.4.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="wntlpdawotaiddoqaiblggazodvtwtuerncdsetygavokojzue", # Replace with your own username
+    version="0.4.0",
+    author="nobody",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
