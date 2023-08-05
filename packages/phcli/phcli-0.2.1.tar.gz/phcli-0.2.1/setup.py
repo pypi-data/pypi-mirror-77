94d7dd9ae08b458150cf3286b53472d617b96c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package phcli/0.2.1/phcli-0.2.1.tar.gz

---
+++ packages/phcli/phcli-0.2.1.tar.gz/phcli-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import io
+import setuptools
+from glob import glob
+
+
+with io.open("./README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phcli",
+    version="0.2.1",
+    author="Alfred Yang",
+    author_email="alfredyang@pharbers.com",
+    maintainer="ClockQ",
+    maintainer_email="zyqi@pharbers.com",
+    description="pharbers dag scheduler config",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+
+    package_data={
+        '': ['template/*.yaml', 'template/*.tmp'],
+    },
+    data_files=[
+        ('', glob('file/ph_data_clean/mapping_table/*')),
+    ],
+    entry_points={
+        'console_scripts': [
+            'phcli = phcli.__main__:phcli',
+        ],
+    }
+)
