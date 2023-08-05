21ab69a3d2190f121ebb0cbb08771cf18f6befb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package collective.contact.plonegroup/1.29/collective.contact.plonegroup-1.29.tar.gz

---
+++ packages/collective.contact.plonegroup/collective.contact.plonegroup-1.29.tar.gz/collective.contact.plonegroup-1.29/src/collective/contact/plonegroup/interfaces.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from plone.theme.interfaces import IDefaultPloneLayer
+from Products.PluggableAuthService.interfaces.events import IPASEvent
+from zope.interface import Interface
+
+
+class ICollectiveContactPlonegroupLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer."""
+
+
+class IPloneGroupContact(Interface):
+    """
+        Marker interface for plonegroup organizations.
+    """
+
+
+class INotPloneGroupContact(Interface):
+    """
+        Marker interface for non plonegroup contacts.
+    """
+
+
+class IPlonegroupGroupCreatedEvent(IPASEvent):
+    """A new Plone group linked to an organization has been created.
+    """
