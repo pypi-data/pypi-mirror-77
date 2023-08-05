b6ea46e6d2233d0e7bad6e6640b404a3be65cad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:19 +0000

    Add package pretty-traceback/2020.1007/pretty-traceback-2020.1007.tar.gz

---
+++ packages/pretty-traceback/pretty-traceback-2020.1007.tar.gz/pretty-traceback-2020.1007/src/pretty_traceback/__init__.py
@@ -0,0 +1,17 @@
+# This file is part of the pretty-traceback project
+# https://gitlab.com/mbarkhau/pretty-traceback
+#
+# Copyright (c) 2020 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+from .hook import install
+from .hook import uninstall
+
+__version__ = "2020.1007"
+
+
+__all__ = [
+    'install',
+    'uninstall',
+    '__version__',
+]
