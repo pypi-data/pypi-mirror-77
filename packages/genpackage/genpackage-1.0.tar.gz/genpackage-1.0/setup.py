c0728100ea99266bdb554eef013847f8d52df30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:28 +0000

    Add package genpackage/1.0/genpackage-1.0.tar.gz

---
+++ packages/genpackage/genpackage-1.0.tar.gz/genpackage-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='genpackage',  
+     version='1.0',
+     author="Pavan Kumar",
+     author_email="pawan.veeramraju@gmail.com",
+     description="Package for performing validation tasks for data of genevare",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=[
+          'pandas','numpy','openpyxl','re','argparse','xlsxwriter'
+      ]
+ )
