222bb59978b3b0a11755da8765b6d40a1b7047ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:47:13 +0000

    Add package stackl-client/0.1.4/stackl-client-0.1.4.tar.gz

---
+++ packages/stackl-client/stackl-client-0.1.4.tar.gz/stackl-client-0.1.4/stackl_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from stackl_client.api.about_api import AboutApi
+from stackl_client.api.configurator_api import ConfiguratorApi
+from stackl_client.api.documents_api import DocumentsApi
+from stackl_client.api.functional_requirements_api import FunctionalRequirementsApi
+from stackl_client.api.policy_agent_api import PolicyAgentApi
+from stackl_client.api.policy_templates_api import PolicyTemplatesApi
+from stackl_client.api.services_api import ServicesApi
+from stackl_client.api.stack_application_templates_api import StackApplicationTemplatesApi
+from stackl_client.api.stack_infrastructure_templates_api import StackInfrastructureTemplatesApi
+from stackl_client.api.stack_instances_api import StackInstancesApi
