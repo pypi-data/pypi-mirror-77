37c348bdd25013d84a03402c25fbd75a110bb2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package pyrocko/2020.8.18/pyrocko-2020.8.18-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2020.8.18-cp36-cp36m-manylinux1_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = 'ca81929f3af4030243f670c4ab9d3e34b9eea249'
+local_modifications = False
+version = '2020.08.18'
+long_version = 'pyrocko-2020.08.18-ca81929f3af4030243f670c4ab9d3e34b9eea249-2020-08-18_09:23:53'  # noqa
+installed_date = '2020-08-18_09:23:53'
