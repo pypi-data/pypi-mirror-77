cb6dba41b060d279571f1235368ebb609d52fdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package pygkquiz/1.1/Pygkquiz-1.1.tar.gz

---
+++ packages/pygkquiz/Pygkquiz-1.1.tar.gz/Pygkquiz-1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+  name="Pygkquiz",
+  version=1.1,
+  long_description=Path("README.md").read_text(),
+  author= "Ashok Kumar",
+  author_email= "ashbhati2@gmail.com",
+  packages=setuptools.find_packages(exclude=["tests","data"])
+)
