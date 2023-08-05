e31f5723610ed34b7e5779324c49aef83cdcbfeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:19 +0000

    Add package pypi-semantic/0.0.0/pypi_semantic-0.0.0-py3-none-any.whl

---
+++ packages/pypi-semantic/pypi_semantic-0.0.0-py3-none-any.whl/pypi_semantic/app.py
@@ -0,0 +1,7 @@
+def main():
+    print('siker.')
+    print('két sort ír ki.')
+
+    
+if __name__ == '__main__':
+    main()
---
+++ packages/pypi-semantic/pypi_semantic-0.0.0-py3-none-any.whl/tests/test_pypi_semantic.py
@@ -0,0 +1,5 @@
+from pypi_semantic import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
