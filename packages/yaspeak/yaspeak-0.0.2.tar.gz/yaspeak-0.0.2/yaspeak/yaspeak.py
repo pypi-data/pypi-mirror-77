064dad3b7993451262c12e7972d4cca416eb81f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:18 +0000

    Add package yaspeak/0.0.2/yaspeak-0.0.2.tar.gz

---
+++ packages/yaspeak/yaspeak-0.0.2.tar.gz/yaspeak-0.0.2/yaspeak/yaspeak.py
@@ -0,0 +1,12 @@
+﻿import requests
+
+class YaSpeak:
+    def __getattr__(self, speaker):
+        return lambda *data: self.say(speaker, *data)
+        
+    def say(self, speaker, text="hello", file="out.mp3"):
+        data = {"text": text}
+        if speaker != "yandex": data.update({"speaker": speaker})
+        output = requests.get("https://station.aimylogic.com/generate", data=data)
+        with open(file, 'wb') as f:
+            f.write(output.content)
