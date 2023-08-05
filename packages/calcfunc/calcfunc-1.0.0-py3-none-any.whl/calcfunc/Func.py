f0500ad1ae6e8993b38d950393d9b7b5ac889f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package calcfunc/1.0.0/calcfunc-1.0.0-py3-none-any.whl

---
+++ packages/calcfunc/calcfunc-1.0.0-py3-none-any.whl/calcfunc/Func.py
@@ -0,0 +1,17 @@
+class ComputeTwoNumbers():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def mul(self):
+        return self.a * self.b
+
+    def div(self):
+        return self.a / self.b
+
---
+++ packages/calcfunc/calcfunc-1.0.0-py3-none-any.whl/calcfunc/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+# File name: __init__.py
+# Author: Yan
+# Created Time: 2020-8-18
+"""
