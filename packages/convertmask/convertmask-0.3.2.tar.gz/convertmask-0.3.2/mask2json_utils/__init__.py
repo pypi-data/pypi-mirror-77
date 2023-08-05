17ff88bb3e97020d9f164741c7cb659a605fba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package convertmask/0.3.2/convertmask-0.3.2.tar.gz

---
+++ packages/convertmask/convertmask-0.3.2.tar.gz/convertmask-0.3.2/mask2json_utils/__init__.py
@@ -0,0 +1,13 @@
+'''
+@lanhuage: python
+@Descripttion: 
+@version: beta
+@Author: xiaoshuyui
+@Date: 2020-07-10 10:09:56
+LastEditors: xiaoshuyui
+LastEditTime: 2020-08-20 09:40:36
+'''
+__version__ = '0.3.2'
+
+__appname__ = 'convertmask'
+
