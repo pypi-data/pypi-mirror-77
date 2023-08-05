7b4b01aa8c12727ca8f944c187c544b3162f6ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:52 +0000

    Add package pf-dre-database-client/0.1.3/pf-dre-database-client-0.1.3.tar.gz

---
+++ packages/pf-dre-database-client/pf-dre-database-client-0.1.3.tar.gz/pf-dre-database-client-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+long_description = long_description.split('### Development', 1)[0]
+
+setup(
+    name="pf-dre-database-client",
+    version="0.1.3",
+    author="Dominic Hains",
+    author_email="d.hains@uq.edu.au",
+    description="Provides a client for all Data interactions required with "
+                "the Meter Management System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=['mms'],
+    install_requires=[
+        'psycopg2',
+        'pandas',
+        'pytz',
+        'python-dotenv'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
