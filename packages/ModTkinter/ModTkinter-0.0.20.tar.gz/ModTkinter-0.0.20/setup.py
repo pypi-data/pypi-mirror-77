ae7687977361ab4cec552a9f8bd02484f2b861af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:54 +0000

    Add package ModTkinter/0.0.20/ModTkinter-0.0.20.tar.gz

---
+++ packages/ModTkinter/ModTkinter-0.0.20.tar.gz/ModTkinter-0.0.20/setup.py
@@ -0,0 +1,33 @@
+
+import setuptools
+#from setuptools import setup, find_packages
+
+
+with open("README.rst","r") as fh:
+    long_description= fh.read()
+
+setuptools.setup(
+    name="ModTkinter", # name
+    version="0.0.20",
+    author="Zin-Lin-Htun",
+    author_email="zinlinhtun34@gmail.com",
+    description='A Mordernised version of Tkinter, HighDPI for Windows 10, contains some new functions and widgets',
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
+    
+    keywords="tkinter",
+    install_requires=['Pillow','numpy'],
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #package_data={'ModTkinter':['ButDef.png'],'ModTkinter':[]},
+    include_package_data = True,
+    python_requires='>=3.8',
+)
+
+
