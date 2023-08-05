13ee312dac0135fdf235999e2be98f62117d0095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:56 +0000

    Add package samoy/0.5/samoy-0.5.tar.gz

---
+++ packages/samoy/samoy-0.5.tar.gz/samoy-0.5/samoy/__init__.py
@@ -0,0 +1,4 @@
+from samoy.Case_Conversion import *
+from samoy.Handling_Duplicate import *
+from samoy.Handling_Missing import *
+from samoy.Handling_Null import *
---
+++ packages/samoy/samoy-0.5.tar.gz/samoy-0.5/setup.py
@@ -0,0 +1,61 @@
+from distutils.core import setup
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+setup(
+name = 'samoy',         # How you named your package folder (MyLib)
+
+packages = ['samoy'],   # Chose the same as "name"
+
+version = '0.5',      # Start with a small number and increase it with every change you make
+
+license='MIT',        
+
+description = 'samoy is a Python package for machine learning and data science, built on top of Pandas inbuilt libraries. This package will be useful for data pre-processing before starting off any machine learning or data science project as it will ease your entire process of data cleaning without much input from the user',
+long_description= color.BOLD+"Auto Data cleaner - samoy"+color.END,      # Type in your E-Mail
+
+long_description_content_type="text/markdown",
+author = 'Abhishek Pailwan,Priyanka Singh',                   # Type in your name
+
+author_email = 'samoyapi@gmail.com',      # Type in your E-Mail
+
+url = 'https://github.com/samoy-pckg/samoy',   # Provide either the link to your github or to your website
+
+download_url =
+
+'https://github.com/samoy-pckg/samoy/archive/v_05.zip',    # I explain this later on
+
+keywords = ['DATA SCIENCE', 'MACHINE LEARNING', 'DATA CLEANING','DATA PREPROCESSING','FEATURE ENGINEERING','DESCRIPTIVE ANALYSIS','PREDICTIVE ANALYSIS','STATISTICAL MODELING','PYTHON','PYSPARK'],   # Keywords that define your package best
+
+classifiers=[
+
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+    'Intended Audience :: Developers',      # Define that your audience are developers
+
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+
+    'Programming Language :: Python :: 3.4',
+
+    'Programming Language :: Python :: 3.5',
+
+    'Programming Language :: Python :: 3.6',
+
+  ],
+
+)
+
+ 
+
