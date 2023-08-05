1b82c068433a21893ff5d1638c6176637730ec8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:56 +0000

    Add package whitehole/0.0.3/whitehole-0.0.3.tar.gz

---
+++ packages/whitehole/whitehole-0.0.3.tar.gz/whitehole-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whitehole", 
+    version="0.0.3",
+    author="QuantMoon Technologies",
+    author_email="contacto@quantmoon.com",
+    description="Package for read and backtest data of financial assets in the most storage-efficient way, taking care of performance and store using zarr format files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quantmoon/whitehole",
+    include_package_data = True,
+    packages=setuptools.find_packages(),
+    install_requires=["pandas>=0.22.0",
+                      "numpy>=1.18.2",
+                      "zarr",
+                      "xarray>=0.13.0",
+                      "pandas-market-calendars>=1.4.2"
+                      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.5',
+)
