c53d8b4640d589a098fd32df39f3d79196744f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:20 +0000

    Add package wqrfproxy/0.6/wqrfproxy-0.6-py2-none-any.whl

---
+++ packages/wqrfproxy/wqrfproxy-0.6-py2-none-any.whl/wqrfproxy/test.py
@@ -0,0 +1,32 @@
+# -*- coding:utf8 -*-
+# import time
+# from wqrfproxy import *
+#
+# start_wqrfproxy()
+#
+# time.sleep(30)
+# try:
+#     assert_proxy('http://api.sugg.sogou.com/su','w')
+#     assert_proxy('http://api.sugg.sogou.com/su','wangzijia')
+# except Exception as e:
+#     print(e)
+#
+# stop_wqrfproxy()
+#
+# import unittest
+#
+# class Test(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         start_wqrfproxy()
+#     @classmethod
+#     def tearDown(self):
+#         stop_wqrfproxy()
+#
+#     def test_1(self):
+#         driver.find_element_by_id('').click()
+#         assert_proxy('http;//***.***.com/***/***','userId')
+
+s = 'aaaaaa'
+
+print(s.count('a'))
