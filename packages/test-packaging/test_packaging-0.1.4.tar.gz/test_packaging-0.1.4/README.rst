2c180123ce3fc8904cd7b9c39c5faad7472083f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:38 +0000

    Add package test-packaging/0.1.4/test_packaging-0.1.4.tar.gz

---
+++ packages/test-packaging/test_packaging-0.1.4.tar.gz/test_packaging-0.1.4/README.rst
@@ -0,0 +1,7 @@
+Test_packaging
+--------
+
+To use (with caution), simply do::
+
+    >>> import test_packaging
+    >>> print(test_packaging.math.sqrt(5))
---
+++ packages/test-packaging/test_packaging-0.1.4.tar.gz/test_packaging-0.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(name='test_packaging',
+      version='0.1.4',
+      description='Test of packaging.',
+      long_description='Test of packaging math, etc.',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Text Processing :: General',
+      ],
+      url='http://github.com/hector-mao-net/test_packaging',
+      author='Hector Mao',
+      author_email='hectormao1025@gmail.com',
+      license='MIT',
+      packages=['test_packaging'],
+      install_requires=[
+          'numpy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
