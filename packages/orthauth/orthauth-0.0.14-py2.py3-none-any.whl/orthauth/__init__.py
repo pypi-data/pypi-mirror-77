8b3294bbe49a64d3b12a0f7f27acecd14704aa1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:01 +0000

    Add package orthauth/0.0.14/orthauth-0.0.14-py2.py3-none-any.whl

---
+++ packages/orthauth/orthauth-0.0.14-py2.py3-none-any.whl/orthauth/__init__.py
@@ -0,0 +1,6 @@
+from orthauth.core import (AuthConfig,
+                           configure,
+                           configure_here,
+                           configure_relative,)
+
+__version__ = '0.0.14'
---
+++ packages/orthauth/orthauth-0.0.14-py2.py3-none-any.whl/orthauth/utils.py
@@ -0,0 +1,97 @@
+import os
+import logging
+from . import exceptions as exc
+
+
+def makeSimpleLogger(name, level=logging.INFO):
+    # TODO use extra ...
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    ch = logging.StreamHandler()  # FileHander goes to disk
+    fmt = ('[%(asctime)s] - %(levelname)8s - '
+           '%(name)14s - '
+           '%(filename)16s:%(lineno)-4d - '
+           '%(message)s')
+    formatter = logging.Formatter(fmt)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+
+log = makeSimpleLogger('orthauth')
+logd = log.getChild('data')
+
+
+def getenv(ordered_vars):
+    for var in ordered_vars:
+        if var in os.environ:
+            value = os.environ[var]
+            if value is not None:
+                return value
+
+
+def branches(tree):
+    for name, subtree in tree.items():
+        if subtree is None:
+            yield [name]
+        else:
+            for subbranch in branches(subtree):
+                yield [name, *subbranch]
+
+
+def parse_paths(raw_paths):
+    for elements in raw_paths:
+        if isinstance(elements, str):
+            elements = elements.split(' ')
+
+        
+        path = []
+        for element in elements:
+            if isinstance(element, int):
+                element = str(element)
+
+            path.append(element)
+
+        yield path
+
+
+class QuietTuple(tuple):
+    """ read only doesn't print, repr, reduce etc. """
+    def __add__(self, value):
+        raise TypeError('NOPE')
+
+    def __repr__(self):
+        return '[secure]'
+
+    def __str__(self):
+        return '[secure]'
+
+    def __reduce__(self):
+        return (tuple, tuple())
+
+
+class QuietDict(dict):
+    """ read only doesn't print, repr, reduce etc. """
+    def copy(self):
+        return None
+
+    def pop(self, key):
+        return None
+
+    def popitem(self, key):
+        return None
+
+    def update(self, value):
+        return None
+
+    def values(self):
+        return QuietTuple(super().values())
+
+    def __repr__(self):
+        return '{secure}'
+
+    def __str__(self):
+        return '{secure}'
+
+    def __reduce__(self):
+        return (dict, tuple())
