d5c0e98ae063793d02c438f8ef36af833bc1a074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:31 +0000

    Add package microspec/0.1.1a7/microspec-0.1.1a7-py3-none-any.whl

---
+++ packages/microspec/microspec-0.1.1a7-py3-none-any.whl/microspeclib/internal/configutil.py
@@ -0,0 +1,20 @@
+
+# Copyright 2020 by Chromation, Inc
+# All Rights Reserved by Chromation, Inc
+
+from microspeclib        import CHROMASPEC_ROOTDIR
+from microspeclib.logger import CHROMASPEC_LOGGER_JSON as log
+import os, sys
+
+def findConfig(filename):
+  log.info("filename=%s", filename)
+  developer = os.path.join(CHROMASPEC_ROOTDIR, "cfg", filename)
+  installed = os.path.join(sys.prefix, "share", "microspeclib", "cfg", filename)
+  local     = os.path.join(".", filename)
+  found     = None
+  if(  os.access(developer, os.R_OK)): found = developer;
+  elif(os.access(installed, os.R_OK)): found = installed;
+  elif(os.access(local,     os.R_OK)): found = local;
+  else: raise Exception("Cannot find config file %s"%(filename))
+  return found
+
