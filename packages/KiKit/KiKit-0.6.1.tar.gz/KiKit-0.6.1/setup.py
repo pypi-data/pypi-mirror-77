ebdf9367f13259db769b9fea266ee1857b731d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package KiKit/0.6.1/KiKit-0.6.1.tar.gz

---
+++ packages/KiKit/KiKit-0.6.1.tar.gz/KiKit-0.6.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+import versioneer
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="KiKit",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jan Mrázek",
+    author_email="email@honzamrazek.cz",
+    description="Automation for KiCAD boards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaqwsx/KiKit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "shapely",
+        "click",
+        "markdown2",
+        "pybars3",
+        "solidpython",
+        "versioneer"
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points = {
+        "console_scripts": [
+            "kikit=kikit.ui:cli"
+        ],
+    }
+)
