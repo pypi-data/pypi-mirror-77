3c3e84fb0eb0dfa9432c4781b8254433ef35a0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:05 +0000

    Add package mathlogic/1.0/mathlogic-1.0.tar.gz

---
+++ packages/mathlogic/mathlogic-1.0.tar.gz/mathlogic-1.0/mathlogic/leaderboardutils.py
@@ -0,0 +1,363 @@
+### Set up Bascis and define functions to 
+from jinja2 import Template
+from IPython.core.display import display, HTML
+import pandas as pd
+import numpy as np
+import hashlib
+import mysql.connector
+from datetime import datetime
+def leaderboard_record_add(eventid,client,phase,user,score,timechar,score_sort,metric,team='',email=''):
+    insert_query= """INSERT INTO mdl_hack_score (eventid,client, phase,user, score,timechar,score_sort,metric,team,email)
+    VALUES ('{}','{}','{}','{}',{},'{}','{}','{}','{}','{}')""".format(eventid,client, phase,user, score,timechar,score_sort,metric,team,email)
+    try:
+        mydb = mysql.connector.connect(host="www.fnmathlogic.com",user="mlportal_hack", password="m@thhackathon", database="mlportal_mdln1")
+        mycursor = mydb.cursor()
+        mycursor.execute(insert_query)
+        mydb.commit()
+        mydb.close()
+        return(0,"Rows Inserted :"+str(mycursor.rowcount))
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return(1,"Something went wrong: {}".format(err))
+
+leaderboard = Template("""
+<!doctype html>
+<html>
+  <head>
+    <title>{{ title }}</title>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Red+Hat+Display:400,900&display=swap");
+        body, html {
+          height: 100%;
+          width: 100%;
+        }
+
+        .center {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          -webkit-transform: translate(-50%, -50%);
+                  transform: translate(-50%, -50%);
+          z-index: 10;
+          font-family: 'Red Hat Display', sans-serif;
+        }
+
+        .top3 {
+          display: -webkit-box;
+          display: flex;
+          -webkit-box-pack: center;
+                  justify-content: center;
+          -webkit-box-align: end;
+                  align-items: flex-end;
+          color: #4B4168;
+        }
+        .top3 .item {
+          box-sizing: border-box;
+          position: relative;
+          background: #0080ff;
+          width: 10rem;
+          height: 12rem;
+          text-align: center;
+          padding: 2.8rem 0 0;
+          margin: 1rem 1rem 2rem;
+          border-radius: 0.5rem;
+          -webkit-transform-origin: bottom;
+                  transform-origin: bottom;
+          cursor: pointer;
+          -webkit-transition: -webkit-transform 200ms ease-in-out;
+          transition: -webkit-transform 200ms ease-in-out;
+          transition: transform 200ms ease-in-out;
+          transition: transform 200ms ease-in-out, -webkit-transform 200ms ease-in-out;
+          box-shadow: 0 0 4rem 0 rgba(0, 0, 0, 0.1), 0 1rem 2rem -1rem rgba(0, 0, 0, 0.3);
+        }
+        .top3 .item .pic {
+          position: absolute;
+          top: -2rem;
+          left: 2.5rem;
+          width: 4rem;
+          height: 4rem;
+          border-radius: 50%;
+          background-size: cover;
+          background-position: center;
+          margin-right: 1rem;
+          background: #ff8000;;
+          box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2), 0 1rem 1rem -0.5rem rgba(0, 0, 0, 0.3);
+        }
+        .top3 .item .pos {
+          font-weight: 900;
+          font-size: 1.5rem;
+          margin-bottom: 0.5rem;
+        }
+        .top3 .item .name {
+          font-size: 1.1rem;
+          color : purple;
+          margin-bottom: 0.5rem;
+        }
+        .top3 .item .score {
+          opacity: 0.5;
+          font-size: 1rem;
+        }
+        .top3 .item .score:after {
+          display: block;
+          content: '{{ post_text }}';
+          opacity: 0.5;
+        }
+        .top3 .item.one {
+          width: 10rem;
+          height: 15rem;
+          padding-top: 3.5rem;
+        }
+        .top3 .item.one .pic {
+          width: 5rem;
+          height: 5rem;
+          left: 2.5rem;
+        }
+        .top3 .item:hover {
+          -webkit-transform: scale(1.05);
+                  transform: scale(1.05);
+        }
+
+        .list {
+          padding-left: 2rem;
+          margin: 0 auto;
+        }
+        .list .item {
+          position: relative;
+          display: -webkit-box;
+          display: flex;
+          -webkit-box-align: center;
+                  align-items: center;
+          background: white;
+          height: 4rem;
+          border-radius: 4rem;
+          margin-bottom: 2rem;
+          margin-right: 2rem;
+          background: #0080ff;
+          -webkit-transform-origin: left;
+                  transform-origin: left;
+          cursor: pointer;
+          -webkit-transition: -webkit-transform 200ms ease-in-out;
+          transition: -webkit-transform 200ms ease-in-out;
+          transition: transform 200ms ease-in-out;
+          transition: transform 200ms ease-in-out, -webkit-transform 200ms ease-in-out;
+          box-shadow: 0 0 4rem 0 rgba(0, 0, 0, 0.1), 0 1rem 2rem -1rem rgba(0, 0, 0, 0.3);
+        }
+        .list .item .pos {
+          font-weight: 900;
+          position: absolute;
+          left: -2rem;
+          text-align: center;
+          font-size: 1.25rem;
+          width: 1.5rem;
+          color: purple;
+          opacity: 0.6;
+          -webkit-transition: opacity 200ms ease-in-out;
+          transition: opacity 200ms ease-in-out;
+        }
+        .list .item .pic {
+          width: 4rem;
+          height: 4rem;
+          border-radius: 50%;
+          background-size: cover;
+          background-position: center;
+          margin-right: 1rem;
+          background-color: #ff8000;
+          box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2), 0 1rem 1rem -0.5rem rgba(0, 0, 0, 0.3);
+        }
+        .list .item .name {
+          -webkit-box-flex: 2;
+                  flex-grow: 2;
+          flex-basis: 10rem;
+          font-size: 1.5rem;
+          color : purple;
+        }
+        .list .item .score {
+          margin-right: 1.5rem;
+          opacity: 0.5;
+          font-size: 1rem;
+        }
+        .list .item .score:after {
+          margin-right: 1rem;
+          content: '{{ post_text }}';
+          opacity: 0.8;
+        }
+        .list .item:hover {
+          -webkit-transform: scale(1.05);
+                  transform: scale(1.05);
+        }
+        .list .item:hover .pos {
+          opacity: 0.8;
+        }
+
+    </style>
+  </head>
+  <body>
+    <div class="list">
+              <h1 style="text-align:center">{{ title }}</h1>
+    </div>
+        <div class="list">
+              <h1 style="text-align:center">{{ client }} : {{ phase}}</h1>
+    </div>
+    <div class="list">
+              <h1 style="text-align:center"></h1>
+    </div>
+        <div class="top3"> {% for data2_records in data2 %}
+            <div class="two item">
+              <div class="pos">
+            {{data2_records.rank}}
+              </div>
+              <div class="pic" style="background-image: url(&#39;{{ data2_records.img_url }}#39;)"></div>
+              <div class="name">
+                {{data2_records.name}}
+              </div>
+              <div class="score">
+                {{data2_records.score}}
+              </div>
+            </div> {% endfor %}
+            <div class="one item"> {% for data1_records in data1 %}
+              <div class="pos">
+                {{data1_records.rank}}
+              </div>
+              <div class="pic" style="background-image: url(&#39;{{ data1_records.img_url }}#39;)"></div>
+              <div class="name">
+                {{data1_records.name}}
+              </div>
+              <div class="score">
+                {{data1_records.score}}
+              </div>
+            </div> {% endfor %}
+            <div class="three item"> {% for data3_records in data3 %}
+              <div class="pos">
+                {{data3_records.rank}}
+              </div>
+              <div class="pic" style="background-image: url(&#39;{{ data3_records.img_url }}#39;)"></div>
+              <div class="name">
+                {{data3_records.name}}
+              </div>
+              <div class="score">
+                {{data3_records.score}}
+              </div>
+            </div>  {% endfor %}
+        </div>
+        <div class="list">
+            {% for records in data %}
+             <div class="item">
+              <div class="pos">
+               {{ records.rank }}
+              </div>
+              <div class="pic" style="background-image: url(&#39;{{ records.img_url }}&#39;)"></div>
+              <div class="name">
+                {{records.name}}
+              </div>
+              <div class="score">
+                {{records.score}}
+              </div>
+            </div>
+          {% endfor %}
+        </div>
+  </body>
+</html>
+""")
+
+def get_leaderboard(eventid,client,phase,return_format): 
+    leaderboard_query = "select eventid,client,phase,user,round(max(score),3) as max_score, round(min(score),3) as min_score, max(email) as email,max(team) as team, max(score_sort) as score_sort, max(metric) as metric  from mdl_hack_score where eventid = '{}' and client = '{}' and phase = '{}' and exclude is Null group by eventid, client, user, phase".format(eventid,client,phase)
+    try:
+        mydb = mysql.connector.connect(host="www.fnmathlogic.com",user="mlportal_hack", password="m@thhackathon", database="mlportal_mdln1")
+        mycursor = mydb.cursor()
+        mycursor.execute(leaderboard_query)
+        df=pd.DataFrame(mycursor.fetchall(),columns=["eventid","client","phase","user","max_score","min_score","email","team","score_sort","metric"])
+        mydb.close()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return "1","Something went wrong: {}".format(err)
+
+    #df=pd.DataFrame(mycursor.fetchall(),columns=["id","eventid","client","phase","user","score","timechar","score_sort","metric","exclude","team","email","sys_time"])
+    if df.shape[0]< 4:
+        if df.shape[0]==0:
+            sort_order = 1
+            metric = ""
+        else:
+            sort_order = df.iloc[0,8]
+            metric = df.iloc[0,9]
+        df2 = pd.DataFrame({"min_score":[1000000,1000000,1000000,1000000],"max_score":[0,0,0,0],"user":["yet.to.submit","yet.to.submit","yet.to.submit","yet.to.submit"],"client":["mathlogic","mathlogic","mathlogic","mathlogic"]})
+        df=df.append(df2[:4-df.shape[0]],ignore_index=True, sort=False)
+    else:
+        sort_order = df.iloc[0,8]
+        metric = df.iloc[0,9]
+    if sort_order == 0:
+        df.rename(columns={'max_score':'score'},inplace=True)
+    else:
+        df.rename(columns={'min_score':'score'},inplace=True)
+
+    df.sort_values(by='score',ascending=True,inplace=True)
+    df.reset_index(inplace=True,drop=True)
+    df['rank']=df.index+1
+
+    df['email']= df.apply(lambda x: x['user']+"@"+x['client']+".com" if x['email'] in [None,'',np.NaN] else x['email'],axis=1)
+    df['img_url']=df.apply(lambda x: "https://www.gravatar.com/avatar/"+hashlib.md5((x['email']).encode()).hexdigest()+"?d=robohash",axis=1)
+    df['name'] = df.apply(lambda x: ".".join([y.capitalize() for y in x['user'].split('@')[0].split(".")]),axis=1)
+    data1 = df.iloc[[0]].to_dict('records')
+    data2 = df.iloc[[1]].to_dict('records')
+    data3 = df.iloc[[2]].to_dict('records')
+    data = df.iloc[3:].to_dict('records')
+    leaderboard_html = leaderboard.render(data1=data1,data2=data2,data3=data3,data=data,post_text=metric,client=client.upper(), phase=phase.capitalize(), title="Hackathon Leaderboard") 
+
+    if return_format == 'dataframe':
+        #print(df)
+        return df
+    elif return_format == 'html':
+        #print(leaderboard_html)
+        return leaderboard_html
+    elif return_format == "display_html":
+        from IPython.core.display import display, HTML
+        display(HTML(leaderboard_html))
+        return None
+    
+
+
+def evaluate_result(indf,eventid,client,phase,metric,act_file,pred_cols):
+    from mathlogic import aws_access_key_id,aws_secret_access_key
+    import pandas as pd
+    import numpy as np
+    import sys
+    import pandas as pd
+    import boto3
+    from io import StringIO
+    s3 = boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    read_file = s3.get_object(Bucket='hackathon.key',Key=act_file)
+    act = pd.read_csv(read_file['Body'],sep=',')
+    if set(indf.columns) == set(pred_cols):
+        indf = indf[pred_cols]
+        cols = pred_cols
+        cols.sort()
+        cols.remove("id")
+    else:
+        print("Solution dataframe for "+ phase +" should contain only the following columns : ",pred_cols, "Exiting...")
+        sys.exit(1)
+    merged=pd.merge(act,indf,on="id",how="inner")
+    if merged.shape[0] == act.shape[0]:
+        import getpass
+        from datetime import datetime
+        user = getpass.getuser()
+        if metric =='LogLoss':
+            from sklearn.metrics import log_loss
+            score=log_loss(merged['y_true'],merged[[*cols]].values)
+            score_sort=1
+        if metric =='RMSE':
+            from sklearn.metrics import mean_squared_error
+            score=mean_squared_error(merged['y_true'],merged['y_pred'],squared=False)
+            score_sort=1
+        if metric =='BinaryCrossEntropy':
+            from tensorflow.keras.losses import BinaryCrossentropy
+            act_cols = [x for x in act.columns if x not in ('id')]
+            act_cols.sort()
+            score = BinaryCrossentropy()(merged[act_cols].values, np.array(merged[cols],dtype=np.float)).numpy()
+            score_sort=1
+        #from mathlogic import leaderboard_record_add,get_leaderboard
+        timechar=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        leaderboard_record_add(eventid=eventid,client=client,phase=phase,user=user,score=score,timechar=timechar,score_sort=score_sort,metric=metric)
+        print(user,score)
+        get_leaderboard(eventid=eventid,client=client,phase=phase,return_format="display_html")
+    else:
+        print("Given Dataframe does not contain all the required predictions")
+
---
+++ packages/mathlogic/mathlogic-1.0.tar.gz/mathlogic-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='mathlogic',
+      version='1.0',
+      description='Python Utilities from MathLogic',
+      url='https://github.com/vermanurag/mathlogic-mathlogic',
+      author='Anurag Verma',
+      author_email='anurag.verma@fnmathlogic.com',
+      license='All Rights Reserved',
+      packages=['mathlogic'],
+      install_requires=[            # I get to this in a second
+          'pandas',
+          'jinja2',
+          'numpy',
+          'mysql',
+          'Ipython'
+      ],
+      zip_safe=False)
