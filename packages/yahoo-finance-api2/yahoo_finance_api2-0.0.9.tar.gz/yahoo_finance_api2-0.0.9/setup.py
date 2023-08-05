2b31f3260a20819b4a0ec5372c10bb7a493bbaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package yahoo-finance-api2/0.0.9/yahoo_finance_api2-0.0.9.tar.gz

---
+++ packages/yahoo-finance-api2/yahoo_finance_api2-0.0.9.tar.gz/yahoo_finance_api2-0.0.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yahoo_finance_api2",
+    version="0.0.9",
+    author="Petr Kout",
+    description="Yahooo Finance API package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pkout/yahoo_finance_api2",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      "pyyaml",
+      "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
