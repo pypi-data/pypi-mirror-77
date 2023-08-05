2ce52c01e6920c21ec7983d6c5d23d2a0f37568e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:51 +0000

    Add package PyBetterGitUp/0.2.0/PyBetterGitUp-0.2.0.tar.gz

---
+++ packages/PyBetterGitUp/PyBetterGitUp-0.2.0.tar.gz/PyBetterGitUp-0.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PyBetterGitUp",
+    version="0.2.0",
+    packages=find_packages(exclude=["tests"]),
+    scripts=['PyBetterGitUp/bgitup.py'],
+    install_requires=[
+        'GitPython==3.1.7',
+        'git-up==1.6.4',
+        'colorama>=0.4.3',
+    ],
+
+    # Executable
+    entry_points={
+        'console_scripts': [
+            'bgitup = bgitup:run'
+        ]
+    },
+
+    # Metadata
+    author="Edward Taylor",
+    author_email="edweird06@users.noreply.github.com",
+    description="A better package for git up",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edweird06/BetterGitUp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
