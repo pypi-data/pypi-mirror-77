f76fbee458f769b2b87eea88795a0034ee70b783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package tidal-dl-test/2020.8.18.4/tidal-dl-test-2020.8.18.4.tar.gz

---
+++ packages/tidal-dl-test/tidal-dl-test-2020.8.18.4.tar.gz/tidal-dl-test-2020.8.18.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'tidal-dl-test',
+    version="2020.8.18.4",
+    license="Apache2",
+    description = "Tidal Music Downloader.",
+
+    author = 'YaronH',
+    author_email = "yaronhuang@foxmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires=["aigpy>=2020.8.18.0", "requests", "pycryptodome", "pydub", "prettytable"],
+    entry_points={'console_scripts': [ 'tidal-dl = tidal_dl:main', ]}
+)
