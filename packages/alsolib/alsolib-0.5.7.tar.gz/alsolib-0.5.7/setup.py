852311ad97a2c64c2c5da81f28f2497683c5be0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package alsolib/0.5.7/alsolib-0.5.7.tar.gz

---
+++ packages/alsolib/alsolib-0.5.7.tar.gz/alsolib-0.5.7/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="alsolib",
+    version="0.5.7",
+    description="学而思爱搜浏览器功能库0.5.7，下载失败请切换python原版,©爱搜浏览器团队 版权所有",
+    author="asunc",
+    author_email="3182305655@qq.com",
+    py_modules=["alsolib\\alsolib",'alsolib\\alsoapi']
+)
