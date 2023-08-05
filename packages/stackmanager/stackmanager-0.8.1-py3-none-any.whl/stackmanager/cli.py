e871c091ffb31f1a3cfbf93531aca7dbe76fd442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:55 +0000

    Add package stackmanager/0.8.1/stackmanager-0.8.1-py3-none-any.whl

---
+++ packages/stackmanager/stackmanager-0.8.1-py3-none-any.whl/stackmanager/cli.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+import click
+import logging
+import stackmanager.packager
+from stackmanager.exceptions import PackagingError, StackError, TransferError, ValidationError
+from stackmanager.loader import load_config
+from stackmanager.messages import error
+from stackmanager.runner import create_runner, create_changeset_runner
+from stackmanager.uploader import create_uploader
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+
+@click.group(chain=True)
+@click.pass_context
+def cli(ctx):
+    """
+    Utility for managing CloudFormation stacks.
+    """
+    ctx.obj = dict()
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-c', '--config', required=True, help='YAML Configuration file')
+@click.option('-e', '--environment', required=True, help='Environment to deploy')
+@click.option('-r', '--region', required=True, help='AWS Region to deploy')
+@click.option('-t', '--template', help='Override template')
+@click.option('--parameter', nargs=2, multiple=True, help='Override a parameter, can be specified multiple times')
+@click.option('--change-set-name', help='Custom ChangeSet name')
+@click.option('--existing-changes', type=click.Choice(['ALLOW', 'FAILED_ONLY', 'DISALLOW'], case_sensitive=False),
+              default='ALLOW', help='Whether deployment is allowed when there are existing ChangeSets')
+@click.option('--auto-apply', is_flag=True, help='Automatically apply created ChangeSet')
+def deploy(ctx, profile, config, environment, region, template, parameter, change_set_name, existing_changes,
+           auto_apply):
+    """
+    Create or update a CloudFormation stack using ChangeSets.
+    """
+    try:
+        cfg = load_config(config, environment, region, Template=template, Parameters=parameter,
+                          ChangeSetName=change_set_name, ExistingChanges=existing_changes, AutoApply=auto_apply)
+        runner = create_runner(profile, cfg)
+        runner.deploy()
+    except (ValidationError, StackError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-c', '--config', help='YAML Configuration file')
+@click.option('-e', '--environment', help='Environment to deploy')
+@click.option('-r', '--region', help='AWS Region to deploy')
+@click.option('--change-set-name', help='Name of ChangeSet to apply')
+@click.option('--change-set-id', help='Identifier of ChangeSet to apply')
+def apply(ctx, profile, config, environment, region, change_set_name, change_set_id):
+    """
+    Apply a CloudFormation ChangeSet to create or update a CloudFormation stack.
+    If using --change-set-name then --config --environment are --region are required.
+    If using --change-set-id no other values are required (although --profile and --region may be needed).
+    """
+    if not change_set_name and not change_set_id:
+        raise click.UsageError("Option '--change-set-name' or '--change-set-id' required.")
+
+    try:
+        if change_set_id:
+            runner = create_changeset_runner(profile, region, change_set_id)
+            runner.execute_change_set()
+        else:
+            if not config:
+                raise click.UsageError("Missing option '-c' / '--config'.")
+            if not environment:
+                raise click.UsageError("Missing option '-e' / '--environment'.")
+
+            cfg = load_config(config, environment, region, False, ChangeSetName=change_set_name)
+            runner = create_runner(profile, cfg)
+            runner.execute_change_set()
+    except (ValidationError, StackError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-c', '--config', help='YAML Configuration file')
+@click.option('-e', '--environment', help='Environment for stack')
+@click.option('-r', '--region', help='AWS Region for stack')
+@click.option('--change-set-name', help='Name of ChangeSet to reject')
+@click.option('--change-set-id', help='Identifier of ChangeSet to reject')
+def reject(ctx, profile, config, environment, region, change_set_name, change_set_id):
+    """
+    Reject a CloudFormation ChangeSet, deleting the stack if in REVIEW_IN_PROGRESS status and has no other ChangeSets.
+    If using --change-set-name then --config --environment are --region are required.
+    If using --change-set-id no other values are required (although --profile and --region may be needed).
+    """
+    if not change_set_name and not change_set_id:
+        raise click.UsageError("Option '--change-set-name' or '--change-set-id' required.")
+
+    try:
+        if change_set_id:
+            runner = create_changeset_runner(profile, region, change_set_id)
+            runner.reject_change_set()
+        else:
+            if not config:
+                raise click.UsageError("Missing option '-c' / '--config'.")
+            if not environment:
+                raise click.UsageError("Missing option '-e' / '--environment'.")
+
+            cfg = load_config(config, environment, region, False, ChangeSetName=change_set_name)
+            runner = create_runner(profile, cfg)
+            runner.reject_change_set()
+    except (ValidationError, StackError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-c', '--config', required=True, help='YAML Configuration file')
+@click.option('-e', '--environment', required=True, help='Environment to deploy')
+@click.option('-r', '--region', required=True, help='AWS Region to deploy')
+@click.option('--retain-resources', multiple=True, help='Logical Ids of resources to retain')
+def delete(ctx, profile, config, environment, region, retain_resources):
+    """
+    Delete a CloudFormation stack.
+    """
+    try:
+        cfg = load_config(config, environment, region, False)
+        runner = create_runner(profile, cfg)
+        runner.delete(retain_resources)
+    except (ValidationError, StackError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-c', '--config', required=True, help='YAML Configuration file')
+@click.option('-e', '--environment', required=True, help='Environment to deploy')
+@click.option('-r', '--region', required=True, help='AWS Region to deploy')
+@click.option('--event-days', type=int, default=7, help='Number of days of events to include in output')
+def status(ctx, profile, config, environment, region, event_days):
+    """
+    Print current status of Stack.
+    Includes pending ChangeSets and recent events.
+    """
+    cfg = load_config(config, environment, region, False)
+    runner = create_runner(profile, cfg)
+    runner.status(event_days)
+
+
+@cli.command(name='build-lambda')
+@click.pass_context
+@click.option('-s', '--source-dir', required=True, help='Source directory')
+@click.option('-o', '--output-dir', required=True, help='Output directory')
+@click.option('--runtime', required=True, help='Lambda Runtime')
+@click.option('--archive-name', help='Override archive name (defaults to source directory name)')
+def build_lambda(ctx, source_dir, output_dir, runtime, archive_name):
+    """Build a Lambda function zip file."""
+    try:
+        stackmanager.packager.build_lambda(source_dir, output_dir, runtime, archive_name)
+    except (PackagingError, ValidationError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+@cli.command()
+@click.pass_context
+@click.option('-p', '--profile', help='AWS Profile, will use default or environment variables if not specified')
+@click.option('-r', '--region', required=True, help='AWS Region to upload to')
+@click.option('-f', '--filename', required=True, help='File to upload')
+@click.option('-b', '--bucket', required=True, help='Bucket to upload to')
+@click.option('-k', '--key', required=True, help='Key to upload to')
+def upload(ctx, profile, region, filename, bucket, key):
+    """
+    Uploads a File to S3.
+    This might be a large CloudFormation template, or a Lambda zip file.
+    """
+    try:
+        uploader = create_uploader(profile, region)
+        uploader.upload(filename, bucket, key)
+    except (TransferError, ValidationError) as e:
+        error(f'\nError: {e}')
+        exit(1)
+
+
+if __name__ == '__main__':
+    cli(prog_name='stackmanager')
---
+++ packages/stackmanager/stackmanager-0.8.1-py3-none-any.whl/stackmanager/loader.py
@@ -0,0 +1,72 @@
+from .exceptions import ValidationError
+from .config import Config, ENVIRONMENT, REGION, TEMPLATE, PARAMETERS, CHANGE_SET_NAME, CHANGE_SET_ID, \
+    EXISTING_CHANGES, AUTO_APPLY
+import yaml
+
+
+def load_config(config_file, environment, region, check_template=True, **kwargs):
+    """
+    Build hierarchy of configurations by loading multi-document config file.
+    There must be a matching config for the environment name and region.
+    :param str config_file: Path to config file
+    :param str environment: Environment being updated
+    :param str region: Region for the Stack
+    :param bool check_template: Check Template exists when validating config
+    :param dict kwargs: Other arguments to be added to arg config
+    :return: Top of Config hierarchy
+    :raises validationException: If config file not found, matching environment not found in config or config is invalid
+    """
+    arg_config = create_arg_config(environment, region, kwargs)
+
+    try:
+        with open(config_file) as c:
+            raw_configs = yaml.safe_load_all(c)
+            all_config = None
+            env_config = None
+            for rc in raw_configs:
+                config = Config(rc)
+                if Config.is_all(config.environment):
+                    all_config = config
+                elif config.environment == environment and config.region == region:
+                    env_config = config
+
+            if not env_config:
+                raise ValidationError(f'Environment {environment} for {region} not found in {config_file}')
+
+            env_config.set_parent(all_config)
+            arg_config.set_parent(env_config)
+
+            # Validate before returning
+            arg_config.validate(check_template)
+            return arg_config
+    except FileNotFoundError:
+        raise ValidationError(f'Config file {config_file} not found')
+
+
+def create_arg_config(environment, region, kwargs):
+    """
+    Create a Configuration from the command line arguments, used as top of hierarchy to
+    optionally override template and parameters.
+    :param str environment: Environment
+    :param str region: Region to deploy
+    :param dict kwargs: Other arguments to be added to arg config
+    :return: Argument Config
+    """
+    raw_config = {
+        ENVIRONMENT: environment,
+        REGION: region
+    }
+    if TEMPLATE in kwargs:
+        raw_config[TEMPLATE] = kwargs[TEMPLATE]
+    if PARAMETERS in kwargs:
+        raw_config[PARAMETERS] = dict(kwargs[PARAMETERS])
+    if CHANGE_SET_NAME in kwargs:
+        raw_config[CHANGE_SET_NAME] = kwargs[CHANGE_SET_NAME]
+    if CHANGE_SET_ID in kwargs:
+        raw_config[CHANGE_SET_ID] = kwargs[CHANGE_SET_ID]
+    if EXISTING_CHANGES in kwargs:
+        raw_config[EXISTING_CHANGES] = kwargs[EXISTING_CHANGES]
+    if AUTO_APPLY in kwargs:
+        raw_config[AUTO_APPLY] = kwargs[AUTO_APPLY]
+
+    return Config(raw_config)
