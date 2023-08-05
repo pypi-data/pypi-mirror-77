d8c209aa524195852937e98aa3465e489ecb6ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:22 +0000

    Add package domonic/0.1.5/domonic-0.1.5.tar.gz

---
+++ packages/domonic/domonic-0.1.5.tar.gz/domonic-0.1.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+        long_description = f.read()
+
+setup(
+  name = 'domonic',
+  version = '0.1.5',
+  author="@byteface",
+  author_email="byteface@gmail.com",
+  license="MIT",
+  url = 'https://github.com/byteface/domonic',
+  download_url = 'https://github.com/byteface/pypals/archive/0.1.5.tar.gz',
+  description = 'generate html with python 3 and quite a bit more',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  keywords = ['html', 'generate', 'templating', 'dom', 'terminal', 'json', 'web', 'template'],
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "Development Status :: 4 - Beta",
+      "Environment :: Web Environment",
+      "Intended Audience :: Developers",
+      "Intended Audience :: Other Audience",
+      "License :: OSI Approved :: MIT License",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Programming Language :: JavaScript",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.8",
+      "Topic :: Internet",
+      "Topic :: Multimedia :: Graphics :: Presentation",
+      "Topic :: Software Development",
+      "Topic :: Software Development :: Code Generators",
+      "Topic :: Terminals",
+      "Topic :: Utilities",
+  ],
+  install_requires=[
+          'requests', 'python-dateutil', 'urllib3'
+  ],
+  packages = ['domonic'],
+  include_package_data = True,
+)
