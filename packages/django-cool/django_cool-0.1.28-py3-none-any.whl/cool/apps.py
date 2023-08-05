f08b50549b8093da381c3e10a6b88a83ea6f4f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package django-cool/0.1.28/django_cool-0.1.28-py3-none-any.whl

---
+++ packages/django-cool/django_cool-0.1.28-py3-none-any.whl/cool/apps.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+from django.apps import AppConfig
+from django.contrib.admin.filters import FieldListFilter, ListFilter
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from cool.admin import filters
+from cool.checks import register_checks
+from cool.settings import cool_settings
+
+
+class CoolConfig(AppConfig):
+    name = 'cool'
+    verbose_name = _("Django Cool")
+
+    def ready(self):
+        register_checks()
+        if cool_settings.FILTER_USE_SELECT:
+            ListFilter.template = 'cool/admin/select_filter.html'
+        if cool_settings.RELATED_FIELD_FILTER_USE_AUTOCOMPLETE:
+            FieldListFilter.register(lambda f: f.remote_field, filters.AutocompleteFieldFilter, True)
+        if cool_settings.DATE_FIELD_FILTER_USE_RANGE:
+            FieldListFilter.register(lambda f: isinstance(f, models.DateField), filters.DateRangeFieldFilter, True)
