38b31dea2bc90b09064b6aaea40fd76dbe5b98ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package easydoc/0.1.2/easydoc-0.1.2.tar.gz

---
+++ packages/easydoc/easydoc-0.1.2.tar.gz/easydoc-0.1.2/README.md
@@ -0,0 +1,23 @@
+
+easydoc:
+
+1. EasyExcel for import and export of excel;
+
+2. EasyWord for import and export of word;
+
+3. EasyPdf for import and export of pdf;
+
+4. obtain fields for models, multithreading for large amounts of data, etc;
+
+5. More functions are waiting for you to use.
+
+How to use?
+
+Please click here.
+
+
+
+
+More
+
+We are working hard to perfect...
---
+++ packages/easydoc/easydoc-0.1.2.tar.gz/easydoc-0.1.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+# author: Frank YCJ
+# email: 1320259466@qq.com
+
+import setuptools
+
+with open("easydoc/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+name="easydoc",
+        version="0.1.2",
+        author="Frank YCJ",
+        author_email="1320259466@qq.com",
+        description="Make data operation easier!",
+        keywords='excel word pdf import export',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/YouAreOnlyOne",
+        # packages=setuptools.find_packages(),
+        packages=["easydoc"],
+        install_requires=['openpyxl>=2.6.4','futures>=3.3.0','docxtpl>=0.5.0'],
+        python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+        license="Apache 2.0 license",
+        Platform="OS All, Python 2.x",
+
+        project_urls={
+        "Bug Tracker": "https://github.com/YouAreOnlyOne/FastFrameJar/issues",
+        "Documentation": "https://github.com/YouAreOnlyOne/FastFrameJar",
+        "Source Code": "https://github.com/YouAreOnlyOne/FastFrameJar",
+    },
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "easydoc": ["README"],
+        "easydoc": ["README.md"],
+        # "easydoc": ["test_report_template.docx"],
+        "": ["README.md"],
+        "": ["test_report_template.docx"],
+        "easydoc": ["*.md"],
+        # "easydoc": ["*.docx"],
+        "easydoc": ["LICENSE"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
