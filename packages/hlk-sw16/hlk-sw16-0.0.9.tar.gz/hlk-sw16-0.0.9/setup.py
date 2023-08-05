9d19c0afa0352561a72edc11f0a23f7688edd75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package hlk-sw16/0.0.9/hlk-sw16-0.0.9.tar.gz

---
+++ packages/hlk-sw16/hlk-sw16-0.0.9.tar.gz/hlk-sw16-0.0.9/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+        name='hlk-sw16',
+        version='0.0.9',
+        description='Python client for HLK-SW16',
+        url='https://github.com/jameshilliard/hlk-sw16',
+        author='James Hilliard',
+        author_email='james.hilliard1@gmail.com',
+        license='MIT',
+        packages=[
+            'hlk_sw16',
+            ],
+        )
