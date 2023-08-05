20a78400f72493e86d862e7b487d11a57587632b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package webspace/2.7.147/webspace-2.7.147-py3-none-any.whl

---
+++ packages/webspace/webspace-2.7.147-py3-none-any.whl/webspace/bakery/views_admin.py
@@ -0,0 +1,31 @@
+import logging
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from webspace.cms import constants
+
+from .celery import bakery_build, app
+from .models import TaskBuild
+
+logger = logging.getLogger('bakery')
+
+
+def index(request):
+    task = TaskBuild.objects.first()
+    return render(
+        request,
+        '%s/bakery.html' % constants.ADMIN_TEMPLATES_PATH,
+        {
+            'cache_icon': 'cog',
+            'ready': task.ready if task else True
+         },
+    )
+
+
+def build(request):
+    task = TaskBuild.objects.first()
+    if not task or task.ready:
+        bakery_build.delay()
+        return HttpResponse("Build is running !")
+    else:
+        return HttpResponse("Build is already running !")
