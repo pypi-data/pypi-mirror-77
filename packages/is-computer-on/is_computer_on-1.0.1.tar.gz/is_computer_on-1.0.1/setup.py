91d275313ecbcbb0715f931be5fd85e02928a125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package is-computer-on/1.0.1/is_computer_on-1.0.1.tar.gz

---
+++ packages/is-computer-on/is_computer_on-1.0.1.tar.gz/is_computer_on-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages = ['is_computer_on']
+print('packages=', packages)
+
+setup(
+    name="is_computer_on",
+
+    version="1.0.1",
+
+    packages=packages,
+    install_requires=[],
+    author="Grant miller",
+    author_email="grant@grant-miller.com",
+    description="A python package to empirically determine if the processing unit is functional.",
+    long_description=long_description,
+    license="PSF",
+    keywords="is computer on",
+    url="https://github.com/GrantGMiller/is_computer_on",  # project home page, if any
+    project_urls={
+        "Source Code": "https://github.com/GrantGMiller/is_computer_on",
+    }
+
+)
+
+# python -m setup.py sdist bdist_wheel
+# twine upload dist/*
