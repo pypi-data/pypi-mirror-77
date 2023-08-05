210d6b2a0a911cdaeced6d658ed4fdb78b7aa425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:23 +0000

    Add package imagesearch/0.2.1/imagesearch-0.2.1.tar.gz

---
+++ packages/imagesearch/imagesearch-0.2.1.tar.gz/imagesearch-0.2.1/imagesearch/__init__.py
@@ -0,0 +1,9 @@
+"""
+Returns the similiarity between a reference image and a set of other images.
+"""
+__version__ = '0.2.1'
+
+from .fingerprint import Algorithm, ImageFingerprint
+from . import exceptions
+from .compare import ImageDiff
+from .dupe import Dupe
