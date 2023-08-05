1977f73d613ca904a05a1e93e5195e20a1fcf204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package ckan-remote-dataloader/0.0.11/ckan-remote-dataloader-0.0.11.tar.gz

---
+++ packages/ckan-remote-dataloader/ckan-remote-dataloader-0.0.11.tar.gz/ckan-remote-dataloader-0.0.11/ckanloader/__init__.py
@@ -0,0 +1,82 @@
+import sys, os
+from pathlib import Path
+import click
+import configparser
+from .loader import connect, update_resource, new_resource
+
+## set up logging
+import logging
+logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
+log = logging.getLogger(__name__)
+
+configFile = os.path.join(str(Path.home()),"ckanconfig.ini")
+
+@click.group()
+def main():
+    """ CKAN Remote Datastore Upload """
+    pass
+
+
+@main.command()
+@click.option('-u', '--url', type=str, required=True, prompt="CKAN Site Url")
+@click.option('-a', '--apikey', type=str, required=True, prompt="API Key")
+def configure(url, apikey):
+    """ Configure CKAN connection"""
+    try:
+        ckan = connect(str(url), str(apikey))
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {'url': str(url),
+                             'apikey': str(apikey)}
+        with open(configFile, 'w') as conf:
+            config.write(conf)
+    except:
+        log.error('connection not established, try again')
+    
+
+@main.command()
+@click.option('-f', '--file', type=str, required=True, help="Input File")
+@click.option('-r', '--resource', type=str, required=True, help='Resource ID')
+def update(**kwargs):
+    """ Update an existing resource """
+    try:
+        config = configparser.ConfigParser()
+        config.read(configFile)
+        ckan_url = config['DEFAULT']['url']
+        api_key = config['DEFAULT']['apikey']
+        try:
+            ckan = connect(str(ckan_url), str(api_key))
+        except KeyError:
+            log.error("Improper Configuration. Run `ckanloader configure` from the command line.")
+        click.echo(kwargs)
+        update_resource(ckan, kwargs.get("file"), kwargs.get("resource"))
+    except FileNotFoundError:
+        log.error("File not found, check file name and try again")
+
+
+@main.command()
+@click.option('-f', '--file', type=str, required=True, help="Input File")
+@click.option('-e', '--existing', is_flag=True, help="Create datastore for existing resource")
+@click.option('-p', '--package', type=str, required=True, help="Package ID (Dataset Name or Resource ID for existing resource)")
+@click.option('-n', '--name', type=str, required=False, help="Provide a name for the new resource")
+def create(**kwargs):
+    """ Create a new resource in an existing dataset """
+    try:
+        config = configparser.ConfigParser()
+        config.read(configFile)
+        ckan_url = config['DEFAULT']['url']
+        api_key = config['DEFAULT']['apikey']
+        try:
+            ckan = connect(str(ckan_url), str(api_key))
+        except KeyError:
+            log.error("Improper Configuration. Run `ckanloader configure` from the command line.")
+        click.echo(kwargs)
+        new_resource(ckan, kwargs.get("existing"), kwargs.get("file"), kwargs.get("package"), kwargs.get("name"))
+    except FileNotFoundError:
+        log.error("File not found, check file name and try again")
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if "--help" in args or len(args) == 1:
+        print("CKAN Remote Datastore Upload")
+    main()
---
+++ packages/ckan-remote-dataloader/ckan-remote-dataloader-0.0.11.tar.gz/ckan-remote-dataloader-0.0.11/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+
+import pathlib
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# automatically captured required modules for install_requires in requirements.txt
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
+
+setup (
+    name = 'ckan-remote-dataloader',
+    description = 'A simple commandline app for uploading tables to a remote CKAN datastore',
+    version = '0.0.11',
+    packages = find_packages(), 
+    install_requires = install_requires,
+    python_requires='>=3.6', 
+    entry_points='''
+        [console_scripts]
+        ckanloader=ckanloader.__init__:main
+    ''',
+    author="John Keniston",
+    keyword="ckan, datastore, remote, loader",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/jkgeo/ckan-remote-dataloader',
+    # download_url='https://github.com/jkgeo/ckan-remote-dataloader/archive/v0.0.0.tar.gz',
+    dependency_links= dependency_links,
+    author_email='jfkeniston@gmail.com',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
