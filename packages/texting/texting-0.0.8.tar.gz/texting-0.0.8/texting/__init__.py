d6d5bf89dce0522bc86d36ad1845187bad33f679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:50 +0000

    Add package texting/0.0.8/texting-0.0.8.tar.gz

---
+++ packages/texting/texting-0.0.8.tar.gz/texting-0.0.8/texting/__init__.py
@@ -0,0 +1,12 @@
+from .bracket import anglebr, br, brace, bracket, parenth, to_br
+from .chars import AEU, CO, COLF, COSP, CR, DA, DASH, DOSP, DOT, DT, ELLIP, LF, QT, RN, RT, RTSP, SP, TB, UL, VO
+from .fold import fold, fold_to_vector
+from .has_ansi import has_ansi, has_astral, has_han
+from .indexing import after_nontab, index_nontab
+from .lange import lange
+from .lines import join_lines, liner
+from .pad import cpad, lpad, rpad
+from .ripper import ripper, split_literal
+from .str_util import StrTemp
+from .str_value import str_value
+from .tap import link, tag, tags
