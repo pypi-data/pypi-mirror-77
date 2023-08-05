5337a9a01e94a64082583ac3c27a2507aefe41a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package schemainspect/0.1.1597727684/schemainspect-0.1.1597727684.tar.gz

---
+++ packages/schemainspect/schemainspect-0.1.1597727684.tar.gz/schemainspect-0.1.1597727684/schemainspect/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from . import pg
+from .command import do_command
+from .get import get_inspector
+from .inspected import ColumnInfo, Inspected
+from .inspector import DBInspector, NullInspector, to_pytype
+
+__all__ = [
+    "DBInspector",
+    "to_pytype",
+    "ColumnInfo",
+    "Inspected",
+    "get_inspector",
+    "do_command",
+    "pg",
+    "NullInspector",
+]
