d1a757a35571327dad00fcc28e38f6d41792dfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package fhir2dataset/0.1.0/fhir2dataset-0.1.0.tar.gz

---
+++ packages/fhir2dataset/fhir2dataset-0.1.0.tar.gz/fhir2dataset-0.1.0/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"), encoding="utf-8",
+) as f:
+    long_description = f.read()
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class NPMInstall(build_py):
+    def run(self):
+        self.run_command("npm install --prefix fhir2dataset/metadata")
+        build_py.run(self)
+
+
+requirements = read("requirements.txt").split()
+
+setup(
+    cmdclass={"npm_install": NPMInstall},
+    name="fhir2dataset",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.1.0",
+    license="Apache License 2.0",
+    description="Transform FHIR to Dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Lucile Saulnier",
+    author_email="contact@arkhn.com",
+    url="https://github.com/arkhn/FHIR2Dataset",
+    keywords=["arkhn", "medical", "fhir", "FHIR", "Dataset", "API"],
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
