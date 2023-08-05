2ff3f0f3af0413b2d302e9b36b7b327620dc8fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:10 +0000

    Add package pyross/1.2.9/pyross-1.2.9-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/pyross/pyross-1.2.9-cp37-cp37m-macosx_10_9_x86_64.whl/pyross/__init__.py
@@ -0,0 +1,14 @@
+import pyross.contactMatrix
+import pyross.control
+import pyross.deterministic
+import pyross.forecast
+import pyross.hybrid
+import pyross.inference
+import pyross.stochastic
+import pyross.utils
+import pyross.utils_python
+import pyross.evidence
+
+## current version of PyRoss
+__version__="1.2.9"
+
