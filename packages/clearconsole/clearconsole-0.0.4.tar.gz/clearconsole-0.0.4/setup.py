2b73a2b6b2ffbd1751dae504e8ec86ab3420ab1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package clearconsole/0.0.4/clearconsole-0.0.4.tar.gz

---
+++ packages/clearconsole/clearconsole-0.0.4.tar.gz/clearconsole-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clearconsole",
+    version="0.0.4",
+    author="greatusername",
+    author_email="alexander.destefano@gmail.com",
+    description="Can be used to clear the screen.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathstar13/clear",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',
+    py_modules=['cleargreatusername'],
+)
