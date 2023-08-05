d0019f2b301dcda52120bb96628f3aae199fbbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package MusFeat/1.0.0/MusFeat-1.0.0.tar.gz

---
+++ packages/MusFeat/MusFeat-1.0.0.tar.gz/MusFeat-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+name: str = "MusFeat"
+version: str = "1.0.0"
+license: str = "MIT"
+author: str = "David Martin-Gutierrez"
+author_email: str = "dmargutierrez@gmail.com"
+
+setup(name=name,
+      version=version,
+      packages=find_packages(),
+      license=license,
+      author=author,
+      author_email=author_email,
+      python_requires='>=3.6'
+      )
