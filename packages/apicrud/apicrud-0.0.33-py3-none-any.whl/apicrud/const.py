7a8e45d7efae031a9e783d30533d4d9c88342c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:06 +0000

    Add package apicrud/0.0.33/apicrud-0.0.33-py3-none-any.whl

---
+++ packages/apicrud/apicrud-0.0.33-py3-none-any.whl/apicrud/const.py
@@ -0,0 +1,33 @@
+"""const.py
+
+Constants for API library
+  Values that can be modified by a container restart belong in config.py
+
+created 31-mar-2019 by richb@instantlinux.net
+"""
+
+
+class Constants:
+    """Constants for apicrud methods"""
+    AUTH_INVITEE = 'invitee'
+    DEFAULT_AWS_REGION = 'us-east-2'
+    LIB_MOD_SPATIALITE = ['/usr/lib/x86_64-linux-gnu/mod_spatialite.so',
+                          '/usr/lib/mod_spatialite.so.7']
+    MIME_IMAGE_TYPES = ("gif", "heic", "jpeg", "png", "svg")
+    MIME_VIDEO_TYPES = ("mp4", "mpeg")
+    PER_PAGE_DEFAULT = 100
+    REDIS_TTL = 3600
+    SERVICE_CONFIG_FILE = 'service_config.yaml'
+
+
+class i18n:
+    """
+    These are keys in a user-provided dict; see the
+    example/i18n_textstrings.py for how to assign string
+    templates for messaging.
+    """
+
+    # TODO - update this doc when messaging code is moved
+    # into this library
+    CONTACT_ADDED_REQUEST = 'contact_add'
+    PASSWORD_RESET = 'password_reset'
---
+++ packages/apicrud/apicrud-0.0.33-py3-none-any.whl/apicrud/grants.py
@@ -0,0 +1,83 @@
+"""grants.py
+
+Grants
+  An account's usage limits are specified here in the grants table;
+  the free-service tier is defined and passed in via load_defaults().
+  Records in grants table are owned by administrator-level user.
+  If a record matches a user uid, the default grant name=value is
+  overridden.
+
+created 27-may-2019 by richb@instantlinux.net
+"""
+
+from flask import g
+from datetime import timedelta
+
+from .access import AccessControl
+from .service_config import ServiceConfig
+from . import utils
+
+GRANTS = {}
+
+
+class Grants(object):
+    """
+    Account usage limits
+
+    Attributes:
+      db_session (obj): existing db session
+      ttl (int): how long to cache a grant in memory
+    """
+
+    def __init__(self, db_session=None, ttl=None):
+        self.ttl = ttl or ServiceConfig().config.REDIS_TTL
+        self.models = ServiceConfig().models
+        try:
+            self.session = db_session or g.db
+        except RuntimeError as ex:
+            if 'Working outside of application context' not in str(ex):
+                raise
+
+    def get(self, name, uid=None):
+        """Get the cached value of a named grant, if it hasn't expired
+        Note that if any grant assigned to a uid expires before
+        others, the earliest expiration applies to all the uid's grants
+
+        Args:
+          name (str): name of a grant, as defined in service config
+          uid (str): user ID
+        Returns:
+          value: granted limit
+        """
+        if name not in GRANTS['defaults']:
+            raise AttributeError('Unknown grant %s' % name)
+        if not uid:
+            uid = AccessControl().uid
+        if uid not in GRANTS or utils.utcnow() > GRANTS[uid]['expires']:
+            records = self.session.query(self.models.Grant).filter_by(
+                uid=uid).all()
+            grants = dict(expires=utils.utcnow() + timedelta(seconds=self.ttl))
+            for record in records:
+                grants[record.name] = record.value
+                if record.expires:
+                    grants['expires'] = min(record.expires, grants['expires'])
+            GRANTS[uid] = grants
+        if name in GRANTS[uid] and utils.utcnow() < GRANTS[uid]['expires']:
+            return GRANTS[uid][name]
+        return GRANTS['defaults'][name]
+
+    def uncache(self, uid):
+        """Remove grants from cache, any time a user's status changes
+
+        Args:
+          uid (str): user ID
+        """
+        GRANTS.pop(uid, None)
+
+    def load_defaults(self, defaults):
+        """Load default values from a dict of keyword: value pairs
+
+        Args:
+          defaults (dict): new defaults
+        """
+        GRANTS['defaults'] = defaults
