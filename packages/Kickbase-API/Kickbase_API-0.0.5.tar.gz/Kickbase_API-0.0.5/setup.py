3639ebde81b7a375e4ea5b21e0f40a8537aada7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:45 +0000

    Add package Kickbase-API/0.0.5/Kickbase_API-0.0.5.tar.gz

---
+++ packages/Kickbase-API/Kickbase_API-0.0.5.tar.gz/Kickbase_API-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Kickbase_API',
+    version='0.0.5',
+    packages=['kickbase_api', 'kickbase_api.models', 'kickbase_api.models.response'],
+    url='https://github.com/kevinskyba/kickbase-api-python',
+    license='MIT',
+    author='kevinskyba',
+    author_email='kevinskyba@live.de',
+    description='Python API library for kickbase',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    install_requires=(
+        'requests',
+    )
+)
