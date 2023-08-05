97902b1a622dac11120f85d8a411a7d90379fe82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:03 +0000

    Add package say-hi/0.0.3/say-hi-0.0.3.tar.gz

---
+++ packages/say-hi/say-hi-0.0.3.tar.gz/say-hi-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='say-hi',
+  version='0.0.3',
+  description='It wishes the person',
+  long_description=open('readme.txt').read(),# + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Balaji Betadur',
+  author_email='balajibetadur@gmail.com',
+#   classifiers=classifiers,
+  keywords=['say hi'],
+#   license='MIT', 
+  packages=['greet'], # find_packages(),
+#   include_package_data=True,
+  install_requires=[] 
+) 
