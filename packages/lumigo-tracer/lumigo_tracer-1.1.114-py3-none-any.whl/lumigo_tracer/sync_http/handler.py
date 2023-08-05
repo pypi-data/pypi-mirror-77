329a95458b459e1e30ea2adeb3ef689211b3a318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package lumigo-tracer/1.1.114/lumigo_tracer-1.1.114-py3-none-any.whl

---
+++ packages/lumigo-tracer/lumigo_tracer-1.1.114-py3-none-any.whl/lumigo_tracer/sync_http/handler.py
@@ -0,0 +1,23 @@
+import os
+import importlib
+
+from lumigo_tracer import lumigo_tracer
+
+ORIGINAL_HANDLER_KEY = "LUMIGO_ORIGINAL_HANDLER"
+
+
+@lumigo_tracer()
+def _handler(*args, **kwargs):
+    try:
+        module_name, unit_name = os.environ[ORIGINAL_HANDLER_KEY].rsplit(".", 1)
+        importable_name = module_name.replace("/", ".")
+        original_handler = getattr(importlib.import_module(importable_name), unit_name)
+    except (ImportError, AttributeError):
+        raise ImportError(
+            "Could not load the original handler. Are you sure that the import is ok?"
+        )
+    except KeyError:
+        raise ValueError(
+            "Could not find the original handler. Please follow lumigo's docs: https://docs.lumigo.io/"
+        )
+    return original_handler(*args, **kwargs)
