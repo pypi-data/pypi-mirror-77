2f5686c88738fa45ab088e864caa97fc51924e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:30 +0000

    Add package clearconsole/0.0.5/clearconsole-0.0.5-py3-none-any.whl

---
+++ packages/clearconsole/clearconsole-0.0.5-py3-none-any.whl/clearconsole/clearconsole.py
@@ -0,0 +1,21 @@
+from os import system, name
+def clear():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
+def cls():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
+def clr():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
+def clearconsole():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
