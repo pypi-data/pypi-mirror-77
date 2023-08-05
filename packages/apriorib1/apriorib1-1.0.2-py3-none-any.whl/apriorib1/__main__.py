09d536085b27fef764e114d3a9bf2da756a4cca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:12 +0000

    Add package apriorib1/1.0.2/apriorib1-1.0.2-py3-none-any.whl

---
+++ packages/apriorib1/apriorib1-1.0.2-py3-none-any.whl/apriorib1/__main__.py
@@ -0,0 +1,32 @@
+from apriorib1.apriorib1 import Apriori
+
+def main():
+    data = [['MILK', 'BREAD', 'BISCUIT'],
+        ['BREAD', 'MILK', 'BISCUIT', 'CORNFLAKES'],
+        ['BREAD', 'TEA', 'BOURNVITA'],
+        ['JAM', 'MAGGI', 'BREAD', 'MILK'],
+        ['MAGGI', 'TEA', 'BISCUIT'],
+        ['BREAD', 'TEA', 'BOURNVITA'],
+        ['MAGGI', 'TEA', 'CORNFLAKES'],
+        ['MAGGI', 'BREAD', 'TEA', 'BISCUIT'],
+        ['JAM', 'MAGGI', 'BREAD', 'TEA'],
+        ['BREAD', 'MILK'],
+        ['COFFEE', 'COCK', 'BISCUIT', 'CORNFLAKES'],
+        ['COFFEE', 'COCK', 'BISCUIT', 'CORNFLAKES'],
+        ['COFFEE', 'SUGER', 'BOURNVITA'],
+        ['BREAD', 'COFFEE', 'COCK'],
+        ['BREAD', 'SUGER', 'BISCUIT'],
+        ['COFFEE', 'SUGER', 'CORNFLAKES'],
+        ['BREAD', 'SUGER', 'BOURNVITA'],
+        ['BREAD', 'COFFEE', 'SUGER'],
+        ['BREAD', 'COFFEE', 'SUGER'],
+        ['TEA', 'MILK', 'COFFEE', 'CORNFLAKES']]
+
+    # Testing the Apriori class
+    apr = Apriori(records=data,min_sup=2,min_conf=50)
+    df1,df2,df3,df4 = apr.show_as_df(stage=1),apr.show_as_df(stage=2),apr.show_as_df(stage=3),apr.show_as_df(stage=4)
+    print("VIEWING THE ITEMSET DATAFRAMES AT THE DIFFERENT STAGES :\nSTAGE 1\n{}\nSTAGE 2\n{}\nSTAGE 3\n{}\nSTAGE 4\n{}".format(df1,df2,df3,df4))
+    apr.checkAssc()
+
+if __name__ == "__main__":
+    main()
