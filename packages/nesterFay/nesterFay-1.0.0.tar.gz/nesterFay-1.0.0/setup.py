7a11b4dd14d8b0ac4d995892ad28992e16289364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:32 +0000

    Add package nesterFay/1.0.0/nesterFay-1.0.0.tar.gz

---
+++ packages/nesterFay/nesterFay-1.0.0.tar.gz/nesterFay-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='nesterFay',
+	version='1.0.0',
+	py_modules=['nester'],
+	author='Fay',
+	author_email='309323122@qq.com',
+	#url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
