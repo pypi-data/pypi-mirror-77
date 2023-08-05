9fc997e0518d098ac20cc4ec67e5b9e8d5fdd8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package torchio/0.17.29/torchio-0.17.29-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.17.29-py2.py3-none-any.whl/torchio/data/io.py
@@ -0,0 +1,216 @@
+import warnings
+from pathlib import Path
+from typing import Tuple
+import torch
+import numpy as np
+import nibabel as nib
+import SimpleITK as sitk
+from .. import TypePath, TypeData
+from ..utils import nib_to_sitk, sitk_to_nib
+
+
+FLIPXY = np.diag([-1, -1, 1, 1])
+
+
+def read_image(
+        path: TypePath,
+        itk_first: bool = False,
+        ) -> Tuple[torch.Tensor, np.ndarray]:
+    if itk_first:
+        try:
+            result = _read_sitk(path)
+        except RuntimeError:  # try with NiBabel
+            result = _read_nibabel(path)
+    else:
+        try:
+            result = _read_nibabel(path)
+        except nib.loadsave.ImageFileError:  # try with ITK
+            result = _read_sitk(path)
+    return result
+
+
+def _read_nibabel(path: TypePath) -> Tuple[torch.Tensor, np.ndarray]:
+    img = nib.load(str(path), mmap=False)
+    data = img.get_fdata(dtype=np.float32)
+    tensor = torch.from_numpy(data)
+    affine = img.affine
+    return tensor, affine
+
+
+def _read_sitk(
+        path: TypePath,
+        transpose_2d: bool = True,
+        ) -> Tuple[torch.Tensor, np.ndarray]:
+    if Path(path).is_dir():  # assume DICOM
+        image = _read_dicom(path)
+    else:
+        image = sitk.ReadImage(str(path))
+    if image.GetDimension() == 2 and transpose_2d:
+        image = sitk.PermuteAxes(image, (1, 0))
+    data, affine = sitk_to_nib(image, keepdim=True)
+    if data.dtype != np.float32:
+        data = data.astype(np.float32)
+    tensor = torch.from_numpy(data)
+    return tensor, affine
+
+
+def _read_dicom(directory: TypePath):
+    directory = Path(directory)
+    if not directory.is_dir():  # unreachable if called from _read_sitk
+        raise FileNotFoundError(f'Directory "{directory}" not found')
+    reader = sitk.ImageSeriesReader()
+    dicom_names = reader.GetGDCMSeriesFileNames(str(directory))
+    if not dicom_names:
+        message = (
+            f'The directory "{directory}"'
+            ' does not seem to contain DICOM files'
+        )
+        raise FileNotFoundError(message)
+    reader.SetFileNames(dicom_names)
+    image = reader.Execute()
+    return image
+
+
+def write_image(
+        tensor: torch.Tensor,
+        affine: TypeData,
+        path: TypePath,
+        itk_first: bool = False,
+        squeeze: bool = True,
+        channels_last: bool = True,
+        ) -> None:
+    args = tensor, affine, path
+    if itk_first:
+        try:
+            _write_sitk(*args, squeeze=squeeze)
+        except RuntimeError:  # try with NiBabel
+            _write_nibabel(*args, squeeze=squeeze, channels_last=channels_last)
+    else:
+        try:
+            _write_nibabel(*args, squeeze=squeeze, channels_last=channels_last)
+        except nib.loadsave.ImageFileError:  # try with ITK
+            _write_sitk(*args, squeeze=squeeze)
+
+
+def _write_nibabel(
+        tensor: TypeData,
+        affine: TypeData,
+        path: TypePath,
+        squeeze: bool = True,
+        channels_last: bool = True,
+        ) -> None:
+    """
+    Expects a path with an extension that can be used by nibabel.save
+    to write a NIfTI-1 image, such as '.nii.gz' or '.img'
+    """
+    assert tensor.ndim == 4
+    if channels_last:
+        tensor = tensor.permute(1, 2, 3, 0)
+    tensor = tensor.squeeze() if squeeze else tensor
+    suffix = Path(str(path).replace('.gz', '')).suffix
+    if '.nii' in suffix:
+        img = nib.Nifti1Image(np.asarray(tensor), affine)
+    elif '.hdr' in suffix or '.img' in suffix:
+        img = nib.Nifti1Pair(np.asarray(tensor), affine)
+    else:
+        raise nib.loadsave.ImageFileError
+    img.header['qform_code'] = 1
+    img.header['sform_code'] = 0
+    img.to_filename(str(path))
+
+
+def _write_sitk(
+        tensor: torch.Tensor,
+        affine: TypeData,
+        path: TypePath,
+        squeeze: bool = True,
+        use_compression: bool = True,
+        transpose_2d: bool = True,
+        ) -> None:
+    assert tensor.ndim == 4
+    path = Path(path)
+    if path.suffix in ('.png', '.jpg', '.jpeg'):
+        warnings.warn(f'Casting to uint 8 before saving to {path}')
+        tensor = tensor.numpy().astype(np.uint8)
+    image = nib_to_sitk(tensor, affine, squeeze=squeeze)
+    if image.GetDimension() == 2 and transpose_2d:
+        image = sitk.PermuteAxes(image, (1, 0))
+    sitk.WriteImage(image, str(path), use_compression)
+
+
+def read_matrix(path: TypePath):
+    """Read an affine transform and convert to tensor."""
+    path = Path(path)
+    suffix = path.suffix
+    if suffix in ('.tfm', '.h5'):  # ITK
+        tensor = _read_itk_matrix(path)
+    elif suffix in ('.txt', '.trsf'):  # NiftyReg, blockmatching
+        tensor = _read_niftyreg_matrix(path)
+    return tensor
+
+
+def write_matrix(matrix: torch.Tensor, path: TypePath):
+    """Write an affine transform."""
+    path = Path(path)
+    suffix = path.suffix
+    if suffix in ('.tfm', '.h5'):  # ITK
+        _write_itk_matrix(matrix, path)
+    elif suffix in ('.txt', '.trsf'):  # NiftyReg, blockmatching
+        _write_niftyreg_matrix(matrix, path)
+
+
+def _to_itk_convention(matrix):
+    """RAS to LPS"""
+    matrix = np.dot(FLIPXY, matrix)
+    matrix = np.dot(matrix, FLIPXY)
+    matrix = np.linalg.inv(matrix)
+    return matrix
+
+
+def _from_itk_convention(matrix):
+    """LPS to RAS"""
+    matrix = np.dot(matrix, FLIPXY)
+    matrix = np.dot(FLIPXY, matrix)
+    matrix = np.linalg.inv(matrix)
+    return matrix
+
+
+def _read_itk_matrix(path):
+    """Read an affine transform in ITK's .tfm format"""
+    transform = sitk.ReadTransform(str(path))
+    parameters = transform.GetParameters()
+    rotation_parameters = parameters[:9]
+    rotation_matrix = np.array(rotation_parameters).reshape(3, 3)
+    translation_parameters = parameters[9:]
+    translation_vector = np.array(translation_parameters).reshape(3, 1)
+    matrix = np.hstack([rotation_matrix, translation_vector])
+    homogeneous_matrix_lps = np.vstack([matrix, [0, 0, 0, 1]])
+    homogeneous_matrix_ras = _from_itk_convention(homogeneous_matrix_lps)
+    return torch.from_numpy(homogeneous_matrix_ras)
+
+
+def _write_itk_matrix(matrix, tfm_path):
+    """The tfm file contains the matrix from floating to reference."""
+    transform = _matrix_to_itk_transform(matrix)
+    transform.WriteTransform(str(tfm_path))
+
+
+def _matrix_to_itk_transform(matrix, dimensions=3):
+    matrix = _to_itk_convention(matrix)
+    rotation = matrix[:dimensions, :dimensions].ravel().tolist()
+    translation = matrix[:dimensions, 3].tolist()
+    transform = sitk.AffineTransform(rotation, translation)
+    return transform
+
+
+def _read_niftyreg_matrix(trsf_path):
+    """Read a NiftyReg matrix and return it as a NumPy array"""
+    matrix = np.loadtxt(trsf_path)
+    matrix = np.linalg.inv(matrix)
+    return torch.from_numpy(matrix)
+
+
+def _write_niftyreg_matrix(matrix, txt_path):
+    """Write an affine transform in NiftyReg's .txt format (ref -> flo)"""
+    matrix = np.linalg.inv(matrix)
+    np.savetxt(txt_path, matrix, fmt='%.8f')
---
+++ packages/torchio/torchio-0.17.29-py2.py3-none-any.whl/torchio/transforms/preprocessing/spatial/resample.py
@@ -0,0 +1,250 @@
+from pathlib import Path
+from numbers import Number
+from typing import Union, Tuple, Optional, List
+
+import torch
+import numpy as np
+import nibabel as nib
+from nibabel.processing import resample_to_output, resample_from_to
+
+from ....data.subject import Subject
+from ....data.image import Image, ScalarImage
+from ....torchio import DATA, AFFINE, TYPE, INTENSITY, TypeData
+from ... import SpatialTransform
+from ... import Interpolation
+
+
+TypeSpacing = Union[float, Tuple[float, float, float]]
+TypeTarget = Tuple[
+    Optional[Union[Image, str]],
+    Optional[Tuple[float, float, float]],
+]
+
+
+class Resample(SpatialTransform):
+    """Change voxel spacing by resampling.
+
+    Args:
+        target: Tuple :math:`(s_h, s_w, s_d)`. If only one value
+            :math:`n` is specified, then :math:`s_h = s_w = s_d = n`.
+            If a string or :py:class:`~pathlib.Path` is given,
+            all images will be resampled using the image
+            with that name as reference or found at the path.
+        pre_affine_name: Name of the *image key* (not subject key) storing an
+            affine matrix that will be applied to the image header before
+            resampling. If ``None``, the image is resampled with an identity
+            transform. See usage in the example below.
+        image_interpolation: String that defines the interpolation technique.
+            Supported interpolation techniques for resampling
+            are ``'nearest'``, ``'linear'`` and ``'bspline'``.
+            Using a member of :py:class:`torchio.Interpolation` is still
+            supported for backward compatibility,
+            but will be removed in a future version.
+        p: Probability that this transform will be applied.
+        keys: See :py:class:`~torchio.transforms.Transform`.
+
+    .. note:: Resampling is performed using
+        :py:meth:`nibabel.processing.resample_to_output` or
+        :py:meth:`nibabel.processing.resample_from_to`, depending on whether
+        the target is a spacing or a reference image.
+
+    Example:
+        >>> import torchio
+        >>> from torchio import Resample
+        >>> from torchio.datasets import Colin27, FPG
+        >>> transform = Resample(1)                     # resample all images to 1mm iso
+        >>> transform = Resample((2, 2, 2))             # resample all images to 2mm iso
+        >>> transform = Resample('t1')                  # resample all images to 't1' image space
+        >>> colin = Colin27()  # this images are in the MNI space
+        >>> fpg = FPG()  # matrices to the MNI space are included here
+        >>> # Resample all images into the MNI space
+        >>> transform = Resample(colin.t1.path, pre_affine_name='affine_matrix')
+        >>> transformed = transform(fpg)  # images in fpg are now in MNI space
+    """
+    def __init__(
+            self,
+            target: Union[TypeSpacing, str, Path],
+            image_interpolation: str = 'linear',
+            pre_affine_name: Optional[str] = None,
+            p: float = 1,
+            keys: Optional[List[str]] = None,
+            ):
+        super().__init__(p=p, keys=keys)
+        self.reference_image, self.target_spacing = self.parse_target(target)
+        self.interpolation_order = self.parse_interpolation(image_interpolation)
+        self.affine_name = pre_affine_name
+
+    def parse_target(
+            self,
+            target: Union[TypeSpacing, str],
+            ) -> TypeTarget:
+        if isinstance(target, (str, Path)):
+            if Path(target).is_file():
+                path = target
+                image = ScalarImage(path)
+                reference_image = image.data, image.affine
+            else:
+                reference_image = target
+            target_spacing = None
+        else:
+            reference_image = None
+            target_spacing = self.parse_spacing(target)
+        return reference_image, target_spacing
+
+    @staticmethod
+    def parse_spacing(spacing: TypeSpacing) -> Tuple[float, float, float]:
+        if isinstance(spacing, tuple) and len(spacing) == 3:
+            result = spacing
+        elif isinstance(spacing, Number):
+            result = 3 * (spacing,)
+        else:
+            message = (
+                'Target must be a string, a positive number'
+                f' or a tuple of positive numbers, not {type(spacing)}'
+            )
+            raise ValueError(message)
+        if np.any(np.array(spacing) <= 0):
+            raise ValueError(f'Spacing must be positive, not "{spacing}"')
+        return result
+
+    def parse_interpolation(self, interpolation: str) -> int:
+        interpolation = super().parse_interpolation(interpolation)
+
+        if interpolation in (Interpolation.NEAREST, 'nearest'):
+            order = 0
+        elif interpolation in (Interpolation.LINEAR, 'linear'):
+            order = 1
+        elif interpolation in (Interpolation.BSPLINE, 'bspline'):
+            order = 3
+        else:
+            message = f'Interpolation not implemented yet: {interpolation}'
+            raise NotImplementedError(message)
+        return order
+
+    @staticmethod
+    def check_affine(affine_name: str, image_dict: dict):
+        if not isinstance(affine_name, str):
+            message = (
+                'Affine name argument must be a string,'
+                f' not {type(affine_name)}'
+            )
+            raise TypeError(message)
+        if affine_name in image_dict:
+            matrix = image_dict[affine_name]
+            if not isinstance(matrix, (np.ndarray, torch.Tensor)):
+                message = (
+                    'The affine matrix must be a NumPy array or PyTorch tensor,'
+                    f' not {type(matrix)}'
+                )
+                raise TypeError(message)
+            if matrix.shape != (4, 4):
+                message = (
+                    'The affine matrix shape must be (4, 4),'
+                    f' not {matrix.shape}'
+                )
+                raise ValueError(message)
+
+    @staticmethod
+    def check_affine_key_presence(affine_name: str, sample: Subject):
+        for image_dict in sample.get_images(intensity_only=False):
+            if affine_name in image_dict:
+                return
+        message = (
+            f'An affine name was given ("{affine_name}"), but it was not found'
+            ' in any image in the sample'
+        )
+        raise ValueError(message)
+
+    def apply_transform(self, sample: Subject) -> dict:
+        use_reference = self.reference_image is not None
+        use_pre_affine = self.affine_name is not None
+        if use_pre_affine:
+            self.check_affine_key_presence(self.affine_name, sample)
+        images_dict = self.get_images_dict(sample).items()
+        for image_name, image in images_dict:
+            # Do not resample the reference image if there is one
+            if use_reference and image_name == self.reference_image:
+                continue
+
+            # Choose interpolator
+            if image[TYPE] != INTENSITY:
+                interpolation_order = 0  # nearest neighbor
+            else:
+                interpolation_order = self.interpolation_order
+
+            # Apply given affine matrix if found in image
+            if use_pre_affine and self.affine_name in image:
+                self.check_affine(self.affine_name, image)
+                matrix = image[self.affine_name]
+                if isinstance(matrix, torch.Tensor):
+                    matrix = matrix.numpy()
+                image[AFFINE] = matrix @ image[AFFINE]
+
+            # Resample
+            if use_reference:
+                if isinstance(self.reference_image, str):
+                    try:
+                        ref_image = sample[self.reference_image]
+                    except KeyError as error:
+                        message = (
+                            f'Reference name "{self.reference_image}"'
+                            ' not found in sample'
+                        )
+                        raise ValueError(message) from error
+                    reference = ref_image[DATA], ref_image[AFFINE]
+                else:
+                    reference = self.reference_image
+                kwargs = dict(reference=reference)
+            else:
+                kwargs = dict(target_spacing=self.target_spacing)
+            image[DATA], image[AFFINE] = self.apply_resample(
+                image[DATA],
+                image[AFFINE],
+                interpolation_order,
+                **kwargs,
+            )
+        return sample
+
+    @staticmethod
+    def apply_resample(
+            tensor: torch.Tensor,
+            affine: np.ndarray,
+            interpolation_order: int,
+            target_spacing: Optional[Tuple[float, float, float]] = None,
+            reference: Optional[Tuple[torch.Tensor, np.ndarray]] = None,
+            ) -> Tuple[torch.Tensor, np.ndarray]:
+        array = tensor.numpy()
+        niis = []
+        arrays_resampled = []
+        if reference is None:
+            for channel in array:
+                nii = resample_to_output(
+                    nib.Nifti1Image(channel, affine),
+                    voxel_sizes=target_spacing,
+                    order=interpolation_order,
+                )
+                arrays_resampled.append(nii.get_fdata(dtype=np.float32))
+        else:
+            reference_tensor, reference_affine = reference
+            reference_array = reference_tensor.numpy()[0]
+            for channel_array in array:
+                nii = resample_from_to(
+                    nib.Nifti1Image(channel_array, affine),
+                    nib.Nifti1Image(reference_array, reference_affine),
+                    order=interpolation_order,
+                )
+                arrays_resampled.append(nii.get_fdata(dtype=np.float32))
+        tensor = torch.Tensor(arrays_resampled)
+        return tensor, nii.affine
+
+    @staticmethod
+    def get_sigma(downsampling_factor, spacing):
+        """Compute optimal standard deviation for Gaussian kernel.
+
+        From Cardoso et al., "Scale factor point spread function matching:
+        beyond aliasing in image resampling", MICCAI 2015
+        """
+        k = downsampling_factor
+        variance = (k ** 2 - 1 ** 2) * (2 * np.sqrt(2 * np.log(2))) ** (-2)
+        sigma = spacing * np.sqrt(variance)
+        return sigma
