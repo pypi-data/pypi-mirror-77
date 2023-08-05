a5488cabf27f3aac52a9dba23f57270f58a77b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package edx-lint/1.5.2/edx-lint-1.5.2.tar.gz

---
+++ packages/edx-lint/edx-lint-1.5.2.tar.gz/edx-lint-1.5.2/setup.py
@@ -0,0 +1,16 @@
+"""
+edx-lint
+========
+
+A collection of code quality tools:
+
+- A few pylint plugins to check for quality issues pylint misses.
+
+- A command-line tool to generate config files like pylintrc from a master
+  file (part of edx_lint), and a repo-specific tweaks file.
+
+"""
+
+import setuptools
+
+setuptools.setup()
