ada9a84f717e917a875947ee494c9fdce0f0b8cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:54 +0000

    Add package stac-pydantic/1.1.4/stac_pydantic-1.1.4-py3-none-any.whl

---
+++ packages/stac-pydantic/stac_pydantic-1.1.4-py3-none-any.whl/stac_pydantic/extensions/eo.py
@@ -0,0 +1,56 @@
+from enum import auto
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+from ..shared import NumType
+from ..utils import AutoValueEnum
+
+
+class CommonBandNames(str, AutoValueEnum):
+    """
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/eo#common-band-names
+    """
+
+    coastal = auto()
+    blue = auto()
+    green = auto()
+    red = auto()
+    yellow = auto()
+    pan = auto()
+    rededge = auto()
+    nir = auto()
+    nir08 = auto()
+    nir09 = auto()
+    cirrus = auto()
+    swir16 = auto()
+    swir22 = auto()
+    lwir = auto()
+    lwir11 = auto()
+    lwir12 = auto()
+
+
+class BandObject(BaseModel):
+    """
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/eo#band-object
+    """
+
+    name: Optional[str]
+    common_name: Optional[CommonBandNames]
+    center_wavelength: Optional[NumType]
+    full_width_half_max: Optional[NumType]
+    description: Optional[str]
+
+
+class ElectroOpticalExtension(BaseModel):
+    """
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/eo#electro-optical-extension-specification
+    """
+
+    gsd: NumType = Field(..., alias="eo:gsd")
+    bands: List[BandObject] = Field(..., alias="eo:bands")
+    cloud_cover: Optional[NumType] = Field(None, alias="eo:cloud_cover")
+
+    class Config:
+        use_enum_values = True
+        allow_population_by_field_name = True
