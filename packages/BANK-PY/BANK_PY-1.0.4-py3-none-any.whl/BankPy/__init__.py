04452097fa6e3b8508b0b60962c54a294fa9a49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:20 +0000

    Add package BANK-PY/1.0.4/BANK_PY-1.0.4-py3-none-any.whl

---
+++ packages/BANK-PY/BANK_PY-1.0.4-py3-none-any.whl/BankPy/__init__.py
@@ -0,0 +1,7 @@
+from Jiton.Create import *
+from Jiton.Connect import *
+from Jiton.Variables import *
+from Jiton.Execute import *
+from Jiton.Delete import *
+
+print('Obrigado Por Utiliza BankPy - v 1.0.4')
