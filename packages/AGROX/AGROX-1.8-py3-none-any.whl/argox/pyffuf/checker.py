50be3b0bd33c0c4bce1aff97a147f3ce65b97cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:48 +0000

    Add package AGROX/1.8/AGROX-1.8-py3-none-any.whl

---
+++ packages/AGROX/AGROX-1.8-py3-none-any.whl/argox/pyffuf/checker.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import sys
+from datetime import datetime
+
+from clint.textui import puts, colored, indent
+from argox.pyffuf.wordcounter import Counter
+
+class CHECKER:
+    def __init__(self, url,wordlist):
+        self.url = url
+        self.wordlist = wordlist
+
+    def printfx(self,cldata, data):
+        with indent(4, quote='>>>'):
+            puts(colored.red(str(cldata)) + data)
+
+    def check(self):
+        if os.path.isfile(self.wordlist):
+            if os.name == "posix":
+                numofword = subprocess.check_output(['wc', '-l', self.wordlist])
+                line = numofword.decode().split(" ")
+            else:
+                line= []
+                c = Counter(self.wordlist)
+                line.append(c.count())
+                line.append(self.wordlist)
+                
+        else:
+            self.printfx("Wordlist File Not found!", "")
+            sys.exit()
+        return line[0]
