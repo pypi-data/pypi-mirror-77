1bc176272f3b76f59907092415aa0963df559411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package tjax/0.4.0/tjax-0.4.0.tar.gz

---
+++ packages/tjax/tjax-0.4.0.tar.gz/tjax-0.4.0/tjax/display.py
@@ -0,0 +1,122 @@
+import sys
+from numbers import Number
+from typing import Any, Type, Union
+
+import colorful as cf
+import networkx as nx
+import numpy as np
+from jax.interpreters.ad import JVPTracer
+from jax.interpreters.batching import BatchTracer
+from jax.interpreters.partial_eval import JaxprTracer
+from jax.interpreters.xla import DeviceArray
+
+__all__ = ['Displayable', 'print_generic']
+
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol, runtime_checkable
+
+    @runtime_checkable
+    class Displayable(Protocol):
+        """
+        This protocol identifies classes that support the `display_generic` mechanism.
+        """
+        def display(self, show_values: bool = True, indent: int = 0) -> str:
+            ...
+else:
+    class Displayable:
+        pass
+
+
+def print_generic(*args: Any, **kwargs: Any) -> None:
+    for value in args:
+        print(display_generic(value))
+    for key, value in kwargs.items():
+        print(display_key_and_value(key, value, "=", True, 0))
+
+
+def display_generic(value: Any, show_values: bool = True, indent: int = 0) -> str:
+    # pylint: disable=too-many-return-statements
+    if isinstance(value, JVPTracer):
+        return cf.magenta(f"JVPTracer {value.shape} {value.dtype}") + "\n"
+    if isinstance(value, JaxprTracer):
+        return cf.magenta(f"JaxprTracer {value.shape} {value.dtype}") + "\n"
+    if isinstance(value, BatchTracer):
+        return cf.magenta(f"BatchTracer {value.shape} {value.dtype} "
+                          f"batched over {value.val.shape[value.batch_dim]}") + "\n"
+    if isinstance(value, (np.ndarray, DeviceArray)):
+        if isinstance(value, np.ndarray):
+            retval = cf.yellow(f"NumPy Array {value.shape}") + "\n"
+        elif isinstance(value, DeviceArray):
+            retval = cf.violet(f"Jax Array {value.shape}") + "\n"
+        if show_values:
+            retval += _show_array(indent + 1, value)
+        return retval
+    if isinstance(value, Number) or value is None:
+        return cf.cyan(str(value)) + "\n"
+    if (isinstance(value, Displayable)
+            if sys.version_info >= (3, 8)
+            else hasattr(value, 'display')):
+        return value.display(show_values=show_values, indent=indent)
+    if isinstance(value, dict):
+        return (display_class(dict)
+                + "".join(display_key_and_value(key, sub_value, "=", show_values, indent)
+                          for key, sub_value in value.items()))
+    if isinstance(value, (tuple, list)):
+        return (display_class(type(value))
+                + "".join(display_key_and_value("", sub_value, "", show_values, indent)
+                          for sub_value in value))
+    if isinstance(value, nx.Graph):
+        return display_graph(value, show_values, indent)
+    return cf.red(str(value)) + "\n"
+
+
+def display_graph(graph: nx.Graph, show_values: bool, indent: int = 0) -> str:
+    directed = isinstance(graph, nx.DiGraph)
+    arrow = cf.base00('⟶  ' if directed else '🡘 ')
+    retval = display_class(type(graph))
+    for name, value in graph.nodes.items():
+        retval += display_key_and_value(name, value['element'], ": ", show_values, indent)
+    for (source, target), value in graph.edges.items():
+        key = f"{source}{arrow}{target}"
+        retval += display_key_and_value(key, value['link'], ": ", show_values, indent)
+    return retval
+
+
+def display_class(cls: Type[Any]) -> str:
+    return cf.orange(cls.__name__) + "\n"
+
+
+def display_key_and_value(
+        key: str, value: Any, separator: str, show_values: bool = True, indent: int = 0) -> str:
+    return (_indent_space(indent + 1) + cf.blue(key) + cf.base00(separator)
+            + display_generic(value, show_values, indent + 1))
+
+
+# Private functions --------------------------------------------------------------------------------
+_indentation = 4
+
+
+def _indent_space(indent: int) -> str:
+    return (indent * _indentation) * " "
+
+
+def _format_number(x: float) -> str:
+    return f"{x:10.4f}"
+
+
+def _show_array(indent: int, array: Union[np.ndarray, DeviceArray]) -> str:
+    if len(array.shape) == 0:
+        return _indent_space(indent) + _format_number(array[()]) + "\n"
+    if np.prod(array.shape) == 0:
+        return ""
+    if 1 in array.shape:
+        return _show_array(indent, array[tuple(0 if s == 1 else slice(None)
+                                               for s in array.shape)])
+    if len(array.shape) == 1:
+        return _indent_space(indent) + "  ".join(_format_number(array[i])
+                                                 for i in range(array.shape[0])) + "\n"
+    if len(array.shape) == 2:
+        return "".join(_show_array(indent, array_slice)
+                       for array_slice in array)
+    return ""
---
+++ packages/tjax/tjax-0.4.0.tar.gz/tjax-0.4.0/tjax/testing.py
@@ -0,0 +1,229 @@
+from functools import partial
+from numbers import Number
+from typing import Any, Optional, Union, cast
+
+import numpy as np
+from jax import numpy as jnp
+from jax.interpreters.xla import DeviceArray
+from jax.tree_util import tree_multimap, tree_reduce
+
+from .annotations import PyTree
+from .dtypes import default_atol, default_rtol
+
+__all__ = ['assert_jax_allclose', 'jax_allclose']
+
+
+def assert_jax_allclose(actual: PyTree,
+                        desired: PyTree,
+                        original_name: Optional[str] = None,
+                        original_value: Optional[PyTree] = None,
+                        *,
+                        rtol: Optional[float] = None,
+                        atol: Optional[float] = None) -> None:
+    """
+    Asserts that every tensor in an actual pytree matches the corresponding tensor in a desired
+    pytree.  If the assertion fails, a passing test string is printed::
+
+    ```python
+    from tjax import assert_jax_allclose, dataclass, Array
+
+    @dataclass
+    class A:
+        x: Array
+        y: Array
+
+    @dataclass
+    class B:
+        z: A
+
+    original = B(A(1.2, 3.4))
+    desired = B(A(3.0, 4.0))
+    actual = B(A(1.2, 5.2))
+
+    assert_jax_allclose(actual, desired, 'original', original)
+    ```
+    This prints::
+    ```
+    JAX trees don't match.  Actual:
+    B
+        z=A
+            x=3.0
+            y=4.0
+
+    Desired:
+    B
+        z=A
+            x=1.2
+            y=5.2
+
+    Test string:
+    original.replace(z=original.z.replace(x=3.0, y=4.0))
+    ```
+    The test string can then be pasted.
+
+    Args:
+        actual: The actual value.
+        desired: The desired value.
+        original_name: The variable name that contains the original value.
+        original_value: The original value.  This is usually a pytree like a dataclass that has the
+            same type as actual and desired, but contains different values.
+        rtol: The relative tolerance of the comparisons in the assertion.
+        atol: The absolute tolerance of the comparisons in the assertion.
+    """
+    if rtol is None:
+        rtol = default_rtol
+    if atol is None:
+        atol = default_atol
+
+    try:
+        tree_multimap(partial(np.testing.assert_allclose, rtol=rtol, atol=atol), actual, desired)
+    except AssertionError:
+        print("JAX trees don't match.\nActual:")
+        print(actual)
+        print("Desired:")
+        print(desired)
+        print("Test string:")
+        if original_name is not None and original_value is not None:
+            print(get_relative_test_string(original_name, actual, original_value, rtol, atol))
+        else:
+            print(get_test_string(actual, rtol, atol))
+        raise
+
+
+def jax_allclose(actual: PyTree,
+                 desired: PyTree,
+                 rtol: Optional[float] = None,
+                 atol: Optional[float] = None) -> bool:
+    """
+    Args:
+        actual: The actual value.
+        desired: The desired value.
+        rtol: The relative tolerance of the comparisons in the comparison.
+        atol: The absolute tolerance of the comparisons in the comparison.
+    """
+    if rtol is None:
+        rtol = default_rtol
+    if atol is None:
+        atol = default_atol
+
+    return cast(
+        bool,
+        tree_reduce(jnp.logical_and,
+                    tree_multimap(partial(np.allclose, rtol=rtol, atol=atol), actual, desired),
+                    True))
+
+
+def _float_to_string_with_precision(x: Union[float, complex], precision: int) -> str:
+    with np.printoptions(precision=precision, floatmode='maxprec'):
+        return repr(np.array(x))[6:-1]
+
+
+def _float_to_string(x: Union[float, complex], rtol: float, atol: float) -> str:
+    for i in range(20):
+        retval = _float_to_string_with_precision(x, i)
+        if np.allclose(float(retval), x, rtol=rtol, atol=atol):
+            break
+    return retval
+
+
+def get_test_string(actual: Any, rtol: float, atol: float) -> str:
+    """
+    Args:
+        actual: The actual value that was produced, and that should be the desired value.
+        rtol: The relative tolerance of the comparisons in the assertion.
+        atol: The absolute tolerance of the comparisons in the assertion.
+    Returns:
+        A string of Python code that produces the desired value.
+    """
+    def fts(x: float) -> str:
+        return _float_to_string(x, rtol, atol)
+
+    if isinstance(actual, (np.ndarray, DeviceArray)):
+        with np.printoptions(formatter={'float_kind': fts,
+                                        'complex_kind': fts}):
+            return "np." + repr(np.asarray(actual)).replace(' ]', ']').replace(' ,', ',').replace(
+                '  ', ' ')
+    if isinstance(actual, (float, complex)):
+        return _float_to_string(actual, rtol, atol)
+    if isinstance(actual, Number):
+        return str(actual)
+    if hasattr(actual, 'display'):
+        retval = f"{type(actual).__name__}("
+        retval += ",\n".join(
+            f"{fn}=" + get_test_string(getattr(actual, fn), rtol, atol)
+            for fn in actual.tree_fields)
+        if actual.tree_fields and actual.hashed_fields:
+            retval += ',\n'
+        retval += ",\n".join(
+            f"{fn}=" + get_test_string(getattr(actual, fn), rtol, atol)
+            for fn in actual.hashed_fields)
+        retval += ")"
+        return retval
+    if isinstance(actual, (list, tuple)):
+        is_list = isinstance(actual, list)
+        return (("[" if is_list else "(")
+                + ", ".join(get_test_string(sub_actual, rtol, atol)
+                            for i, sub_actual in enumerate(actual))
+                + ("]" if is_list else ")"))
+    if isinstance(actual, dict):
+        return str({key: get_test_string(sub_actual, rtol, atol)
+                    for key, sub_actual in actual.items()})
+    return str(actual)
+
+
+def get_relative_test_string(original_name: str,
+                             actual: Any,
+                             original: Any,
+                             rtol: float,
+                             atol: float) -> str:
+    """
+    Args:
+        original_name: The name of the variable containing an original value.
+        actual: The actual value that was produced, and that should be the desired value.
+        original: The original value.
+        rtol: The relative tolerance of the comparisons in the assertion.
+        atol: The absolute tolerance of the comparisons in the assertion.
+    Returns:
+        A string of Python code that produces the desired value from an "original value" (could be
+        zeroed-out, for example).
+    """
+    def fts(x: float) -> str:
+        return _float_to_string(x, rtol, atol)
+
+    if isinstance(actual, (np.ndarray, DeviceArray)):
+        with np.printoptions(formatter={'float_kind': fts,
+                                        'complex_kind': fts}):
+            return "np." + repr(np.asarray(actual)).replace(' ]', ']').replace(' ,', ',').replace(
+                '  ', ' ')
+    if isinstance(actual, (float, complex)):
+        return _float_to_string(actual, rtol, atol)
+    if isinstance(actual, Number):
+        return str(actual)
+    if hasattr(actual, 'display'):
+        retval = f"{original_name}.replace("
+        retval += ",\n".join(
+            f"{fn}=" + get_relative_test_string(f"{original_name}.{fn}",
+                                                getattr(actual, fn),
+                                                getattr(original, fn),
+                                                rtol,
+                                                atol)
+            for fn in actual.tree_fields
+            if not jax_allclose(getattr(actual, fn), getattr(original, fn), rtol=rtol, atol=atol))
+        retval += ")"
+        return retval
+    if isinstance(actual, (list, tuple)):
+        is_list = isinstance(actual, list)
+        return (("[" if is_list else "(")
+                + ", ".join(get_relative_test_string(f"{original_name}[{i}]",
+                                                     sub_actual, sub_original,
+                                                     rtol, atol)
+                            for i, (sub_actual, sub_original) in enumerate(zip(actual, original)))
+                + ("]" if is_list else ")"))
+    if isinstance(actual, dict):
+        if not isinstance(original, dict):
+            raise TypeError
+        return str({key: get_relative_test_string(f"{original_name}[{key}]",
+                                                  sub_actual, original[key],
+                                                  rtol, atol)
+                    for key, sub_actual in actual.items()})
+    return str(actual)
