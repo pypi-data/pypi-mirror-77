e4fcb04222920a1f469385312ae0aa4edae3dfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:29 +0000

    Add package git-annex-remote-globus/1.2.3/git-annex-remote-globus-1.2.3.tar.gz

---
+++ packages/git-annex-remote-globus/git-annex-remote-globus-1.2.3.tar.gz/git-annex-remote-globus-1.2.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name='git-annex-remote-globus',
+    version='1.2.3',
+    author='Giulia Ippoliti',
+    author_email='giuly.ippoliti@gmail.com',
+    scripts=['git-annex-remote-globus'],
+    keywords='git-annex remote globus',
+    url='https://github.com/CONP-PCNO/git-annex-remote-globus',
+    description='git annex special remote for Globus',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['annexremote==1.3.1', 'globus_sdk', 'fair-research-login'],
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Software Development :: Libraries',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
