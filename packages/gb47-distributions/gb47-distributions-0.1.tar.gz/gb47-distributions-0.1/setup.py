bf5d3a1f64246e0e26a964caf7177932ecd71e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package gb47-distributions/0.1/gb47-distributions-0.1.tar.gz

---
+++ packages/gb47-distributions/gb47-distributions-0.1.tar.gz/gb47-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gb47-distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['gb47-distributions'],
+      zip_safe=False)
