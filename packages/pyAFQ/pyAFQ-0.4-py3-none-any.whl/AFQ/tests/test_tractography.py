249c14b7d5aa1ce04a2572bece87b7c6bb965270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package pyAFQ/0.4/pyAFQ-0.4-py3-none-any.whl

---
+++ packages/pyAFQ/pyAFQ-0.4-py3-none-any.whl/AFQ/tests/test_tractography.py
@@ -0,0 +1,59 @@
+import os.path as op
+import numpy as np
+import numpy.testing as npt
+
+import nibabel.tmpdirs as nbtmp
+
+from AFQ.models.csd import fit_csd
+from AFQ.models.dti import fit_dti
+from AFQ.tractography import track
+from AFQ.utils.testing import make_tracking_data
+
+
+seeds = np.array([[-80., -120., -60.],
+                  [-81, -121, -61],
+                  [-81, -120, -60]])
+
+
+tmpdir = nbtmp.InTemporaryDirectory()
+fbval = op.join(tmpdir.name, 'dti.bval')
+fbvec = op.join(tmpdir.name, 'dti.bvec')
+fdata = op.join(tmpdir.name, 'dti.nii.gz')
+make_tracking_data(fbval, fbvec, fdata)
+
+min_length = 20
+step_size = 0.5
+
+
+def test_csd_tracking():
+    for sh_order in [4, 8, 10]:
+        fname = fit_csd(fdata, fbval, fbvec,
+                        response=((0.0015, 0.0003, 0.0003), 100),
+                        sh_order=8, lambda_=1, tau=0.1, mask=None,
+                        out_dir=tmpdir.name)
+        for directions in ["det", "prob"]:
+            sl = track(fname, directions,
+                       odf_model="CSD",
+                       max_angle=30.,
+                       sphere=None,
+                       seed_mask=None,
+                       n_seeds=seeds,
+                       stop_mask=None,
+                       step_size=step_size,
+                       min_length=min_length).streamlines
+
+            npt.assert_(len(sl[0]) >= step_size * min_length)
+
+
+def test_dti_tracking():
+    fdict = fit_dti(fdata, fbval, fbvec)
+    for directions in ["det", "prob"]:
+        sl = track(fdict['params'],
+                   directions,
+                   max_angle=30.,
+                   sphere=None,
+                   seed_mask=None,
+                   n_seeds=1,
+                   step_size=step_size,
+                   min_length=min_length).streamlines
+        npt.assert_(len(sl[0]) >= min_length * step_size)
---
+++ packages/pyAFQ/pyAFQ-0.4-py3-none-any.whl/AFQ/tractography.py
@@ -0,0 +1,154 @@
+import numpy as np
+import nibabel as nib
+import dipy.reconst.shm as shm
+import logging
+
+import dipy.data as dpd
+from dipy.direction import (DeterministicMaximumDirectionGetter,
+                            ProbabilisticDirectionGetter)
+import dipy.tracking.utils as dtu
+from dipy.io.stateful_tractogram import StatefulTractogram, Space
+from dipy.tracking.stopping_criterion import ThresholdStoppingCriterion
+
+from AFQ._fixes import VerboseLocalTracking, tensor_odf
+
+
+def track(params_file, directions="det", max_angle=30., sphere=None,
+          seed_mask=None, seed_threshold=0, n_seeds=1, random_seeds=False,
+          rng_seed=None, stop_mask=None, stop_threshold=0, step_size=0.5,
+          min_length=10, max_length=1000, odf_model="DTI"):
+    """
+    Tractography
+
+    Parameters
+    ----------
+    params_file : str, nibabel img.
+        Full path to a nifti file containing CSD spherical harmonic
+        coefficients, or nibabel img with model params.
+    directions : str
+        How tracking directions are determined.
+        One of: {"deterministic" | "probablistic"}
+    max_angle : float, optional.
+        The maximum turning angle in each step. Default: 30
+    sphere : Sphere object, optional.
+        The discretization of direction getting. default:
+        dipy.data.default_sphere.
+    seed_mask : array, optional.
+        Float or binary mask describing the ROI within which we seed for
+        tracking.
+        Default to the entire volume (all ones).
+    seed_threshold : float, optional.
+        A value of the seed_mask below which tracking is terminated.
+        Default to 0.
+    n_seeds : int or 2D array, optional.
+        The seeding density: if this is an int, it is is how many seeds in each
+        voxel on each dimension (for example, 2 => [2, 2, 2]). If this is a 2D
+        array, these are the coordinates of the seeds. Unless random_seeds is
+        set to True, in which case this is the total number of random seeds
+        to generate within the mask.
+    random_seeds : bool
+        Whether to generate a total of n_seeds random seeds in the mask.
+        Default: XXX.
+    rng_seed : int
+        random seed used to generate random seeds if random_seeds is
+        set to True. Default: None
+    stop_mask : array, optional.
+        A float or binary mask that determines a stopping criterion (e.g. FA).
+        Default to no stopping (all ones).
+    stop_threshold : float, optional.
+        A value of the stop_mask below which tracking is terminated. Default to
+        0 (this means that if no stop_mask is passed, we will stop only at
+        the edge of the image)
+    step_size : float, optional.
+        The size (in mm) of a step of tractography. Default: 1.0
+    min_length: int, optional
+        The miminal length (mm) in a streamline. Default: 10
+    max_length: int, optional
+        The miminal length (mm) in a streamline. Default: 250
+    odf_model : str, optional
+        One of {"DTI", "CSD"}. Defaults to use "DTI"
+    Returns
+    -------
+    list of streamlines ()
+    """
+    logger = logging.getLogger('AFQ.tractography')
+
+    logger.info("Loading Image...")
+    if isinstance(params_file, str):
+        params_img = nib.load(params_file)
+    else:
+        params_img = params_file
+
+    model_params = params_img.get_fdata()
+    affine = params_img.affine
+
+    logger.info("Generating Seeds...")
+    if isinstance(n_seeds, int):
+        if seed_mask is None:
+            seed_mask = np.ones(params_img.shape[:3])
+        elif seed_mask.dtype != 'bool':
+            seed_mask = seed_mask > seed_threshold
+        if random_seeds:
+            seeds = dtu.random_seeds_from_mask(seed_mask, seeds_count=n_seeds,
+                                               seed_count_per_voxel=False,
+                                               affine=affine,
+                                               random_seed=rng_seed)
+        else:
+            seeds = dtu.seeds_from_mask(seed_mask,
+                                        density=n_seeds,
+                                        affine=affine)
+    else:
+        # If user provided an array, we'll use n_seeds as the seeds:
+        seeds = n_seeds
+    if sphere is None:
+        sphere = dpd.default_sphere
+
+    logger.info("Getting Directions...")
+    if directions == "det":
+        dg = DeterministicMaximumDirectionGetter
+    elif directions == "prob":
+        dg = ProbabilisticDirectionGetter
+
+    if odf_model == "DTI" or odf_model == "DKI":
+        evals = model_params[..., :3]
+        evecs = model_params[..., 3:12].reshape(params_img.shape[:3] + (3, 3))
+        odf = tensor_odf(evals, evecs, sphere)
+        dg = dg.from_pmf(odf, max_angle=max_angle, sphere=sphere)
+    elif odf_model == "CSD":
+        dg = dg.from_shcoeff(model_params, max_angle=max_angle, sphere=sphere)
+
+    if stop_mask is None:
+        stop_mask = np.ones(params_img.shape[:3])
+
+    if stop_mask.dtype == 'bool':
+        stopping_criterion = ThresholdStoppingCriterion(stop_mask,
+                                                        0.5)
+    else:
+        stopping_criterion = ThresholdStoppingCriterion(stop_mask,
+                                                        stop_threshold)
+
+    logger.info("Tracking...")
+
+    return _local_tracking(seeds, dg, stopping_criterion, params_img,
+                           step_size=step_size, min_length=min_length,
+                           max_length=max_length, random_seed=rng_seed)
+
+
+def _local_tracking(seeds, dg, stopping_criterion, params_img,
+                    step_size=0.5, min_length=10, max_length=1000,
+                    random_seed=None):
+    """
+    Helper function
+    """
+    if len(seeds.shape) == 1:
+        seeds = seeds[None, ...]
+    tracker = VerboseLocalTracking(dg,
+                                   stopping_criterion,
+                                   seeds,
+                                   params_img.affine,
+                                   step_size=step_size,
+                                   min_length=min_length,
+                                   max_length=max_length,
+                                   random_seed=random_seed)
+
+    return StatefulTractogram(tracker, params_img, Space.RASMM)
