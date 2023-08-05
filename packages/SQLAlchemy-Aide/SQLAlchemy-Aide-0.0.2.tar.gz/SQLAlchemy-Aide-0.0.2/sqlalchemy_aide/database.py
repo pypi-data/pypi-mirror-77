9912388af6ef403e83c4b652f7335e424b6c823b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:24 +0000

    Add package SQLAlchemy-Aide/0.0.2/SQLAlchemy-Aide-0.0.2.tar.gz

---
+++ packages/SQLAlchemy-Aide/SQLAlchemy-Aide-0.0.2.tar.gz/SQLAlchemy-Aide-0.0.2/sqlalchemy_aide/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+from . import SQLAlchemy
+
+
+db = SQLAlchemy()
+base = declarative_base()
+db.register_base(base)
---
+++ packages/SQLAlchemy-Aide/SQLAlchemy-Aide-0.0.2.tar.gz/SQLAlchemy-Aide-0.0.2/sqlalchemy_aide/mixins/columns.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import DateTime
+from sqlalchemy import func
+
+
+class PrimaryKeyMixin:
+    id = Column(Integer, primary_key=True)
+
+
+class CreationTimestampMixin:
+    created = Column(DateTime, default=func.now())
+
+
+class UpdateMixin:
+    updated = Column(DateTime, default=func.now())
+
+
+class ActiveMixin:
+    updated = Column(DateTime, default=func.now())
+
+
+class SuperMixin(PrimaryKeyMixin, CreationTimestampMixin, UpdateMixin, ActiveMixin):
+    __abstract__ = True
