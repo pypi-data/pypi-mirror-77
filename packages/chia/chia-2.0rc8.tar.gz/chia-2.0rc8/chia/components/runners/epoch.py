c53e95ddbad8d75dbededc804b564b8bd0fbd8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package chia/2.0rc8/chia-2.0rc8.tar.gz

---
+++ packages/chia/chia-2.0rc8.tar.gz/chia-2.0rc8/chia/components/runners/epoch.py
@@ -0,0 +1,50 @@
+import time
+
+from chia.components.runners.runner import Runner
+
+
+class EpochRunner(Runner):
+    def __init__(self, experiment_container, epochs, max_test_samples=None):
+        super().__init__(experiment_container)
+        self.epochs = epochs
+        self.max_test_samples = max_test_samples
+
+    def run(self):
+        # Get out some members of container
+        dataset = self.experiment_container.dataset
+        base_model = self.experiment_container.base_model
+
+        # Build training data
+        training_samples = dataset.train_pool(0, "label_gt")
+
+        # Build test data
+        if self.max_test_samples is not None:
+            test_samples = dataset.test_pool(0, "label_gt")[: self.max_test_samples]
+        else:
+            test_samples = dataset.test_pool(0, "label_gt")
+
+        # "Interact"
+        training_samples = self.experiment_container.interactor.query_annotations_for(
+            training_samples, "label_gt", "label_ann"
+        )
+        for epoch in range(self.epochs):
+            epoch_begin_time = time.time()
+
+            self.log_info(f"Start of epoch {epoch + 1} of {self.epochs}")
+            base_model.observe(training_samples, "label_ann")
+
+            predicted_test_samples = base_model.predict(test_samples, "label_pred")
+
+            # Go over all evaluators
+            result_dict = {}
+            for evaluator in self.experiment_container.evaluators:
+                evaluator.update(predicted_test_samples, "label_gt", "label_pred")
+                result_dict.update(evaluator.result())
+                evaluator.reset()
+
+            # Store epoch duration
+            epoch_end_time = time.time()
+            epoch_duration = epoch_end_time - epoch_begin_time
+            result_dict.update({"epoch_duration": epoch_duration})
+
+            self.report_result(result_dict, step=epoch + 1)
