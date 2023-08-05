d756bfc155d6b150d29822e959c428104f476a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:29 +0000

    Add package datamodeltest/0.0.3/datamodeltest-0.0.3-py3-none-any.whl

---
+++ packages/datamodeltest/datamodeltest-0.0.3-py3-none-any.whl/tests_pkg/chck_log.py
@@ -0,0 +1,42 @@
+import sys
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    version=1,
+    formatters={
+        'verbose': {
+            'format': ("[%(asctime)s] %(levelname)s "
+                       "[%(name)s:%(lineno)s] %(message)s"),
+            'datefmt': "%d/%b/%Y %H:%M:%S",
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    handlers={
+        'batch-process-logger': {'class': 'logging.handlers.RotatingFileHandler',
+                             'formatter': 'verbose',
+                             'level': logging.DEBUG,
+                             'filename': 'logs/batch.log',
+                             'maxBytes': 52428800,
+                             'backupCount': 7},
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'stream': sys.stdout,
+        },
+    },
+    loggers={
+        'batch_process_logger': {
+            'handlers': ['batch-process-logger', 'console'],
+            'level': logging.DEBUG
+        }
+    }
+)
+
+dictConfig(logging_config)
+
+api_logger = logging.getLogger('api_logger')
+batch_process_logger = logging.getLogger('batch_process_logger')
