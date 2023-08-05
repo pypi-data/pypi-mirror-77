a6559553e8b942cc8d7e14ef234b19a8cb348f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:01 +0000

    Add package asteroid/0.3.1rc1/asteroid-0.3.1rc1-py3-none-any.whl

---
+++ packages/asteroid/asteroid-0.3.1rc1-py3-none-any.whl/asteroid/__init__.py
@@ -0,0 +1,20 @@
+import pathlib
+from .utils import deprecation_utils, torch_utils  # noqa
+from .models import ConvTasNet, DPRNNTasNet, DPTNet, LSTMTasNet
+
+project_root = str(pathlib.Path(__file__).expanduser().absolute().parent.parent)
+__version__ = "0.3.1rc1"
+
+
+def show_available_models():
+    from .utils.hub_utils import MODELS_URLS_HASHTABLE
+
+    print(" \n".join(list(MODELS_URLS_HASHTABLE.keys())))
+
+
+__all__ = [
+    "ConvTasNet",
+    "DPRNNTasNet",
+    "DPTNet",
+    "show_available_models",
+]
