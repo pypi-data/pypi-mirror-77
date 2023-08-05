156a940f497a2535b39b489d85d09cc4f6029be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:19 +0000

    Add package pretty-traceback/2020.1007/pretty_traceback-2020.1007-py2.py3-none-any.whl

---
+++ packages/pretty-traceback/pretty_traceback-2020.1007-py2.py3-none-any.whl/pretty_traceback/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# This file is part of the pretty-traceback project
+# https://gitlab.com/mbarkhau/pretty-traceback
+#
+# Copyright (c) 2020 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from .hook import install
+from .hook import uninstall
+__version__ = '2020.1007'
+__all__ = ['install', 'uninstall', '__version__']
