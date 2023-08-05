807929549815266286320af1265427c2aa72bcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:51 +0000

    Add package Bella-Ciao/0.0.1/Bella_Ciao-0.0.1-py3-none-any.whl

---
+++ packages/Bella-Ciao/Bella_Ciao-0.0.1-py3-none-any.whl/BellaCiao/BellaCiao.py
@@ -0,0 +1,6 @@
+class bellaciao:
+    def run(self):
+        print ("Hello, world!")
+
+if __name__ == '__main__':
+    bellaciao().run()
---
+++ packages/Bella-Ciao/Bella_Ciao-0.0.1-py3-none-any.whl/BellaCiao/bellaml.py
@@ -0,0 +1,19 @@
+class bellaml:
+	def __init__(self):
+		print ("Ciao, Bella!")
+		
+
+	def printMembers(self):
+		print('Printing members of the Birds class')
+
+#if __name__ == '__main__':
+#    bellaml().run()
+	
+	
+	
+
+ 
+#    def printMembers(self):
+ #       print('Printing members of the Mammals class')
+  #      for member in self.members:
+   #         print('\t%s ' % member)
