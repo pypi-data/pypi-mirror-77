302d25eab00ccaf085340bee093bdd77431c96f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:56 +0000

    Add package okdoodle/0.0.3/okdoodle-0.0.3.tar.gz

---
+++ packages/okdoodle/okdoodle-0.0.3.tar.gz/okdoodle-0.0.3/README.md
@@ -0,0 +1,19 @@
+
+
+"""
+## Installation
+Run the following to install:
+
+'''python
+pip install okdoodle
+"""
+
+## Usage
+'''Python
+from okdoodle import sayhello
+
+# Generate "Hello, world!"
+sayhello()
+
+# Generate "Hello, Everybody!"
+sayhello("Everybody")
---
+++ packages/okdoodle/okdoodle-0.0.3.tar.gz/okdoodle-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='okdoodle',
+    version = '0.0.3',
+    description = 'say hello!',
+    py_modlues = ["okdoodle"],
+    package_dir = {'': 'src'},
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    url = "https://github.com/Alan-Cyriac/okdoodle",
+    author = ["Alan Cyriac", "Abhijith M"],
+    author_email = "hr.doodleai@gmail.com",
+)
