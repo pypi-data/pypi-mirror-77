15746a146cc922415cf12d0fe14e8afaa9e2076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package glean-sdk/32.1.0/glean_sdk-32.1.0-cp36-abi3-manylinux1_x86_64.whl

---
+++ packages/glean-sdk/glean_sdk-32.1.0-cp36-abi3-manylinux1_x86_64.whl/glean/__init__.py
@@ -0,0 +1,34 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Top-level package for Glean SDK."""
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+from .glean import Glean
+from .config import Configuration
+from ._loader import load_metrics, load_pings
+
+
+__version__: str = "unknown"
+try:
+    __version__ = str(get_distribution("glean-sdk").version)
+except DistributionNotFound:  # pragma: no cover
+    pass
+
+
+__author__ = "The Glean Team"
+__email__ = "glean-team@mozilla.com"
+
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__version__",
+    "Glean",
+    "Configuration",
+    "load_metrics",
+    "load_pings",
+]
