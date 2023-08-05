1a812c631630496a7df4779c7dd7161264c66004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:56 +0000

    Add package etc-ml/0.1/etc-ml-0.1.tar.gz

---
+++ packages/etc-ml/etc-ml-0.1.tar.gz/etc-ml-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='etc-ml',
+	version='0.1',
+	description='etc project',
+	url='https://github.com/UFMG-Database-lab/etc',
+	author='Vitor Mangaravite',
+	license='MIT',
+	packages=['etc'],
+	zip_safe=False)
