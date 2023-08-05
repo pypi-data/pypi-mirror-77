b38fc50f0cc03f3f6f6ecd72bb372578a8ae06e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package springlabs-python/0.5/springlabs_python-0.5.tar.gz

---
+++ packages/springlabs-python/springlabs_python-0.5.tar.gz/springlabs_python-0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='springlabs_python',
+    version='0.5',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author="Alejandro Barcenas",
+    author_email="barcenas.r.2510@gmail.com",
+    description="Springlabs Projects Python Standard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    install_requires=['Click', "requests==2.24.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        springlabs_python=springlabs_python:cli
+    ''',
+)
