47380988684002896c6c7ddb4af50446abe092de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package onefeed/0.2.3/onefeed-0.2.3-py3.6.egg

---
+++ packages/onefeed/onefeed-0.2.3-py3.6.egg/onefeed/retriever/retriever.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from twisted.internet import defer
+from crochet import setup, wait_for
+
+from .utils import get_spiders
+
+if not sys.argv[0] in ['crawl', 'shell']:  # when debugging scrapy
+    setup()
+
+
+@wait_for(timeout=60)
+def fetch_once():
+    d_list = []
+    for spider, setting in get_spiders().items():
+        default_setting = get_project_settings()
+        default_setting.update(setting)
+        crawler_runner = CrawlerRunner(default_setting)
+        d_list.append(crawler_runner.crawl(spider))
+    d = defer.DeferredList(d_list)
+    return d
