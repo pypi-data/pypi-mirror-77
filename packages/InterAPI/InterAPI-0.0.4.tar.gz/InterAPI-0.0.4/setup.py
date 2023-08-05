da5cf89fbf9b61aae9ad7a5f7a3551a74736f445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:24 +0000

    Add package InterAPI/0.0.4/InterAPI-0.0.4.tar.gz

---
+++ packages/InterAPI/InterAPI-0.0.4.tar.gz/InterAPI-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+f = open('LICENSE.txt', 'r')
+l = f.read()
+
+setup(
+    name='InterAPI',
+    version='0.0.4',
+    packages=['InterAPI'],
+    url='https://github.com/btmluiz/InterAPI',
+    license=l,
+    author='btmluiz',
+    author_email='luiz@selectbrasil.com.br',
+    description='API para integração com banco inter',
+    install_requires=[
+        'requests>=2.24.0'
+    ],
+    python_requires='>=3'
+)
+
+f.close()
