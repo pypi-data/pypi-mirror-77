8d5896be5030c0f66dfc04ca85237481f1a41b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:38 +0000

    Add package rubin-jupyter-utils.helpers/0.30.0/rubin_jupyter_utils.helpers-0.30.0-py2.py3-none-any.whl

---
+++ packages/rubin-jupyter-utils.helpers/rubin_jupyter_utils.helpers-0.30.0-py2.py3-none-any.whl/rubin_jupyter_utils/helpers/__init__.py
@@ -0,0 +1,51 @@
+"""LSST JupyterHub utility classes and functions.
+"""
+from .functions import (
+    rreplace,
+    sanitize_dict,
+    get_execution_namespace,
+    make_logger,
+    str_bool,
+    str_true,
+    listify,
+    intify,
+    floatify,
+    list_duplicates,
+    list_digest,
+    get_access_token,
+    parse_access_token,
+    assemble_gids,
+    get_fake_gid,
+    make_passwd_line,
+    make_group_lines,
+    add_user_to_groups,
+    get_supplemental_gids,
+    resolve_groups,
+)
+from .singleton import Singleton
+from ._version import __version__
+
+all = [
+    rreplace,
+    sanitize_dict,
+    get_execution_namespace,
+    make_logger,
+    str_bool,
+    str_true,
+    listify,
+    intify,
+    floatify,
+    list_duplicates,
+    list_digest,
+    get_access_token,
+    parse_access_token,
+    assemble_gids,
+    get_fake_gid,
+    make_passwd_line,
+    make_group_lines,
+    add_user_to_groups,
+    get_supplemental_gids,
+    resolve_groups,
+    Singleton,
+    __version__,
+]
