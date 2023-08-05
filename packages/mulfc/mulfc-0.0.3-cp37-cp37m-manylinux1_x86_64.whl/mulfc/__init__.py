86d3c247b6a299f358667ed57b138687b907fcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package mulfc/0.0.3/mulfc-0.0.3-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/mulfc/mulfc-0.0.3-cp37-cp37m-manylinux1_x86_64.whl/mulfc/__init__.py
@@ -0,0 +1,6 @@
+from .LFC import (get_version,
+                     locfield,
+                     dipten,
+                     find_largest_sphere)
+
+__version__ = "{}.{}.{}".format(* get_version() )
