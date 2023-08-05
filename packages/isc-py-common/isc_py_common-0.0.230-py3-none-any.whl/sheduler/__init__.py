ee7f60718284ea79146508d777fda756ad7da921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:32 +0000

    Add package isc-py-common/0.0.230/isc_py_common-0.0.230-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.230-py3-none-any.whl/sheduler/__init__.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+
+
+def make_shedurer_tasks():
+    from isc_common.management.commands.grop_tmp_mat_views import grop_mat_views
+    from pyriodic import Scheduler, DurationJob
+
+    s = Scheduler()
+
+    def drop_tbl():
+        grop_mat_views(prefix='ready')
+        grop_mat_views(prefix='tmp')
+
+    s.add_job(DurationJob(drop_tbl, when=settings.SHEDULLER_PERIOD, name='grop_ready_mat_views'))
+
+    # print(s.next_run_times())
+    return s
+
+
+def st_sheduler():
+    try:
+        if callable(settings.SHEDULLER):
+            from threading import Timer
+            def st():
+                if callable(settings.SHEDULLER):
+                    s = make_shedurer_tasks()
+                    settings.SHEDULLER(s)
+
+            t = Timer(10.0, st)
+            t.start()
+    except AttributeError:
+        print('Sheduler not started.')
+        pass
+
+
+st_sheduler()
