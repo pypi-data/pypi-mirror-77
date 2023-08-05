73edcc382146a752024fc02eb495b333883e9d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:07 +0000

    Add package sss-beneficiarios-hospitales/0.8.118/sss_beneficiarios_hospitales-0.8.118.tar.gz

---
+++ packages/sss-beneficiarios-hospitales/sss_beneficiarios_hospitales-0.8.118.tar.gz/sss_beneficiarios_hospitales-0.8.118/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+     name='sss_beneficiarios_hospitales',
+     version='0.8.118',
+     license='MIT',
+     entry_points={
+        },
+     author="Andres Vazquez",
+     author_email="andres@data99.com.ar",
+     description="API Superintendencia de Servicios de Salud Argentino para informacion sobre beneficiarios de hospitales",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/cluster311/sss-beneficiarios",
+     install_requires=required,
+     # package_dir={'': 'src'},
+     packages=['sss_beneficiarios_hospitales'],  # setuptools.find_packages(),
+     package_data = {
+        'html-samples': ['*']
+        },
+     include_package_data=True,
+     classifiers=[
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+         'Intended Audience :: Developers', 
+     ],
+     python_requires='>=3.6',
+ )
