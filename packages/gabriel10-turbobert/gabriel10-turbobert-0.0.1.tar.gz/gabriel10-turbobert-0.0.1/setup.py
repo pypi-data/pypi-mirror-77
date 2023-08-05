e8d7493a7f0136c677cd4a00e849417ebbde3eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package gabriel10-turbobert/0.0.1/gabriel10-turbobert-0.0.1.tar.gz

---
+++ packages/gabriel10-turbobert/gabriel10-turbobert-0.0.1.tar.gz/gabriel10-turbobert-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gabriel10-turbobert",
+    version="0.0.1",
+    author="Robert Degen",
+    author_email="os@turbobert.de",
+    description="Very Simple PDF TypeWriter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/turbobert/gabriel10",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    python_requires=">=3.6"
+)
