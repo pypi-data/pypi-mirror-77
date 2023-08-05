23c687a36483b7af05a06d3265b822ba6765c880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:02 +0000

    Add package qondor/0.63/qondor-0.63.tar.gz

---
+++ packages/qondor/qondor-0.63.tar.gz/qondor-0.63/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name          = 'qondor',
+    version       = '0.63',
+    license       = 'BSD 3-Clause License',
+    description   = 'Description text',
+    url           = 'https://github.com/tklijnsma/qondor.git',
+    download_url  = 'https://github.com/tklijnsma/qondor/archive/v0_63.tar.gz',
+    author        = 'Thomas Klijnsma',
+    author_email  = 'tklijnsm@gmail.com',
+    packages      = ['qondor'],
+    zip_safe      = False,
+    scripts       = [
+        'bin/qondor-submit',
+        'bin/qondor-sleepuntil'
+        ],
+    install_requires     = ['seutils'],
+    include_package_data = True,
+    )
