83b6cac56138c63f8e11e7243f07d5e560366fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:47:14 +0000

    Add package flask-dictabase/1.0.2/flask_dictabase-1.0.2.tar.gz

---
+++ packages/flask-dictabase/flask_dictabase-1.0.2.tar.gz/flask_dictabase-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages = ['flask_dictabase']
+
+setup(
+    name="flask_dictabase",
+
+    version="1.0.2",
+
+    packages=packages,
+    install_requires=[
+        'flask',
+        'dataset',
+    ],
+
+    author="Grant miller",
+    author_email="grant@grant-miller.com",
+    description="A dict() like interface to your database.",
+    long_description="A dict() like interface to your database.",
+    license="PSF",
+    keywords="grant miller flask database",
+    url="https://github.com/GrantGMiller/flask_dictabase",  # project home page, if any
+    project_urls={
+        "Source Code": "https://github.com/GrantGMiller/flask_dictabase",
+    }
+
+)
