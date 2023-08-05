271f43af678562e14e1c4eafb2314645454507d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package cryptostore/0.3.0/cryptostore-0.3.0-py3-none-any.whl

---
+++ packages/cryptostore/cryptostore-0.3.0-py3-none-any.whl/cryptostore/data/influx.py
@@ -0,0 +1,95 @@
+'''
+Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+from decimal import Decimal
+from collections import defaultdict
+
+from cryptofeed.defines import TRADES, L2_BOOK, L3_BOOK, TICKER, FUNDING, OPEN_INTEREST
+import requests
+
+from cryptostore.data.store import Store
+
+
+def chunk(iterable, length):
+    return (iterable[i : i + length] for i in range(0, len(iterable), length))
+
+
+class InfluxDB(Store):
+    def __init__(self, config: dict):
+        self.data = None
+        self.host = config.host
+        self.db = config.db
+        self.addr = f"{config.host}/write?db={config.db}"
+        if 'create' in config and config.create:
+            r = requests.post(f'{config.host}/query', data={'q': f'CREATE DATABASE {config.db}'})
+            r.raise_for_status()
+
+    def aggregate(self, data):
+        self.data = data
+
+    def write(self, exchange, data_type, pair, timestamp):
+        if not self.data:
+            return
+        agg = []
+        # influx cant handle duplicate data (?!) so we need to
+        # incremement timestamps on data that have the same timestamp
+        used_ts = set()
+        if data_type == TRADES:
+            for entry in self.data:
+                ts = int(Decimal(entry["timestamp"]) * 1000000000)
+                while ts in used_ts:
+                    ts += 1
+                used_ts.add(ts)
+                if 'id' in entry:
+                    agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange} side="{entry["side"]}",id="{entry["id"]}",amount={entry["amount"]},price={entry["price"]},timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]} {ts}')
+                else:
+                    agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange} side="{entry["side"]}",amount={entry["amount"]},price={entry["price"]},timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]} {ts}')
+        elif data_type == TICKER:
+            for entry in self.data:
+                ts = int(Decimal(entry["timestamp"]) * 1000000000)
+                agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange} bid={entry["bid"]},ask={entry["ask"]},timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]} {ts}')
+
+        elif data_type == L2_BOOK:
+            for entry in self.data:
+                ts = int(Decimal(entry["timestamp"]) * 1000000000)
+                while ts in used_ts:
+                    ts += 1
+                used_ts.add(ts)
+
+                agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange},delta={entry["delta"]} side="{entry["side"]}",timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]},price={entry["price"]},amount={entry["size"]} {ts}')
+        elif data_type == L3_BOOK:
+            for entry in self.data:
+                ts = int(Decimal(entry["timestamp"]) * 1000000000)
+                while ts in used_ts:
+                    ts += 1
+                used_ts.add(ts)
+
+                agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange},delta={entry["delta"]} side="{entry["side"]}",id="{entry["order_id"]}",timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]},price="{entry["price"]}",amount="{entry["size"]}" {ts}')
+                ts += 1
+        elif data_type == FUNDING:
+            for entry in self.data:
+                formatted = [f"{key}={value}" for key, value in entry.items() if isinstance(value, float)]
+                formatted = ','.join(formatted + [f'{key}="{value}"' for key, value in entry.items() if not isinstance(value, float)])
+                agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange} {formatted}')
+        elif data_type == OPEN_INTEREST:
+            for entry in self.data:
+                ts = int(Decimal(entry["timestamp"]) * 1000000000)
+                agg.append(f'{data_type}-{exchange},pair={pair},exchange={exchange} open_interest={entry["open_interest"]},timestamp={entry["timestamp"]},receipt_timestamp={entry["receipt_timestamp"]} {ts}')
+
+        # https://v2.docs.influxdata.com/v2.0/write-data/best-practices/optimize-writes/
+        # Tuning docs indicate 5k is the ideal chunk size for batch writes
+        for c in chunk(agg, 5000):
+            c = '\n'.join(c)
+            r = requests.post(self.addr, data=c)
+            r.raise_for_status()
+        self.data = None
+
+    def get_start_date(self, exchange: str, data_type: str, pair: str) -> float:
+        try:
+            r = requests.get(f"{self.host}/query?db={self.db}", params={'q': f'SELECT first(timestamp) from "{data_type}-{exchange}" where pair=\'{pair}\''})
+            return r.json()['results'][0]['series'][0]['values'][0][1]
+        except Exception:
+            return None
---
+++ packages/cryptostore/cryptostore-0.3.0-py3-none-any.whl/cryptostore/data/parquet.py
@@ -0,0 +1,149 @@
+'''
+Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+import os
+import glob
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from cryptostore.data.store import Store
+from cryptostore.data.gc import google_cloud_write, google_cloud_read, google_cloud_list
+from cryptostore.data.s3 import aws_write, aws_read, aws_list
+from cryptostore.data.gd import GDriveConnector
+from cryptostore.exceptions import InconsistentStorage
+
+
+class Parquet(Store):
+
+    default_path = lambda self, exchange, data_type, pair: f'{exchange}/{data_type}/{pair}'
+
+    def __init__(self, exchanges, config=None):
+        self._write = []
+        self._read = []
+        self._list = []
+        self.bucket = []
+        self.kwargs = []
+        self.prefix = []
+        self.data = None
+        self.del_file = True
+        self.file_name = config.get('file_format') if config else None
+        self.path = config.get('path') if config else None
+        self.comp_codec = None
+        self.comp_level = None
+        if config and 'compression' in config:
+            self.comp_codec = config['compression']['codec']
+            if 'level' in config['compression']:
+                self.comp_level = config['compression']['level']
+
+        if config:
+            self.del_file = config.get('del_file', True)
+
+            if 'GCS' in config:
+                self._write.append(google_cloud_write)
+                self._read.append(google_cloud_read)
+                self._list.append(google_cloud_list)
+                self.bucket.append(config['GCS']['bucket'])
+                self.prefix.append(config['GCS']['prefix'])
+                self.kwargs.append({'creds': config['GCS']['service_account']})
+            if 'S3' in config:
+                self._write.append(aws_write)
+                self._read.append(aws_read)
+                self._list.append(aws_list)
+                self.bucket.append(config['S3']['bucket'])
+                self.prefix.append(config['S3']['prefix'])
+                self.kwargs.append({'creds': (config['S3']['key_id'], config['S3']['secret']), 'endpoint': config['S3'].get('endpoint')})
+            if 'GD' in config:
+                g_drive = GDriveConnector(config['GD']['service_account'], exchanges, config['GD']['prefix'], self.default_path)
+                self._write.append(g_drive.write)
+                self.prefix.append(None)
+                self.bucket.append(None)
+                self.kwargs.append({'dummy': 'Dummy'})
+
+
+    def aggregate(self, data):
+        names = list(data[0].keys())
+        cols = {name: [] for name in names}
+
+        for entry in data:
+            for key in entry:
+                val = entry[key]
+                cols[key].append(val)
+        arrays = [pa.array(cols[col]) for col in cols]
+        table = pa.Table.from_arrays(arrays, names=names)
+        self.data = table
+
+    def write(self, exchange, data_type, pair, timestamp):
+        if not self.data:
+            return
+        file_name = ''
+        if self.file_name:
+            for var in self.file_name:
+                if var == 'timestamp':
+                    file_name += f"{int(timestamp)}-"
+                elif var == 'data_type':
+                    file_name += f"{data_type}-"
+                elif var == "exchange":
+                    file_name += f"{exchange}-"
+                elif var == "pair":
+                    file_name += f"{pair}-"
+                else:
+                    raise ValueError("Invalid file format specified for parquet file")
+            file_name = file_name[:-1] + ".parquet"
+        else:
+            file_name = f'{exchange}-{data_type}-{pair}-{int(timestamp)}.parquet'
+
+        if self.path:
+            file_name = os.path.join(self.path, file_name)
+
+        pq.write_table(self.data, file_name, compression=self.comp_codec, compression_level=self.comp_level)
+        self.data = None
+
+        if self._write:
+            for func, bucket, prefix, kwargs in zip(self._write, self.bucket, self.prefix, self.kwargs):
+                path = self.default_path(exchange, data_type, pair) + f'/{exchange}-{data_type}-{pair}-{int(timestamp)}.parquet'
+                if prefix:
+                    path = f"{prefix}/{path}"
+                func(bucket, path, file_name, **kwargs)
+            if self.del_file:
+                os.remove(file_name)
+
+    def get_start_date(self, exchange: str, data_type: str, pair: str) -> float:
+        objs = []
+        files = []
+
+        if not self.del_file:
+            file_pattern = f'{exchange}-{data_type}-{pair}-[0-9]*.parquet'
+            files = glob.glob(file_pattern)
+
+        if self._read:
+            for func, bucket, prefix, kwargs in zip(self._list, self.bucket, self.prefix, self.kwargs):
+                path = f'{exchange}/{data_type}/{pair}/'
+                if prefix:
+                    path = f"{prefix}/{path}"
+                ret = func(bucket, path, limit=1, **kwargs)
+                objs.append(ret)
+        if not files and not any(objs):
+            return None
+
+        if files:
+            files = sorted(files)
+            start = files[0]
+        else:
+            start = objs[0][0]
+
+        for entry in objs:
+            if entry[0] != start:
+                raise InconsistentStorage("Stored data differs, cannot backfill")
+
+        if files:
+            return float(pq.read_table(files[0], columns=['timestamp']).to_pandas().timestamp[0])
+        else:
+            tmp = f'{exchange}-{pair}-temp.parquet'
+            self._read[0](self.bucket[0], objs[0][0], tmp, **self.kwargs[0])
+            start = float(pq.read_table(tmp, columns=['timestamp']).to_pandas().timestamp[0])
+            os.remove(tmp)
+            return start
