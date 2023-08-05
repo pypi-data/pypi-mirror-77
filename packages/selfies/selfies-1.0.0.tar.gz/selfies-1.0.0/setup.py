08aadff5bedfa310635cf2f8b46ff1b9e3775635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package selfies/1.0.0/selfies-1.0.0.tar.gz

---
+++ packages/selfies/selfies-1.0.0.tar.gz/selfies-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selfies",
+    version="1.0.0",
+    author="Mario Krenn",
+    author_email="mario.krenn@utoronto.ca, alan@aspuru.com",
+    description="SELFIES (SELF-referencIng Embedded Strings) is a "
+                "general-purpose, sequence-based, robust representation of "
+                "semantically constrained graphs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aspuru-guzik-group/selfies",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
