b9b552c7fb17771c9150a7d54b60790083e0834e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:23 +0000

    Add package bdshare/0.1.3/bdshare-0.1.3-py3-none-any.whl

---
+++ packages/bdshare/bdshare-0.1.3-py3-none-any.whl/bdshare/util/vars.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*- 
+
+DSE_URL = "http://dsebd.org"
+DSE_LSP_URL = "http://dsebd.org/latest_share_price_scroll_l.php"
+DSE_DEA_URL = "http://dsebd.org/day_end_archive.php"
+DSE_AGM_URL = "https://www.dsebd.org/Company_AGM.htm"
+DSE_LPE_URL = "https://www.dsebd.org/latest_PE_all.php"
+DSE_NEWS_URL = "https://www.dsebd.org/news_archive_view.php"
+
+DSE_MARKET_INF_URL = "http://dsebd.org/recent_market_information.php"
+DSE_CLOSE_PRICE_URL = "http://dsebd.org/dse_close_price_archive.php"
+
+CSE_LSP_URL = "https://www.cse.com.bd/market/current_price"
