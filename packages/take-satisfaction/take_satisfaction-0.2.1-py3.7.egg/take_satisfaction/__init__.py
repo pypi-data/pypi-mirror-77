2d55741d3021c0af412d9416285e80dbc064afda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package take-satisfaction/0.2.1/take_satisfaction-0.2.1-py3.7.egg

---
+++ packages/take-satisfaction/take_satisfaction-0.2.1-py3.7.egg/take_satisfaction/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Gabriel Salgado, Juliana Guamá, Milo Utsch e Rogers Damas'
+__version__ = '0.2.1'
+__description__ = 'This project calculates the satisfaction score for BLiP chatbots.'
+
+from .run import run
