74ec35573b1147bfa8a4661d2b7c0dce8e0adcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:54 +0000

    Add package landlab/2.2.0/landlab-2.2.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/landlab/landlab-2.2.0-cp38-cp38-macosx_10_9_x86_64.whl/landlab/io/__init__.py
@@ -0,0 +1,30 @@
+"""Modules that read/write ModelGrids from various file formats."""
+from .esri_ascii import (
+    BadHeaderLineError,
+    DataSizeError,
+    KeyTypeError,
+    KeyValueError,
+    MismatchGridDataSizeError,
+    MismatchGridXYLowerLeft,
+    MismatchGridXYSpacing,
+    MissingRequiredKeyError,
+    read_asc_header,
+    read_esri_ascii,
+    write_esri_ascii,
+)
+from .shapefile import read_shapefile
+
+__all__ = [
+    "read_esri_ascii",
+    "read_asc_header",
+    "read_shapefile",
+    "write_esri_ascii",
+    "MissingRequiredKeyError",
+    "KeyTypeError",
+    "DataSizeError",
+    "BadHeaderLineError",
+    "KeyValueError",
+    "MismatchGridDataSizeError",
+    "MismatchGridXYSpacing",
+    "MismatchGridXYLowerLeft",
+]
