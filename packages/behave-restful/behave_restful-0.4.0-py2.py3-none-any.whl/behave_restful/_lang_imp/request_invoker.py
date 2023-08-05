90a112373e50b7f9f0e4853b6f312c5112209ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:17 +0000

    Add package behave-restful/0.4.0/behave_restful-0.4.0-py2.py3-none-any.whl

---
+++ packages/behave-restful/behave_restful-0.4.0-py2.py3-none-any.whl/behave_restful/_lang_imp/request_invoker.py
@@ -0,0 +1,59 @@
+"""
+"""
+
+def send_get(context):
+    """
+    """
+    params = _get_params(context)
+    context.response = context.session.get(
+        context.request_url,
+        params=params
+    )
+
+
+def send_post(context):
+    """
+    """
+    params = _get_params(context)
+    context.response = context.session.post(
+        context.request_url,
+        params=params,
+        json=context.request_json_payload
+    )
+
+
+def send_put(context):
+    """
+    """
+    params = _get_params(context)
+    context.response = context.session.put(
+        context.request_url,
+        params=params,
+        json=context.request_json_payload
+    )
+
+
+def send_patch(context):
+    """
+    """
+    params = _get_params(context)
+    context.response = context.session.patch(
+        context.request_url,
+        params=params,
+        json=context.request_json_payload
+    )
+
+
+def send_delete(context):
+    """
+    """
+    params = _get_params(context)
+    context.response = context.session.delete(
+        context.request_url,
+        params=params
+    )
+
+
+
+def _get_params(context):
+    return context.request_params if hasattr(context, 'request_params') else None
