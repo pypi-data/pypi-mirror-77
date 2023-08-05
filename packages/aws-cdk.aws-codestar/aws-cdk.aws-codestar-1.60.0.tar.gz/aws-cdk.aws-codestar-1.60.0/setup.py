22f538ac976feafcfad96350722ee391d0dab34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:20 +0000

    Add package aws-cdk.aws-codestar/1.60.0/aws-cdk.aws-codestar-1.60.0.tar.gz

---
+++ packages/aws-cdk.aws-codestar/aws-cdk.aws-codestar-1.60.0.tar.gz/aws-cdk.aws-codestar-1.60.0/setup.py
@@ -0,0 +1,60 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-cdk.aws-codestar",
+    "version": "1.60.0",
+    "description": "The CDK Construct Library for AWS::CodeStar",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_codestar",
+        "aws_cdk.aws_codestar._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_codestar._jsii": [
+            "aws-codestar@1.60.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_codestar": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.9.0, <2.0.0",
+        "publication>=0.0.3",
+        "aws-cdk.aws-s3==1.60.0",
+        "aws-cdk.core==1.60.0",
+        "constructs>=3.0.2, <4.0.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
