71dc1843a7f3e956107e3aed46be680ad939cb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package sergepdf/1.2/sergepdf-1.2.tar.gz

---
+++ packages/sergepdf/sergepdf-1.2.tar.gz/sergepdf-1.2/sergepdf/pdf2text.py
@@ -0,0 +1,15 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+def convert():
+    """
+    Convert the given PDF to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
