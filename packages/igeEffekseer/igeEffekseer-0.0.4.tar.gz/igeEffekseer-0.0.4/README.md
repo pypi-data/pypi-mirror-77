32446e4aaa0800abbc978418f73be1419cc6ea0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:50 +0000

    Add package igeEffekseer/0.0.4/igeEffekseer-0.0.4.tar.gz

---
+++ packages/igeEffekseer/igeEffekseer-0.0.4.tar.gz/igeEffekseer-0.0.4/README.md
@@ -0,0 +1,245 @@
+# igeEffekseer
+
+C++ extension Effekseer for 3D and 2D games.
+
+## Before running this tutorial, you have to install igeEffekseer
+	[pip install igeEffekseer]
+    
+
+## The Editor
+Get from link : https://effekseer.github.io/en/download.html
+
+## Tutorial
+https://github.com/indigames/pyxieTutorials/tree/dev/igeEffekseer
+
+## Support
+
+- Effeck : .efk
+- Texture : .pyxi
+
+## APIs
+### Functions
+- static
+	- texture_loader
+		- set the texture loader callback
+		- use igeCore as a helper to load the texture then give back the texture information (width, height, id, has_mipmap)
+		```python
+		textures = {}
+		def f_texture_loader(name, type):
+			print('f_texture_loader - ' + name)
+			tex = core.texture(name)    
+			textures[name] = tex
+			return (tex.width, tex.height, tex.id, tex.numMips > 1)
+
+		igeEffekseer.texture_loader(f_texture_loader)
+		```
+
+- getset
+	```python
+	_particle = igeEffekseer.particle()
+	```
+	- projection_ortho
+		- projection is ortho or perspective
+		- __False__ as default
+		```
+		_particle.projection_ortho = False
+		```
+	- projection_viewport
+		- the viewport for rendering.
+		- __Optional__
+		__(-1.0, -1.0)__ as default => the viewport set to full size
+		```python
+		_particle.projection_viewport = (_w, _h)
+		```
+	- projection_viewsize
+		- the viewsize used for detecting (ortho width and height in Ortho mode) or (aspect ratio in Perspective mode)
+		- __(480.0, 640.0)__ as default
+		```python
+		_particle.projection_ortho = True
+		_particle.projection_viewsize = (_w / 10, _h / 10)
+		```
+
+	- projection_near
+		- projection near
+		- __1.0__ as default
+		```
+		_particle.projection_near = 1.0
+		```
+	- projection_far
+		- projection far
+		- __1000__ as default
+		```
+		_particle.projection_near = 1.0
+		```
+	- projection_fov
+		- projection fov for perspective view
+		- __90__ degree as default
+		```
+		_particle.projection_fov = 90.0
+		```
+	- camera_eye
+		- the camera eye position
+		- __(0.0, 0.0, -20.0)__ as default
+		```python
+		_particle.camera_eye = (10.0, 5.0, -20.0)
+		```
+	- camera_at
+		- the camera target position
+		- __(0.0, 0.0, 0.0)__ as default
+		```python
+		_particle.camera_at = (0.0, 0.0, 0.0)
+		```
+	- camera_up
+		- the camera up
+		- __(0.0, 1.0, 0.0)__ as default
+		```python
+		_particle.camera_up = (0.0, 1.0, 0.0)
+		```
+	- framerate
+		- the desired framerate
+		- __60__ fps as default
+		```python
+		_particle.framerate = 60.0
+		```
+- particle
+	```python
+	_particle = igeEffekseer.particle()
+	```
+	- shoot
+		- update then render the effekseer system
+		- *shoot(delta_time, clean_screen)*
+			- delta_time : float
+			- clean_screen : bool optional
+		```python
+		_particle.shoot(core.getElapsedTime())
+		```
+	- add
+		- add the effect to the effekseer system
+		- *handle = _particle.add(name, loop, position)*
+			- name : string
+				- the effect string path
+			- loop : bool optional
+				- is looping or not
+			- position : tuple
+				- the position optional
+		```python
+		_distortion_handle = _particle.add(name='Simple_Distortion.efk', loop=True, position=(0.0, 0.0, 0.0))
+		```
+	- remove
+		- remove the effect from effekseer system
+		- *_particle.remove(handle)*
+			- handle : int
+				- the effect handle
+		```python
+		_particle.remove(_distortion_hd)
+		```
+	- stop_all_effects
+		- remove | stop all the effect from effekseer system
+		```python
+		_particle.stop_all_effects()
+		```
+	- set_target_location
+		- set target location for the effect handle
+		- *_particle.set_target_location(handle, x, y, z)*
+			- handle : int
+				- the effect handle
+			- x, y, z : float location
+	- get_location
+		- get location from the effect handle
+		- *result = _particle.get_location(handle)*
+			- handle : int
+				- the effect handle
+			- result : tuple location
+	- set_location
+		- set location for the effect handle
+		- *_particle.set_location(handle, x, y, z)*
+			- handle : int
+				- the effect handle
+			- x, y, z : float location
+	- set_rotation
+		- set rotation for the effect handle
+		- *_particle.set_rotation(handle, x, y, z)*
+			- handle : int
+				- the effect handle
+			- x, y, z : float rotation
+	- set_scale
+		- set scale for the effect handle
+		- *_particle.set_scale(handle, x, y, z)*
+			- handle : int
+				- the effect handle
+			- x, y, z : float scale
+	- set_color
+		- set scale for the effect handle
+		- *_particle.set_scale(handle, red, green, blue, alpha)*
+			- handle : int
+				- the effect handle
+			- red, green, blue, alpha : int value channel (0-255)
+	- get_speed
+		- get speed from the effect handle
+		- *result = _particle.get_speed(handle)*
+			- handle : int
+				- the effect handle
+			- result : float speed
+	- set_speed
+		- set speed for the effect handle
+		- *_particle.set_speed(handle, speed)*
+			- handle : int
+				- the effect handle
+			- speed : float value
+	- is_playing
+		- checking the effect is playing
+		- *result = _particle.is_playing(handle)*
+			- handle : int
+				- the effect handle
+			- result : bool. True if the effect is playing
+	- set_pause
+		- pause the effect handle
+		- *_particle.set_pause(handle, paused)*
+			- handle : int
+				- the effect handle
+			- paused : bool value
+	- get_pause
+		- get the pause status from the effect handle
+		- *result = _particle.get_pause(handle)*
+			- handle : int
+				- the effect handle
+			- result : bool value
+	- set_shown
+		- switch between show or hide of the effect handle
+		- *_particle.set_shown(handle, shown)*
+			- handle : int
+				- the effect handle
+			- shown : bool value
+	- get_shown
+		- get the show status from the effect handle
+		- *result = _particle.get_shown(handle)*
+			- handle : int
+				- the effect handle
+			- result : bool value
+	- set_loop
+		- loop (true / false)
+		- *_particle.set_loop(handle, loop)*
+			- handle : int
+				- the effect handle
+			- loop : bool value
+	- get_loop
+		- get the loop status from the effect handle
+		- *result = _particle.get_loop(handle)*
+			- handle : int
+				- the effect handle
+			- result : bool value
+- statistic
+	- drawcall_count
+		- return the drawcall count
+		- *count = _particle.drawcall_count()*
+	- vertex_count
+		- return the draw vertex count
+		- *count = _particle.vertex_count()*
+	- update_time
+		- return the update time take
+		- *time = _particle.update_time()*
+		- time : int (micro second)
+	- draw_time
+		- return the draw time take
+		- *time = _particle.draw_time()*
+		- time : int (micro second)
---
+++ packages/igeEffekseer/igeEffekseer-0.0.4.tar.gz/igeEffekseer-0.0.4/pyxieEffekseer.cpp
@@ -0,0 +1,377 @@
+
+#if defined _WIN32
+#   include <glew.h>
+#   include <gl/gl.h>
+#   include <gl/glu.h>
+#	include <wglew.h>
+#endif
+
+#include "pyxieEffekseer.h"
+#include <string>
+
+TextureLoaderFunc pyxieEffekseer::textureLoaderFunc = nullptr;
+
+class pyxieTextureLoader : public TextureLoader
+{
+public:
+	pyxieTextureLoader();
+	virtual ~pyxieTextureLoader();
+
+public:
+	TextureData* Load(const EFK_CHAR* path, TextureType textureType) override;
+	void Unload(TextureData* data) override;
+};
+
+
+pyxieTextureLoader::pyxieTextureLoader()
+{
+}
+
+pyxieTextureLoader::~pyxieTextureLoader()
+{
+}
+
+static std::u16string getFilenameWithoutExt(const char16_t* path)
+{
+	int start = 0;
+	int end = 0;
+
+	for (int i = start; path[i] != 0; i++)
+	{
+		if (path[i] == u'.')
+		{
+			end = i;
+		}
+	}
+
+	std::vector<char16_t> ret;
+
+	for (int i = start; i < end; i++)
+	{
+		ret.push_back(path[i]);
+	}
+	ret.push_back(0);
+
+	return std::u16string(ret.data());
+}
+
+TextureData* pyxieTextureLoader::Load(const EFK_CHAR* path, TextureType textureType)
+{
+	auto _path = getFilenameWithoutExt(path);	
+
+	char path_[300];
+	ConvertUtf16ToUtf8((int8_t*)path_, 300, (const int16_t*)_path.c_str());
+	if (pyxieEffekseer::textureLoaderFunc != nullptr)
+	{
+		TextureLoaderCallback callback = { path_ , TextureLoaderType::LOAD};
+		auto textureData = pyxieEffekseer::textureLoaderFunc(callback);
+
+		return textureData;
+	}
+	return new TextureData();
+}
+
+void pyxieTextureLoader::Unload(TextureData* data)
+{
+
+}
+
+pyxieEffekseer::pyxieEffekseer()
+	: manager(nullptr)
+	, renderer(nullptr)
+	, desiredFramerate(60.0)
+	, projection_ortho(false)
+	, projection_fov(90)
+	, projection_aspect(480.0 / 640.0)
+	, projection_near(1.0)
+	, projection_far(1000)
+	, camera_eye(Vector3D(0.0, 0.0, -20.0))
+	, camera_at(Vector3D(0.0, 0.0, 0.0))
+	, camera_up(Vector3D(0.0, 1.0, 0.0))
+	, viewport(Vector2D(-1.0, -1.0))
+	, viewsize(Vector2D(480.0, 640.0))
+{
+}
+
+pyxieEffekseer::~pyxieEffekseer()
+{
+	effect_map.clear();
+}
+
+void pyxieEffekseer::init()
+{	
+#if defined(_WIN32)
+	renderer = EffekseerRendererGL::Renderer::Create(8000, EffekseerRendererGL::OpenGLDeviceType::OpenGL3);
+#else
+	renderer = EffekseerRendererGL::Renderer::Create(8000, EffekseerRendererGL::OpenGLDeviceType::OpenGLES3);
+#endif
+	renderer->SetTextureUVStyle(UVStyle::VerticalFlipped);	// adapt with igeCore.texture
+
+	manager = Manager::Create(8000);
+
+	manager->SetSpriteRenderer(renderer->CreateSpriteRenderer());
+	manager->SetRibbonRenderer(renderer->CreateRibbonRenderer());
+	manager->SetRingRenderer(renderer->CreateRingRenderer());
+	manager->SetTrackRenderer(renderer->CreateTrackRenderer());
+	manager->SetModelRenderer(renderer->CreateModelRenderer());
+
+	manager->SetTextureLoader(new pyxieTextureLoader()); //renderer->CreateTextureLoader() || new pyxieTextureLoader()
+	manager->SetModelLoader(renderer->CreateModelLoader());
+	manager->SetMaterialLoader(renderer->CreateMaterialLoader());
+}
+
+void pyxieEffekseer::release()
+{
+	for (auto it = effect_map.begin(); it != effect_map.end(); it++)
+	{
+		(*it).second.effect->Release();
+	}
+	effect_map.clear();
+
+	if (manager != nullptr)
+	{
+		manager->Destroy();
+		manager = nullptr;
+	}
+
+	if (renderer != nullptr)
+	{
+		renderer->Destroy();
+		renderer = nullptr;
+	}
+}
+
+void pyxieEffekseer::update(float dt)
+{
+	if (projection_ortho)
+	{
+		renderer->SetProjectionMatrix(Matrix44().OrthographicRH(viewsize.X, viewsize.Y, projection_near, projection_far));
+		renderer->SetCameraMatrix(Matrix44().LookAtRH(camera_eye, camera_at, camera_up));
+	}
+	else
+	{
+		renderer->SetProjectionMatrix(Matrix44().PerspectiveFovRH(projection_fov / 180.0f * 3.14f, projection_aspect, projection_near, projection_far));
+		renderer->SetCameraMatrix(Matrix44().LookAtRH(camera_eye, camera_at, camera_up));
+	}
+
+	auto it = effect_map.begin();
+	auto it_end = effect_map.end();
+
+	while(it != it_end)
+	{	
+		Handle handle = (*it).first;
+		if (!manager->Exists(handle))
+		{
+			if ((*it).second.isLooping)
+			{
+				auto effect = (*it).second.effect;
+				Handle hd = manager->Play(effect, (*it).second.position);
+				effect_map[hd] = { effect, true, (*it).second.position };
+				
+				(*it).second.isLooping = false;
+				++it;
+
+				effect->AddRef();
+			}
+			else
+			{
+				(*it).second.effect->Release();
+				effect_map.erase(it++);
+			}
+		}
+		else
+		{
+			++it;
+		}
+	}
+
+	manager->Update(desiredFramerate * dt);	
+}
+
+int pyxieEffekseer::play(const char* name, bool loop, const Effekseer::Vector3D& position)
+{
+	EFK_CHAR path[300];
+	ConvertUtf8ToUtf16((int16_t*)path, 300, (const int8_t*)name);
+
+	auto _effect = Effect::Create(manager, path);	
+	int handle = manager->Play(_effect, position);
+
+	effect_map[handle] = { _effect, loop, position};
+	_effect->AddRef();
+	
+	return handle;
+}
+
+void pyxieEffekseer::stop(int handle)
+{
+	auto it = effect_map.find(handle);
+	if (it != effect_map.end())
+	{
+		(*it).second.effect->Release();
+		effect_map.erase(it);
+	}
+
+	if (manager->Exists(handle))
+	{
+		manager->StopEffect(handle);
+	}
+}
+
+void pyxieEffekseer::stopAll()
+{
+	manager->StopAllEffects();
+
+	for (auto it = effect_map.begin(); it != effect_map.end(); it++)
+	{
+		(*it).second.effect->Release();
+	}
+	effect_map.clear();
+}
+
+void pyxieEffekseer::clearScreen()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+void pyxieEffekseer::setup(bool isClear)
+{
+#if defined(__ANDROID__) || TARGET_OS_IPHONE
+    int fbo = 0;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);    
+#endif   
+	
+	if (viewport.X > 0.0 && viewport.Y > 0.0)
+	{
+		glViewport(0, 0, viewport.X, viewport.Y);
+	}
+
+	if (isClear)
+	{
+		clearScreen();
+	}
+}
+
+void pyxieEffekseer::render(bool isClear)
+{
+	setup(isClear);
+
+	renderer->BeginRendering();
+	manager->Draw();
+	renderer->EndRendering();
+}
+
+int32_t pyxieEffekseer::getDrawcallCount()
+{
+	int32_t count = renderer->GetDrawCallCount();
+	renderer->ResetDrawCallCount();
+
+	return count;
+}
+
+int32_t pyxieEffekseer::getDrawVertexCount()
+{
+	int32_t count = renderer->GetDrawVertexCount();
+	renderer->ResetDrawVertexCount();
+
+	return count;
+}
+
+int32_t pyxieEffekseer::getUpdateTime()
+{
+	return manager->GetUpdateTime();
+}
+
+int32_t pyxieEffekseer::getDrawTime()
+{
+	return manager->GetDrawTime();
+}
+
+void pyxieEffekseer::setTextureLoader(TextureLoaderFunc loader)
+{
+	textureLoaderFunc = loader;
+}
+
+void pyxieEffekseer::SetTargetLocation(Handle handle, float x, float y, float z)
+{
+	manager->SetTargetLocation(handle, x, y, z);
+}
+
+const Vector3D& pyxieEffekseer::GetLocation(Handle handle)
+{
+	return manager->GetLocation(handle);
+}
+void pyxieEffekseer::SetLocation(Handle handle, float x, float y, float z)
+{
+	manager->SetLocation(handle, x, y, z);
+}
+
+void pyxieEffekseer::SetRotation(Handle handle, float x, float y, float z)
+{
+	manager->SetRotation(handle, x, y, z);
+}
+
+void pyxieEffekseer::SetScale(Handle handle, float x, float y, float z)
+{
+	manager->SetScale(handle, x, y, z);
+}
+
+void pyxieEffekseer::SetAllColor(Handle handle, Color color)
+{
+	manager->SetAllColor(handle, color);
+}
+
+void pyxieEffekseer::SetSpeed(Handle handle, float speed)
+{
+	manager->SetSpeed(handle, speed);
+}
+
+float pyxieEffekseer::GetSpeed(Handle handle)
+{
+	return manager->GetSpeed(handle);
+}
+
+bool pyxieEffekseer::IsPlaying(Handle handle)
+{
+	return manager->Exists(handle);
+}
+
+void pyxieEffekseer::SetPause(Handle handle, bool paused)
+{
+	manager->SetPaused(handle, paused);
+}
+
+bool pyxieEffekseer::GetPause(Handle handle)
+{
+	return manager->GetPaused(handle);
+}
+
+void pyxieEffekseer::SetShown(Handle handle, bool shown)
+{
+	manager->SetShown(handle, shown);
+}
+
+bool pyxieEffekseer::GetShown(Handle handle)
+{
+	return manager->GetShown(handle);
+}
+
+void pyxieEffekseer::SetLoop(Handle handle, bool loop)
+{
+	auto it = effect_map.find(handle);
+	if (it != effect_map.end())
+	{
+		effect_map[handle].isLooping = loop;
+	}
+}
+
+bool pyxieEffekseer::GetLoop(Handle handle)
+{
+	auto it = effect_map.find(handle);
+	if (it != effect_map.end())
+	{
+		return it->second.isLooping;
+	}
+	return false;
+}
