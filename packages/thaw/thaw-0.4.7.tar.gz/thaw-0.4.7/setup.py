a9ad99cf2aa99b580982b901213ba40ba3ba440a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:19 +0000

    Add package thaw/0.4.7/thaw-0.4.7.tar.gz

---
+++ packages/thaw/thaw-0.4.7.tar.gz/thaw-0.4.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="thaw",
+    version="0.4.7",
+    author="Kate Raskauskas",
+    author_email="kateapault@gmail.com",
+    description="Generates report showing where updates will affect your project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kateapault/pip-thaw",
+    download_url="https://github.com/kateapault/pip-thaw/archive/v0.4.7.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.0',
+    keywords="thaw update freeze pip requirements library manage package development project",
+)
