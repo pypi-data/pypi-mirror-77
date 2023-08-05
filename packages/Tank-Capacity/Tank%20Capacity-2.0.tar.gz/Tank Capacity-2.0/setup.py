aacf1f6f43867f6d56cdc5950b45654f8a8f3cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:40 +0000

    Add package Tank-Capacity/2.0/Tank%20Capacity-2.0.tar.gz

---
+++ packages/Tank-Capacity/Tank%20Capacity-2.0.tar.gz/Tank Capacity-2.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+    name="Tank Capacity", #发布包文件的名称
+    version="2.0", # 发布包的版本号
+    description="2020-08-20 by NORKHE" # 发布包的描述信息# 作者名称等等
+    # py_modules = ['','',''] # 发布包中的模块文件列表
+)
