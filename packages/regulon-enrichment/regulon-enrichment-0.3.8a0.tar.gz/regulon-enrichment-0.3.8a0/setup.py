f1ef3ec7f537d0c5482920a24d79dd2e5801a298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:42 +0000

    Add package regulon-enrichment/0.3.8a0/regulon-enrichment-0.3.8a0.tar.gz

---
+++ packages/regulon-enrichment/regulon-enrichment-0.3.8a0.tar.gz/regulon-enrichment-0.3.8a0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+setuptools.setup(name='regulon-enrichment',
+      version='0.3.8a0',
+      description="""This package leverages pathway information and gene expression data to produce
+        regulon-based protein activity scores""",
+      long_description = """regulon-enrichment leverages pathway information and gene expression data to produce
+        regulon-based protein activity scores""",
+      author='Joseph Estabrook',
+      author_email='estabroj@ohsu.edu',
+      entry_points={'console_scripts':['enrich=enricher.enrich:main']},
+      packages=setuptools.find_packages(
+          exclude=["enricher.tests.*", "enricher.tests", "enricher.plot"]),
+      data_files=[('data',['data/Homo_sapiens.GRCh38.95.gtf.gz',
+                           'data/Mus_musculus.GRCm38.95.gtf.gz',
+                           'data/PathwayCommons9.All.hgnc.sif.gz',
+                           'data/primary_intx_regulon.pkl',
+                           'secondary_intx_regulon.pkl'])],
+      url = 'https://github.com/JEstabrook/regulon-enrichment',
+      classifiers = [
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+      ],
+      python_requires = '>=3.6',
+      install_requires=[
+          'numpy>=1.17.3',
+          'pandas>=0.25.3',
+          'scikit-learn>=0.21.3',
+          'scipy>=1.3.1',
+          'tqdm>=4.38.0',
+          'dill>=0.3.1.1'
+        ]
+     )
