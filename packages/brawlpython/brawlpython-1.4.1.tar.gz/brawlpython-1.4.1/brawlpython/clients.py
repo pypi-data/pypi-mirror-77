75947797637836a736329804e033736af0e5f8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package brawlpython/1.4.1/brawlpython-1.4.1.tar.gz

---
+++ packages/brawlpython/brawlpython-1.4.1.tar.gz/brawlpython-1.4.1/brawlpython/clients.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+
+from .async_init import AsyncInitObject
+from .sessions import AsyncSession, SyncSession
+
+from types import TracebackType
+from typing import (
+    Any,
+    Coroutine,
+    Dict,
+    Generator,
+    Generic,
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    Set,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+)
+
+__all__ = (
+    "AsyncClient",
+    "SyncClient",
+    "make_headers",
+)
+
+
+class AsyncClient(AsyncInitObject):
+    async def __init__(self, token: str) -> None:
+        self.session = await AsyncSession(token)
+
+    async def close(self) -> None:
+        """Close session"""
+        await self.session.close()
+
+    @property
+    def closed(self) -> bool:
+        """Is client session closed.
+        A readonly property.
+        """
+        return self.session.closed
+
+    def __enter__(self) -> None:
+        raise TypeError("Use `async with` instead")
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[TracebackType]) -> None:
+        # __exit__ should exist in pair with __enter__ but never executed
+        pass
+
+    async def __aenter__(self) -> "AsyncClient":
+        return self
+
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.close()
+
+
+class SyncClient:
+    def __init__(self, token: str) -> None:
+        self.session = SyncSession(token)
+
+    def close(self) -> None:
+        """Close session"""
+        self.session.close()
+
+    @property
+    def closed(self) -> bool:
+        """Is client session closed.
+        A readonly property.
+        """
+        return self.session.closed
+
+    def __enter__(self) -> "SyncClient":
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[TracebackType]) -> None:
+        self.close()
+
+    async def __aenter__(self) -> None:
+        raise TypeError("Use `with` instead")
+
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> None:
+        # __aexit__ should exist in pair with __aenter__ but never executed
+        pass
