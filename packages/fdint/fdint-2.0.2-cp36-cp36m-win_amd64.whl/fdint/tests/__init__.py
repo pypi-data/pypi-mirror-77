db5afb4b79e4ef79b8ba92bd63d73f129fcafaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:00 +0000

    Add package fdint/2.0.2/fdint-2.0.2-cp36-cp36m-win_amd64.whl

---
+++ packages/fdint/fdint-2.0.2-cp36-cp36m-win_amd64.whl/fdint/tests/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2015, Scott J Maddox. All rights reserved.
+# Use of this source code is governed by the BSD-3-Clause
+# license that can be found in the LICENSE file.
+
+from .test_fd import *
+from .test_ifd import *
+from .test_gfd import *
+from .test_scfd import *
