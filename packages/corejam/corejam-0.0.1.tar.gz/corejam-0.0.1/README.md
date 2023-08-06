247182bfaa00f600afc50ae53230326a9edd1316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:32 +0000

    Add package corejam/0.0.1/corejam-0.0.1.tar.gz

---
+++ packages/corejam/corejam-0.0.1.tar.gz/corejam-0.0.1/README.md
@@ -0,0 +1,5 @@
+# corejam
+
+[![PyPI](https://img.shields.io/pypi/v/corejam.svg)](https://pypi.python.org/pypi/corejam)
+[![codecov.io](https://codecov.io/github/aaronmussig/corejam/coverage.svg?branch=master)](https://codecov.io/github/aaronmussig/corejam?branch=master)
+[![DOI](https://zenodo.org/badge/290902170.svg)](https://zenodo.org/badge/latestdoi/290902170)
---
+++ packages/corejam/corejam-0.0.1.tar.gz/corejam-0.0.1/corejam/__init__.py
@@ -0,0 +1,28 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+__title__ = 'corejam'
+__description__ = 'corejam'
+__url__ = 'https://github.com/aaronmussig/corejam'
+__version__ = '0.0.1'
+__author__ = 'Aaron Mussig'
+__author_email__ = 'aaronmussig@gmail.com'
+__license__ = 'GPL3'
+__keywords__ = 'corejam'
+__bug_url__ = 'https://github.com/aaronmussig/corejam/issues'
+__doc_url__ = 'https://github.com/aaronmussig/corejam'
+__src_url__ = 'https://github.com/aaronmussig/corejam'
