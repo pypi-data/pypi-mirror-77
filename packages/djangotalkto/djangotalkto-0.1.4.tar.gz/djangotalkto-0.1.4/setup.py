b54cd4bc0d9f86487cb8b2ca6240e71c67ea86ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:40 +0000

    Add package djangotalkto/0.1.4/djangotalkto-0.1.4.tar.gz

---
+++ packages/djangotalkto/djangotalkto-0.1.4.tar.gz/djangotalkto-0.1.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+setup(
+    name="djangotalkto",
+    version='0.1.4',
+    description="A Django app to talk to APIs.",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    url="https://github.com/sirrobot01/djangotalkto",
+    author="Akere Mukhtar",
+    author_email="akeremukhtar10@gmail.com",
+    license="BSD-3-Clause",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    install_requires=[
+        "httpx",
+        "django>=2.2"
+    ],
+    packages=find_packages(),
+    extras_require={
+        'rest': ["djangorestframework"]
+    },
+    project_urls={
+        'Source': 'https://github.com/sirrobot01/djangotalkto',
+    },
+)
