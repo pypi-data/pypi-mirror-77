88ce661b3592f7575ce836f1916af964363df1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:22 +0000

    Add package aa-fleet/1.1.0/aa_fleet-1.1.0-py3-none-any.whl

---
+++ packages/aa-fleet/aa_fleet-1.1.0-py3-none-any.whl/fleet/__init__.py
@@ -0,0 +1,3 @@
+default_app_config = 'fleet.apps.FleetConfig'
+
+__version__ = '1.1.0'
---
+++ packages/aa-fleet/aa_fleet-1.1.0-py3-none-any.whl/fleet/app_settings.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+# put your app settings here
+
+"""
+FLEET_SETTING_ONE = getattr(
+    settings, 
+    'FLEET_SETTING_ONE', 
+    None
+)
+"""
