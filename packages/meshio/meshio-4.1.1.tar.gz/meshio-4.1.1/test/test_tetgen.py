f71e06306d1d24fb2dc30f4239d6a660f640aa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package meshio/4.1.1/meshio-4.1.1.tar.gz

---
+++ packages/meshio/meshio-4.1.1.tar.gz/meshio-4.1.1/test/test_tetgen.py
@@ -0,0 +1,13 @@
+import helpers
+import pytest
+
+import meshio
+
+test_set = [helpers.tet_mesh]
+
+
+@pytest.mark.parametrize("mesh", test_set)
+def test(mesh):
+    helpers.write_read(
+        meshio.tetgen.write, meshio.tetgen.read, mesh, 1.0e-15, extension=".node"
+    )
