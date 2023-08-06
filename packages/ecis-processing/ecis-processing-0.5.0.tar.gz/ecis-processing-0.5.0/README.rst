1e9ba3e4e38567ac76835181d1dfea84ba26ece3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:45 +0000

    Add package ecis-processing/0.5.0/ecis-processing-0.5.0.tar.gz

---
+++ packages/ecis-processing/ecis-processing-0.5.0.tar.gz/ecis-processing-0.5.0/README.rst
@@ -0,0 +1,18 @@
+ECIS Data Processing
+=====================
+
+.. code-block::
+
+    usage: ecis-processing [-h] [-o OUTPUT] DIRECTORY
+
+    Process diagnosis and medication data.
+
+    positional arguments:
+      DIRECTORY             path to input directory
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -o OUTPUT, --output OUTPUT
+                            output file name, defaults to 'processed.csv'
+
+
---
+++ packages/ecis-processing/ecis-processing-0.5.0.tar.gz/ecis-processing-0.5.0/ecis_processing/__main__.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import pathlib
+
+from .diagnosis_data_processor import DiagnosisDataProcessor
+from .medication_data_processor import MedicationDataProcessor
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="ecis-processing",
+        description="Process diagnosis and medication data."
+    )
+    parser.add_argument(
+        "DIRECTORY",
+        type=dir_path,
+        help="path to input directory"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="output file name, defaults to 'processed.csv'",
+        default="processed.csv",
+    )
+    return parser.parse_args()
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return pathlib.Path(path)
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+
+
+def main():
+    args = parse_arguments()
+
+    diagnosis_input = args.DIRECTORY / "Diagnoses" / "STC_dx.json"
+    my_diagnosis_data_processor = DiagnosisDataProcessor(diagnosis_input)
+    diagnoses_df = my_diagnosis_data_processor.get_processed_data(
+        ["dx_code", "dx_name"], "dx"
+    )
+
+    medications_inputs = [
+        args.DIRECTORY / "Medications_Allscripts" / "STC_meds_alls_180710.json",
+        args.DIRECTORY / "Medications_EPIC" / "STC_meds_e_180710.json",
+        args.DIRECTORY / "Medications_Soarian" / "STC_meds_soarian_180710.json",
+    ]
+    my_medication_data_processor = MedicationDataProcessor(*medications_inputs)
+    medications_df = my_medication_data_processor.get_processed_data(
+        ["rx_name", "rx_code", "source", "rx_status"], "rx"
+    )
+
+    # Create new DataFrame from rows and save to csv
+    df_processed = diagnoses_df.merge(medications_df, how="outer", on="patient_id")
+    df_processed.to_csv(args.output)
+    print(f"Done! Processed data saved to {args.output}")
+
+
+if __name__ == "__main__":
+    main()
