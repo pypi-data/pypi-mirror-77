29bbd5151525315d1e91ab205e93adac089e3410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:16 +0000

    Add package pomop/0.1.9/pomop-0.1.9.tar.gz

---
+++ packages/pomop/pomop-0.1.9.tar.gz/pomop-0.1.9/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="pomop",
+    version="0.1.9",
+    description="(Poor man) pomodoro technique for productivity",
+    long_description=open("README.rst").read(),
+    author="Viet Hung Nguyen",
+    author_email="hvn@familug.org",
+    url="https://github.com/hvnsweeting/pomop",
+    packages=["pomop"],
+    license="MIT",
+    classifiers=["Environment :: Console"],
+    entry_points={"console_scripts": ["pomop=pomop.pomop:cli", "pmp=pomop.pomop:cli"]},
+)
