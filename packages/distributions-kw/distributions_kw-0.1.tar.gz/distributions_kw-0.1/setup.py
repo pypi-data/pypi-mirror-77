edf0258944bf3aad900172289486bf20a16f5a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:27 +0000

    Add package distributions-kw/0.1/distributions_kw-0.1.tar.gz

---
+++ packages/distributions-kw/distributions_kw-0.1.tar.gz/distributions_kw-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions_kw',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Tan Ke Wei',
+      author_email = 'keweiUTP@gmail.com',
+      zip_safe=False #so that package can be run directly from a zipped file
+     )
