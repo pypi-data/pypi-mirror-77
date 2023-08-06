eb80f6f797ab6826089a1d095f2cf74a57688767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:41 +0000

    Add package python-sonarqube-api/1.0.5/python-sonarqube-api-1.0.5.tar.gz

---
+++ packages/python-sonarqube-api/python-sonarqube-api-1.0.5.tar.gz/python-sonarqube-api-1.0.5/sonarqube/webservices.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import (
+    API_WEBSERVICES_LIST_ENDPOINT,
+    API_WEBSERVICES_RESPONSE_EXAMPLE_ENDPOINT
+)
+
+
+class SonarQubeWebservices:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def list_web_services(self, include_internals="false"):
+        """
+        List web services
+
+        :param include_internals: Include web services that are implemented for internal use only.
+          Their forward-compatibility is not assured
+        :return:
+        """
+        params = {'include_internals': include_internals}
+
+        resp = self.sonarqube.make_call('get', API_WEBSERVICES_LIST_ENDPOINT, **params)
+        response = resp.json()
+        return response['webServices']
+
+    def web_service_response_example(self, action, controller):
+        """
+        Display web service response example
+
+        :param action: Action of the web service
+        :param controller: Controller of the web service
+        :return:
+        """
+        params = {
+            'action': action,
+            'controller': controller
+        }
+
+        resp = self.sonarqube.make_call('post', API_WEBSERVICES_RESPONSE_EXAMPLE_ENDPOINT, **params)
+        return resp.json()
