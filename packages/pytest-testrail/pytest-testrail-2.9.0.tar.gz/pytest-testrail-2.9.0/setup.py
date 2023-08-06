3799d9015989f0e16aca09d6abb2f6bc3fd2c5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:45 +0000

    Add package pytest-testrail/2.9.0/pytest-testrail-2.9.0.tar.gz

---
+++ packages/pytest-testrail/pytest-testrail-2.9.0.tar.gz/pytest-testrail-2.9.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(fname) as f:
+        return f.read()
+
+
+setup(
+    name='pytest-testrail',
+    description='pytest plugin for creating TestRail runs and adding results',
+    long_description=read_file('README.rst'),
+    version='2.9.0',
+    author='Allan Kilpatrick',
+    author_email='allanklp@gmail.com',
+    url='http://github.com/allankp/pytest-testrail/',
+    packages=[
+        'pytest_testrail',
+    ],
+    package_dir={'pytest_testrail': 'pytest_testrail'},
+    install_requires=[
+        'pytest>=3.6',
+        'requests>=2.20.0',
+    ],
+    include_package_data=True,
+    entry_points={'pytest11': ['pytest-testrail = pytest_testrail.conftest']},
+)
