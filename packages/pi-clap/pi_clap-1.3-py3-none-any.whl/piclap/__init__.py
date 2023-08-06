69f84775d69775511d0d541377f86a18c3b4493e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:43 +0000

    Add package pi-clap/1.3/pi_clap-1.3-py3-none-any.whl

---
+++ packages/pi-clap/pi_clap-1.3-py3-none-any.whl/piclap/__init__.py
@@ -0,0 +1,17 @@
+"""A python package for clap detection
+
+.. module:: pi-clap
+   :platform: All
+   :synopsis: A clap detection python module
+
+.. moduleauthor:: Nikhil John <ceo@jwala.diamonds>
+
+"""
+from ._settings import Settings
+from ._listener import Listener
+from ._processor import SignalProcessor
+
+__project__ = 'pi-clap'
+__version__ = '1.3'
+__author__ = 'Nikhil John'
+__all__ = ['Settings', 'Listener', 'SignalProcessor']
