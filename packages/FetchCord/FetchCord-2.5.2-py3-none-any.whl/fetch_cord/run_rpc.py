4e1ff0ea8efe8711a887de98f42bc8dfbbd4f207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:43 +0000

    Add package FetchCord/2.5.2/FetchCord-2.5.2-py3-none-any.whl

---
+++ packages/FetchCord/FetchCord-2.5.2-py3-none-any.whl/fetch_cord/run_rpc.py
@@ -0,0 +1,384 @@
+# Import cool new rpc module that gives us more control and gets rid of headaches :)
+from pypresence import Presence, exceptions
+import time
+import sys
+import os
+import psutil
+# import info about system
+from fetch_cord.args import parse_args
+from fetch_cord.bash import BashError, exec_bash
+from fetch_cord.testing import gpuid, cpuappid, appid
+from fetch_cord.out import gpuinfo, sysosline, sysosid, cpuinfo, memline
+if os.name != "nt":
+    from fetch_cord.testing import desktopid, termappid, hostappid
+    from fetch_cord.out import packagesline, termid, shellid, kernelline, shell_line, termfontline, \
+        dewmid, termline, lapordesk, hostline, resline, themeline
+elif os.name == "nt":
+    from fetch_cord.out import moboline
+    from fetch_cord.testing import moboid
+
+uptime = psutil.boot_time()
+args = parse_args()
+
+
+def main():
+    if os.name != "nt":
+        if hostline == "" and args.nodistro and args.noshell and args.nohardware:
+            print("ERROR: no hostline is available!")
+            sys.exit(1)
+    # printing info with debug switch
+    if args.debug:
+        print("----run_rpc----\n")
+        print("uptime in epoch: %s" % uptime)
+        print("cpuid: %s" % appid)
+        print("cpuappid: %s" % cpuappid)
+        if os.name != "nt":
+            print("termappid: %s" % termappid)
+            if hostline:
+                print("hostappid: %s" % hostappid)
+            print(packagesline[0])
+    if os.name == "nt":
+        wandowz()
+    else:
+        loonix(memline, packagesline)
+
+def first_connect():
+    try:
+        client_id = appid
+        RPC = Presence(client_id)
+        RPC.connect()
+        print("RPC Connection Successful.")
+    except ConnectionRefusedError:
+        rpc_tryconnect(RPC)
+
+print("Connecting")
+try:
+    time.sleep(5)
+except KeyboardInterrupt:
+    print("Stopping connection.")
+    sys.exit(0)
+# discord uses unix time to interpret time for rich presnse, this is uptime in unix time
+start_time = float(uptime)
+
+
+def rpc_tryconnect(RPC):
+    while True:
+        try:
+            RPC.connect()
+            break
+        except ConnectionRefusedError:
+            print("RPC connection refused (is Discord open?); trying again in 30 seconds")
+            time.sleep(30)
+
+
+
+def rpc_tryclear(RPC):
+    try:
+        RPC.clear(pid=os.getpid())
+    except exceptions.InvalidID:
+        pass
+
+
+def rpc_tryupdate(RPC, state, details, large_image, large_text, small_image, small_text, start):
+    try:
+        RPC.update(state=state, details=details, large_image=large_image,
+                    large_text=large_text, small_image=small_image, small_text=small_text,
+                    start=start)
+    # ConnectionResetError is here to avoid crashing if Discord is still just starting
+    except (ConnectionResetError, exceptions.InvalidID):
+        pass
+
+
+def runmac(client_id):
+    from fetch_cord.testing import devicetype, product, bigicon, ver
+    client_id = '740822755376758944'  # macos appid for discord rpc
+    if args.debug:
+        print("runmac")
+        print("devicetype: %s" % devicetype)
+        print("product %s" % product)
+        print("bigicon: %s" % bigicon)
+        print("ver: %s" % ver)
+        print("uptime: %s" % uptime)
+        print("client_id: %s" % client_id)
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+                state=packagesline[0],  # update state as packages
+                details=kernelline[0],  # update details as kernel
+                large_image=bigicon,  # set icon
+                large_text=sysosline[0],  # set large icon text
+                small_image=devicetype,  # set small image icon
+                small_text=product,  # set small image text
+                start=start_time)
+    if args.time:
+        custom_time()
+    elif args.nohost and args.nohardware and args.noshell:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+def custom_time():
+    ctime = int(args.time)
+    time.sleep(ctime)
+
+
+# cycle
+
+
+def cycle0(packagesline):
+    if args.debug:
+        print("cycle 0")
+    client_id = appid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=packagesline[0],
+               details=kernelline[0],
+               large_image="big",
+               large_text=sysosline[0],
+               small_image=desktopid,
+               small_text=dewmid,
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+
+    if args.time:
+        custom_time()
+    elif args.nohost and args.nohardware and args.noshell:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+# cycle
+
+
+def cycle1(memline):
+    if args.debug:
+        print("cycle 1")
+    client_id = cpuappid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=cpuinfo,
+               details=memline[0],
+               large_image="big",
+               large_text=cpuinfo,
+               small_image=gpuid,
+               small_text=gpuinfo,
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nodistro and args.noshell and args.nohost:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+# cycle
+
+
+def cycle2():
+    if args.debug:
+        print("cycle 2")
+    client_id = termappid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=themeline[0],
+               details=termfontline,
+               large_image="big",
+               large_text=termline[0],
+               small_image=shellid,
+               small_text=shell_line[0],
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nodistro and args.nohardware and args.nohost:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+def cycle3():
+    # if not then forget it
+    if hostline:
+        if args.debug:
+            print("cycle 3")
+        client_id = hostappid
+        RPC = Presence(client_id)
+        rpc_tryconnect(RPC)
+        rpc_tryupdate(RPC,
+                state=resline,
+                details=hostline[0],
+                large_image="big",
+                large_text=hostline[0],
+                small_image=lapordesk,
+                small_text=lapordesk,
+                start=start_time)
+        if args.debug:
+            print("appid: %s" % client_id)
+        if args.time:
+            custom_time()
+        elif args.nodistro and args.nohardware and args.noshell:
+            time.sleep(9999)
+        else:
+            time.sleep(30)
+    # back from whence you came
+    else:
+        loonix(memline, packagesline)
+    rpc_tryclear(RPC)
+
+
+def pause():
+    if args.debug:
+        print("pause_cycle")
+    if args.time:
+        custom_time()
+    else:
+        time.sleep(30)
+
+
+def w_cycle0():
+    if args.debug:
+        print("w_cycle 0")
+    client_id = appid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=sysosline[0],
+               details=memline[0],
+               large_image="big",
+               large_text=sysosline[0],
+               small_image=moboid,
+               small_text=moboline[0],
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nohardware:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+def w_cycle1():
+    if args.debug:
+        print("w_cycle 1")
+    client_id = cpuappid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=cpuinfo,
+               details=gpuinfo,
+               large_image="big",
+               large_text=cpuinfo,
+               small_image=gpuid,
+               small_text=gpuinfo,
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nodistro:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+def check_change(memline, packagesline):
+    neofetchwin = False
+    if os.name == "nt":
+        neofetchwin = os.popen("neofetch --noart").read()
+    else:
+        try:
+            baseinfo = exec_bash("neofetch --stdout --config none")
+        except BashError as e:
+            print("Could not run neofetch, is neofetch installed? %s" % str(e))
+    mem = "Memory:"
+    memline = []
+    packages = "Packages:"
+    packagesline = []
+    if neofetchwin:
+        filepath = "tmp.txt"
+        with open(filepath, 'w') as f:
+            print(neofetchwin, file=f)
+        with open(filepath, 'rt') as f:
+            for line in f:
+                if line.find(mem) != -1:
+                    memline.append(line.rstrip('\n'))
+
+    elif not neofetchwin:
+        filepath = "tmp.txt"
+        with open(filepath, 'w') as f:
+            print(baseinfo, file=f)
+        with open(filepath, 'rt') as f:
+            for line in f:
+                if line.find(mem) != -1:
+                    memline.append(line.rstrip('\n'))
+                if line.find(packages) != -1:
+                    packagesline.append(line.rstrip('\n'))
+
+    try:
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+    except FileNotFoundError:
+        pass
+
+    return loonix(memline, packagesline)
+
+def loonix(memline, packagesline):
+    try:
+        i = 0
+        if i == 0:
+            first_connect()
+        while i < 10:
+            if not args.nodistro and sysosid.lower() != "macos":
+                cycle0(packagesline)
+            if not args.nohardware:
+                cycle1(memline)
+            if not args.noshell:
+                cycle2()
+            if not args.nohost and sysosid.lower() != "macos":
+                cycle3()
+            if sysosid.lower() == "macos":
+                runmac()
+            if args.pause_cycle:
+                pause()
+            i += 1
+        if not args.nohardware and not args.nodistro:
+            check_change(memline, packagesline)
+        else:
+            loonix(memline, packagesline)
+    except (KeyboardInterrupt, ConnectionResetError):
+        if KeyboardInterrupt:
+            print("Closing connection.")
+            sys.exit(0)
+        else:
+            rpc_tryconnect(RPC)
+
+
+def wandowz():
+    try:
+        first_connect()
+        while True:
+            if not args.nodistro:
+                w_cycle0()
+            if not args.nohardware:
+                w_cycle1()
+    except (KeyboardInterrupt, ConnectionResetError):
+        if KeyboardInterrupt:
+            print("Closing connection.")
+            sys.exit(0)
+        else:
+            rpc_tryconnect(RPC)
---
+++ packages/FetchCord/FetchCord-2.5.2-py3-none-any.whl/fetch_cord/testing.py
@@ -0,0 +1,318 @@
+# import shit as usual
+import os
+import sys
+from fetch_cord.args import parse_args
+from fetch_cord.bash import exec_bash
+from fetch_cord.out import cpumodel, cpuvendor, gpuvendor, sysosid
+if os.name != "nt":
+    from fetch_cord.out import wmid, deid, termid, shellid, sysosid, hostline, termline
+elif os.name == "nt":
+    from fetch_cord.out import moboline
+
+# macOS hardwawre
+
+if os.name != "nt":
+    def laporp(product):
+        if product[0:7] == "MacBook":
+            devicetype = "laptop"
+        else:
+            devicetype = "desktop"
+        return devicetype
+
+    def macos():
+        devicetype = "none"
+        ver = os.popen("sw_vers -productVersion").read()
+        product = os.popen("sysctl -n hw.model").read()
+        bigicon = "none"
+        try:
+            bigicon = versions[ver[0:5]]
+        except IndexError:
+            bigicon = "bigslurp"
+        except KeyError:
+            print("Unsupported MacOS version")
+        laporp(product)
+        return product, devicetype, bigicon, ver
+    # this is staying
+    def iUnity():
+        # this is to check wether the user is actually using unity
+        # or using unity as an xdg value to fix issues with electron apps
+        if wmid.lower() == "compiz":
+            desktopid = "unity"
+        else:
+            desktopid = wmid
+        return desktopid
+
+amdcpus = {
+    "ryzen 3": '741153175779803146',
+    "ryzen 5": '741152732756312125',
+    "ryzen 7": '740752899054895105',
+    "ryzen 9": '741152930899427364',
+    "ryzen threadripper": '742075019257184338',
+}
+intelcpus = {
+    "intel i3": '741044208512532570',
+    "intel i5": '741099939198926920',
+    "intel i7": '741100300219187335',
+    "intel i9": '741100622040006719',
+    "intel pentium": '741203845706940467',
+    "intel celeron": '742904581360713849',
+    "pentium": '741203845706940467',
+    "intel core 2 duo": '746527640382603375',
+}
+gpus = {
+    "intel": "intel",
+    "intel(r)": "intel",
+    "nvidia": "nvidia",
+    "amd": "amd",
+    "vmware": "vmware",
+    "virtio": "virtio",
+    # multi GPUs
+    "nvidiaintel": "nvidia-intel",
+    "nvidiaamd": "nvidia-amd",
+    "amdintel": "amd-intel",
+    "nvidiaamdintel": "nvidia-amd-intel",
+}
+distros = {
+    "ubuntu": '740434138036699178',
+    "opensuseleap": '740156532137787433',
+    "arch": '740476198437650473',
+    "arco": '745435867971321866',
+    "artix": '741918141248045107',
+    "fedora": '740485660703719464',
+    "void": '740484961353597039',
+    "gentoo/linux": '740484380652208140',
+    "funtoo": '740484380652208140',
+    "centos": '740483295388631071',
+    "debian": '740490017218232392',
+    "opensusetumbleweed": '742180413132505088',
+    "manjaro": '740614258177605642',
+    "linuxmint": '740633577481568317',
+    "lmde": '741726946588622988',
+    "pop!_os": '740660055925587978',
+    "endeavouros": '740809641545564170',
+    "windows10": '741949889465942099',
+    "windows7": '741952383512346696',
+    "windows8": '741952179488948324',
+    "windows8.1": '741952065294827520',
+    "nixos": '744644133494325329',
+    "instantos": '744784599653285938',
+    "freebsd": '745054697047457822',
+    "funtoo": '740484380652208140',
+    "solus": '745689115944681683',
+    "zorin": '745689865798156379'
+}
+versions = {
+    "10.13": "hsierria",
+    "10.14": "mojave",
+    "10.15": "catalina",
+}
+# window managers
+windowmanagers = {
+    "dwm": "dwm",
+    "i3": "i3",
+    "awesome": "awesome",
+    "enlightenment": "enlightenment",
+    "bspwm": "bspwm",
+    "xmonad": "xmonad",
+    "sway": "sway",
+    "openbox": "opebox", # not a typo i accidently added all the openbox as 'opebox'
+}
+# desktops
+if os.name != "nt":
+    desktops = {
+        "kde": "kde",
+        "plasma": "kde",
+        "xfce": "xfce",
+        "budgie": "budgie",
+        "gnome": "gnome",
+        "deepin": "deepin",
+        "cinnamon": "cinnamon",
+        "mate": "mate",
+        "unity": iUnity(),
+        "aero": "aero",
+        "pantheon": "pantheon",
+    }
+terminals = {
+    "st": '741280043220861030',
+    "kitty": '741285676250824725',
+    "alacritty": '741291339945345045',
+    "xterm": '741287143187546125',
+    "konsole": '741286819676553258',
+    "dolphin": '741286819676553258',
+    "gnome-terminal": '741328861115056160',
+    "cool-retro-term": '741731097498353794',
+    "urxvt": '743246048968835092',
+    "xfce4-terminal": '744332423072055296',
+    "apple_terminal": '744950796298354689',
+    "lxterminal": '745701997503840380',
+    "mate-terminal": '746531413360377856',
+}
+
+
+shells = [
+    "fish",
+    "zsh",
+    "bash",
+    "ruby",
+]
+hosts= {
+    "inspiron": "743970870631858288",
+    "latitude": "743970870631858288",
+    "g3": "743970870631858288",
+    "hp": "743971270395297852",
+    "tuf": '744330890343219262',
+    "asus": "743936082780880928",
+    "asustek": "743936082780880928",
+    "acer": '744326890512318544',
+    "thinkpad": '744326223412461630',
+    "lenovo": '744326223412461630',
+    "gigabyte": '746447043190456561',
+}
+motherboards= {
+    "inspiron": "dell",
+    "latitude": "dell",
+    "g3": "dell",
+    "hp": "hp",
+    "tuf": 'tuf',
+    "asus": "asus",
+    "asustek": "asus",
+    "acer": 'acer',
+    "thinkpad": 'lenovo',
+    "lenovo": 'lenovo',
+    "gigabyte": 'gigabyte',
+}
+
+args = parse_args()
+
+hostlist = ['Acer', 'TUF', 'HP', 'ThinkPad', 'Inspiron', 'Lenovo', 'Latitude', 'G3', 'Asus', 'ASUSTeK', 'Gigabyte']
+hostid = ""
+if os.name != "nt":
+    if hostline:
+        hostsplit = hostline[0].split()
+        hostid = []
+        for line in range(len(hostsplit)):
+            if hostsplit[line] in hostlist:
+                hostid.append(hostsplit[line].rstrip('\n'))
+        try:
+            hostid = hostid[0]
+        except IndexError:
+            hostid = ""
+            pass
+
+moboid = ""
+if os.name == "nt":
+    if moboline:
+        mobosplit = moboline[0].split()
+        moboid = []
+        for line in range(len(mobosplit)):
+            if mobosplit[line] in hostlist:
+                moboid.append(mobosplit[line].rstrip('\n'))
+        try:
+            moboid = moboid[0]
+        except IndexError:
+            moboid = ""
+            pass
+
+
+terminallist = ["st", "kitty", "alacritty", "xterm", "konsole", "gnome-terminal", "cool-retro-term", "urxvt", "xfce4-terminal", "terminator", "tmux"]
+if args.terminal:
+    if args.terminal in terminallist:
+        termid = args.terminal
+        termline[0] = "Terminal: %s" % args.terminal
+    else:
+        print("\nInvalid terminal, only %s are supported.\n"
+              "Please make a github issue if you would like to have your terminal added.\n"
+              "https://github.com/MrPotatoBobx/FetchCord" % terminallist)
+        sys.exit(1)
+
+
+    if args.debug:
+        print("hostsplit: %s" % hostsplit)
+        print("hostid: %s" % hostid)
+
+# bunch of try except blocks to catch keyerrors and tell the enduser that thier distro/others arent supported
+if os.name != "nt":
+    try:
+        termappid = terminals[termid.lower()]
+    except KeyError:
+        print("Unsupported Terminal. contact us on github to resolve this.(Keyerror)")
+        termappid = '745691250186911796'
+
+
+    try:
+        shell = [s for s in shells if shellid in s]
+    except KeyError:
+        print("Unsupported Shell, contact us on guthub to resolve this.(Keyerror)")
+        shell = "unknown"
+
+    try:
+        if sysosid.lower() != "macos":
+            hostappid = hosts[hostid.lower()]
+    except KeyError:
+        print("Unknown Host, contact us on github to resolve this.(Keyerror)")
+        hostappid = "742887089179197462"
+
+    try:
+        if deid != "N/A" and sysosid.lower() != "macos":
+            desktopid = desktops[deid.lower()]
+    except KeyError:
+        print("Unsupported De contact us on github to resolve this.(Keyerror)")
+        desktopid = 'unknown'
+
+    try:
+        if deid == "N/A" and sysosid.lower() != "macos":
+            desktopid = windowmanagers[wmid.lower()]
+    except KeyError:
+        print("Unsupported Wm contact us on github to resolve this.(Keyerror)")
+        desktopid = 'unknown'
+
+try:
+    appid = distros[sysosid.lower()]
+except KeyError:
+    print("Unsupported Distro, contact us on the GitHub page to resolve this.(keyerror)")
+    appid = '742993278143692821'
+
+try:
+    if cpuvendor == "AMD":
+        cpuappid = amdcpus[cpumodel.lower()]
+    elif cpuvendor in ["Intel", "Pentium"]:
+        cpuappid = intelcpus[cpumodel.lower()]
+except KeyError:
+    print("unknown CPU, contact us on github to resolve this.(Keyerror)")
+    cpuappid = '742887089179197462'
+
+try:
+    gpuid = gpus[gpuvendor.lower()]
+except KeyError:
+    print("Unknown GPU, contact us on github to resolve this.(Keyerror)")
+    gpuid = 'unknown'
+
+if os.name == "nt":
+    try:
+        moboid = motherboards[moboid.lower()]
+    except KeyError:
+        print("Unknown Host, contact us on github to resolve this problem.(Keyerror)")
+        moboid = 'unknown'
+
+elif sysosid.lower() == "macos":
+    macos()
+
+
+if args.debug:
+    print("\n----testing.py----")
+    if os.name != "nt":
+        print("----DE/WM----\n")
+        print("deid: %s" % deid)
+        print("wmid: %s" % wmid)
+        print("\n----TERMINAL/SHELL----\n")
+        print("termid: %s" % termid)
+        print("shellid: %s" % shellid)
+        print("\n----HOST INFO----\n")
+        print("hostid: %s" % hostid)
+    elif os.name == "nt":
+        print("moboid: %s" % moboid)
+        print("moboline: %s" % moboline)
+    print("\n----GPU INFO----\n")
+    print("gpuvendor: %s" % gpuvendor)
+    print("\n----CPU INFO----\n")
+    print("cpumodel: %s\n" % cpumodel)
