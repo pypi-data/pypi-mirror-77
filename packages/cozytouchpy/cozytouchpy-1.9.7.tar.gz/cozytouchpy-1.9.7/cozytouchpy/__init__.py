cfabb46a332d4a29b5ebeead76ea482527957863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:57 +0000

    Add package cozytouchpy/1.9.7/cozytouchpy-1.9.7.tar.gz

---
+++ packages/cozytouchpy/cozytouchpy-1.9.7.tar.gz/cozytouchpy-1.9.7/cozytouchpy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""Provides authentification and row access to Cozytouch modules."""
+from .client import CozytouchClient
+from .exception import AuthentificationFailed, CozytouchException, HttpRequestFailed
+
+name = "cozytouchpy"
+__version__ = "1.9.7"
+__all__ = [
+    "CozytouchClient",
+    "AuthentificationFailed",
+    "CozytouchException",
+    "HttpRequestFailed",
+]
