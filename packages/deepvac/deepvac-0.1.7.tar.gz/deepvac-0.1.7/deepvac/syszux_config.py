3172f832e65034648d4a945775a4edb6142164e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:41 +0000

    Add package deepvac/0.1.7/deepvac-0.1.7.tar.gz

---
+++ packages/deepvac/deepvac-0.1.7.tar.gz/deepvac-0.1.7/deepvac/syszux_config.py
@@ -0,0 +1,46 @@
+class AttrDict(dict):
+    def __getattr__(self, key):
+        return self.get(key)
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            self.__dict__[key] = value
+        else:
+            self[key] = value
+
+config = AttrDict()
+config.train = AttrDict()
+config.val = AttrDict()
+config.test = AttrDict()
+
+## ------------------ common ------------------
+config.device = "cuda"
+config.output_dir = "output"
+config.log_dir = "log"
+config.log_every = 10
+
+## ------------------ ddp --------------------
+config.dist_url = "tcp://localhost:27030"
+config.world_size = 1
+
+## ------------------ optimizer  ------------------
+config.lr = 0.01
+config.lr_step = None
+config.lr_factor = 0.1
+config.momentum = 0.9
+config.nesterov = False
+config.weight_decay = None
+
+## -------------------- loader ------------------
+config.num_workers = 3
+
+## ------------------- train ------------------
+config.train.batch_size = 128
+config.epoch_num = 30
+#model save number duriong an epoch
+config.save_num = 5
+
+## ------------------ val ------------------
+config.val.batch_size = None
+
+
