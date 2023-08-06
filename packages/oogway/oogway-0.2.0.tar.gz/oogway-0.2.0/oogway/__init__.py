8d97dc312d347c12edcfb89d2c05f80c33fdc1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package oogway/0.2.0/oogway-0.2.0.tar.gz

---
+++ packages/oogway/oogway-0.2.0.tar.gz/oogway-0.2.0/oogway/__init__.py
@@ -0,0 +1,8 @@
+from .key import Key
+from .validate import validate
+from .convert import convert
+from .operation import operation
+from .fees import get_fees
+
+name = "oogway"
+__version__ = "0.2.0"
