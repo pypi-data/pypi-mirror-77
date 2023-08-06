1895ab82fad5253d35186cdd4c8cf4ff813d8d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:22 +0000

    Add package Kickbase-API/0.0.7/Kickbase_API-0.0.7.tar.gz

---
+++ packages/Kickbase-API/Kickbase_API-0.0.7.tar.gz/Kickbase_API-0.0.7/kickbase_api/models/market.py
@@ -0,0 +1,16 @@
+from kickbase_api.models.base_model import BaseModel
+from kickbase_api.models.market_player import MarketPlayer
+
+
+class Market(BaseModel):
+    closed: bool = None
+    players: [MarketPlayer] = None
+    
+    def __init__(self, d: dict):
+        self._json_transform = {
+            "players": lambda v: [MarketPlayer(v_) for v_ in v]
+        }
+        self._json_mapping = {
+            "c": "closed"
+        }
+        super().__init__(d)
