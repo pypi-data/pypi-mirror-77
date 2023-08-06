aed62c33a8419b90adb6884891ecb737351cd5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package cbw-api-toolbox/2.0.1/cbw_api_toolbox-2.0.1-py3-none-any.whl

---
+++ packages/cbw-api-toolbox/cbw_api_toolbox-2.0.1-py3-none-any.whl/cbw_api_toolbox/__routes__.py
@@ -0,0 +1,13 @@
+"""API routes"""
+
+ROUTE_AGENTS = "/api/v3/agents"
+ROUTE_CVE_ANNOUNCEMENTS = "/api/v3/cve_announcements"
+ROUTE_GROUPS = "/api/v3/groups"
+ROUTE_HOSTS = "/api/v3/hosts"
+ROUTE_IMPORTER = "/api/v2/cbw_scans/scripts"
+ROUTE_NODES = "/api/v3/nodes"
+ROUTE_PING = "/api/v3/ping"
+ROUTE_REMOTE_ACCESSES = "/api/v3/remote_accesses"
+ROUTE_SECURITY_ISSUES = "/api/v3/security_issues"
+ROUTE_SERVERS = "/api/v3/servers"
+ROUTE_USERS = "/api/v3/users"
