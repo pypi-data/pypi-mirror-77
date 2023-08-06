7ed6f48ceda8d8ce8ee81838032116c3af69fa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:41 +0000

    Add package nlpaug/0.0.17/nlpaug-0.0.17-py3-none-any.whl

---
+++ packages/nlpaug/nlpaug-0.0.17-py3-none-any.whl/nlpaug/augmenter/sentence/sentence_augmenter.py
@@ -0,0 +1,27 @@
+import re
+
+from nlpaug.util import Method
+from nlpaug.util.text.tokenizer import Tokenizer
+from nlpaug import Augmenter
+
+
+class SentenceAugmenter(Augmenter):    
+    def __init__(self, action, name='Sentence_Aug', stopwords=None, tokenizer=None, reverse_tokenizer=None,
+                 device='cuda', include_detail=False, verbose=0):
+        super().__init__(
+            name=name, method=Method.SENTENCE, action=action, aug_min=None, aug_max=None, device=device,
+            verbose=verbose, include_detail=include_detail)
+        self.tokenizer = tokenizer or Tokenizer.tokenizer
+        self.reverse_tokenizer = reverse_tokenizer or Tokenizer.reverse_tokenizer
+        self.stopwords = stopwords
+
+    @classmethod
+    def clean(cls, data):
+        return data.strip()
+
+    @classmethod
+    def is_duplicate(cls, dataset, data):
+        for d in dataset:
+            if d == data:
+                return True
+        return False
