f96cadce528ef780dff110b579d74a632ebc3ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:46 +0000

    Add package protox-encoding/0.0.7/protox_encoding-0.0.7.tar.gz

---
+++ packages/protox-encoding/protox_encoding-0.0.7.tar.gz/protox_encoding-0.0.7/setup.py
@@ -0,0 +1,30 @@
+import os
+from distutils.core import Extension, setup
+
+use_cython = bool(
+    os.environ.get('USE_CYTHON', '').strip()
+)
+
+if use_cython:
+    from Cython.Build import cythonize
+
+if use_cython:
+    ext = 'pyx'
+else:
+    ext = 'c'
+
+extension = Extension(
+    name='protox_encoding',
+    sources=['protox_encoding.' + ext]
+)
+
+if use_cython:
+    ext_modules = cythonize(extension)
+else:
+    ext_modules = [extension]
+
+setup(
+    name='protox_encoding',
+    version='0.0.7',
+    ext_modules=ext_modules
+)
