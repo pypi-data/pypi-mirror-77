7fdb8915503edcd3fd8314aa6f5ebec865ecc594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:57 +0000

    Add package MLB-StatsAPI/1.0.1/MLB-StatsAPI-1.0.1.tar.gz

---
+++ packages/MLB-StatsAPI/MLB-StatsAPI-1.0.1.tar.gz/MLB-StatsAPI-1.0.1/README.md
@@ -0,0 +1,15 @@
+# MLB-StatsAPI
+
+Python wrapper for MLB Stats API
+
+Created by Todd Roberts
+
+https://pypi.org/project/MLB-StatsAPI/
+
+Issues: https://github.com/toddrob99/MLB-StatsAPI/issues
+
+Wiki/Documentation: https://github.com/toddrob99/MLB-StatsAPI/wiki
+
+## Copyright Notice
+
+This package and its author are not affiliated with MLB or any MLB team. This API wrapper interfaces with MLB's Stats API. Use of MLB data is subject to the notice posted at http://gdx.mlb.com/components/copyright.txt.
---
+++ packages/MLB-StatsAPI/MLB-StatsAPI-1.0.1.tar.gz/MLB-StatsAPI-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from statsapi import version
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MLB-StatsAPI",
+    version=version.VERSION,
+    author="Todd Roberts",
+    author_email="todd@toddrob.com",
+    description="MLB Stats API Wrapper for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toddrob99/MLB-StatsAPI",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
