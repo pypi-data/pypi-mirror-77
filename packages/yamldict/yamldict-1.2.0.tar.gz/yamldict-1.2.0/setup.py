b21d7630c9cf6a74f945f592aa6582dc1c5bf25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package yamldict/1.2.0/yamldict-1.2.0.tar.gz

---
+++ packages/yamldict/yamldict-1.2.0.tar.gz/yamldict-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+packages = find_packages()
+
+setup(
+    name="yamldict",
+    version="1.2.0",
+    description="yamldict",
+    long_description=readme,
+    author="Bogdan Mustiata",
+    author_email="bogdan.mustiata@gmail.com",
+    license="BSD",
+    install_requires=[
+        "PyYAML==5.1.2",
+    ],
+    packages=packages,
+    package_data={
+        "": ["*.txt", "*.rst"],
+        "yamldict": ["py.typed"],
+    },
+)
