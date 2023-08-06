042f823cf63ee9a1eaf7c985b4ffad0895a47c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:44 +0000

    Add package ufit/1.3.4/ufit-1.3.4.tar.gz

---
+++ packages/ufit/ufit-1.3.4.tar.gz/ufit-1.3.4/ufit/__init__.py
@@ -0,0 +1,31 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# ufit, a universal scattering fitting suite
+#
+# Copyright (c) 2013-2019, Georg Brandl and contributors.  All rights reserved.
+# Licensed under a 2-clause BSD license, see LICENSE.
+# *****************************************************************************
+
+from ufit.version import get_version
+
+__version__ = get_version()
+
+
+class UFitError(Exception):
+    pass
+
+
+import ufit.qt
+import matplotlib
+
+if ufit.qt.QTVER == 4:
+    matplotlib.use('Qt4Agg')
+else:
+    matplotlib.use('Qt5Agg')
+
+
+from ufit.result import *
+from ufit.param import *
+from ufit.data import *
+from ufit.models import *
+from ufit.backends import *
