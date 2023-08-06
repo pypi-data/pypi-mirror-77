7853d2cd5971c2d1598d35eeb33f7fe98a51e9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:19 +0000

    Add package orc/0.1.0/orc-0.1.0-py3-none-any.whl

---
+++ packages/orc/orc-0.1.0-py3-none-any.whl/orc/__init__.py
@@ -0,0 +1,12 @@
+
+
+from .framework.lib.program import singleton_dec
+
+
+__title__ = 'orc'
+__description__ = 'Simple web framework.'
+__author__ = 'Jingping Zhao'
+__author_email__ = '<orange21cn@126.com>'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2014-2020 Jingping Zhao'
