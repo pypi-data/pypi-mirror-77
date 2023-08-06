f5a94aa93dcb9dcc373bd613aff31f7ac42b76e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:41 +0000

    Add package torch-pdegraph/1.1.1/torch_pdegraph-1.1.1.tar.gz

---
+++ packages/torch-pdegraph/torch_pdegraph-1.1.1.tar.gz/torch_pdegraph-1.1.1/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE =  pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+install_requires = [
+    'torch',
+    'numpy',
+    'mnist',
+    'tqdm',
+    'scikit-build',
+    'scipy',
+    'scikit-learn',
+    'scikit-image',
+    'matplotlib',
+    'open3d-python',
+    'opencv-python',
+]
+
+setup(
+    name="torch_pdegraph",
+    version="1.1.1",
+    description="Running paritial difference equations (PDEs) on graphs",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aGIToz/Pytorch_pdegraph",
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    packages=find_packages(),
+    author="Amitoz AZAD",
+    author_email="amitoz.sudo@gmail.com",
+    license="MIT",
+    include_package_data=True,
+    )
