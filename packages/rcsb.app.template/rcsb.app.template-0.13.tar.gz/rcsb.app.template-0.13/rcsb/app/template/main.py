cd6c5457602523833b361efb1bd5d28fd6b0af32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:55 +0000

    Add package rcsb.app.template/0.13/rcsb.app.template-0.13.tar.gz

---
+++ packages/rcsb.app.template/rcsb.app.template-0.13.tar.gz/rcsb.app.template-0.13/rcsb/app/template/main.py
@@ -0,0 +1,54 @@
+##
+# File: main.py
+# Date: 11-Aug-2020
+#
+# Template/skeleton web service application
+#
+##
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "john.westbrook@rcsb.org"
+__license__ = "Apache 2.0"
+
+import logging
+
+from fastapi import FastAPI
+
+from . import ConfigProvider
+from . import LogFilterUtils
+from . import serverStatus
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+# The following mimics the default Gunicorn logging format
+formatter = logging.Formatter("%(asctime)s [%(process)d] [%(levelname)s] [%(module)s.%(funcName)s] %(message)s", "[%Y-%m-%d %H:%M:%S %z]")
+# The following mimics the default Uvicorn logging format
+# formatter = logging.Formatter("%(levelname)s:     %(asctime)s-%(module)s.%(funcName)s: %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = True
+# Apply logging filters -
+lu = LogFilterUtils.LogFilterUtils()
+lu.addFilters()
+# ---
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startupEvent():
+    logger.info("Startup - running application startup placeholder method")
+    cp = ConfigProvider.ConfigProvider()
+    _ = cp.getConfig()
+    _ = cp.getData()
+    #
+
+
+@app.on_event("shutdown")
+def shutdownEvent():
+    logger.info("Shutdown - running application shutdown placeholder method")
+
+
+# Example entry point
+app.include_router(serverStatus.router)
---
+++ packages/rcsb.app.template/rcsb.app.template-0.13.tar.gz/rcsb.app.template-0.13/rcsb/app/template/serverStatus.py
@@ -0,0 +1,40 @@
+##
+# File: serverStatus.py
+# Date: 11-Aug-2020
+#
+##
+# pylint: skip-file
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "john.westbrook@rcsb.org"
+__license__ = "Apache 2.0"
+
+import logging
+from fastapi import APIRouter
+
+from . import ConfigProvider
+from rcsb.utils.io.ProcessStatusUtil import ProcessStatusUtil
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.get("/status", tags=["status"])
+def serverStatus():
+    cp = ConfigProvider.ConfigProvider()
+    cD = cp.getConfig()
+    psU = ProcessStatusUtil()
+    psD = psU.getInfo()
+    return {"msg": "Status is nominal!", "versionNumber": cD["versionNumber"], "status": psD}
+
+
+@router.get("/", tags=["status"])
+def rootServerStatus():
+
+    return {"msg": "Service is up!"}
+
+
+@router.get("/healthcheck", tags=["status"])
+def rootHealthCheck():
+    return "UP"
