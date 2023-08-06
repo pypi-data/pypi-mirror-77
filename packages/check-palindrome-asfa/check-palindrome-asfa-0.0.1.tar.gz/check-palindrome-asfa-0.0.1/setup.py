02ad5811549fcb2474b82e11e6de17f57a8e59f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package check-palindrome-asfa/0.0.1/check-palindrome-asfa-0.0.1.tar.gz

---
+++ packages/check-palindrome-asfa/check-palindrome-asfa-0.0.1.tar.gz/check-palindrome-asfa-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'check-palindrome-asfa',
+    version = '0.0.1',
+    description = "This is a palindrome checking",
+    long_description = "long desc",
+    url = "https://jdfbjdbvkj",
+    author = 'Mohammed Abdul Raheem',
+    keywords = ['paliondrome','python pallindrome'],
+    license = 'MIT',
+    packages = ['is_palindrome_asfa'],
+    install_requires =['']
+)
