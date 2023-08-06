7c06402a471cb23b48cb78134fbe88d86e59344d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:23 +0000

    Add package check-palindrome-anir/0.0.1/check-palindrome-anir-0.0.1.tar.gz

---
+++ packages/check-palindrome-anir/check-palindrome-anir-0.0.1.tar.gz/check-palindrome-anir-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'check-palindrome-anir',
+    version = '0.0.1',
+    description = 'This is a palindrome checking function.',
+    long_description = 'long_description',
+    url = 'https://balajibetadur.github.io',
+    author = 'Balaji Betadur',
+    keywords = ['palindrome','python palindrome','palindrome evaluator'],
+    license = 'MIT',
+    packages = ['is_palindrome_anir'],
+    install_requires = ['']
+
+)
