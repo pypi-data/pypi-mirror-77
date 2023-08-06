9c32e94618d7fac1a4dd852fc3bd903a7eb8d597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package tjax/0.4.2/tjax-0.4.2-py3-none-any.whl

---
+++ packages/tjax/tjax-0.4.2-py3-none-any.whl/tjax/gradient/transforms.py
@@ -0,0 +1,54 @@
+from typing import Any, Generic, Mapping, Optional, Tuple, Union
+
+from chex import Numeric
+from optax import ScaleByAdamState, ScaleState, scale, scale_by_adam
+
+from ..dataclass import dataclass
+from .meta_parameter import MetaParameter
+from .transform import GradientTransformation, Weights
+
+__all__ = ['Scale', 'ScaleByAdam']
+
+
+NumericOrMeta = Union[Numeric, MetaParameter]
+
+
+@dataclass
+class Scale(GradientTransformation[ScaleState, Weights], Generic[Weights]):
+
+    step_size: NumericOrMeta
+
+    def init(self, parameters: Weights) -> ScaleState:
+        return scale(self.step_size).init(parameters)
+
+    def update(self,
+               gradient: Weights,
+               state: ScaleState,
+               parameters: Optional[Weights],
+               meta_parameters: Optional[Mapping[Any, Numeric]] = None) -> (
+                   Tuple[Weights, ScaleState]):
+        x = self.replace_meta_parameters_with_defaults(meta_parameters)
+        return scale(x.step_size).update(gradient, state, parameters, meta_parameters)
+
+
+@dataclass
+class ScaleByAdam(GradientTransformation[ScaleByAdamState, Weights], Generic[Weights]):
+
+    beta1: NumericOrMeta = 0.9
+    beta2: NumericOrMeta = 0.999
+    epsilon: NumericOrMeta = 1e-8
+    epsilon_root: NumericOrMeta = 0.0
+
+    def init(self, parameters: Weights) -> ScaleByAdamState:
+        return scale_by_adam(self.beta1, self.beta2, self.epsilon, self.epsilon_root).init(
+            parameters)
+
+    def update(self,
+               gradient: Weights,
+               state: ScaleByAdamState,
+               parameters: Optional[Weights],
+               meta_parameters: Optional[Mapping[Any, Numeric]] = None) -> (
+                   Tuple[Weights, ScaleState]):
+        x = self.replace_meta_parameters_with_defaults(meta_parameters)
+        return scale_by_adam(x.beta1, x.beta2, x.epsilon, x.epsilon_root).update(
+            gradient, state, parameters, meta_parameters)
