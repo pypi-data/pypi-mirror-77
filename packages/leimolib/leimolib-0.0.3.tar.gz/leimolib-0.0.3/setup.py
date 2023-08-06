7e60c208a55fd66c0b1adfa66f50ec588f803b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:57 +0000

    Add package leimolib/0.0.3/leimolib-0.0.3.tar.gz

---
+++ packages/leimolib/leimolib-0.0.3.tar.gz/leimolib-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="leimolib",
+    version="0.0.3",
+    description="雷默库",
+    author="chenhaoxin",
+    author_email="2055709349@qq.com",
+    py_modules=['leimeimolib\\lm_jqr','leimolib\\log_in','leimolib\\print_logo_lm','leimolib\\print_one_by_one']
+)
