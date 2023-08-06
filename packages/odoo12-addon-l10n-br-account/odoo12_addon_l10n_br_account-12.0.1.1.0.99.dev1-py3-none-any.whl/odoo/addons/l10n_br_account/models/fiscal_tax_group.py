bc7d98412b38ee531d5a585756cfa62a7c98ba51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:04 +0000

    Add package odoo12-addon-l10n-br-account/12.0.1.1.0.99.dev1/odoo12_addon_l10n_br_account-12.0.1.1.0.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-account/odoo12_addon_l10n_br_account-12.0.1.1.0.99.dev1-py3-none-any.whl/odoo/addons/l10n_br_account/models/fiscal_tax_group.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class FiscalTaxGroup(models.Model):
+    _inherit = 'l10n_br_fiscal.tax.group'
+
+    colect_account_id = fields.Many2one(
+        comodel_name='account.account',
+        domain=[('deprecated', '=', False)],
+        string='Tax Account',
+        company_dependent=True,
+        ondelete='restrict',
+    )
+
+    recover_account_id = fields.Many2one(
+        comodel_name='account.account',
+        domain=[('deprecated', '=', False)],
+        string='Tax Account on Credit Notes',
+        company_dependent=True,
+        ondelete='restrict',
+    )
+
+    @api.multi
+    def account_tax_group(self):
+        self.ensure_one()
+        return self.env['account.tax.group'].search(
+            [('fiscal_tax_group_id', 'in', self.ids)], limit=1)
