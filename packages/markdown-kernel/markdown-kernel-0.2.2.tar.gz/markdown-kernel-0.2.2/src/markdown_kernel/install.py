41247e11d9741e873618925c877fff673bec8196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package markdown-kernel/0.2.2/markdown-kernel-0.2.2.tar.gz

---
+++ packages/markdown-kernel/markdown-kernel-0.2.2.tar.gz/markdown-kernel-0.2.2/src/markdown_kernel/install.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+import json
+import os
+import sys
+import argparse
+import shutil
+import logging
+
+from jupyter_client.kernelspec import KernelSpecManager
+from IPython.utils.tempdir import TemporaryDirectory
+
+kernel_json = {
+    "argv": [
+        sys.executable, "-m", "markdown_kernel.kernel", "-f",
+        "{connection_file}"
+    ],
+    "display_name": "Markdown",
+    "language": "markdown",
+}
+
+
+def _is_root():
+    try:
+        return os.geteuid() == 0
+    except AttributeError:
+        return False  # assume not an admin on non-Unix platforms
+
+
+def install_markdown_kernel_spec(args):
+    user = False
+    prefix = None
+    if args.sys_prefix:
+        prefix = sys.prefix
+    elif args.prefix:
+        prefix = args.prefix
+    elif args.user or not _is_root():
+        user = True
+
+    with TemporaryDirectory() as td:
+        os.chmod(td, 0o755)  # Starts off as 700, not user readable
+        with open(os.path.join(td, 'kernel.json'), 'w') as f:
+            json.dump(kernel_json, f, sort_keys=True)
+        # Copy resources once they're specified
+        # shutil.copy(os.path.join(os.path.split(__file__)[
+        #             0], 'kernel.js'), os.path.join(td, 'kernel.js'))
+        # Copy resources once they're specified
+        shutil.copy(
+            os.path.join(os.path.split(__file__)[0], 'logo-64x64.png'),
+            os.path.join(td, 'logo-64x64.png'))
+
+        KS = KernelSpecManager()
+        KS.log.setLevel(logging.ERROR)
+        KS.install_kernel_spec(td, 'markdown', user=user, prefix=prefix)
+        print('markdown jupyter kernel spec is installed')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Install KernelSpec for markdown Kernel')
+    prefix_locations = parser.add_mutually_exclusive_group()
+    prefix_locations.add_argument(
+        '--user',
+        help='Install KernelSpec in user homedirectory',
+        action='store_true')
+    prefix_locations.add_argument(
+        '--sys-prefix',
+        help='Install KernelSpec in sys.prefix. Useful in conda / virtualenv',
+        action='store_true',
+        dest='sys_prefix')
+    prefix_locations.add_argument(
+        '--prefix', help='Install KernelSpec in this prefix', default=None)
+    args = parser.parse_args()
+    install_markdown_kernel_spec(args)
---
+++ packages/markdown-kernel/markdown-kernel-0.2.2.tar.gz/markdown-kernel-0.2.2/src/markdown_kernel/kernel.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+from ipykernel.kernelbase import Kernel
+from ._version import __version__
+
+
+class markdown_kernel(Kernel):
+    implementation = 'markdown'
+    implementation_version = __version__
+    banner = "markdown kernel"
+    help_links = 'http://github.com/vatlab/markdown_kernel'
+    language = 'markdown'
+    language_info = {
+        'mimetype': 'text/markdown',
+        'name': 'Markdown',
+        'file_extension': '.md',
+        'pygments_lexer': 'markdown',
+        'codemirror_mode': 'markdown',
+    }
+
+    def do_execute(self,
+                   code,
+                   silent,
+                   store_history=True,
+                   user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            self.send_response(self.iopub_socket, 'display_data', {
+                'metadata': {},
+                'data': {
+                    'text/markdown': code
+                }
+            })
+        return {
+            'status': 'ok',
+            'payload': [],
+            'execution_count': self.execution_count,
+            'user_expressions': {}
+        }
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=markdown_kernel)
