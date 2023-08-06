8edbf989dde2b6cae3a13a37d811e5a55ae5ea7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:02 +0000

    Add package BsbClaci/0.0.1/BsbClaci-0.0.1.tar.gz

---
+++ packages/BsbClaci/BsbClaci-0.0.1.tar.gz/BsbClaci-0.0.1/README.txt
@@ -0,0 +1,3 @@
+This is a simple Python Library.
+This is just a basic calculator for regular use.
+More mathematical functions will be updated soon.
---
+++ packages/BsbClaci/BsbClaci-0.0.1.tar.gz/BsbClaci-0.0.1/__init__.py
@@ -0,0 +1,9 @@
+def add(num1,num2):
+    return "The addition result is "+str(num1+num2)
+def subst(num1,num2):
+    return "The substraction result is "+str(num1-num2)
+def mult(num1,num2):
+    return "The multiplication result is "+str(num1*num2)
+def divide(num1,num2):
+    return "The division result is "+str(num1/num2)
+
