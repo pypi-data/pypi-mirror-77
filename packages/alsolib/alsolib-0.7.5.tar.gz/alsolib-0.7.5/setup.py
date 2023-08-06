62637a9cdb9795e8b3a69d48d7dcaebb01806b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:18 +0000

    Add package alsolib/0.7.5/alsolib-0.7.5.tar.gz

---
+++ packages/alsolib/alsolib-0.7.5.tar.gz/alsolib-0.7.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alsolib",
+    version="0.7.5",
+    author="asunc",
+    author_email="3182305655@qq.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
