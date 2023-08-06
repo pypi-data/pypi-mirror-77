42337307d363fd15cb5d8bbc7a0448cb1a015fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:55 +0000

    Add package python-artmap/0.4.1/python_artmap-0.4.1.tar.gz

---
+++ packages/python-artmap/python_artmap-0.4.1.tar.gz/python_artmap-0.4.1/python_artmap/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Python ARTMAP."""
+
+__author__ = """David Vinicius"""
+__email__ = 'de.175@hotmail.com'
+__version__ = '0.1.0'
+
+from .python_artmap import *
+from .art import ART
+from .art_fuzzy import ARTFUZZY
+from .artmap_fuzzy import ARTMAPFUZZY
