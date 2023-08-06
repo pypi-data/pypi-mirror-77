0fa50bb2a44ef7388e0cf2273f9947f1018c0c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:13 +0000

    Add package GitHubCommitsDownloader/1.3.6/GitHubCommitsDownloader-1.3.6.tar.gz

---
+++ packages/GitHubCommitsDownloader/GitHubCommitsDownloader-1.3.6.tar.gz/GitHubCommitsDownloader-1.3.6/GitHubCommitsDownloader/parseOptions.py
@@ -0,0 +1,66 @@
+import os, argparse
+from .request import request
+
+def toDict(opts):
+	if type(opts) == argparse.Namespace:
+		return vars(opts)
+	else:
+		return opts
+
+def checkUser(username):
+	request()
+
+def parseOptions(options):
+	options = toDict(options)
+	
+	try:
+		root = options.get('workingdir')
+	except:
+		root = os.getcwd()
+	
+	if isinstance(options, str):
+		user, repo, _ = options.split("/")
+		if not user:
+			raise Exception("User is not correctly entered")
+		if not repo:
+			raise Exception("Reposetory is not correctly entered")
+		if _ is not None:
+			raise Exception("Enter only support \"user/repo\" format")
+
+
+	package = options.get("package")
+	if package:
+		user, repo, _ = options.split("/")
+		if not user:
+			raise Exception("User is not correctly entered")
+		if not repo:
+			raise Exception("Reposetory is not correctly entered")
+		if _ is not None:
+			raise Exception("Enter only support \"user/repo\" format")
+
+	else:
+		user = options.get("user")
+		repo = options.get("repo")
+		
+		if not user:
+			raise Exception("User is not correctly entered")
+		if not repo:
+			raise Exception("Reposetory is not correctly entered")
+	
+	userData = request(f"https://api.github.com/users/{user}")
+	if userData.get("message") == "Not Found":
+		raise Exception("User is not Defined")
+
+	repoData = request(f"https://api.github.com/users/{user}/repos")
+	repos = []
+	for _repo in repoData:
+		repos.append(repo.name)
+	
+	if not repo in repos:
+		raise Exception("Entered reposetory is not Defined")
+
+	branch = options.get("branch")
+	if not branch:
+		branch = "all"
+	
+	return root, user, repo, branch
