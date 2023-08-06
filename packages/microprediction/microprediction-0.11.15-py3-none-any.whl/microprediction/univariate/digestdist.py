1831572a191005da08fe8ac3375f63358e885983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:00 +0000

    Add package microprediction/0.11.15/microprediction-0.11.15-py3-none-any.whl

---
+++ packages/microprediction/microprediction-0.11.15-py3-none-any.whl/microprediction/univariate/digestdist.py
@@ -0,0 +1,17 @@
+from microprediction.univariate.distmachine import DistMachine
+from tdigest import TDigest
+
+
+class DigestDist(DistMachine):
+
+    # Default DistMachine used in the SequentialStreamCrawler
+
+    def __init__(self,**ignored):
+        super().__init__()
+        self.digest = TDigest()
+
+    def update(self, value, dt=None, **ignored):
+        self.digest.update(value)
+
+    def inv_cdf(self, p):
+        return self.digest.percentile(100. * p)
