0ea02bcf3c4571a0ba38cbba4827e9855668969f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:51 +0000

    Add package resample/1.0.0/resample-1.0.0-py3-none-any.whl

---
+++ packages/resample/resample-1.0.0-py3-none-any.whl/resample/__init__.py
@@ -0,0 +1,9 @@
+"""
+resample package.
+"""
+
+from . import bootstrap  # noqa
+from . import empirical  # noqa
+from . import jackknife  # noqa
+from . import permutation  # noqa
+from .version import version as __version__  # noqa
