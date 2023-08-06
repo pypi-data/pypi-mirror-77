fca355e59f886bb8478cb73df74de994c0e2a1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package actuarydesk/0.0.1/actuarydesk-0.0.1.tar.gz

---
+++ packages/actuarydesk/actuarydesk-0.0.1.tar.gz/actuarydesk-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(\
+    name='actuarydesk',\
+    version='0.0.1',\
+    author='Arief Anbiya',\
+    author_email='anbarief@live.com',\
+    description='Tools for actuary to model and analyze simple products or for actuarial students to practice.',\
+    url='https://github.com/pypa/actuarydesk',\
+    packages=setuptools.find_packages(),\
+    classifiers=["Programming Language :: Python :: 3",\
+                 "License :: OSI Approved :: MIT License",\
+                 "Operating System :: OS Independent"],\
+    python_requires='>=3.6')
