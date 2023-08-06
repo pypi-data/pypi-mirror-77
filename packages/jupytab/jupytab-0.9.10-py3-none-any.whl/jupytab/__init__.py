935e6244a264c5158afa063d59051dc1a41d1df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:52 +0000

    Add package jupytab/0.9.10/jupytab-0.9.10-py3-none-any.whl

---
+++ packages/jupytab/jupytab-0.9.10-py3-none-any.whl/jupytab/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2019 Capital Fund Management
+# SPDX-License-Identifier: MIT
+
+from .table import Tables
+from .dataframe_table import DataFrameTable
+from .function import Function, Functions
+
+__all__ = [Tables, DataFrameTable, Functions, Function]
