77ee6b5cdf525c2ebcc1cf21203e5e811637e653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:56 +0000

    Add package falcon-oas/0.5.0/falcon_oas-0.5.0-py2.py3-none-any.whl

---
+++ packages/falcon-oas/falcon_oas-0.5.0-py2.py3-none-any.whl/falcon_oas/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .__version__ import __version__  # noqa: F401
+from .factories import OAS  # noqa: F401
+from .middlewares import Middleware  # noqa: F401
+from .request import Request  # noqa: F401
