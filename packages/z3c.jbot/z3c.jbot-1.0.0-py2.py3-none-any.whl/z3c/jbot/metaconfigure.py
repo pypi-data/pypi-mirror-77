88211085360e37d6c76982b9e06f60fde96b2399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package z3c.jbot/1.0.0/z3c.jbot-1.0.0-py2.py3-none-any.whl

---
+++ packages/z3c.jbot/z3c.jbot-1.0.0-py2.py3-none-any.whl/z3c/jbot/metaconfigure.py
@@ -0,0 +1,46 @@
+from zope import component
+from zope import interface
+
+from . import interfaces
+from . import manager
+
+
+def handler(directory, layer):
+    lookup_all = component.getGlobalSiteManager().adapters.lookupAll
+
+    # check if a template manager already exists
+    factories = set(
+        factory
+        for name, factory in lookup_all((layer,), interfaces.ITemplateManager)
+    )
+
+    # this might yield several factories (template managers); we check
+    # if one is registered for exactly our layer
+    base_factories = set()
+    if layer is not interface.Interface:
+        for base in layer.__bases__:
+            for name, factory in lookup_all(
+                (base,), interfaces.ITemplateManager
+            ):
+                base_factories.add(factory)
+
+    try:
+        factory = factories.difference(base_factories).pop()
+    except KeyError:
+        name = directory
+        factory = manager.TemplateManagerFactory(name)
+        component.provideAdapter(
+            factory, (layer,), interfaces.ITemplateManager, name=name
+        )
+
+    template_manager = factory(layer)
+    template_manager.registerDirectory(directory)
+    return template_manager
+
+
+def templateOverridesDirective(_context, directory, layer=interface.Interface):
+    _context.action(
+        discriminator=('jbot', directory, layer),
+        callable=handler,
+        args=(directory, layer),
+    )
