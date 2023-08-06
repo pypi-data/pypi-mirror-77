e745ec2c3cfafe64e8d3ef5fcaa51c3e87fc96ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:50 +0000

    Add package django-reactor/2.1.4b0/django-reactor-2.1.4b0.tar.gz

---
+++ packages/django-reactor/django-reactor-2.1.4b0.tar.gz/django-reactor-2.1.4b0/reactor/json.py
@@ -0,0 +1,34 @@
+from typing import Generator
+import orjson
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+
+
+def loads(text_data):
+    return orjson.loads(text_data)
+
+
+def dumps(obj, indent=False):
+    option = (
+        orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY
+    )
+    if indent:
+        option |= orjson.OPT_INDENT_2
+    return orjson.dumps(obj, default=default, option=option).decode()
+
+
+def default(o):
+    if isinstance(o, models.Model):
+        return o.pk
+
+    if isinstance(o, models.QuerySet):
+        return list(o.values_list('pk', flat=True))
+
+    if isinstance(o, (Generator, set)):
+        return list(o)
+
+    if hasattr(o, '__json__'):
+        return o.__json__()
+
+    return DjangoJSONEncoder().default(o)
