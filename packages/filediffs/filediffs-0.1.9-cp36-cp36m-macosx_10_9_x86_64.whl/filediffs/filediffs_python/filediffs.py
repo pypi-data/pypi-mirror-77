6e9f2c86d71a1fcdbe6a1456fe18842eedf2bb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:14 +0000

    Add package filediffs/0.1.9/filediffs-0.1.9-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/filediffs/filediffs-0.1.9-cp36-cp36m-macosx_10_9_x86_64.whl/filediffs/filediffs_python/filediffs.py
@@ -0,0 +1,17 @@
+from filediffs.filediffs_cython.filediffs_cy import file_diffs_cy
+
+
+def file_diffs(filename_1,
+               filename_2,
+               outpath_lines_present_in_both_files="lines_present_in_both_files.txt",
+               outpath_lines_present_only_in_file1="lines_present_only_in_file1.txt",
+               outpath_lines_present_only_in_file2="lines_present_only_in_file2.txt",
+               verbose=True):
+    # call cython function
+    file_diffs_cy(
+        filename_1=bytes(filename_1, "utf-8"), filename_2=bytes(filename_2, "utf-8"),
+        outpath_lines_present_in_both_files=bytes(outpath_lines_present_in_both_files, "utf-8"),
+        outpath_lines_present_only_in_file1=bytes(outpath_lines_present_only_in_file1, "utf-8"),
+        outpath_lines_present_only_in_file2=bytes(outpath_lines_present_only_in_file2, "utf-8"),
+        verbose=verbose
+    )
