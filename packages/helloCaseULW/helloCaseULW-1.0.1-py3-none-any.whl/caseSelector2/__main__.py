a4f19b8165bbf95a985a9b486cec3771134f67a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:10 +0000

    Add package helloCaseULW/1.0.1/helloCaseULW-1.0.1-py3-none-any.whl

---
+++ packages/helloCaseULW/helloCaseULW-1.0.1-py3-none-any.whl/caseSelector2/__main__.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def main():
+
+    if sys.argv[1] == "-u":
+        print("HELLO WORLD")
+    elif sys.argv[1] == "-l":
+        print("hello world")
+    else:
+        print("Please enter -l or -u as arguments ")
+
+
+if __name__ == "__main__":
+    main()
