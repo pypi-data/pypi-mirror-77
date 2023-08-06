e7948c5a51c694f34a8d6b0a5457bb11074554ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package aws-ci-cd-lambda/3.4.0/aws_ci_cd_lambda-3.4.0.tar.gz

---
+++ packages/aws-ci-cd-lambda/aws_ci_cd_lambda-3.4.0.tar.gz/aws_ci_cd_lambda-3.4.0/HISTORY.md
@@ -0,0 +1,44 @@
+# Release history
+
+#### 3.4.0
+Add md files.
+
+#### 3.3.0
+Dependency update 1.60.0 - 2.0.0.
+
+#### 3.2.0
+Force update AWS CDK dependency version to 1.44.0.
+
+#### 3.1.0
+Added the ability to add custom commands to buildspec.
+
+#### 3.0.1
+Bug fix.
+
+#### 3.0.0
+Moved SSH parameters to a separate class.
+
+#### 2.3.0
+Add ability to specify KMS key for secret decryption.
+
+#### 2.2.0
+Add ability to specify arguments for install and test scripts.
+
+#### 2.1.0
+Add error and throttles alarms. Add ability to specify lambda environment.
+Add reserved concurrent executions.
+
+#### 2.0.1
+Bug fix.
+
+#### 2.0.0
+Refactor. Increase clarity.
+
+#### 1.1.0
+Major refactoring and minor updates and fixes.
+
+#### 1.0.1
+Made use CDK version 1.27.0
+
+#### 1.0.0
+Initial commit.
---
+++ packages/aws-ci-cd-lambda/aws_ci_cd_lambda-3.4.0.tar.gz/aws_ci_cd_lambda-3.4.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+setup(
+    name='aws_ci_cd_lambda',
+    version='3.4.0',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    packages=find_packages(exclude=['venv', 'test']),
+    description=(
+        'AWS CDK package that helps deploying a lambda function.'
+    ),
+    long_description=README + '\n\n' + HISTORY,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[
+        # AWS CDK dependencies.
+        'aws_cdk.core>=1.60.0,<2.0.0',
+        'aws_cdk.aws_iam>=1.60.0,<2.0.0',
+        'aws_cdk.custom_resources>=1.60.0,<2.0.0',
+        'aws_cdk.aws_lambda>=1.60.0,<2.0.0',
+        'aws_cdk.aws_codecommit>=1.60.0,<2.0.0',
+        'aws_cdk.aws_codebuild>=1.60.0,<2.0.0',
+        'aws_cdk.aws_codepipeline>=1.60.0,<2.0.0',
+        'aws_cdk.aws_codepipeline_actions>=1.60.0,<2.0.0',
+        'aws_cdk.aws_ec2>=1.60.0,<2.0.0',
+
+        # Other dependencies.
+        'aws-empty-bucket>=2.0.0,<3.0.0'
+    ],
+    author='Deividas Tamkus',
+    author_email='dtamkus@gmail.com',
+    keywords='AWS CDK Ci/Cd Lambda Pipeline',
+    url='https://github.com/idenfy/AwsCiCdLambda.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
