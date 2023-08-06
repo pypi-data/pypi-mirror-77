62df71677ba9a1fceab86f2634ecc67b031b204e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:26 +0000

    Add package django-compat-patcher/0.8/django-compat-patcher-0.8.tar.gz

---
+++ packages/django-compat-patcher/django-compat-patcher-0.8.tar.gz/django-compat-patcher-0.8/src/django_compat_patcher/default_settings.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+# Filters applied first, the special value "*" means "all"
+DCP_INCLUDE_FIXER_IDS = "*"
+DCP_INCLUDE_FIXER_FAMILIES = []
+
+# Filters applied last
+DCP_EXCLUDE_FIXER_IDS = ["fix_behaviour_core_management_parser_optparse"]
+DCP_EXCLUDE_FIXER_FAMILIES = []
+
+DCP_PATCH_INJECTED_OBJECTS = "__dcp_injected__"
+
+DCP_LOGGING_LEVEL = "INFO"
+DCP_ENABLE_WARNINGS = True
---
+++ packages/django-compat-patcher/django-compat-patcher-0.8.tar.gz/django-compat-patcher-0.8/src/django_compat_patcher/fixers/django1_11.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from functools import partial
+
+from ..registry import register_django_compatibility_fixer
+
+# for backward-compatibility fixers
+django1_11_bc_fixer = partial(
+    register_django_compatibility_fixer,
+    fixer_reference_version="1.11",
+    fixer_applied_from_version="1.11",
+)
+
+
+@django1_11_bc_fixer()
+def fix_behaviour_widget_build_attrs(utils):
+    """
+    Preserve compatibility with the old signature of Widget.build_attrs(): extra_attrs=None, **kwargs.
+    """
+    from django.forms.widgets import Widget
+
+    # original_build_attrs = Widget.build_attrs  UNUSED
+
+    def build_attrs(self, base_attrs=None, extra_attrs=None, **kwargs):
+        if kwargs:
+            # old version
+            assert not extra_attrs
+            attrs = dict(self.attrs, **kwargs)
+            if base_attrs:  # actually the old "extra_attrs"
+                attrs.update(base_attrs)
+        else:
+            # new version
+            attrs = base_attrs.copy()
+            if extra_attrs is not None:
+                attrs.update(extra_attrs)
+        return attrs
+
+    utils.inject_callable(Widget, "build_attrs", build_attrs)
+
+
+""" CHANGED UTILITY:
+
+def build_attrs(self, extra_attrs=None, **kwargs):
+    "Helper function for building an attribute dictionary."
+    attrs = dict(self.attrs, **kwargs)
+    if extra_attrs:
+        attrs.update(extra_attrs)
+    return attrs
+
+def build_attrs(self, base_attrs, extra_attrs=None):
+    "Helper function for building an attribute dictionary."
+    attrs = base_attrs.copy()
+    if extra_attrs is not None:
+        attrs.update(extra_attrs)
+    return attrs
+"""
