8e879ddb287300c0a8dd1a5c146c5e4adf1ef6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package viztracer/0.3.3/viztracer-0.3.3.tar.gz

---
+++ packages/viztracer/viztracer-0.3.3.tar.gz/viztracer-0.3.3/src/viztracer/modules/snaptrace.h
@@ -0,0 +1,17 @@
+// Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+// For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+#ifndef __SNAPTRACE_H__
+#define __SNAPTRACE_H__
+
+#define SNAPTRACE_MAX_STACK_DEPTH (1 << 0)
+#define SNAPTRACE_INCLUDE_FILES (1 << 1)
+#define SNAPTRACE_EXCLUDE_FILES (1 << 2)
+#define SNAPTRACE_IGNORE_C_FUNCTION (1 << 3)
+
+#define SET_FLAG(reg, flag) ((reg) |= (flag))
+#define UNSET_FLAG(reg, flag) ((reg) &= (~(flag)))
+
+#define CHECK_FLAG(reg, flag) (((reg) & (flag)) != 0) 
+
+#endif
