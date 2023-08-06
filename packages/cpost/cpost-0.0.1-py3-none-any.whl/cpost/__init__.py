c7ab00afce46d70a68cbee1c7a1b418600235214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package cpost/0.0.1/cpost-0.0.1-py3-none-any.whl

---
+++ packages/cpost/cpost-0.0.1-py3-none-any.whl/cpost/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""API/Python lib for Czech addresses
+ 
+Sources:
+    * https://b2c.cpost.cz
+Todo:
+    * More sources
+    * Caching
+"""
+
+import pkg_resources
+from .czechpost import *
+
+try:
+    __version__ = pkg_resources.get_distribution("cpost").version
+except:
+    __version__ = None
