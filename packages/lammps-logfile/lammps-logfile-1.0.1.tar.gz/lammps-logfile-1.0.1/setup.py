47335424502b246f1440c3f071723b9e1a2e672b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package lammps-logfile/1.0.1/lammps-logfile-1.0.1.tar.gz

---
+++ packages/lammps-logfile/lammps-logfile-1.0.1.tar.gz/lammps-logfile-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='lammps-logfile',
+      version='1.0.1',
+      description='Tool to read lammps log files into python data structure',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/henriasv/lammps-logfile',
+      author='Henrik Andersen Sveinsson',
+      author_email='henrik.sveinsson@me.com',
+      license='GNU GPL v3.0',
+      packages=setuptools.find_packages(),
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['pandas', 'numpy', 'matplotlib'],
+      entry_points={
+        'console_scripts': [
+            'lammps_logplotter=lammps_logfile.cmd_interface:run'
+        ]
+      },
+      zip_safe=False)
