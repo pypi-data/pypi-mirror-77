f406858950e78d3ed91940f67253dc90dc08ddd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:11 +0000

    Add package py2json/0.0.1/py2json-0.0.1.tar.gz

---
+++ packages/py2json/py2json-0.0.1.tar.gz/py2json-0.0.1/py2json/inspire/example_fakit_use.py
@@ -0,0 +1,231 @@
+##### Utils #####################################################################################
+## To be able to do partial with positionals too
+
+
+# Explicit version of partial_positionals(isinstance, {1: types})
+from py2json.util import mk_isinstance_cond, mk_scan_mapper
+import numpy as np
+
+isinstance_mapping = mk_isinstance_cond(np.ndarray)
+assert isinstance_mapping([1, 2, 3]) == False
+assert isinstance_mapping(np.array([1, 2, 3])) == True
+
+
+def serialized_attr_dict(obj, serializer, attrs=None):
+    attrs = attrs or dir(obj)
+    return {a: serializer(getattr(obj, a)) for a in attrs}
+
+
+class Struct:
+    def __init__(self, **kwargs):
+        for a, val in kwargs.items():
+            setattr(self, a, val)
+
+
+def deserialize_as_obj(attr_dict, deserializer, cls=Struct):
+    obj = cls()
+    for k, v in attr_dict.items():
+        setattr(obj, k, deserializer(v))
+    return obj
+
+
+##### Use #####################################################################################
+import numpy
+import pandas
+from py2json.fakit import refakit
+
+from py2json.util import is_types_spec, Literal
+from py2json.fakit import is_valid_fak
+from i2.deco import postprocess, preprocess
+
+
+@postprocess(dict)
+def mk_cond_map_from_types_map(types_map):
+    for types, serializer in types_map.items():
+        if is_types_spec(types):
+            types = mk_isinstance_cond(types)
+        assert callable(types), f"types spec should be a callable at this point: {types}"
+        # TODO: Would lead to shorter spec language, but needs "arg injection" of sorts
+        # if isinstance(serializer, (dict, tuple, list)):
+        #     assert is_valid_fak(serializer), f"Should be a valid fak: {serializer}"
+        #     fak_spec = serializer
+        #
+        #     def serializer(x):
+        #         return {'$fak': fak_spec}
+        yield types, serializer
+
+
+def asis(x):
+    return x
+
+
+def mk_serializer_and_deserializer_for_types_map(types_map):
+    cond_map = mk_cond_map_from_types_map(types_map)
+    scan_mapper = mk_scan_mapper(cond_map, dflt=asis)
+
+    def serializer(obj):
+        return scan_mapper(obj)(obj)
+
+    return serializer, refakit
+
+
+# TODO: much to factor out into a mini-language here
+# TODO: See how the specs complexify if we want to use orient='records' kw in DataFrame (de)serialization
+type_cond_map = {
+    numpy.ndarray: lambda x: {'$fak': ('numpy.array', (numpy.ndarray.tolist(x),))},
+    pandas.DataFrame: lambda x: {'$fak': {
+        'f': 'pandas.DataFrame.from_dict',
+        'k': {'data': pandas.DataFrame.to_dict(x, orient='index'),
+              'orient': 'index'}}
+    }
+}
+
+fak_serialize, fak_deserialize = mk_serializer_and_deserializer_for_types_map(type_cond_map)
+
+arr = numpy.array([1, 2, 3])
+serialized_arr = fak_serialize(arr)
+assert serialized_arr == {'$fak': ('numpy.array', ([1, 2, 3],))}
+assert all(fak_deserialize(serialized_arr) == arr)
+
+df = pandas.DataFrame({'foo': [1, 2, 3], 'bar': [10, 20, 30]})
+serialized_df = fak_serialize(df)
+assert serialized_df == {'$fak': {
+    'f': 'pandas.DataFrame.from_dict',
+    'k': {'data': {0: {'foo': 1, 'bar': 10},
+                   1: {'foo': 2, 'bar': 20},
+                   2: {'foo': 3, 'bar': 30}},
+          'orient': 'index'}}}
+assert all(fak_deserialize(serialized_df) == df)
+
+
+##### Now use with sklearn model! #########################################################
+
+
+class GenericEstimator(Struct):
+    fit = None  # or sklearn will complain
+
+
+from functools import partial, wraps
+from i2.footprints import attrs_used_by_method
+import json
+
+
+# def jsonize_output(func):
+#     @wraps(func)
+#     def wrapper(x):
+#         try:
+#             json.dumps()
+
+def mk_serializer_and_deserializer_from_instance_and_methods(
+        instance,
+        methods,
+        deserialize_to_cls=None,
+        jsonize=True
+):
+    cls = instance.__class__
+    cls_has_attr = lambda a: hasattr(cls, a)
+    instance_has_attr = lambda a: hasattr(instance, a)
+
+    @postprocess(set)
+    def get_needed_attrs():
+        for method in filter(cls_has_attr, methods):
+            target_func = getattr(cls, method)
+            for attr in filter(instance_has_attr, attrs_used_by_method(target_func)):
+                yield attr
+
+    deserialize_to_cls = deserialize_to_cls or cls
+    serializer = partial(serialized_attr_dict, serializer=fak_serialize, attrs=get_needed_attrs())
+    deserializer = partial(deserialize_as_obj, deserializer=fak_deserialize, cls=deserialize_to_cls)
+
+    if jsonize:
+        serializer = postprocess(json.dumps, verbose_error_message=2)(serializer)
+        deserializer = preprocess(json.loads)(deserializer)
+
+    return serializer, deserializer
+
+
+#
+#
+# my_serializer = partial(serialized_attr_dict, serializer=fak_serialize)
+# my_deserializer = partial(deserialize_as_obj, deserializer=fak_deserialize, cls=GenericEstimator)
+
+# make something to serialize
+from sklearn.decomposition import PCA
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs()
+model = PCA().fit(X)
+
+serialize, deserialize = mk_serializer_and_deserializer_from_instance_and_methods(
+    model, methods=['predict', 'transform'], deserialize_to_cls=None)
+
+serialized_model = serialize(model)
+deserialized_model = deserialize(serialized_model)
+
+assert isinstance(serialized_model, str)
+if hasattr(model, 'transform'):
+    target_func = model.__class__.transform
+else:
+    target_func = model.__class__.predict
+
+assert numpy.allclose(target_func(deserialized_model, X), target_func(model, X))
+
+
+# and in case you forgot the equivalence:
+# assert (model.transform(X) == target_func(model, X)).all()
+
+
+def mk_serialize_deserialize_pipeline_for_model(model,
+                                                methods=('predict', 'transform'),
+                                                deserialize_to_cls=None):
+    serialize, deserialize = mk_serializer_and_deserializer_from_instance_and_methods(
+        model, methods=methods, deserialize_to_cls=deserialize_to_cls)
+
+    def alt_model(model):
+        return deserialize(serialize(model))
+
+    return alt_model
+
+
+from py2json.inspire.serializing_sklearn_estimators import estimator_test_df
+
+mk_alt_model = lambda model: mk_serialize_deserialize_pipeline_for_model(model)(model)
+
+kwargs_for_behavioral_test_kwargs_for_estimator = dict(
+    mk_alt_model=mk_alt_model,  # Returns an alt object by serializing and deserializing the fitted model
+    methods=('predict', 'transform'),  # The methods to use to make behavior_funcs,
+)
+
+
+def test_estimators_serialization(estimator_classes=None,
+                                  ignore_warnings=True
+                                  ):
+    return estimator_test_df(estimator_classes,
+                             ignore_warnings=ignore_warnings,
+                             **kwargs_for_behavioral_test_kwargs_for_estimator)
+
+
+def get_estimator_classes_that_are_behaviorally_equivalent_wrt_fakit():
+    df = test_estimators_serialization()
+    ok_to_test_classes = tuple(df[df['kind'] == 'ok']['cls'].values)
+    return ok_to_test_classes
+
+
+if __name__ == '__main__':
+    from collections import Counter
+    import pandas as pd
+    from py2json.inspire.serializing_sklearn_estimators import all_estimator_classes
+
+    n = len(all_estimator_classes)
+    print(f"Testing all {n} estimators...")
+    df = test_estimators_serialization()
+
+    c = Counter(df.kind)
+    print(pd.Series(c).to_string())
+
+    print(f"So {c['ok']}/{n} estimators can be automatically created, "
+          f"fitted, json-serialized, and deserialized to an object that is "
+          f"behaviorally equivalent to the original "
+          f"(as far as predict and transform methods are concerned).")
+    print("\nThese are those methods...\n")
+    print(*[x.__name__ for x in df.cls], sep='\t')
---
+++ packages/py2json/py2json-0.0.1.tar.gz/py2json-0.0.1/py2json/inspire/serializing_sklearn_estimators.py
@@ -0,0 +1,323 @@
+from operator import eq
+from functools import partial, wraps
+
+import numpy as np
+from py2json.util import mk_func_to_kwargs_from_a_val_for_argname_map
+
+from sklearn.utils import all_estimators
+from i2.signatures import Sig
+
+all_estimator_classes = tuple([obj for name, obj in all_estimators()])
+
+
+def funcs_that_need_args(funcs, func_to_kwargs=None, self_name=None):
+    """
+    >>> from sklearn.utils import all_estimators
+    >>> estimator_classes = [obj for name, obj in all_estimators()]
+    >>> estimator_names = list(map(lambda x: x.__name__, funcs_that_need_args(all_estimator_classes)))
+    >>> expected = [
+    ... 'ClassifierChain', 'ColumnTransformer', 'FeatureUnion', 'GridSearchCV', 'MultiOutputClassifier',
+    ... 'MultiOutputRegressor', 'OneVsOneClassifier', 'OneVsRestClassifier', 'OutputCodeClassifier',
+    ... 'Pipeline', 'RFE', 'RFECV', 'RandomizedSearchCV', 'RegressorChain', 'SelectFromModel',
+    ... 'SparseCoder', 'StackingClassifier', 'StackingRegressor', 'VotingClassifier', 'VotingRegressor']
+    >>>
+    >>> import sklearn
+    >>> if sklearn.__version__ == '0.23.1':
+    ...      assert estimator_names == expected
+    """
+    for func in funcs:
+        required_args = set(Sig(func).without_defaults.parameters) - {self_name}
+        if func_to_kwargs is not None:
+            required_args -= func_to_kwargs(func).keys()
+        if required_args:
+            yield func
+
+
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.linear_model import ElasticNetCV
+from sklearn.preprocessing import Normalizer
+from scipy.stats import uniform
+from collections import UserDict  # using UserDict instead of dict because want the dict to have a doc
+
+estimator_cls_val_for_argname = {
+    'estimator': ElasticNetCV,
+    'base_estimator': ElasticNetCV,
+    'estimators': (PCA, ElasticNetCV),
+    'transformers': [("norm1", Normalizer(norm='l1'), [0, 1]),
+                     ("norm2", Normalizer(norm='l1'), slice(2, 4))],
+    'transformer_list': [("pca", PCA(n_components=1)),
+                         ("svd", TruncatedSVD(n_components=2))],
+    'steps': [('PCA', PCA), ('ElasticNetCV', ElasticNetCV)],
+    'param_grid': {'kernel': ('linear', 'rbf'), 'C': [1, 10]},
+    'param_distributions': dict(C=uniform(loc=0, scale=4), penalty=['l2', 'l1']),
+}
+
+estimator_cls_val_for_argname = UserDict(estimator_cls_val_for_argname)
+estimator_cls_val_for_argname.__doc__ = """Valid values for default-less arguments of sklearn estimators.
+A dict whose keys are all (but one -- sklearn.decomposition._dict_learning.SparseCoder)
+the default-less arguments of all sklearn estimators and 
+the values provide a valid default for them.
+
+How did you do it, you ask?
+
+Well, I 
+>>> val_for_argname = {}  # a mapping from argname and val I updated at every "iteration"
+>>> # make a func_to_kwargs from it:
+>>> func_to_kwargs = sse.mk_func_to_kwargs_from_a_val_for_argname_map(val_for_argname)
+>>>
+>>> # make a filter function for that func_to_kwargs
+>>> not_covered_by_our_policy = sse.missing_args_func(func_to_kwargs)
+>>>
+>>> # get the next class that's not covered
+>>> not_covered_cls = next(filter(not_covered_by_our_policy, sse.estimator_classes), None)
+>>>
+>>> if not_covered_cls is not None:
+...     from i2.doc_mint import doctest_string_print
+...     doctest_string_print(not_covered_cls)
+>>> # then I read that example code, figured out a valid input, and re-iterated.
+"""
+
+estimator_cls_to_kwargs = mk_func_to_kwargs_from_a_val_for_argname_map(estimator_cls_val_for_argname)
+
+estimator_classes_without_resolved_kwargs = set(funcs_that_need_args(
+    all_estimator_classes, estimator_cls_to_kwargs))
+
+
+def is_behaviorally_equivalent(obj1, obj2, behavior_func, output_comp=eq):
+    """Checks if two objects obj1 and obj2 are behaviorally equivalent,
+    according to behavior func (a callable that will be called on the objects) and
+    output_comp (the function that will be called on the outputs of these calls to
+    decide whether they can be considered equivalent.
+
+    You'll notice that `behavior_func` takes no other input than `obj` (or `other_obj`).
+    Most behaviors depend on other variables you say? We can hardly say that two objects are equivalent
+    if they agree on only one data point you say? Yes, I agree. But `is_behaviorally_equivalent` doesn't rule these out.
+
+    What to do if you want to validate behavioral equivalence like the following?
+    ```python
+    def many_validation_pts():
+        for x, y, z in some_large_set_of_xyz_combinations:
+            yield output_comp(behavior_func(x, y, obj, z), behavior_func(x, y, other_obj, z))
+    assert(all(many_validation_pts()))
+    ```
+
+    Well, instead of loading the `is_behaviorally_equivalent` with `args`, `kwargs`, and `argname_of_obj`
+    to be able to acheive the above, we simply ask the user to encompass it all in the provided `behavior_func` itself,
+    using standard tools such as `functools.partial` or custom decorators.
+    For example:
+
+    ```python
+    from functools import partial
+    def many_validation_pts():
+        for x, y, z in some_large_set_of_xyz_combinations:
+            _behavior_func = partial(behavior_func, x=x, y=y, z=z)
+            yield is_behaviorally_equivalent(obj, other_obj, _behavior_func, output_comp)
+    assert(all(many_validation_pts()))
+    ```
+
+    Suggestions:
+        output_comp=np.allclose or np.isclose
+    """
+    return output_comp(behavior_func(obj1), behavior_func(obj2))
+
+
+def init_params_for_cls(estimator_cls):
+    """Gets valid params to initialize an estimator_cls
+    :param estimator_cls: A (sklearn estimator) class
+    :return: init_params such that estimator_cls(**init_params) is valid
+    """
+    return estimator_cls_to_kwargs(estimator_cls)
+    # raise NotImplementedError("")
+
+
+def xy_for_learner(learner):
+    """Gets valid params to initialize an estimator_cls
+    :param learner: A (sklearn estimator) INSTANCE
+    :return: (X, y) pair such that learner.fit(X, y) works in a meaningful way
+    """
+    raise NotImplementedError("")
+
+
+def xy_and_fitted_model_for_estimator_cls(estimator_cls):
+    """
+    """
+    init_params = init_params_for_cls(estimator_cls)
+    learner = estimator_cls(**init_params)
+    X, y = xy_for_learner(learner)
+    return X, y, learner.fit(X, y)  # assuming sklearn style
+
+
+def compose(func1, func2):
+    sig1 = Sig(func1)
+    sig2 = Sig(func2)
+
+    @sig1
+    def composed_funcs(*args, **kwargs):
+        return func2(func1(*args, **kwargs))
+
+    composed_funcs.__return_annotation__ = sig2.return_annotation
+    return composed_funcs
+
+
+import pickle
+
+dflt_mk_alt_model = compose(pickle.dumps, pickle.loads)
+
+from sklearn.datasets import make_blobs
+
+_X, _y = make_blobs(center_box=(0, 20.0))
+
+
+def dflt_xy_for_learner(learner):
+    """Returns some random (but fixed after first import) (X, y) pair, insensitive to the learner"""
+    return _X, _y
+
+
+def behavioral_test_kwargs_for_estimator(
+        estimator_cls,
+        init_params_for_cls=estimator_cls_to_kwargs,  # Returns valid params to initialize an estimator_cls
+        xy_for_learner=dflt_xy_for_learner,  # Returns an (X, y) pair for the learner to fit on
+        mk_alt_model=dflt_mk_alt_model,  # Returns an alt object by serializing and deserializing the fitted model
+        methods=('predict', 'transform'),  # The methods to use to make behavior_funcs,
+        output_comp=np.allclose
+):
+    """Generate `is_behaviorally_equivalent` kwargs from `estimator_cls`
+
+    :param estimator_cls:
+    :param init_params_for_cls: Returns valid params to initialize an estimator_cls
+    :param xy_for_learner: Returns an (X, y) pair for the learner to fit on
+    :param mk_alt_obj: Returns an alt object by serializing and deserializing the fitted model
+    :param methods: The methods to use to make behavior_funcs
+    :return Generator of (obj, alt_obj, behavior_func, output_comp) tuples
+    """
+    model, X, y = model_X_y(estimator_cls, init_params_for_cls, xy_for_learner)
+    alt_model = mk_alt_model(model)
+    for method in methods:
+        if hasattr(estimator_cls, method):
+            method_func = getattr(estimator_cls, method)
+            yield dict(
+                obj1=model,
+                obj2=alt_model,
+                behavior_func=partial(method_func, X=X),
+                output_comp=output_comp)
+
+
+def model_X_y(estimator_cls, init_params_for_cls, xy_for_learner):
+    estimator_params = init_params_for_cls(estimator_cls)
+    learner = estimator_cls(**estimator_params)
+    X, y = xy_for_learner(learner)
+    model = learner.fit(X, y)
+    return model, X, y
+
+
+from py2json.util import catch_errors
+
+
+def fitting_x_y_works(learner, X, y):
+    try:
+        learner.fit(X, y)
+        return True
+    except Exception as e:
+        return False
+
+
+@catch_errors(on_error=None)
+def cls_to_learner_or_none(cls, init_params_for_cls=init_params_for_cls):
+    return cls(**init_params_for_cls(cls))
+
+
+@catch_errors(on_error=None)
+def fit_or_return_none(learner, X, y):
+    learner.fit(X, y)
+    return learner
+
+
+def fit_learners(learners, X, y, ignore_warnings=True):
+    with warnings.catch_warnings():
+        if ignore_warnings:
+            warnings.simplefilter("ignore")
+        for i, learner in enumerate(learners):
+            try:
+                yield fit_or_return_none(learner, X, y)
+            except Exception as e:
+                print(f"{i}: {learner.__class__.__name__}: {e}")
+                yield None
+
+
+import warnings
+
+
+class ReprControlledTuple(tuple):
+    def __repr__(self):
+        t = super().__repr__()
+        n = len(t)
+        if n > 100:
+            return f"{t[:100]}... ({len(self)} elements)"
+        else:
+            return t
+
+
+def model_X_y_gen(estimator_classes=None,
+                  init_params_for_cls=estimator_cls_to_kwargs,  # Returns valid params to initialize an estimator_cls
+                  xy_for_learner=dflt_xy_for_learner,  # Returns an (X, y) pair for the learner to fit on
+                  on_error='raise'
+                  ):
+    if estimator_classes is None:
+        estimator_classes = all_estimator_classes
+    for cls in estimator_classes:
+        try:
+            yield model_X_y(cls, init_params_for_cls, xy_for_learner)
+        except Exception as e:
+            if on_error == 'raise':
+                raise
+            elif on_error == 'ignore':
+                continue
+            elif on_error == 'yield_context':
+                dict(kind='error_in_model_X_y', cls=cls, error=e)
+            else:
+                raise ValueError(f"Unrecognized on_error: {on_error}")
+
+
+def test_estimators(estimator_classes=None,
+                    ignore_warnings=True,
+                    **kwargs_for_behavioral_test_kwargs_for_estimator):
+    if estimator_classes is None:
+        estimator_classes = all_estimator_classes
+    with warnings.catch_warnings():
+        if ignore_warnings:
+            warnings.simplefilter("ignore")
+
+        for estimator_cls in estimator_classes:
+            try:
+                for kws in behavioral_test_kwargs_for_estimator(estimator_cls,
+                                                                **kwargs_for_behavioral_test_kwargs_for_estimator):
+                    try:
+                        if is_behaviorally_equivalent(**kws):
+                            yield dict(kind='ok', cls=estimator_cls)
+                        else:
+                            yield dict(kind='behaviorally_different', cls=estimator_cls, kws=kws)
+                    except Exception as e:
+                        yield dict(kind='error_in_test', cls=estimator_cls, kws=kws, error=e)
+            except Exception as e:
+                yield dict(kind='error_making_test_kws', cls=estimator_cls, error=e)
+
+
+def estimator_test_df(estimator_classes=None,
+                      ignore_warnings=True,
+                      **kwargs_for_behavioral_test_kwargs_for_estimator
+                      ):
+    import pandas as pd
+    if estimator_classes is None:
+        estimator_classes = all_estimator_classes
+    return pd.DataFrame(list(test_estimators(estimator_classes,
+                                             ignore_warnings,
+                                             **kwargs_for_behavioral_test_kwargs_for_estimator)))
+
+
+def get_estimator_classes_that_are_behaviorally_equivalent_wrt_pickle():
+    df = estimator_test_df()
+    ok_to_test_classes = tuple(df[df['kind'] == 'ok']['cls'].values)
+    len(ok_to_test_classes)
+
+
+from py2json.inspire.example_fakit_use import mk_serializer_and_deserializer_from_instance_and_methods
