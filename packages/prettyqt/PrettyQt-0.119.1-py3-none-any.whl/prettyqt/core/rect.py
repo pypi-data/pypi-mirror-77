8357658fad77dd40373977fc9ae910e7981461e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package prettyqt/0.119.1/PrettyQt-0.119.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.119.1-py3-none-any.whl/prettyqt/core/rect.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtCore
+
+
+class Rect(QtCore.QRect):
+    def __repr__(self):
+        return f"Rect({self.x()}, {self.y()}, {self.width()}, {self.height()})"
---
+++ packages/prettyqt/PrettyQt-0.119.1-py3-none-any.whl/prettyqt/core/rectf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtCore
+
+
+class RectF(QtCore.QRectF):
+    def __repr__(self):
+        return f"RectF({self.x()}, {self.y()}, {self.width()}, {self.height()})"
