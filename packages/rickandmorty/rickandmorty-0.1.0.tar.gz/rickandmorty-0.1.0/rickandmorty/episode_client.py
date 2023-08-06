85c538275a75d4257a150ac2aad356c9551f2b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:15 +0000

    Add package rickandmorty/0.1.0/rickandmorty-0.1.0.tar.gz

---
+++ packages/rickandmorty/rickandmorty-0.1.0.tar.gz/rickandmorty-0.1.0/rickandmorty/episode_client.py
@@ -0,0 +1,39 @@
+import requests
+from rickandmorty.base  import Base
+
+class Episodes(Base):
+    def getAll(self):
+        """
+            Args: No arg necessary
+
+            Returns: All episodes information in the json.
+        """
+        response = requests.get(self.url+'episode/')
+        return response.json()
+    def getSingle(self, episode):
+        """
+            Args: Only one  int value between 1-42.
+
+            Returns: The properties of the given number's defined episode.
+        """
+        response = requests.get(self.url+'episode/{}'.format(episode))
+        return response.json()
+    def getMultiple(self, *episodes):
+        """
+            Args: int values between 1-42
+
+            Returns: The properties of the given numbers' defined episodes.
+        """
+        y = list(episodes)
+        response = requests.get(self.url+'episode/{}'.format(y))
+        return response.json()
+    def filter(self , name='', episode=''):
+        """
+            Args: name =filter by the given name.\n
+            episode=filter by the given episode code.\n
+
+            Returns: Episodes matching the given values.
+        """
+        payload = {'name': name, 'episode': episode}
+        response = requests.get(self.url+'episode/', params=payload)
+        return response.json()
---
+++ packages/rickandmorty/rickandmorty-0.1.0.tar.gz/rickandmorty-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='rickandmorty',
+    author='Dogukan Baloglu',
+    author_email='dogukanbaloglu99@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(include=['rickandmorty'],exclude=['tests*']),
+    version='0.1.0',
+    description='Rick and Morty client for Rick and Morty API (https://rickandmortyapi.com/) ',
+    long_description= readme ,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    install_requires=['requests'],
+    test_require=[''],
+    keywords='rickandmorty',
+    url='https://github.com/DogukanBaloglu/rickandmorty'
+)
