be6b133f9d031698e4f96fd9b6159ec7c6aea545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:12 +0000

    Add package markdown-refdocs/1.4.0/markdown_refdocs-1.4.0-py3-none-any.whl

---
+++ packages/markdown-refdocs/markdown_refdocs-1.4.0-py3-none-any.whl/markdown_refdocs/markdown.py
@@ -0,0 +1,185 @@
+import re
+from typing import Dict, List
+
+from .types import ParsedClass, ParsedFunction, ParsedModule, ParsedVariable, ADMONITIONS
+
+
+def create_type_link(type_name: str, types_links: Dict[str, str] = {}) -> str:
+    tokens = re.split(r'(\[|\]|\s|,)', str(type_name))
+
+    linked = []
+    contains_link = False
+    for token in tokens:
+        if token in types_links:
+            contains_link = True
+            linked.append(f'[{token}]({types_links[token]})')
+        elif token in ['str', 'int', 'bool', 'float']:
+            linked.append(f'`{token}`')
+        elif token in [']', '[']:
+            linked.append(f'\\{token}')
+        else:
+            linked.append(token)
+
+    if not contains_link:
+        return f'`{type_name}`'
+    return ''.join(linked)
+
+
+def argument_md(
+    name: str = '',
+    type: str = None,
+    description: str = '',
+    default_value: str = None,
+    hidden: bool = False,
+    types_links: Dict[str, str] = {},
+    **kwargs,
+) -> str:
+    md = ''
+    if hidden:
+        return ''
+    if type:
+        type = create_type_link(type, types_links)
+
+    if type and not name:
+        md = f'- {type}'
+    elif name:
+        md = f'- {name}'
+        if type:
+            md = f'- {name} ({type})'
+
+    if description:
+        md += f': {description}'
+    return md
+
+
+def admonitions_to_markdown(parsed: Dict[str, List[str]]) -> str:
+    """
+    Generate markdown-style admonitions
+
+    Examples:
+        admonitions_to_markdown({'note': ['make a note of this']})
+
+        admonitions_to_markdown({'warning': ['this is important']})
+    """
+    md: List[str] = []
+    for admonition in ADMONITIONS:
+        if parsed.get(admonition, []):
+            md.append(f'!!! {admonition}')
+            for line in parsed[admonition]:
+                md.append(f'\t{line}')
+            md.append('')
+    return '\n'.join(md)
+
+
+def function_to_markdown(
+    parsed: ParsedFunction, heading_level: int = 2, types_links: Dict[str, str] = {}
+) -> str:
+    """
+    Generate a markdown string for a function definition
+    """
+    if parsed.get('hidden', False):
+        return ''
+    heading = '#' * heading_level
+    name = parsed['name'].replace('_', '\\_')
+    md = [f'{heading} {name}()\n']
+    if parsed.get('description', ''):
+        md.extend([parsed['description'], ''])
+    if parsed.get('source_definition', ''):
+        md.append(f'```python\n{parsed["source_definition"]}\n```\n')
+
+    if parsed.get('parameters', ''):
+        md.append('**Args**\n')
+        for arg in parsed['parameters']:
+            md.append(argument_md(types_links=types_links, **arg))
+        md.append('')
+
+    if parsed.get('returns', ''):
+        md.append('**Returns**\n')
+        md.append(argument_md(types_links=types_links, **parsed['returns']))
+        md.append('')
+
+    if parsed.get('raises', ''):
+        md.append('**Raises**\n')
+        for arg in parsed['raises']:
+            md.append(argument_md(types_links=types_links, **arg))
+        md.append('')
+
+    if parsed.get('examples', ''):
+        md.append('**Examples**\n')
+        for example in parsed['examples']:
+            md.append(f'```python\n{example}\n```\n')
+        md.append('')
+
+    admon_md = admonitions_to_markdown(parsed)
+    if admon_md:
+        md.append(admon_md)
+
+    return '\n'.join(md)
+
+
+def class_to_markdown(parsed: ParsedClass, types_links: Dict[str, str] = {}) -> str:
+    if parsed.get('hidden', False):
+        return ''
+    md = [f'## class {parsed["name"]}', '']
+    if parsed.get('inherits', False):
+        md.extend(
+            [
+                '**inherits** '
+                + ' '.join([create_type_link(t, types_links) for t in parsed['inherits']]),
+                '',
+            ]
+        )
+
+    if parsed.get('description', ''):
+        md.extend([parsed['description'], ''])
+
+    if parsed.get('attributes', ''):
+        md.append('**Attributes**\n')
+        for attr in parsed['attributes']:
+            md.append(argument_md(types_links=types_links, **attr))
+        md.append('')
+
+    admon_md = admonitions_to_markdown(parsed)
+    if admon_md:
+        md.append(admon_md)
+
+    if parsed.get('functions', ''):
+        for func in parsed['functions']:
+            md.append(function_to_markdown(func, heading_level=3, types_links=types_links))
+        md.append('')
+    return '\n'.join(md)
+
+
+def constant_to_markdown(parsed: ParsedVariable, types_links: Dict[str, str] = {}) -> str:
+    md = [f'## {parsed["name"]}', '']
+    if parsed['source_code']:
+        md.append(f'```python\n{parsed["source_code"]}\n```\n')
+
+    if parsed.get('attributes', ''):
+        md.append('**Attributes**\n')
+        for attr in parsed['attributes']:
+            md.append(argument_md(types_links=types_links, **attr))
+        md.append('')
+
+    return '\n'.join(md)
+
+
+def module_to_markdown(parsed: ParsedModule, types_links: Dict[str, str] = {}) -> str:
+    if parsed.get('hidden', False):
+        return ''
+    md = [f'# {parsed["name"]}\n']
+
+    if parsed['description']:
+        md.extend([parsed['description'], ''])
+
+    if parsed['variables']:
+        for variable in parsed['variables']:
+            md.append(constant_to_markdown(variable, types_links=types_links))
+
+    for cls in parsed['classes']:
+        md.append(class_to_markdown(cls, types_links=types_links))
+
+    for func in parsed['functions']:
+        md.append(function_to_markdown(func, types_links=types_links))
+
+    return '\n'.join(md)
---
+++ packages/markdown-refdocs/markdown_refdocs-1.4.0-py3-none-any.whl/tests/test_parsers.py
@@ -0,0 +1,85 @@
+from markdown_refdocs.parsers import parse_google_docstring
+
+
+class TestParseGoogleDocstring:
+    def test_nested_types(self):
+        result = parse_google_docstring(
+            """
+Args:
+    arg2 (List[Dict]): list of records
+"""
+        )
+        assert len(result['parameters']) == 1
+        assert result['parameters'][0]['type'] == 'List[Dict]'
+
+    def test_add_extra_returns_to_description(self):
+        result = parse_google_docstring(
+            """
+Returns:
+    response: the response
+    data (list, dict or None): The ids and type of resource object(s) in this relationship.
+    meta (dict): meta information about the response
+        total (int): total records available in the relationship
+        count (int): total records returned in the response
+        limit (int): the page size
+        offset (int): starting point of the page
+    links (dict): paging links to prev and next page, plus link to current request
+"""
+        )
+        assert (
+            result['returns']['description']
+            == 'the response data (list, dict or None): The ids and type of resource object(s) in this relationship. meta (dict): meta information about the response total (int): total records available in the relationship count (int): total records returned in the response limit (int): the page size offset (int): starting point of the page links (dict): paging links to prev and next page, plus link to current request'
+        )
+
+    def test_long_docstring_with_newlines(self):
+        result = parse_google_docstring(
+            """
+    Split the input text into a prefix and suffix, according to the following patterns:
+
+    If the input string is letters followed by numbers, return them separately in a tuple.
+    If the input string is only letters, return a tuple of ({letters}, None).
+    If the input string is only numbers, return a tuple of (None, {numbers}).
+    Otherwise return a tuple of Nones.
+    Args:
+        text (str): the input to split.
+    Returns:
+        tuple: The result of splitting the input string.
+    Examples:
+        prefix_split('A12345') == ('A', '12345')
+
+        prefix_split('HTMCP_1') == ('HTMCP_', '1')
+
+        prefix_split('ABC') == ('ABC', None)
+
+        prefix_split('12345') == (None, '12345')
+        prefix_split('A123B') == (None, None)"""
+        )
+        expected = """Split the input text into a prefix and suffix, according to the following patterns:
+
+If the input string is letters followed by numbers, return them separately in a tuple.
+If the input string is only letters, return a tuple of ({letters}, None).
+If the input string is only numbers, return a tuple of (None, {numbers}).
+Otherwise return a tuple of Nones."""
+        assert result['description'] == expected
+        assert len(result['examples']) == 4
+
+    def test_todo(self):
+        docstring = """
+validator for pk_select endpoint
+Todo:
+    * validate pk_col exists on table
+    * validate group_by is a column/list of columns on table
+    * validate filter_col exists on table
+    * validate dist_table joins to table?
+"""
+        result = parse_google_docstring(docstring)
+
+        expected = [
+            '* validate pk_col exists on table',
+            '* validate group_by is a column/list of columns on table',
+            '* validate filter_col exists on table',
+            '* validate dist_table joins to table?',
+        ]
+
+        for index, line in enumerate(result['todo']):
+            assert line == expected[index]
