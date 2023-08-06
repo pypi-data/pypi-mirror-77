32ebf5b66cae0c7987c33c227421323a27ede837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:06 +0000

    Add package agi-probability/0.1/agi_probability-0.1.tar.gz

---
+++ packages/agi-probability/agi_probability-0.1.tar.gz/agi_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='agi_probability',
+      version='0.1',
+      description='Package agi_probability',
+      packages=['agi_probability'],
+      zip_safe=False)
