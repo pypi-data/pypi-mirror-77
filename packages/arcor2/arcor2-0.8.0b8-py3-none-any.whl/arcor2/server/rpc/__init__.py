26d88b38961079481cddbff86f0568cd7ef4e567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package arcor2/0.8.0b8/arcor2-0.8.0b8-py3-none-any.whl

---
+++ packages/arcor2/arcor2-0.8.0b8-py3-none-any.whl/arcor2/server/rpc/__init__.py
@@ -0,0 +1,13 @@
+from arcor2.server.rpc import execution
+from arcor2.server.rpc import objects
+from arcor2.server.rpc import project
+from arcor2.server.rpc import robot
+from arcor2.server.rpc import scene
+
+__all__ = [
+    execution.__name__,
+    objects.__name__,
+    project.__name__,
+    robot.__name__,
+    scene.__name__
+]
