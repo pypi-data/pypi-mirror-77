bfe91e87ac250d8493e2df06fb19edf1c0fe0675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package rasode-mei-kaun-tha/0.1.2/rasode_mei_kaun_tha-0.1.2.tar.gz

---
+++ packages/rasode-mei-kaun-tha/rasode_mei_kaun_tha-0.1.2.tar.gz/rasode_mei_kaun_tha-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rasode_mei_kaun_tha", # Replace with your own username
+    version="0.1.2",
+    author="imzain448",
+    author_email="ahmadzain.448@gmail.com",
+    description="the mystery unveiled",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imZain448/dummy_pypi_package",
+    packages=setuptools.find_packages(),
+    package_data = {"":['rasode_mei_kaun_tha/disp.jpg']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe = False,
+    python_requires='>=3.6',
+    install_requires = ['matplotlib'],
+    include_package_data=True
+)
