ed3844ca642c9c51234931da0eed6b61b18e25aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:03 +0000

    Add package classy-json/1.7.2/classy_json-1.7.2-py3-none-any.whl

---
+++ packages/classy-json/classy_json-1.7.2-py3-none-any.whl/classyjson/CustomTypes.py
@@ -0,0 +1,25 @@
+
+def nice(val):
+    if isinstance(val, list):
+        return CustomList(val)
+
+    if isinstance(val, dict):
+        return CustomDict(val)
+
+    return val
+
+
+class CustomList(list):
+    def __init__(self, _list):
+        list.__init__(self, map(nice, _list))
+
+
+class CustomDict(dict):
+    def __init__(self, _dict):
+        dict.__init__(self, {k: nice(v) for k, v, in _dict.items()})
+
+    def __getattr__(self, name):  # CustomDict.a
+        return dict.__getitem__(self, name)
+
+    __setattr__ = __setitem__
+    __delattr__ = __delitem__
