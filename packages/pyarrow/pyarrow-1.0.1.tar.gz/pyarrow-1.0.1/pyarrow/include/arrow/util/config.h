a1956bec3b0e8c0fd002c3d4a323da2b1e477385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:23 +0000

    Add package pyarrow/1.0.1/pyarrow-1.0.1.tar.gz

---
+++ packages/pyarrow/pyarrow-1.0.1.tar.gz/pyarrow-1.0.1/pyarrow/include/arrow/util/config.h
@@ -0,0 +1,37 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#define ARROW_VERSION_MAJOR 2
+#define ARROW_VERSION_MINOR 0
+#define ARROW_VERSION_PATCH 0
+#define ARROW_VERSION ((ARROW_VERSION_MAJOR * 1000) + ARROW_VERSION_MINOR) * 1000 + ARROW_VERSION_PATCH
+
+#define ARROW_VERSION_STRING "2.0.0-SNAPSHOT"
+
+#define ARROW_SO_VERSION "200"
+#define ARROW_FULL_SO_VERSION "200.0.0"
+
+#define ARROW_CXX_COMPILER_ID "AppleClang"
+#define ARROW_CXX_COMPILER_VERSION "11.0.3.11030032"
+#define ARROW_CXX_COMPILER_FLAGS " -Qunused-arguments -fcolor-diagnostics -ggdb -O0"
+
+#define ARROW_GIT_ID "bc01fff4baf2645109a0972d2a276a37ed68f07f"
+#define ARROW_GIT_DESCRIPTION "apache-arrow-1.0.0-142-gbc01fff4b-dirty"
+
+#define ARROW_PACKAGE_KIND ""
+
+/* #undef GRPCPP_PP_INCLUDE */
