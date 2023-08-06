84a312f35162b02a5b05cfa5529b46445112590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:01 +0000

    Add package django-securebox/0.1.5/django_securebox-0.1.5-py2.py3-none-any.whl

---
+++ packages/django-securebox/django_securebox-0.1.5-py2.py3-none-any.whl/django_securebox/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Django SecureBox."""
+
+__author__ = """Henryk Plötz"""
+__email__ = 'henryk@ploetzli.ch'
+__version__ = '0.1.5'
+
+default_app_config = 'django_securebox.apps.DjangoSecureBoxConfig'
