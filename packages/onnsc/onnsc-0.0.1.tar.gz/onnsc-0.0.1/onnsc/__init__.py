7bf12e76b63fab29061be44d8f833d4d376399cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:32 +0000

    Add package onnsc/0.0.1/onnsc-0.0.1.tar.gz

---
+++ packages/onnsc/onnsc-0.0.1.tar.gz/onnsc-0.0.1/onnsc/__init__.py
@@ -0,0 +1,12 @@
+"""Convenience Python module for Smart Check
+## Example
+
+## Installation
+
+## Contact
+* Code: [onnsc](https://github.com/OzNetNerd/onnsc)
+* Blog: [oznetnerd.com](https://oznetnerd.com)
+* Email: [will@oznetnerd.com](mailto:will@oznetnerd.com)
+"""
+
+from .sc import Sc
