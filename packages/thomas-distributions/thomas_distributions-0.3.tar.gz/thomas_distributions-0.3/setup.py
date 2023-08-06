3604077478c5a5fa92b0c38282f1af08e4b4f672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:23 +0000

    Add package thomas-distributions/0.3/thomas_distributions-0.3.tar.gz

---
+++ packages/thomas-distributions/thomas_distributions-0.3.tar.gz/thomas_distributions-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='thomas_distributions',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['thomas_distributions'],
+      zip_safe=False)
