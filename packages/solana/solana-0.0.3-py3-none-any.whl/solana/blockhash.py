09d4736cd4ac3132d17b5fd6b8505f61d48a8b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:45 +0000

    Add package solana/0.0.3/solana-0.0.3-py3-none-any.whl

---
+++ packages/solana/solana-0.0.3-py3-none-any.whl/solana/blockhash.py
@@ -0,0 +1,9 @@
+"""Type for blockhash.
+
+>>> # An arbitrary base58 encoded blockhash:
+>>> Blockhash("EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k")
+'EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k'
+"""
+from typing import NewType
+
+Blockhash = NewType("Blockhash", str)
