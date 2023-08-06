fd6ee0f203f683920946270a5a147d52f46b34db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:42 +0000

    Add package ddtrace/0.34.2/ddtrace-0.34.2-cp27-cp27mu-manylinux1_i686.whl

---
+++ packages/ddtrace/ddtrace-0.34.2-cp27-cp27mu-manylinux1_i686.whl/ddtrace/__init__.py
@@ -0,0 +1,51 @@
+import sys
+
+import pkg_resources
+
+from .monkey import patch, patch_all  # noqa: E402
+from .pin import Pin  # noqa: E402
+from .span import Span  # noqa: E402
+from .tracer import Tracer  # noqa: E402
+from .settings import config  # noqa: E402
+
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    # package is not installed
+    __version__ = None
+
+
+# a global tracer instance with integration settings
+tracer = Tracer()
+
+__all__ = [
+    'patch',
+    'patch_all',
+    'Pin',
+    'Span',
+    'tracer',
+    'Tracer',
+    'config',
+]
+
+
+_ORIGINAL_EXCEPTHOOK = sys.excepthook
+
+
+def _excepthook(tp, value, traceback):
+    tracer.global_excepthook(tp, value, traceback)
+    if _ORIGINAL_EXCEPTHOOK:
+        return _ORIGINAL_EXCEPTHOOK(tp, value, traceback)
+
+
+def install_excepthook():
+    """Install a hook that intercepts unhandled exception and send metrics about them."""
+    global _ORIGINAL_EXCEPTHOOK
+    _ORIGINAL_EXCEPTHOOK = sys.excepthook
+    sys.excepthook = _excepthook
+
+
+def uninstall_excepthook():
+    """Uninstall the global tracer except hook."""
+    sys.excepthook = _ORIGINAL_EXCEPTHOOK
