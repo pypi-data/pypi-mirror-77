070aba3740394e1307b980b4ed04f5a81f14372a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:16 +0000

    Add package is-palindrom-bala/0.0.1/is_palindrom_bala-0.0.1.tar.gz

---
+++ packages/is-palindrom-bala/is_palindrom_bala-0.0.1.tar.gz/is_palindrom_bala-0.0.1/is_palindrome_bala/__init__.py
@@ -0,0 +1,8 @@
+def is_palindrome(string):
+    string2 = string[::-1]
+    if string == string2:
+        return True
+    else:
+        return False
+
+# def ais_anagram()
---
+++ packages/is-palindrom-bala/is_palindrom_bala-0.0.1.tar.gz/is_palindrom_bala-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'is_palindrom_bala',
+    version = '0.0.1',
+    description = 'This is a palindrome checking function.',
+    long_description = open('Readme.txt').read(),
+    url = 'https://balajibetadur.github.io',
+    author = 'Balaji Betadur',
+    keywords = ['palindrome','python palindrome','palindrome evaluator'],
+    license = 'MIT',
+    packages = ['is_palindrome_bala'],
+    install_requires = ['']
+)
