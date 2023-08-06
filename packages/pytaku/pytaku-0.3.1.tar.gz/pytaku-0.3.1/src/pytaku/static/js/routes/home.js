0331e720a8c27fcbcce18512c34be81d066779a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package pytaku/0.3.1/pytaku-0.3.1.tar.gz

---
+++ packages/pytaku/pytaku-0.3.1.tar.gz/pytaku-0.3.1/src/pytaku/static/js/routes/home.js
@@ -0,0 +1,15 @@
+import { Auth } from "../models.js";
+
+const Home = {
+  oncreate: (vnode) => {
+    document.title = "Pytaku";
+  },
+  view: (vnode) => {
+    return m("div.content", [
+      m("p", "Try searching for some manga title using the box above."),
+      m("p", "Logging in allows you to follow manga titles."),
+    ]);
+  },
+};
+
+export default Home;
