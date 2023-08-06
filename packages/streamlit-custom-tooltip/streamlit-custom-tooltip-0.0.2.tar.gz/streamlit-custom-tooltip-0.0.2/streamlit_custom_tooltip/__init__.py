c9f74c27978c8ba7f428b9218d1c3df5bbf78bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:20 +0000

    Add package streamlit-custom-tooltip/0.0.2/streamlit-custom-tooltip-0.0.2.tar.gz

---
+++ packages/streamlit-custom-tooltip/streamlit-custom-tooltip-0.0.2.tar.gz/streamlit-custom-tooltip-0.0.2/streamlit_custom_tooltip/__init__.py
@@ -0,0 +1,16 @@
+import streamlit.components.v1 as components
+import os
+
+# Create a function _component_func which will call the frontend component when run
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+_component_func = components.declare_component(
+    "streamlit_custom_tooltip",
+    path=build_dir,  # Fetch frontend component from local webserver
+)
+
+# Define a public function for the package,
+# which wraps the caller to the frontend code
+def st_custom_tooltip(label: str, selectedAttributes: list, sentence: str):
+    component_value = _component_func(label=label, selectedAttributes=selectedAttributes, sentence=sentence)
+    return component_value
