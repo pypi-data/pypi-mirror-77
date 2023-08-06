3092605b010e96c3f3d9cf9f3cf6921c0422f330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:21 +0000

    Add package XTCryptoSignals/0.8.1/XTCryptoSignals-0.8.1-py3-none-any.whl

---
+++ packages/XTCryptoSignals/XTCryptoSignals-0.8.1-py3-none-any.whl/xtcryptosignals/tasks/utils.py
@@ -0,0 +1,44 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = [
+    "Paulo Antunes",
+]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+from importlib import import_module
+
+
+def get_class(folder, module):
+    exchange_module = import_module("{}.{}".format(folder, module))
+    module_class_name = "".join([x.capitalize() for x in module.split("_")])
+    return getattr(exchange_module, module_class_name)
+
+
+def convert_to_seconds(x):
+    _t = x[-1]
+    try:
+        seconds = int(x[:-1])
+    except ValueError:
+        raise ValueError("Invalid item: {}".format(x))
+    if _t == "s":
+        return seconds
+    minutes = seconds * 60
+    if _t == "m":
+        return minutes
+    hours = minutes * 60
+    if _t == "h":
+        return hours
+    days = hours * 24
+    if _t == "d":
+        return days
+    weeks = days * 7
+    if _t == "w":
+        return weeks
+    months = weeks * 4
+    year = months * 12
+    if _t == "y":
+        return year
+    raise ValueError("Undefined item: {}".format(x))
