3e0e5a53650011193fa83461d9c0e8c509b7709b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package tacp/5.1.3.dev1/tacp-5.1.3.dev1-py3-none-any.whl

---
+++ packages/tacp/tacp-5.1.3.dev1-py3-none-any.whl/tacp/api/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from tacp.api.actions_api import ActionsApi
+from tacp.api.application_groups_api import ApplicationGroupsApi
+from tacp.api.applications_api import ApplicationsApi
+from tacp.api.categories_api import CategoriesApi
+from tacp.api.compute_nodes_api import ComputeNodesApi
+from tacp.api.datacenters_api import DatacentersApi
+from tacp.api.defaults_api import DefaultsApi
+from tacp.api.edit_applications_api import EditApplicationsApi
+from tacp.api.events_api import EventsApi
+from tacp.api.firewall_profiles_api import FirewallProfilesApi
+from tacp.api.flash_pools_api import FlashPoolsApi
+from tacp.api.locations_api import LocationsApi
+from tacp.api.marketplace_templates_api import MarketplaceTemplatesApi
+from tacp.api.migration_zones_api import MigrationZonesApi
+from tacp.api.storage_blocks_api import StorageBlocksApi
+from tacp.api.storage_controllers_api import StorageControllersApi
+from tacp.api.tags_api import TagsApi
+from tacp.api.templates_api import TemplatesApi
+from tacp.api.users_api import UsersApi
+from tacp.api.version_api import VersionApi
+from tacp.api.vlans_api import VlansApi
+from tacp.api.vnets_api import VnetsApi
