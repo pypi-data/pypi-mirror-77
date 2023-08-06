d3ba53347cb3289d651f434b778f33e72472d0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:07 +0000

    Add package streamlit-location/0.0.2/streamlit_location-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-location/streamlit_location-0.0.2-py3-none-any.whl/streamlit_location/frontend/build/precache-manifest.a11dc30c29defd85fe44e0a6cfa7c651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfd7245c69ba17bd9b14862b132c48c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fcf67e5ca337523731f",
+    "url": "./static/js/2.330a583f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.330a583f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7d2fe3c88433d6bc5e",
+    "url": "./static/js/main.44a07802.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.44a07802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1737b29fe479bd51b7b0",
+    "url": "./static/js/runtime-main.085ce4f3.js"
+  }
+]);
