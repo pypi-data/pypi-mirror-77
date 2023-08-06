77c9601c4dc0ff6b569f40aa3112fc624e6b679b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package pgradd/2.1/pgradd-2.1.tar.gz

---
+++ packages/pgradd/pgradd-2.1.tar.gz/pgradd-2.1/setup.py
@@ -0,0 +1,56 @@
+#
+#
+# setup.py
+#
+# Installation script to get setuptools to install pgradd into
+# a Python environment.
+#
+
+import sys
+import setuptools
+
+# Import the lengthy rich-text README as the package's long
+# description:
+with open('README.rst', 'r') as fh:
+	long_description = fh.read()
+
+setuptools_info = {
+	'name': 'pgradd',
+	'version': '2.1',
+	'author': 'Vlachos Research Group',
+	'author_email': 'vlachos@udel.edu',
+	'description': 'Python package implements the Group Additivity (GA) method for estimating thermodynamic properties',
+	'long_description': long_description,
+	'zip_safe': True,
+	'url': 'https://github.com/VlachosGroup/PythonGroupAdditivity',
+	'packages': setuptools.find_packages(),
+	'include_package_data': True,
+	'exclude_package_data': {
+		'': [ 'README.rst', 'docs', 'example', 'tests', 'PKG-INFO', 'LICENSE.md' ]
+	    },
+	'install_requires': [
+		'pmutt>=1.2.14',
+		'scipy>=1.1.0',
+		'numpy>=1.15.1',
+		'IPython>=7.0.0',
+		'PyYAML>=3.0',
+	    ],
+	'dependency_links': [
+		'git+https://github.com/rdkit/rdkit/',
+	    ],
+	'classifiers': [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Chemistry",
+	    ],
+    }
+
+if sys.version_info[0] >= 3:
+	#
+	# Augment for Python 3 setuptools:
+	#
+	setuptools_info['long_description_content_type'] = 'text/x-rst'
+
+setuptools.setup(**setuptools_info)
