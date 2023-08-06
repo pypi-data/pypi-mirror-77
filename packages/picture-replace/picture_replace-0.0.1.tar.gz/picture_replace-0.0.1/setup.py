cc5e8123ad5ceaf9391b52ba3012921535ad4dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package picture-replace/0.0.1/picture_replace-0.0.1.tar.gz

---
+++ packages/picture-replace/picture_replace-0.0.1.tar.gz/picture_replace-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="picture_replace",
+    version="0.0.1",
+    author="panxianxian",
+    description=" focus! ",
+    long_description="i'm a happy girl, welcome to my library!",
+    packages=["panpan"],
+    python_requires='>=3',
+)
