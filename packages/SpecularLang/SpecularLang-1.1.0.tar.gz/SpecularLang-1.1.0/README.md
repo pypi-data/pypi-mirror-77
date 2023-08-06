d37c0b41f257bbfcb4e6679fef04680851e24d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:23 +0000

    Add package SpecularLang/1.1.0/SpecularLang-1.1.0.tar.gz

---
+++ packages/SpecularLang/SpecularLang-1.1.0.tar.gz/SpecularLang-1.1.0/README.md
@@ -0,0 +1,7 @@
+# SpecularLang
+![Python package](https://github.com/Bluepuff71/SpecularLang/workflows/Python%20package/badge.svg)
+[![PyPI version](https://img.shields.io/pypi/v/SpecularLang)](https://pypi.org/project/SpecularLang/)
+
+A language for a project I'm working on
+
+Documentation coming soon!
---
+++ packages/SpecularLang/SpecularLang-1.1.0.tar.gz/SpecularLang-1.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='SpecularLang',
+    version='1.1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/Bluepuff71/SpecularLang',
+    license='MIT',
+    author='Emery Porter',
+    author_email='emeryporter99@gmail.com',
+    description='Programming language that allows writers to create event-driven, complex character scenes within game engines using Specular.',
+    install_requires=['antlr4-python3-runtime', 'antlr_denter'],
+    entry_points={
+        "console_scripts": [
+            "slang = SpecularLang.compile:main",
+        ]
+    },
+    python_requires='>=3.5'
+)
