9a16c238b786370563147e4e7139247043792912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package thetravelers.online-Api/1.0.1/thetravelers.online%20Api-1.0.1.tar.gz

---
+++ packages/thetravelers.online-Api/thetravelers.online%20Api-1.0.1.tar.gz/thetravelers.online Api-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='thetravelers.online Api',
+      version='1.0.1',
+      description='Python API for thetravelers.online',
+      long_description='An API for thetravelers.online written in python using a headless firefox. You need firefox installed first.',
+      author='LightningWB',
+      url='https://github.com/LightningWB/thetravelers.online-API',
+      install_requires=['selenium']
+     )
