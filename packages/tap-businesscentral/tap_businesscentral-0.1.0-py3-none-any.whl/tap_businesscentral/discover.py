f74d082685f625de5d84f9ffcf87a258fb16d1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:09 +0000

    Add package tap-businesscentral/0.1.0/tap_businesscentral-0.1.0-py3-none-any.whl

---
+++ packages/tap-businesscentral/tap_businesscentral-0.1.0-py3-none-any.whl/tap_businesscentral/discover.py
@@ -0,0 +1,76 @@
+from singer.catalog import Catalog, CatalogEntry, Schema
+
+selected_tables = [
+    "companies",
+    "productionOrders"
+]
+
+def get_schema(odata_schema):
+    json_props = {}
+    metadata = []
+    pks = []
+    for odata_prop in odata_schema.get("properties", []):
+        odata_type = odata_prop["type"]
+        prop_name = odata_prop["name"]
+        json_type = "string"
+        json_format = None
+
+        inclusion = "automatic"
+        if odata_prop["is_primary_key"] == True:
+            pks.append(prop_name)
+
+        metadata.append(
+            {
+                "breadcrumb": ["properties", prop_name],
+                "metadata": {"inclusion": inclusion},
+            }
+        )
+
+        if odata_type in ["Edm.Date", "Edm.DateTime", "Edm.DateTimeOffset"]:
+            json_format = "date-time"
+        elif odata_type in ["Edm.Int16", "Edm.Int32", "Edm.Int64"]:
+            json_type = "integer"
+        elif odata_type in ["Edm.Double", "Edm.Decimal"]:
+            json_type = "decimal"
+        elif odata_type == "Edm.Boolean":
+            json_type = "boolean"
+
+        prop_json_schema = {"type": ["null", json_type]}
+
+        if json_format:
+            prop_json_schema["format"] = json_format
+
+        json_props[prop_name] = prop_json_schema
+    
+    json_schema = {
+        "type": "object",
+        "additionalProperties": False,
+        "properties": json_props,
+    }
+
+    return json_schema, metadata, pks
+
+def discover(service):
+    catalog = Catalog([])
+    for entity_name, entity in service.entities.items():
+        if entity_name not in selected_tables:
+            continue
+        schema_dict, metadata, pks = get_schema(entity.__odata_schema__)
+        metadata.append({"breadcrumb": [], "metadata": {"selected": True}})
+        schema = Schema.from_dict(schema_dict)
+            
+        catalog.streams.append(
+            CatalogEntry(
+                stream=entity_name,
+                tap_stream_id=entity_name,
+                key_properties=pks,
+                schema=schema,
+                metadata=metadata,
+                replication_method="INCREMENTAL"
+                if schema_dict.get("properties", None).get("createdon", None)
+                else "FULL_TABLE",
+            )
+        )
+
+    return catalog
+
---
+++ packages/tap-businesscentral/tap_businesscentral-0.1.0-py3-none-any.whl/tap_businesscentral/sync.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+
+import singer
+from singer import metrics, metadata, Transformer
+from singer.bookmarks import set_currently_syncing
+
+from discover import discover
+
+LOGGER = singer.get_logger()
+
+MODIFIED_DATE_FIELD = "modifiedon"
+
+
+def get_bookmark(state, stream_name, default):
+    return state.get("bookmarks", {}).get(stream_name, default)
+
+def write_bookmark(state, stream_name, value):
+    if "bookmarks" not in state:
+        state["bookmarks"] = {}
+    state["bookmarks"][stream_name] = value
+    singer.write_state(state)
+
+def write_schema(stream):
+    schema = stream.schema.to_dict()
+    singer.write_schema(stream.tap_stream_id, schema, stream.key_properties)
+
+def getNavigationTarget(stream_name):
+    return stream_name[:1].upper() + stream_name[1:]
+
+def sync_stream(service, catalog, state, start_date, stream, mdata):
+    stream_name = stream.tap_stream_id
+    last_datetime = get_bookmark(state, stream_name, start_date)
+
+    write_schema(stream)
+
+    max_modified = last_datetime
+
+    ## TODO: add metrics?
+    entitycls = service.entities[stream_name]
+    CompanyEntity = service.entities['companies']
+    query = service.query(CompanyEntity)
+    expandedCompanyStream = getattr(CompanyEntity, getNavigationTarget(stream_name))
+    query.expand(expandedCompanyStream)
+
+    if hasattr(entitycls, MODIFIED_DATE_FIELD):
+        LOGGER.info(
+            "{} - Syncing data since {}".format(stream.tap_stream_id, last_datetime)
+        )
+        query = query.filter(
+            getattr(entitycls, MODIFIED_DATE_FIELD)
+            >= singer.utils.strptime_with_tz(last_datetime)
+        ).order_by(getattr(entitycls, MODIFIED_DATE_FIELD).asc())
+    else:
+        LOGGER.info("{} - Syncing using full replication".format(stream.tap_stream_id))
+
+    schema = stream.schema.to_dict()
+    with metrics.record_counter(stream.tap_stream_id) as counter:
+        for record in query:
+            currentRecord = getattr(record, getNavigationTarget(stream_name))
+
+            for streamInstance in currentRecord:
+                dict_record = {}
+                for odata_prop in entitycls.__odata_schema__["properties"]:
+                    prop_name = odata_prop["name"]
+                    value = getattr(streamInstance, prop_name)
+                    if isinstance(value, datetime):
+                        value = singer.utils.strftime(value)
+                    dict_record[prop_name] = value
+
+                if MODIFIED_DATE_FIELD in dict_record:
+                    if dict_record[MODIFIED_DATE_FIELD] > max_modified:
+                        max_modified = dict_record[MODIFIED_DATE_FIELD]
+                    else:
+                        continue
+
+                with Transformer() as transformer:
+                    dict_record = transformer.transform(dict_record, schema, mdata)
+                singer.write_record(stream.tap_stream_id, dict_record)
+                counter.increment()
+
+        write_bookmark(state, stream_name, max_modified)
+
+def update_current_stream(state, stream_name=None):
+    set_currently_syncing(state, stream_name)
+    singer.write_state(state)
+
+
+def sync(service, catalog, state, start_date):
+    if not catalog:
+        catalog = discover(service)
+        selected_streams = catalog.streams
+    else:
+        selected_streams = catalog.get_selected_streams(state)
+
+    for stream in selected_streams:
+        if not stream.tap_stream_id == 'companies':
+            mdata = metadata.to_map(stream.metadata)
+            update_current_stream(state, stream.tap_stream_id)
+
+            sync_stream(service, catalog, state, start_date, stream, mdata)
+
+        update_current_stream(state)
