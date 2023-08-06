47924af05f10761ae364dc93016c179ca7437a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package zconfigparser/0.1.0/zconfigparser-0.1.0.tar.gz

---
+++ packages/zconfigparser/zconfigparser-0.1.0.tar.gz/zconfigparser-0.1.0/README.rst
@@ -0,0 +1,17 @@
+
+Zconfigparser
+=============
+
+This is a python3 (3.6+) library
+to provide some section inheritance functionality
+to `configparser <https://docs.python.org/3/library/configparser.html>`__.
+
+For details, see `documentation <http://zconfigparser.readthedocs.io/>`__.
+
+
+License
+-------
+
+The software is licensed under The MIT License. See `LICENSE`_.
+
+.. _LICENSE: https://github.com/openandclose/zconfigparser/blob/master/LICENSE
---
+++ packages/zconfigparser/zconfigparser-0.1.0.tar.gz/zconfigparser-0.1.0/setup.py
@@ -0,0 +1,42 @@
+
+"""zconfigparser setup file."""
+
+# from setuptools import setup, find_packages
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('VERSION') as f:
+    version = f.read().strip()
+
+
+setup(
+    name='zconfigparser',
+    version=version,
+    url='https://github.com/openandclose/zconfigparser',
+    license='MIT',
+    author='Open Close',
+    author_email='openandclose23@gmail.com',
+    description='Extend ConfigParser to add some inheritance functionality',
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+    keywords='ini config configparser',
+    # packages=find_packages(exclude=['docs', 'tests', 'nonpubfiles']),
+    py_modules=['zconfigparser'],
+    python_requires='~=3.6',
+    extras_require={
+        'test': ['pytest'],
+        'dev': ['pytest', 'sphinx'],
+    },
+    zip_safe=False,
+)
