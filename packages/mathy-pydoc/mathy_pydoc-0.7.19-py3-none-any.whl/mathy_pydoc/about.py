c9dbbf7f111f1af87dc40c2e8058780b2d31fd6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:28 +0000

    Add package mathy-pydoc/0.7.19/mathy_pydoc-0.7.19-py3-none-any.whl

---
+++ packages/mathy-pydoc/mathy_pydoc-0.7.19-py3-none-any.whl/mathy_pydoc/about.py
@@ -0,0 +1,7 @@
+__title__ = "mathy_pydoc"
+__version__ = "0.7.19"
+__summary__ = "Minimal python API documentation generation for https://mathy.ai"
+__uri__ = "https://mathy.ai"
+__author__ = "Justin DuJardin"
+__email__ = "justin@dujardinconsulting.com"
+__license__ = "MIT"
