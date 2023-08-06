54c8c28f5204b10132055e31c734ffec97fcb7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:11 +0000

    Add package py2json/0.0.1/py2json-0.0.1-py3-none-any.whl

---
+++ packages/py2json/py2json-0.0.1-py3-none-any.whl/py2json/fakit.py
@@ -0,0 +1,317 @@
+"""
+A general language for json-serialization of a function call.
+- Any construction of a python object needs to go through a function call that makes it so
+this approach is general.
+- It’s also simple at its base, but open (and intended for) extensions to specialize
+and compress the language as well as add layers for security.
+
+Note: "fakit" can be pronounced with the "a" as in "bake" or a
+"""
+import os
+import importlib
+from functools import partial
+from collections.abc import Mapping
+
+from py2json.util import compose
+
+FAK = '$fak'
+
+
+# TODO: Make a config_utils.py module to centralize config tools (configs for access is just one -- serializers another)
+# TODO: Integrate (external because not standard lib) other safer tools for secrets, such as:
+#  https://github.com/SimpleLegal/pocket_protector
+
+
+def getenv(name, default=None):
+    """Like os.getenv, but removes a suffix \\r character if present (problem with some env var systems)"""
+    v = os.getenv(name, default)
+    if v.endswith('\r'):
+        return v[:-1]
+    else:
+        return v
+
+
+def assert_callable(f: callable) -> callable:
+    assert callable(f), f"Is not callable: {f}"
+    return f
+
+
+def dotpath_to_obj(dotpath: str):
+    """Loads and returns the object referenced by the string DOTPATH_TO_MODULE.OBJ_NAME"""
+    first, *remaining = dotpath.split('.')
+    obj = importlib.import_module(first)  # assume it's a module
+    for item in remaining:
+        obj = getattr(obj, item)
+    return obj
+
+
+def obj_to_dotpath(obj):
+    """
+    >>> from inspect import Signature
+    >>> obj_to_dotpath(Signature.replace)
+    'inspect.Signature.replace'
+    >>> obj_to_dotpath(func_to_dotpath)  # note that here, it's not a "full path"
+    'fakit.func_to_dotpath'
+
+    func_to_dotpath is the inverse of dotpath_to_func
+    >>> assert dotpath_to_obj(obj_to_dotpath(Signature.replace)) == Signature.replace
+
+    """
+    return '.'.join((obj.__module__, obj.__qualname__))
+
+
+def func_to_dotpath(func: callable) -> str:
+    return obj_to_dotpath(assert_callable(func))
+
+
+def dotpath_to_func(dotpath: str) -> callable:
+    """Loads and returns the function referenced by f,
+    which could be a callable or a DOTPATH_TO_MODULE.FUNC_NAME dotpath string to one.
+
+    >>> f = dotpath_to_func('os.path.join')
+    >>> assert callable(f)  # I got a callable!
+    >>> assert f.__name__ == 'join'  # and indeed, it's name is join
+    >>> from inspect import signature
+    >>> signature(f)  # and it's signature is indeed that of os.path.join:
+    <Signature (a, *p)>
+    >>>
+    >>> # and just for fun...
+    >>> assert signature(dotpath_to_func('inspect.signature')) == signature(signature)
+
+    dotpath_to_func is the inverse of func_to_dotpath
+    >>> assert dotpath_to_func(func_to_dotpath(signature)) == signature
+
+    """
+    obj = dotpath_to_obj(dotpath)
+    return assert_callable(obj)
+
+
+def dflt_func_loader(f) -> callable:
+    """Loads and returns the function referenced by f,
+    which could be a callable or a DOTPATH_TO_MODULE.FUNC_NAME dotpath string to one, or a pipeline of these.
+    """
+    if callable(f):
+        return f
+    elif isinstance(f, str):
+        return dotpath_to_func(f)
+    elif isinstance(f, dict) and len(f) == 1 and FAK in f:
+        return fakit(f[FAK])
+    else:
+        return compose(*map(dflt_func_loader, f))
+
+
+def _fakit(f: callable, a: (tuple, list), k: dict):
+    """The function that actually executes the fak command.
+    Simply: `f(*(a or ()), **(k or {}))`
+
+    >>> _fakit(print, ('Hello world!',), {})
+    Hello world!
+
+    """
+    return f(*(a or ()), **(k or {}))  # slightly protected form of f(*a, **k)
+
+
+def extract_fak(fak):
+    """Extracts the (raw) (f, a, k) triple from a dict or tuple/list fak.
+    Also asserts the validity of input fak.
+
+    >>> extract_fak(('func', (1, 2), {'keyword': 3}))
+    ('func', (1, 2), {'keyword': 3})
+
+    If fak has only two input items and the second is a dict, the second output will be an empty tuple.
+    >>> extract_fak(('func', {'keyword': 3}))
+    ('func', (), {'keyword': 3})
+
+    If fak has only two input items and the second is a tuple, the second output will be an empty dict.
+    >>> extract_fak(['func', (1, 2)])
+    ('func', (1, 2), {})
+
+    If you only have only one element in your list/tuple input...
+    >>> extract_fak(['func'])
+    ('func', (), {})
+
+    If your input is a dict
+    >>> extract_fak({'f': 'func', 'a': (1, 2), 'k': {'keyword': 3}})
+    ('func', (1, 2), {'keyword': 3})
+    >>> extract_fak({'f': 'func', 'k': {'keyword': 3}})
+    ('func', (), {'keyword': 3})
+    >>> extract_fak({'f': 'func', 'a': (1, 2)})
+    ('func', (1, 2), {})
+    """
+    if isinstance(fak, dict):
+        if 'f' not in fak:
+            raise ValueError(f"There needs to be an `f` key, was not: {fak}")
+        f = fak['f']
+        a = fak.get('a', ())
+        k = fak.get('k', {})
+    else:
+        assert isinstance(fak, (tuple, list)), f"fak should be dict, tuple, or list, was not: {fak}"
+        assert len(fak) >= 1, f"fak should have at least one element (the function component): {fak}"
+        f = fak[0]
+        a = ()
+        k = {}
+        assert len(fak) in {1, 2, 3}, "A tuple fak must be of length 1, 2, or 3. No more, no less."
+        if len(fak) > 1:
+            if isinstance(fak[1], dict):
+                k = fak[1]
+            else:
+                a = fak[1]
+                assert isinstance(a, (tuple, list)), "argument specs should be dict, tuple, or list"
+            if len(fak) > 2:
+                if isinstance(fak[2], dict):
+                    assert not k, "can only have one kwargs"
+                    k = fak[2]
+                else:
+                    assert isinstance(fak[2], (tuple, list)), "argument specs should be dict, tuple, or list"
+                    assert not a, "can only have one args"
+                    a = fak[2]
+
+    assert isinstance(a, (tuple, list)), f"a kind is not a tuple or list: {fak}"
+    assert isinstance(k, Mapping), f"k kind is not a mapping: {fak}"
+    return f, a, k
+
+
+def validate_fak(fak):
+    """Returns the input iff (f, a, k) could be extracted and validated from input fak"""
+    _ = extract_fak(fak)
+    return fak
+
+
+def is_valid_fak(fak):
+    try:
+        validate_fak(fak)
+        return True
+    except Exception:
+        return False
+
+
+def extract_and_load(fak, func_loader=dflt_func_loader):
+    f, a, k = extract_fak(fak)
+    f = func_loader(f)
+    return f, a, k
+
+
+def fakit(fak, func_loader=dflt_func_loader):
+    """Execute a fak with given (f, a, k) tuple or {f: f, a: a, k: k} dict, and a function loader.
+
+    Essentially returns `func_loader(f)(*a, **k)` where `(f, a, k)` are flexibly specified by `fak`.
+
+    The `func_loader` is where you specify any validation of func specification and/or how to get a callable from it.
+    The default `func_loader` will produce a callable from a dot path (e.g. `'os.path.join'`),
+    But note that the intended use is for the user to use their own `func_loader`.
+    The user should do this, amongst other things:
+    - For security purposes, like not allowing `subprocess.call` or such.
+    - For expressivity purposes, like to create their own domain specific mini-language
+     that maps function specification to actual function.
+
+    Args:
+        fak: A (f, a, k) specification. Could be a tuple or a dict (with 'f', 'a', 'k' keys). All but f are optional.
+        func_loader: A function returning a function.
+
+    Returns: A python object.
+
+    >>> fak = {'f': 'os.path.join', 'a': ['I', 'am', 'a', 'filepath']}
+    >>> assert fakit(fak) =='I/am/a/filepath' or fakit(fak) == 'I\am\a\filepath'
+
+    >>> A = fakit(['collections.namedtuple', ('A', 'x y z')])
+    >>> A('no', 'defaults', 'here')
+    A(x='no', y='defaults', z='here')
+
+    ... you can also use a dict (which will be understood to be the keyword arguments (`**k`)):
+
+    >>> A = fakit(['collections.namedtuple', {'typename': 'A', 'field_names': 'x y z'}])
+    >>> A('no', 'defaults', 'here')
+    A(x='no', y='defaults', z='here')
+
+    ... or both:
+
+    >>> A = fakit(['collections.namedtuple', ('A', 'x y z'), {'defaults': ('has', 'defaults')}])
+    >>> A('this one')
+    A(x='this one', y='has', z='defaults')
+
+    >>> from inspect import signature
+    >>>
+    >>> A = fakit({'f': 'collections.namedtuple', 'a': ['A', 'x y z'], 'k': {'defaults': (2, 3)}})
+    >>> # A should be equivalent to `collections.namedtuple('A', 'x y z', defaults=(2, 3))`
+    >>> signature(A)
+    <Signature (x, y=2, z=3)>
+    >>> A(1)
+    A(x=1, y=2, z=3)
+    >>> A(42, z='forty two')
+    A(x=42, y=2, z='forty two')
+
+    >>> def foo(x, y, z=3):
+    ...     return x + y * z
+    >>> func_map = {
+    ...     'foo': foo,
+    ...     'bar': (lambda a, b='world': f"{a} {b}!"),
+    ...     'sig': signature}
+    >>> from functools import partial
+    >>> call_func = partial(fakit, func_loader=func_map.get)
+    >>> call_func({'f': 'foo', 'a': (1, 10)})
+    31
+
+    Common gotcha: Forgetting that `a` is iterpreted as an iterable of function args. For example
+
+    >>> fakit(('builtins.print', 'hello'))  # not correct
+    Traceback (most recent call last):
+      ...
+    AssertionError: argument specs should be dict, tuple, or list
+    >>> fakit(('builtins.print', ['hello']))  # correct
+    hello
+
+    >>> fakit(('builtins.sum', [1, 2, 3]))  # not correct
+    Traceback (most recent call last):
+      ...
+    TypeError: sum() takes at most 2 arguments (3 given)
+    >>> fakit(('builtins.sum', ([1, 2, 3],)))  # correct
+    6
+
+    """
+    if isinstance(fak, dict) and FAK in fak:
+        fak = fak[FAK]
+    return _fakit(*extract_and_load(fak, func_loader))
+
+
+fakit.w_func_loader = lambda func_loader: partial(fakit, func_loader=func_loader)
+
+
+def fakit_if_marked_for_it(x, func_loader=dflt_func_loader):
+    if isinstance(x, dict) and FAK in x:
+        return fakit(x[FAK], func_loader)
+    else:
+        return x
+
+
+def refakit(x, func_loader=dflt_func_loader):
+    """Fakit recursively looking for nested {'$fak': ...} specifications of python objects
+
+    :param x:
+    :param func_loader:
+    :return:
+
+    >>> def prod(a, b):
+    ...     return a * b
+    >>> t = {'$fak': ('builtins.sum', ([1,2,3],))}
+    >>> refakit(t)  # it works with one level
+    6
+    >>> tt = {'$fak': ('__main__.prod', (10, t))}  # it works with two levels (the t is a fak)
+    >>> refakit(tt)
+    60
+
+    TODO: It doesn't work for this situation: ttt = {'$fak': ('builtins.sum', [[t, tt]])}
+        But to work in ALL situations, we need to specify more "enter recursion" cases.
+        Not sure how general we should be out-of-the-box. Perhaps better add some control in args for that.
+
+    See also: `fakit`, the one level only version of `refakit`.
+    """
+    if not isinstance(x, dict) or FAK not in x:
+        return x
+    else:
+        f, a, k = extract_fak(x[FAK])
+
+        # recurse over inputs to see if there's some that are expressed with a $fak dict
+        a = [refakit(aa, func_loader) for aa in a]
+        k = {kk: refakit(vv, func_loader) for kk, vv in k.items()}
+
+        return fakit((f, a, k), func_loader)
---
+++ packages/py2json/py2json-0.0.1-py3-none-any.whl/py2json/util.py
@@ -0,0 +1,246 @@
+from i2.routing_forest import *
+from i2.footprints import *
+from i2.signatures import Sig
+from dataclasses import dataclass
+
+
+@dataclass
+class Literal:
+    obj: object
+
+
+def missing_args_func(func_to_kwargs=None, ignore=None):
+    """Returns a function that returns a set of missing arg names.
+    Missing means that that the arg is required for a func (has no default)
+    and hasn't been found by the func_to_kwargs policy.
+
+    The returned function can be used to diagnose the coverage of the func_to_kwargs policy,
+    or as a filter to find those functions that are not covered by the policy.
+
+    :param func_to_kwargs: Callable that returns valid kwargs for a given func.
+    :param ignore: If not None, should be an iterable to names not to check
+    :return: A missing_args_func that returns a set of arg names that are missing.
+
+    >>> from collections import namedtuple
+    >>> assert missing_args_func()(namedtuple) == {'field_names', 'typename'}
+    >>> func_to_kwargs = lambda f: {namedtuple: {'typename': 'Unspecified'}}.get(f, {})
+    >>>
+    >>> missing_args = missing_args_func(func_to_kwargs)
+    >>> missing_args(namedtuple)
+    {'field_names'}
+    >>> def foo(x=1, y=2): ...  # defaults cover all arguments
+    >>> assert list(filter(missing_args, (namedtuple, foo))) == [namedtuple]
+    """
+
+    def missing_args_func_(func):
+        missing_args = set(Sig(func).without_defaults.parameters) - set(ignore or ())
+        if func_to_kwargs is not None:
+            missing_args -= func_to_kwargs(func).keys()
+        return missing_args
+
+    return missing_args_func_
+
+
+def mk_func_to_kwargs_from_a_val_for_argname_map(val_for_argname=None):
+    """Returns a function func_to_kwargs that returns kwargs for a given callable func.
+    The intent being that these kwargs can be used as valid inputs of func as such:
+    ```
+        func(**func_to_kwargs)
+    ```
+
+    Does so by taking the intersection of those arguments of the func that don't have defaults
+    and the input val_for_argname mapping.
+
+    Note that if no val_for_argname is given, or non matches the default-less arguments of func,
+    then {} is returned.
+
+    >>> val_for_argname = {'typename': 'Unspecified', 'x': 0}
+    >>> func_to_kwargs = mk_func_to_kwargs_from_a_val_for_argname_map(val_for_argname)
+    >>> missing_args = missing_args_func(func_to_kwargs)
+    >>>
+    >>> from collections import namedtuple
+    >>> def foo(typename, x, y=2): ...
+    >>> def bar(x, z=None): ...
+    >>> assert missing_args(namedtuple) == {'field_names'}
+    >>> assert missing_args(foo) == set()
+    >>> assert missing_args(bar) == set()
+    >>> assert list(filter(missing_args, (namedtuple, foo, bar))) == [namedtuple]
+    """
+    val_for_argname = val_for_argname or {}
+
+    def func_to_kwargs(func):
+        return {k: val_for_argname[k] for k in val_for_argname.keys() & set(Sig(func).without_defaults)}
+
+    return func_to_kwargs
+
+
+def is_valid_kwargs(func, kwargs):
+    """Test if kwargs constitute a valid input for func simply by trying func(**kwargs) out.
+
+    :param func: A callable
+    :param kwargs: A dict of keyword arguments
+    :return: True if, and only if `func(**kwargs)` doesn't fail, and False if it does raise an Exception.
+
+    >>> def f(a, b=1):
+    ...     return a * b
+    >>> is_valid_kwargs(f, {'a': 10})
+    True
+    >>> is_valid_kwargs(f, {'a': 1, 'b': 10})
+    True
+    >>> is_valid_kwargs(f, {'b': 2, 'c': 4})  # c is not a valid argument name, so...
+    False
+    >>> is_valid_kwargs(f, {})  # a has no default value, so you need at least that argument, so...
+    False
+    """
+    try:
+        func(**kwargs)
+        return True
+    except Exception as e:
+        return False
+
+
+from inspect import signature
+from functools import wraps, reduce
+from i2.signatures import Sig
+import warnings
+
+
+def is_not_none(x):
+    return x is not None
+
+
+def ignore_warnings(func):
+    """Couldn't make this work"""
+    from i2.signatures import Sig
+
+    raise RuntimeError("Couldn't make this work. Don't use!")
+
+    @Sig.from_objs(func, lambda ignore_warnings=True: ...)
+    def _func(*args, ignore_warnings=True, **kwargs):
+        with warnings.catch_warnings():
+            if ignore_warnings:
+                warnings.simplefilter("ignore")
+            return func(*args, **kwargs)
+
+    return _func
+
+
+def catch_errors(errors=(Exception,), on_error=lambda e: print(e)):
+    if not callable(on_error):
+        on_error_val = on_error
+        on_error = lambda e: on_error_val
+    else:
+        nargs = len(signature(on_error).parameters)
+        if nargs > 1:
+            raise ValueError(f"on_error should be a value or callable with 0 or 1 arguments")
+        elif nargs == 0:
+            on_error_func = on_error
+            on_error = lambda e: on_error_func()
+
+    def wrap_func(func):
+        @wraps(func)
+        def func_with_errors_caught(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except errors as e:
+                return on_error(e)
+
+        return func_with_errors_caught
+
+    return wrap_func
+
+
+class Nones:
+    """
+    >>> x, y, z = Nones(3)
+    >>> x, y, z
+    None, None, None
+    >>> bool(Nones(3))
+    False
+    """
+
+    def __init__(self, n_items: int):
+        self.n_items = n_items
+
+    def __iter__(self):
+        return (None for _ in range(self.n_items))
+
+    def __bool__(self):
+        return False
+
+
+def partial_positionals(func, fix_args, **fix_kwargs):
+    """Like functools.partial, but with positionals as well"""
+
+    def wrapper(*args, **kwargs):
+        arg = iter(args)
+        return func(*(fix_args[i] if i in fix_args else next(arg)
+                      for i in range(len(args) + len(fix_args))),
+                    **{**fix_kwargs, **kwargs})
+
+    return wrapper
+
+
+def is_types_spec(types) -> bool:
+    """Returns True iff input types is a type or an iterable of types"""
+    if isinstance(types, type):
+        types = (types,)
+    try:
+        return len(types) > 0 and all(isinstance(x, type) for x in types)
+    except Exception:
+        return False
+
+
+def mk_isinstance_cond(types) -> Callable[[Any], bool]:
+    """Makes a boolean function that verifies if objects are of a target type (or types)"""
+
+    assert is_types_spec(types), f"types need to be a single or an iterable of types"
+
+    def isinstance_of_target_types(obj):
+        return isinstance(obj, types)
+
+    return isinstance_of_target_types
+
+
+def mk_scan_mapper(condition_map, dflt=None):
+    """Make function implementing an if/elif/.../else logic from a {bool_func: x, ...} map"""
+
+    def scan_mapping(x):
+        for condition, then in condition_map.items():
+            if condition(x):
+                return then
+        return dflt
+
+    return scan_mapping
+
+
+def types_to_cond(types_map):
+    """Convert a {type(s): x, ...} map into a {is_of_that_type: x, ...} map"""
+    return {mk_isinstance_cond(types): x for types, x in types_map.items()}
+
+
+def types_map_to_scan_mapper(types_map, dflt=None):
+    """Make function implementing an if/elif/.../else logic from a {type(s): x, ...} map.
+    The returned mapper will be such that `mapper(obj)` will return a value x
+    according to the first `isinstance(obj, types)` check that is found in {types: x, ...}
+    types_map.
+
+    >>> mapper = types_map_to_scan_mapper({dict: 'a dict!', (list, tuple): 'list-like'},
+    ...     dflt='nothing found')
+    >>> mapper({'a': 'dict'})
+    'a dict!'
+    >>> mapper((1, 2, 3))
+    'list-like'
+    >>> mapper(['a', 'list'])
+    'list-like'
+    >>> mapper(lambda x: x)  # a function: No match for that!
+    'nothing found'
+    >>> mapper(mapper)
+    'nothing found'
+    """
+    return mk_scan_mapper(types_to_cond(types_map), dflt)
+
+
+def compose(*functions):
+    """Make a function that is the composition of the input functions"""
+    return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
