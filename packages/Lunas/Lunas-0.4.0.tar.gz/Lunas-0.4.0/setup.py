00db47b5ee9797c5c96c2079da13b6f60e0ad33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:46 +0000

    Add package Lunas/0.4.0/Lunas-0.4.0.tar.gz

---
+++ packages/Lunas/Lunas-0.4.0.tar.gz/Lunas-0.4.0/setup.py
@@ -0,0 +1,30 @@
+import sys
+
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 7):
+    sys.exit('Sorry, Python 3.7+ is required for Lunas.')
+
+with open('requirements.txt') as r:
+    requires = [l.strip() for l in r]
+
+setup(
+    name='Lunas',
+    version='0.4.0',
+    author='Seann Zhang',
+    author_email='pluiefox@live.com',
+    packages=find_packages(),
+    url='https://github.com/pluiez/lunas',
+    license='LICENSE',
+    description='Building customisable data processing pipeline and data iterators for machine learning.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+
+)
