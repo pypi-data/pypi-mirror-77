334843962628d2523d689ef401b5b5667124fe67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:11 +0000

    Add package cuenca-validations/0.5.1/cuenca_validations-0.5.1.tar.gz

---
+++ packages/cuenca-validations/cuenca_validations-0.5.1.tar.gz/cuenca_validations-0.5.1/cuenca_validations/types/queries.py
@@ -0,0 +1,58 @@
+import datetime as dt
+from typing import Optional
+
+from pydantic import BaseModel, Extra
+from pydantic.types import ConstrainedInt
+
+from ..typing import DictStrAny
+from ..validators import sanitize_dict
+from .general import StrictPositiveInt
+
+MAX_PAGE_SIZE = 100
+
+
+class PageSize(ConstrainedInt):
+    gt = 0
+    le = MAX_PAGE_SIZE
+
+
+class QueryParams(BaseModel):
+    count: bool = False
+    page_size: PageSize = PageSize(MAX_PAGE_SIZE)
+    limit: Optional[StrictPositiveInt] = None
+    user_id: Optional[str] = None
+    created_before: Optional[dt.datetime] = None
+    created_after: Optional[dt.datetime] = None
+
+    class Config:
+        extra = Extra.forbid  # raise ValidationError if there are extra fields
+
+    def dict(self, *args, **kwargs) -> DictStrAny:
+        d = super().dict(exclude_none=True, exclude_unset=True)
+        if self.count:
+            d['count'] = 1
+        sanitize_dict(d)
+        return d
+
+
+class TransactionQuery(QueryParams):
+    status: Optional[str] = None
+
+
+class TransferQuery(TransactionQuery):
+    account_number: Optional[str] = None
+    idempotency_key: Optional[str] = None
+
+
+class ApiKeyQuery(QueryParams):
+    active: Optional[bool] = None
+
+
+class CardQuery(QueryParams):
+    number: Optional[str] = None
+    exp_month: Optional[int] = None
+    exp_year: Optional[int] = None
+    cvv: Optional[str] = None
+    cvv2: Optional[str] = None
+    icvv: Optional[str] = None
+    pinblock: Optional[str] = None
