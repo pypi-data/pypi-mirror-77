4fe2da7a0a0bfae7efd10b20481b4cfe42234e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:19 +0000

    Add package pyeem/0.1.1/pyeem-0.1.1-py3-none-any.whl

---
+++ packages/pyeem/pyeem-0.1.1-py3-none-any.whl/pyeem/plots/__init__.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from pkg_resources import resource_filename
+
+from .analysis import model_history, prediction_parity_plots
+from .augmentations import (
+    mixture_animation,
+    plot_prototypical_spectra,
+    single_source_animation,
+)
+from .base import _colorbar, eem_plot
+from .preprocessing import plot_calibration_curves, plot_preprocessing
+
+pyeem_base_style = resource_filename("pyeem.plots", "pyeem_base.mplstyle")
+plt.style.use(pyeem_base_style)
+
+__all__ = [
+    "mixture_animation",
+    "plot_prototypical_spectra",
+    "single_source_animation",
+    "eem_plot",
+    "plot_calibration_curves",
+    "plot_preprocessing",
+]
