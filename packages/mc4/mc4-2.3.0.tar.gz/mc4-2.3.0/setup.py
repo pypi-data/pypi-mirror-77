0c53b7f3f6552b5b1d3c765cd9e14172079c6a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:07 +0000

    Add package mc4/2.3.0/mc4-2.3.0.tar.gz

---
+++ packages/mc4/mc4-2.3.0.tar.gz/mc4-2.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mc4",
+    version="2.3.0",
+    author="Ayan Kumar Saha",
+    author_email="ayankumarsaha96@gmail.com",
+    description="A python package for implementing Markov Chain Type 4 rank aggregation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='LICENSE',
+    url="https://github.com/kalyaniuniversity/MC4",
+    packages=find_packages(exclude=['docs', 'test*']),
+    install_requires=['numpy>=1.19.0', 'pandas>=1.0.5'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['mc4_aggregator=mc4.command_line:main'],
+    },
+    keywords='Machine Learning, Ranking, Markov Chain',
+    python_requires='>=3.6',
+    project_urls={  
+        'Full Documentation': 'https://github.com/kalyaniuniversity/MC4/wiki',
+        'Bug Reports': 'https://github.com/kalyaniuniversity/MC4/issues',
+        'Follow Author': 'https://github.com/Ayan-Kumar-Saha'
+    }
+)
