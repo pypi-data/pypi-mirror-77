a42e773a7f97ca8125f6fb4c0bfd8429d91e8a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:07 +0000

    Add package pyrandstring/0.0.6/pyrandstring-0.0.6.tar.gz

---
+++ packages/pyrandstring/pyrandstring-0.0.6.tar.gz/pyrandstring-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyrandstring',
+  packages = ['pyrandstring'], # this must be the same as the name above
+  version = '0.0.6',
+  description = 'String Random Generation',
+  author = 'Alvaro De Leon',
+  author_email = 'deleon@adl.com.uy',
+  url = 'https://github.com/alvarodeleon/pyrandstring', # use the URL to the github repo
+  download_url = 'https://github.com/alvarodeleon/pyrandstring/tarball/0.0.6',
+  keywords = ['password', 'generator', 'random'],
+  classifiers = [],
+)
