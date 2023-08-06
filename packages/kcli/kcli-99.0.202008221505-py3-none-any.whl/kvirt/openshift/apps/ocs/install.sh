f71349f435efff22440eebfb081a10bd443b3820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:13 +0000

    Add package kcli/99.0.202008221505/kcli-99.0.202008221505-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202008221505-py3-none-any.whl/kvirt/openshift/apps/ocs/install.sh
@@ -0,0 +1,30 @@
+oc create -f install.yml
+{% if not ocs_deploycluster %}
+exit
+{% endif %}
+sleep 10
+{%- if not ocs_nodes %}
+{%- set ocs_nodes = [] %}
+{%- for num in range(0, workers) %}
+{%- if ocs_nodes|length < replicas %}
+{%- do ocs_nodes.append("%s-worker-%s.%s.%s" % (cluster, num|string, cluster, domain)) %}
+{% endif %}
+{%- endfor %}
+{%- for num in range(0, masters) %}
+{%- if ocs_nodes|length < replicas %}
+{%- do ocs_nodes.append("%s-master-%s.%s.%s" % (cluster, num|string, cluster, domain)) %}
+{%- endif %}
+{%- endfor %}
+{%- endif %}
+{%- if ocs_nodes|length < replicas %}
+echo "Number of available nodes is lower than expected number of replicas"
+exit 1
+{%- endif %}
+{%- for node in ocs_nodes %}
+oc label {{ node }} cluster.ocs.openshift.io/openshift-storage=''
+oc label node {{ node }} topology.rook.io/rack=rack{{ loop.index }}
+{%- endfor %}
+oc wait --for=condition=Ready pod -l name=ocs-operator -n openshift-storage
+oc create -f cr.yml
+oc patch storageclass ocs-storagecluster-cephfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+oc patch OCSInitialization ocsinit -n openshift-storage --type json --patch  '[{ "op": "replace", "path": "/spec/enableCephTools", "value": true }]'
