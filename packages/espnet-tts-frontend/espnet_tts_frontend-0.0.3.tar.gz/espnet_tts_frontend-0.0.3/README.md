1247530a44f7482230c2515cafe51cb74b83932e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:03 +0000

    Add package espnet-tts-frontend/0.0.3/espnet_tts_frontend-0.0.3.tar.gz

---
+++ packages/espnet-tts-frontend/espnet_tts_frontend-0.0.3.tar.gz/espnet_tts_frontend-0.0.3/README.md
@@ -0,0 +1,76 @@
+# ESPnet TTS Frontend
+[![PyPI version](https://badge.fury.io/py/espnet-tts-frontend.svg)](https://badge.fury.io/py/espnet-tts-frontend)
+[![Python Versions](https://img.shields.io/pypi/pyversions/espnet-tts-frontend.svg)](https://pypi.org/project/espnet-tts-frontend/)
+[![Downloads](https://pepy.tech/badge/espnet-tts-frontend)](https://pepy.tech/project/espnet-tts-frontend)
+![CI](https://github.com/espnet/espnet_tts_frontend/workflows/CI/badge.svg)
+
+A tools collection of text frontend for [ESPnet](https://github.com/espnet/espnet) tts recipes. 
+
+## Install
+```bash
+pip install espnet_tts_frontend
+```
+
+### Install with pyopenjtalk
+
+- Requires cmake and cython
+
+```bash
+git clone https://github.com/espnet/espnet_tts_frontend
+cd espnet_tts_frontend/
+python setup.py pyopenjtalk
+# OR
+# pip install --global-option=pyopenjtalk -e espnet_tts_frontend
+```
+
+FIXME(kamo):  Support direct installation from git repo.
+
+## Usage
+
+### English:
+#### tacotron_cleaner
+Derived from https://github.com/keithito/tacotron
+
+```python
+>>> import tacotron_cleaner.cleaners
+>>> tacotron_cleaner.cleaners.custom_english_cleaners("(Hello-World);   &  jr. & dr.")
+'HELLO WORLD, AND JUNIOR AND DOCTOR'
+```
+#### g2p_en
+https://github.com/Kyubyong/g2p
+
+```python
+>>> from g2p_en import G2p  # Automatically run downloading for the first time
+[nltk_data] Downloading package averaged_perceptron_tagger to
+...
+>>> g2p = G2p()
+>>> g2p("hello world")
+['HH', 'AH0', 'L', 'OW1', ' ', 'W', 'ER1', 'L', 'D']
+```
+
+### Japanese
+#### jaconv
+https://github.com/ikegami-yukino/jaconv/
+
+```python
+>>> jaconv.normalize("”あらゆる”　現実を　〜　’すべて’ 自分の　ほうへ　ねじ曲げたのだ。")
+'"あらゆる" 現実を ー \'すべて\' 自分の ほうへ ねじ曲げたのだ。'
+```
+
+#### pyopenjtalk
+https://github.com/r9y9/pyopenjtalk
+
+```python
+# Automatically run downloading for the first time
+>>> pyopenjtalk.g2p("あらゆる 現実を すべて 自分の ほうへ ねじ曲げたのだ。", kana=True)
+'アラユル\u3000ゲンジツヲ\u3000スベテ\u3000ジブンノ\u3000ホーエ\u3000ネジマゲタノダ。'
+>>> pyopenjtalk.g2p("あらゆる 現実を すべて 自分の ほうへ ねじ曲げたのだ。", kana=False)
+'a r a y u r u pau g e N j i ts u o pau s u b e t e pau j i b u N n o pau h o o e pau n e j i m a g e t a n o d a'
+```
+
+### Vietnamese
+#### vietnamese_cleaner
+
+```python
+>>> import vietnamese_cleaner.vietnamese_cleaner
+```
---
+++ packages/espnet-tts-frontend/espnet_tts_frontend-0.0.3.tar.gz/espnet_tts_frontend-0.0.3/setup.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.install import install
+
+dirname = os.path.dirname(__file__)
+if dirname == "":
+    dirname = "."
+dirname = os.path.abspath(dirname)
+
+
+def install_hts_engine_API():
+    subprocess.check_call(
+        "cd " + dirname + " && "
+        "git clone --depth 1 https://github.com/r9y9/hts_engine_API && "
+        "cd hts_engine_API/src && "
+        "./waf configure --prefix=../../build && ./waf build install",
+        shell=True,
+    )
+
+
+def install_open_jtalk():
+    subprocess.check_call(
+        "cd " + dirname + " && "
+        "git clone --depth 1 https://github.com/r9y9/open_jtalk && "
+        "mkdir -p open_jtalk/src/build &&"
+        "cd open_jtalk/src/build && "
+        "cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON "
+        "-DCMAKE_INSTALL_PREFIX=../../../build  .. && "
+        "make install",
+        shell=True,
+    )
+
+
+def install_pyopenjtalk():
+    subprocess.check_call(
+        "cd " + dirname + " && "
+        "git clone --depth 1 https://github.com/r9y9/pyopenjtalk && "
+        "OPEN_JTALK_INSTALL_PREFIX=" + dirname + "/build "
+        "pip install -e pyopenjtalk",
+        shell=True,
+    )
+
+
+class pyopenjtalk_install(install):
+    def run(self):
+        install_hts_engine_API()
+        install_open_jtalk()
+        install_pyopenjtalk()
+        super(pyopenjtalk_install, self).run()
+
+
+requirements = {
+    "install": [
+        "unidecode>=1.0.22",
+        "inflect>=1.0.0",
+        # FIXME(kamo): undertheseq requires too tight resriction to the dependencies,
+        #   joblib, scikit-learn, ntltk
+        # "underthesea",
+        "jaconv",
+        "g2p_en",
+        "pypinyin",
+    ],
+    "setup": [],
+    "test": [],
+    "doc": []
+}
+install_requires = requirements["install"]
+setup_requires = requirements["setup"]
+tests_require = requirements["test"]
+extras_require = {
+    k: v for k, v in requirements.items() if k not in ["install", "setup"]
+}
+
+setup(
+    name="espnet_tts_frontend",
+    version="0.0.3",
+    long_description=open(os.path.join(dirname, 'README.md'),
+                          encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/espnet/espnet_tts_frontend",
+    packages=find_packages(include=["tacotron_cleaner*", "vietnamese_cleaner*"]),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    cmdclass={"pyopenjtalk": pyopenjtalk_install},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
