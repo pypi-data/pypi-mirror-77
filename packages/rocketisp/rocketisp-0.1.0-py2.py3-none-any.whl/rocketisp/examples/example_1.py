111aeff3af5313a499cf56a5e6e776aece17a816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:08 +0000

    Add package rocketisp/0.1.0/rocketisp-0.1.0-py2.py3-none-any.whl

---
+++ packages/rocketisp/rocketisp-0.1.0-py2.py3-none-any.whl/rocketisp/examples/example_1.py
@@ -0,0 +1,14 @@
+"""
+This example demonstrates the proper use of project: rocketisp
+"""
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../../"))  # needed to find rocketisp development version
+
+from rocketisp.rocket_isp import IspDelivered
+
+# TODO: create working example of project rocketisp
+
+my_class = IspDelivered()
+
