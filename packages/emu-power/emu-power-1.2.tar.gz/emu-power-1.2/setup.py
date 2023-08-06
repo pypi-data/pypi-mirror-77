4d3eefa457f53614860b1d2cc9680127b8d31faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:25 +0000

    Add package emu-power/1.2/emu-power-1.2.tar.gz

---
+++ packages/emu-power/emu-power-1.2.tar.gz/emu-power-1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="emu-power",
+    version="1.2",
+    author="Steven Bertolucci",
+    author_email="srbertol@mtu.edu",
+    description="A Python3 API to interface with Rainforest Automation's EMU-2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Steve0320/Emu-Api",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'serial'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
