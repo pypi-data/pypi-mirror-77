64f3b8a5ff794b62d63fc896b04067a38793545d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:17 +0000

    Add package python-pensionpro/0.6/python-pensionpro-0.6.tar.gz

---
+++ packages/python-pensionpro/python-pensionpro-0.6.tar.gz/python-pensionpro-0.6/pensionpro/errors.py
@@ -0,0 +1,19 @@
+from requests import HTTPError
+
+class PensionProError(HTTPError):
+    """ Base Error Class """
+
+class PensionProBadRequest(PensionProError):
+    """ Most 40X and 501 errors """
+
+class PensionProUnauthorized(PensionProError):
+    """ 401 Unauthorized """
+
+class PensionProRateLimited(PensionProError):
+    """ 429 Rate Limit Reached """
+
+class PensionProNotFound(PensionProError):
+    """ 404 Not Found """
+
+class PensionProServerError(PensionProError):
+    """ 50X Errors """
