c14622cb64a28a14e9b0395bd32b722f1c19a5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:42 +0000

    Add package django-monologue/0.1.0/django_monologue-0.1.0-py3-none-any.whl

---
+++ packages/django-monologue/django_monologue-0.1.0-py3-none-any.whl/monologue/models.py
@@ -0,0 +1,34 @@
+import markdown
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    permalink = models.CharField(max_length=128, blank=True)
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def render_content(self):
+        return markdown.markdown(self.content, extensions=["tables", "toc"])
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+    content = models.CharField(max_length=1024)
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ packages/django-monologue/django_monologue-0.1.0-py3-none-any.whl/monologue/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
+
+from . import forms
+from .models import Comment, Post, Tag
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.order_by('-created_at').all()
+    tags = Tag.objects.all()
+    return render(request, "index.html", {
+        'posts': posts,
+        'tags': tags,
+    })
+
+
+def post_view(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post, id=post_id)
+    tags = Tag.objects.all()
+    comments = Comment.objects.filter(post=post).order_by('created_at').all()
+    if request.method == "POST":
+        form = forms.CommentForm(request.POST)
+        if not form.is_valid():
+            return HttpResponseBadRequest(form.errors.as_ul())
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "post.html", {
+        'post': post,
+        'tags': tags,
+        "comments": comments,
+    })
+
+
+def tag_view(request: HttpRequest, tag_name: str) -> HttpResponse:
+    tag = get_object_or_404(Tag, name=tag_name)
+    posts = Post.objects.filter(tags=tag).order_by('-created_at').all()
+    tags = Tag.objects.all()
+    return render(request, "index.html", {
+        'posts': posts,
+        'tags': tags,
+    })
