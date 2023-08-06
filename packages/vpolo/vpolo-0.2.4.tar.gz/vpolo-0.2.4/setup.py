455e0086c02166b5bc8a0b9d88517927f6f63b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package vpolo/0.2.4/vpolo-0.2.4.tar.gz

---
+++ packages/vpolo/vpolo-0.2.4.tar.gz/vpolo-0.2.4/setup.py
@@ -0,0 +1,40 @@
+import sys
+
+import setuptools
+try:
+    from setuptools_rust import RustExtension, Binding
+except ImportError:
+    import subprocess
+
+    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust"])
+    if errno:
+        print("Please install setuptools-rust package")
+        raise SystemExit(errno)
+    else:
+        from setuptools_rust import RustExtension, Binding
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vpolo",
+    version="0.2.4",
+    author="Avi Srivastava",
+    author_email="asrivastava@cs.stonybrook.edu",
+    description="Support packages for Alevin tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/k3yavi/vpolo",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    zip_safe=False,
+    rust_extensions=[RustExtension("sce.sce", binding=Binding.RustCPython)],
+    install_requires=[
+       'pandas',
+       'scipy',
+    ]
+)
