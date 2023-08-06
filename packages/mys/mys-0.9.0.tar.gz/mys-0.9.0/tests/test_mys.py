9cf2174054ed2704840b43c68018a4b0facd7795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:32 +0000

    Add package mys/0.9.0/mys-0.9.0.tar.gz

---
+++ packages/mys/mys-0.9.0.tar.gz/mys-0.9.0/tests/test_mys.py
@@ -0,0 +1,25 @@
+import unittest
+import mys
+from .utils import read_file
+
+
+class MysTest(unittest.TestCase):
+
+    maxDiff = None
+
+    def assert_equal_to_file(self, actual, expected):
+        open(expected, 'w').write(actual)
+        self.assertEqual(actual, read_file(expected))
+
+    def test_all(self):
+        datas = [
+            'basics',
+            'hello_world',
+            'loops',
+            'calc'
+        ]
+
+        for data in datas:
+            self.assert_equal_to_file(
+                mys.transpile(read_file(f'tests/files/{data}.mys')),
+                f'tests/files/{data}.mys.dev.cpp')
