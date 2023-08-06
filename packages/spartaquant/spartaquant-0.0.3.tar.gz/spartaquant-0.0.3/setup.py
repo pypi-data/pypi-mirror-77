69f3691431a7ab0a7c137d7f5dbf384b2f67487c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package spartaquant/0.0.3/spartaquant-0.0.3.tar.gz

---
+++ packages/spartaquant/spartaquant-0.0.3.tar.gz/spartaquant-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spartaquant", # Replace with your own username
+    version="0.0.3",
+    author="Benjamin Meyer",
+    author_email="spartaquant@gmail.com",
+    description="SpartaQuant Python API",
+    long_description="This API provides many services you can use as a SpartaQuant developer",
+    long_description_content_type="text/markdown",
+    url="https://spartaquant.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
