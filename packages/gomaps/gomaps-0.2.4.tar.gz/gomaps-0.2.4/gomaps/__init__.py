ba3d03316586dec75ae31caa2b2b344dfee9a545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:41 +0000

    Add package gomaps/0.2.4/gomaps-0.2.4.tar.gz

---
+++ packages/gomaps/gomaps-0.2.4.tar.gz/gomaps-0.2.4/gomaps/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Author: David J. Morfe
+Application Name: gomaps
+Functionality Purpose: Acquire google maps data to utilize for web scraping
+Version: Beta
+'''
+
+name = "gomaps"
+from .gmapss import GoogleMaps, GoogleMapsResults, maps_search
