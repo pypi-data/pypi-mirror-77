5929a12660e697e1f03c4e267905f310ce79c9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:19 +0000

    Add package lrucli/2020.8.22/lrucli-2020.8.22.tar.gz

---
+++ packages/lrucli/lrucli-2020.8.22.tar.gz/lrucli-2020.8.22/lrucli/__main__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# by: Cody Kochmann
+
+import sys
+from argparse import ArgumentParser
+from functools import lru_cache
+
+__doc__ = '''
+This program provides lru cache functionality for
+lines running through stdin. This is helpful when
+you want to see a deduped view of some long input
+stream but don't want to wait for `sort | uniq`.
+'''
+
+DEFAULT_CACHE_SIZE = 4096
+
+
+def main():
+
+    parser = ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--maxsize',
+        type=int,
+        default=DEFAULT_CACHE_SIZE,
+        metavar=DEFAULT_CACHE_SIZE,
+        help='how many items to cache'
+    )
+
+    try:
+        set(
+            map(
+                lru_cache(parser.parse_args().maxsize)(sys.stdout.write),
+                sys.stdin
+            )
+        )
+    except Exception as e:
+        parser.print_help()
+        #raise e
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/lrucli/lrucli-2020.8.22.tar.gz/lrucli-2020.8.22/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+version = '2020.8.22'
+package_name = 'lrucli'
+packages = find_packages()
+
+assert package_name in packages, [package_name, packages]  # if package package_name doesnt show up, something is wrong
+
+setup(
+  name = package_name,
+  version = version,
+  packages = packages,
+  install_requires = [],
+  zip_safe=True,
+  description = 'command line interface for pythons lru_cache for more command line fu',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/{}'.format(package_name),
+  download_url = 'https://github.com/CodyKochmann/{}/tarball/{}'.format(package_name, version),
+  keywords = [package_name, 'cli', 'lru', 'dedup', 'uniq', 'stdin', 'shell', 'bash'],
+  entry_points = {
+    'console_scripts': [
+      'lru = lrucli.__main__:main'
+    ]
+  },
+  python_requires='>=3.2',
+  classifiers = []
+)
