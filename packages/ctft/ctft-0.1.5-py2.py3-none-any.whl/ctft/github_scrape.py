a62ee8f8dea979cfdc09e839c57b49ecc53a4242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:23 +0000

    Add package ctft/0.1.5/ctft-0.1.5-py2.py3-none-any.whl

---
+++ packages/ctft/ctft-0.1.5-py2.py3-none-any.whl/ctft/github_scrape.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from souper import souper
+import html2text
+from formatter import formatter
+import asyncio
+
+async def github_scraper(url):
+    soup = await souper(url)
+    article = soup.find("article")
+    if article:
+        markdwn = formatter(article)
+        return markdwn
