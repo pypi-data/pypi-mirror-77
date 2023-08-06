c69f7e989cc66300a2dd1ba73aa3f401f5505124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:29 +0000

    Add package kooft-api/0.5/kooft_api-0.5.tar.gz

---
+++ packages/kooft-api/kooft_api-0.5.tar.gz/kooft_api-0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kooft_api',
+      version='0.5',
+      description='Kooft API',
+      packages=['kooft_api'],
+      author_email='likel0vsky@gmail.com',
+      zip_safe=False)
