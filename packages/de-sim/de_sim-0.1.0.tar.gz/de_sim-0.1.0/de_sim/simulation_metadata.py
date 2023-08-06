75e730eb211fa211f802b1b148a3fec21335be9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package de-sim/0.1.0/de_sim-0.1.0.tar.gz

---
+++ packages/de-sim/de_sim-0.1.0.tar.gz/de_sim-0.1.0/de_sim/simulation_metadata.py
@@ -0,0 +1,146 @@
+""" Classes to represent the metadata of a simulation run
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2017-08-18
+:Copyright: 2016-2020, Karr Lab
+:License: MIT
+"""
+
+from dataclasses import dataclass
+from datetime import datetime
+import getpass
+import os
+import socket
+
+from de_sim.errors import SimulatorError
+from de_sim.simulation_config import SimulationConfig
+from wc_utils.util.misc import EnhancedDataClass
+from wc_utils.util.git import RepositoryMetadata
+
+
+@dataclass
+class RunMetadata(EnhancedDataClass):
+    """ Represent a simulation's run
+
+    Attributes:
+        ip_address (:obj:`str`): ip address of the machine that ran the simulation
+        start_time (:obj:`datetime`): simulation clock start time
+        run_time (:obj:`float`): simulation run time in real seconds
+    """
+
+    ip_address: str = None
+    start_time: datetime = None
+    run_time: float = None
+
+    def record_ip_address(self):
+        self.ip_address = socket.gethostbyname(socket.gethostname())
+
+    def record_start(self):
+        self.start_time = datetime.now()
+
+    def record_run_time(self):
+        self.run_time = (datetime.now() - self.start_time).total_seconds()
+
+    def __setattr__(self, name, value):
+        """ Validate an attribute when it is changed """
+        try:
+            super().__setattr__(name, value)
+        except TypeError as e:
+            raise SimulatorError(e)
+
+    def semantically_equal(self, other):
+        """ Evaluate whether two instances of :obj:`RunMetadata` are semantically equal
+
+        Always returns :obj:`True`, because none of the attributes in :obj:`RunMetadata` are important
+        to a simulations' results
+
+        Args:
+            other (:obj:`Object`): other object
+
+        Returns:
+            :obj:`bool`: :obj:`True`
+        """
+        return True
+
+
+@dataclass
+class AuthorMetadata(EnhancedDataClass):
+    """ Represents a simulation's author
+
+    Attributes:
+        name (:obj:`str`): authors' name
+        email (:obj:`str`): author's email address
+        username (:obj:`str`): authors' username
+        organization (:obj:`str`): author's organization
+    """
+
+    name: str = None
+    email: str = None
+    username: str = None
+    organization: str = None
+
+    def __post_init__(self):
+
+        if self.username is None:
+            try:
+                self.username = getpass.getuser()
+            except Exception:    # pragma: no cover
+                self.username = None
+
+    def __setattr__(self, name, value):
+        """ Validate an attribute when it is changed """
+        try:
+            super().__setattr__(name, value)
+        except TypeError as e:
+            raise SimulatorError(e)
+
+
+@dataclass
+class SimulationMetadata(EnhancedDataClass):
+    """ Represents the metadata of a discrete-event simulation run
+
+    Attributes:
+        simulation_config (:obj:`SimulationConfig`): information about the simulation's configuration
+            (e.g. start time, maximum time)
+        run (:obj:`RunMetadata`): information about the simulation's run (e.g. start time, duration)
+        author (:obj:`AuthorMetadata`): information about the person who ran the simulation
+            (e.g. name, email)
+        simulator (:obj:`RepositoryMetadata`): metadata about this simulator's git repository
+    """
+
+    simulation_config: SimulationConfig
+    run: RunMetadata
+    author: AuthorMetadata
+    simulator_repo: RepositoryMetadata = None
+
+    @staticmethod
+    def get_pathname(dirname):
+        """ See docstring in :obj:`EnhancedDataClass`
+        """
+        return os.path.join(dirname, 'simulation_metadata.pickle')
+
+    def __setattr__(self, name, value):
+        """ Validate an attribute when it is changed """
+        try:
+            super().__setattr__(name, value)
+        except TypeError as e:
+            raise SimulatorError(e)
+
+    def semantically_equal(self, other):
+        """ Are two instances semantically equal with respect to a simulation's predictions?
+
+        Overrides `semantically_equal` in :obj:`EnhancedDataClass`.
+        Ignore run, as a :obj:`RunMetadata` is not semantically meaningful to a simulation's predictions
+        and depends on a simulation's performance.
+
+        Args:
+            other (:obj:`Object`): other object
+
+        Returns:
+            :obj:`bool`: :obj:`True` if `other` is semantically equal to `self`, :obj:`False` otherwise
+        """
+        return self.simulation_config.semantically_equal(other.simulation_config) and \
+            self.author.semantically_equal(other.author) and \
+            self.simulator_repo == other.simulator_repo # RepositoryMetadata not an EnhancedDataClass,
+                                                        # so it doesn't define semantically_equal()
---
+++ packages/de-sim/de_sim-0.1.0.tar.gz/de_sim-0.1.0/de_sim/template_sim_objs.py
@@ -0,0 +1,67 @@
+""" Template simulation objects
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-05-16
+:Copyright: 2018-2020, Karr Lab
+:License: MIT
+"""
+
+from de_sim.errors import SimulatorError
+import de_sim
+
+
+class NextEvent(de_sim.EventMessage):
+    "Schedule the next event"
+
+
+class TemplatePeriodicSimulationObject(de_sim.SimulationObject):
+    """ Template self-clocking `SimulationObject`
+
+    Events occur at time `start_time`, `start_time + period`, `start_time + 2*period`, ...
+
+    To minimize roundoff errors in event times track the number of periods, and multiply by period
+    to determine event times.
+
+    Attributes:
+        period (:obj:`float`): interval between events, in simulated time units
+        num_periods (:obj:`int`): number of periods executed
+        start_time (:obj:`float`, optional): the time of the first periodic event
+    """
+
+    def __init__(self, name, period, start_time=0.):
+        if period <= 0:
+            raise SimulatorError("period must be positive, but is {}".format(period))
+        self.period = period
+        self.start_time = start_time
+        self.num_periods = 0
+        super().__init__(name, start_time=start_time)
+
+    def schedule_next_event(self):
+        """ Schedule the next event in `self.period` simulated time units
+        """
+        next_event_time = self.start_time + self.num_periods * self.period
+        self.num_periods += 1
+        self.send_event_absolute(next_event_time, self, NextEvent())
+
+    def handle_event(self):
+        """ Handle the periodic event
+
+        Derived classes must override this method and actually handle the event
+        """
+        pass    # pragma: no cover     # must be overridden
+
+    def init_before_run(self):
+        # create the initial event
+        self.schedule_next_event()
+
+    def handle_simulation_event(self, event):
+        self.handle_event()
+        self.schedule_next_event()
+
+    def get_state(self):
+        return ''    # pragma: no cover
+
+    event_handlers = [(NextEvent, handle_simulation_event)]
+
+    # register the message type sent
+    messages_sent = [NextEvent]
