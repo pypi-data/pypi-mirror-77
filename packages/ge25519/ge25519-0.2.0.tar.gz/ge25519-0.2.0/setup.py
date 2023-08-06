5ced3f686e14b3365adc4a2dcef923f00a61523f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:15 +0000

    Add package ge25519/0.2.0/ge25519-0.2.0.tar.gz

---
+++ packages/ge25519/ge25519-0.2.0.tar.gz/ge25519-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ge25519",
+    version="0.2.0",
+    packages=["ge25519",],
+    install_requires=[
+        "fe25519>=0.2.0",
+        "parts>=0.2.1",
+        "bitlist>=0.3.1",
+        "fountains>=0.2.1",
+    ],
+    license="MIT",
+    url="https://github.com/nthparty/ge25519",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Native Python implementation of Ed25519 "+\
+                "group elements and operations.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
