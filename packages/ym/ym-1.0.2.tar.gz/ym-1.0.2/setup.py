33b8f927d6e8c817a3a3d9e19e349d89707de556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:39 +0000

    Add package ym/1.0.2/ym-1.0.2.tar.gz

---
+++ packages/ym/ym-1.0.2.tar.gz/ym-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ym",
+    version="1.0.2",
+    description="Data type that handles year and month values.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="hoto17296",
+    license="MIT",
+    url="https://github.com/hoto17296/python-ym",
+    py_modules=["ym"],
+)
