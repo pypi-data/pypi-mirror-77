796c52c15c8d3489412a01fb3ee36cd05438edd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:34 +0000

    Add package simple-pypi-template/1.0.1/simple-pypi-template-1.0.1.tar.gz

---
+++ packages/simple-pypi-template/simple-pypi-template-1.0.1.tar.gz/simple-pypi-template-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-pypi-template",
+    version="1.0.1",
+    author="Ramón Invarato Menéndez",
+    author_email="r.invarato@gmail.com",
+    description="Template and instructions to create a Python module and upload to PyPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Invarato/simple_pypi_template_project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
