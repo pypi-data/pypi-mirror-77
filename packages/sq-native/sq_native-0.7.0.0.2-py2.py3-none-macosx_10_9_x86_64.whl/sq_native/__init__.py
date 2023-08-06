c5741ad9aed9a8c14c13c046200f1abba4239b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:20 +0000

    Add package sq-native/0.7.0.0.2/sq_native-0.7.0.0.2-py2.py3-none-macosx_10_9_x86_64.whl

---
+++ packages/sq-native/sq_native-0.7.0.0.2-py2.py3-none-macosx_10_9_x86_64.whl/sq_native/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, 2017, 2018, 2019 Sqreen. All rights reserved.
+# Please refer to our terms for more information:
+#
+#     https://www.sqreen.io/terms.html
+#
+"""Sqreen Python Native Module"""
