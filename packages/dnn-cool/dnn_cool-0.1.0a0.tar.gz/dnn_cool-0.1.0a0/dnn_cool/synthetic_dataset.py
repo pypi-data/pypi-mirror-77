e280eadb77e8b42022c6480c6c027f363ce4705b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:49 +0000

    Add package dnn-cool/0.1.0a0/dnn_cool-0.1.0a0.tar.gz

---
+++ packages/dnn-cool/dnn_cool-0.1.0a0.tar.gz/dnn_cool-0.1.0a0/dnn_cool/synthetic_dataset.py
@@ -0,0 +1,343 @@
+from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
+import cv2
+import torch
+
+from functools import partial
+
+from torch.utils.data import DataLoader
+
+from dnn_cool.converters import TypeGuesser, ValuesConverter, TaskConverter, Converters
+from dnn_cool.decoders import Decoder, BoundedRegressionDecoder
+from dnn_cool.project import Project
+from dnn_cool.task_flow import BoundedRegressionTask, BinaryClassificationTask, TaskFlow, ClassificationTask, \
+    MultilabelClassificationTask
+from dnn_cool.utils import torch_split_dataset
+from dnn_cool.value_converters import binary_value_converter, classification_converter, multilabel_converter
+from torch import nn
+
+
+def generate_camera_blocked_image():
+    img = np.zeros((64, 64, 3), dtype=np.uint8)
+
+    # randomly add some noise
+    n = 40
+    i = np.random.randint(0, 64, size=n)
+    j = np.random.randint(0, 64, size=n)
+    c = np.random.randint(0, 3, size=n)
+    values = np.random.randint(0, 255, size=n)
+    img[i, j, c] = values
+
+    return img, {'camera_blocked': True}
+
+
+def generate_door_open_image():
+    img = np.ones((64, 64, 3), dtype=np.uint8) * 255
+
+    # randomly add some noise
+    n = 40
+    i = np.random.randint(0, 64, size=n)
+    j = np.random.randint(0, 64, size=n)
+    c = np.random.randint(0, 3, size=n)
+    values = np.random.randint(0, 255, size=n)
+    img[i, j, c] = values
+
+    return img, {'camera_blocked': False, 'door_open': True, 'person_present': False}
+
+
+def generate_door_closed_image(door_locked):
+    img, res = generate_door_open_image()
+
+    offsets = np.random.randint(-10, 10, size=4)
+    x1, y1 = int(10 + offsets[0]), int(10 + offsets[1])
+    x2, y2 = int(50 + offsets[2]), int(50 + offsets[2])
+
+    img = cv2.rectangle(img, (x1, y1), (x2, y2), color=(139, 69, 19), thickness=-1)
+
+    if door_locked:
+        lock_start = x1, int((y1 + y2) / 2)
+        lock_end = lock_start[0] + 30, lock_start[1] + 30
+        img = cv2.rectangle(img, lock_start, lock_end, color=(255, 255, 255), thickness=-1)
+
+    res['door_locked'] = door_locked
+    res['camera_blocked'] = False
+    res['door_open'] = False
+    return img, res
+
+
+def select_facial_characteristics():
+    choices = [
+        [(255, 0, 255), (0, 2)],
+        [(0, 255, 255), (1, 2)],
+        [(255, 255, 0), (0, 1)],
+        [(0, 0, 0), ()],
+        [(255, 0, 0), (0,)],
+        [(0, 255, 0), (1,)],
+        [(0, 0, 255), (2,)],
+    ]
+    choice = np.random.randint(0, len(choices), size=1)[0]
+    return choices[choice]
+
+
+def draw_person(img, res, shirt_type='blue'):
+    head_radius = 6
+    offsets = np.random.randint(-4, 4, size=4)
+    head = int(30 + offsets[0]), int(10 + offsets[1])
+
+    color, face_characteristics = select_facial_characteristics()
+    img = cv2.circle(img, head, head_radius, color=color, thickness=-1)
+
+    res['person_present'] = True
+    res['face_x1'] = head[0] - head_radius
+    res['face_y1'] = head[1] - head_radius
+    res['face_w'] = 2 * head_radius
+    res['face_h'] = 2 * head_radius
+    res['facial_characteristics'] = ','.join(map(str, face_characteristics))
+
+    offsets = np.random.randint(-2, 2, size=4)
+    d = head_radius * 2
+    rec_start = head[0] - head_radius + offsets[0], head[1] + head_radius + offsets[1]
+    rec_end = rec_start[0] + d + offsets[2], rec_start[1] + 30 + offsets[3]
+
+    if shirt_type == 'blue':
+        color = (0, 0, 255)
+        shirt_label = 0
+    elif shirt_type == 'red':
+        color = (255, 0, 0)
+        shirt_label = 1
+    elif shirt_type == 'yellow':
+        color = (255, 255, 0)
+        shirt_label = 2
+    elif shirt_type == 'cyan':
+        color = (0, 255, 255)
+        shirt_label = 3
+    elif shirt_type == 'magenta':
+        color = (255, 0, 255)
+        shirt_label = 4
+    elif shirt_type == 'green':
+        color = (0, 255, 0)
+        shirt_label = 5
+    else:
+        # black
+        color = (0, 0, 0)
+        shirt_label = 6
+
+    cv2.rectangle(img, rec_start, rec_end, color=color, thickness=-1)
+
+    res['body_x1'] = rec_start[0]
+    res['body_y1'] = rec_start[1]
+    res['body_w'] = rec_end[0] - rec_start[0]
+    res['body_h'] = rec_end[1] - rec_start[1]
+    res['shirt_type'] = shirt_label
+
+    return img, res
+
+
+def generate_image_with_person(shirt_type='blue'):
+    img, res = generate_door_open_image()
+    img, res = draw_person(img, res, shirt_type)
+    return img, res
+
+
+def generate_sample():
+    generators = [generate_camera_blocked_image,
+                  generate_door_open_image,
+                  partial(generate_door_closed_image, door_locked=True),
+                  partial(generate_door_closed_image, door_locked=False),
+                  partial(generate_image_with_person, shirt_type='blue'),
+                  partial(generate_image_with_person, shirt_type='red'),
+                  partial(generate_image_with_person, shirt_type='yellow'),
+                  partial(generate_image_with_person, shirt_type='cyan'),
+                  partial(generate_image_with_person, shirt_type='magenta'),
+                  partial(generate_image_with_person, shirt_type='green'),
+                  partial(generate_image_with_person, shirt_type='black')
+                  ]
+    choice = np.random.randint(0, len(generators), size=1)[0]
+    return generators[choice]()
+
+
+def create_df_and_images_tensor():
+    imgs = []
+    rows = []
+    names = []
+    for i in range(int(1e4)):
+        img, row = generate_sample()
+        imgs.append(torch.tensor(img).permute(2, 0, 1))
+        rows.append(row)
+        names.append(f'{i}.jpg')
+
+    df = pd.DataFrame(rows)
+    df['img'] = names
+    df.loc[:5, 'camera_blocked'] = np.nan
+    return torch.stack(imgs, dim=0).float() / 255., df
+
+
+def synthenic_dataset_preparation():
+    def bounded_regression_converter(values):
+        values = values.astype(float) / 64
+        values[np.isnan(values)] = -1
+        return torch.tensor(values).float().unsqueeze(dim=-1)
+
+    def bounded_regression_task(name, labels):
+        return BoundedRegressionTask(name, labels, module=nn.Linear(256, 1), decoder=BoundedRegressionDecoder(scale=64))
+
+    def binary_classification_task(name, labels):
+        return BinaryClassificationTask(name, labels, module=nn.Linear(256, 1))
+
+    def classification_task(name, labels):
+        n_classes = len(labels[labels >= 0].unique())
+        return ClassificationTask(name, labels, module=nn.Linear(256, n_classes))
+
+    def multilabel_task(name, labels):
+        n_classes = labels.shape[1]
+        return MultilabelClassificationTask(name, labels, module=nn.Linear(256, n_classes))
+
+    imgs, df = create_df_and_images_tensor()
+    output_col = ['camera_blocked', 'door_open', 'person_present', 'door_locked',
+                  'face_x1', 'face_y1', 'face_w', 'face_h',
+                  'facial_characteristics',
+                  'body_x1', 'body_y1', 'body_w', 'body_h', 'shirt_type']
+    type_guesser = TypeGuesser()
+    type_guesser.type_mapping['camera_blocked'] = 'binary'
+    type_guesser.type_mapping['door_open'] = 'binary'
+    type_guesser.type_mapping['person_present'] = 'binary'
+    type_guesser.type_mapping['door_locked'] = 'binary'
+    type_guesser.type_mapping['face_x1'] = 'continuous'
+    type_guesser.type_mapping['face_y1'] = 'continuous'
+    type_guesser.type_mapping['face_w'] = 'continuous'
+    type_guesser.type_mapping['face_h'] = 'continuous'
+    type_guesser.type_mapping['body_x1'] = 'continuous'
+    type_guesser.type_mapping['body_y1'] = 'continuous'
+    type_guesser.type_mapping['body_w'] = 'continuous'
+    type_guesser.type_mapping['body_h'] = 'continuous'
+    type_guesser.type_mapping['img'] = 'img'
+    type_guesser.type_mapping['shirt_type'] = 'category'
+    type_guesser.type_mapping['facial_characteristics'] = 'multilabel'
+
+    values_converter = ValuesConverter()
+    values_converter.type_mapping['img'] = lambda x: imgs
+    values_converter.type_mapping['binary'] = binary_value_converter
+    values_converter.type_mapping['continuous'] = bounded_regression_converter
+    values_converter.type_mapping['category'] = classification_converter
+    values_converter.type_mapping['multilabel'] = multilabel_converter
+
+    task_converter = TaskConverter()
+
+    task_converter.type_mapping['binary'] = binary_classification_task
+    task_converter.type_mapping['continuous'] = bounded_regression_task
+    task_converter.type_mapping['category'] = classification_task
+    task_converter.type_mapping['multilabel'] = multilabel_task
+
+    converters = Converters()
+    converters.task = task_converter
+    converters.type = type_guesser
+    converters.values = values_converter
+
+    project = Project(df, input_col='img', output_col=output_col, converters=converters, project_dir='./security_project')
+
+    @project.add_flow
+    def face_regression(flow, x, out):
+        out += flow.face_x1(x.face_localization)
+        out += flow.face_y1(x.face_localization)
+        out += flow.face_w(x.face_localization)
+        out += flow.face_h(x.face_localization)
+        out += flow.facial_characteristics(x.features)
+        return out
+
+    @project.add_flow
+    def body_regression(flow, x, out):
+        out += flow.body_x1(x.body_localization)
+        out += flow.body_y1(x.body_localization)
+        out += flow.body_w(x.body_localization)
+        out += flow.body_h(x.body_localization)
+        out += flow.shirt_type(x.features)
+        return out
+
+    @project.add_flow
+    def person_regression(flow, x, out):
+        out += flow.face_regression(x)
+        out += flow.body_regression(x)
+        return out
+
+    @project.add_flow
+    def full_flow(flow, x, out):
+        out += flow.camera_blocked(x.features)
+        out += flow.door_open(x.features) | (~out.camera_blocked)
+        out += flow.door_locked(x.features) | (~out.door_open)
+        out += flow.person_present(x.features) | out.door_open
+        out += flow.person_regression(x) | out.person_present
+        return out
+
+    dataset = project.get_full_flow().get_dataset()
+    train_dataset, val_dataset = torch_split_dataset(dataset, random_state=42)
+    train_loader = DataLoader(train_dataset, batch_size=32 * torch.cuda.device_count(), shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32 * torch.cuda.device_count(), shuffle=False)
+    nested_loaders = OrderedDict({
+        'train': train_loader,
+        'valid': val_loader
+    })
+
+    class SecurityModule(nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.seq = nn.Sequential(
+                nn.Conv2d(3, 64, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 128, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.AvgPool2d(2),
+                nn.Conv2d(128, 128, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(128, 256, kernel_size=5),
+                nn.AvgPool2d(2),
+                nn.ReLU(inplace=True),
+            )
+
+            self.features_seq = nn.Sequential(
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.AdaptiveAvgPool2d(1),
+                nn.Flatten(),
+            )
+
+            self.face_localization_seq = nn.Sequential(
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.AdaptiveAvgPool2d(1),
+                nn.Flatten(),
+            )
+
+            self.body_localization_seq = nn.Sequential(
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=5),
+                nn.ReLU(inplace=True),
+                nn.AdaptiveAvgPool2d(1),
+                nn.Flatten(),
+            )
+
+            self.flow_module = full_flow.torch()
+
+        def forward(self, x):
+            res = {}
+            common = self.seq(x['img'])
+            res['features'] = self.features_seq(common)
+            res['face_localization'] = self.face_localization_seq(common)
+            res['body_localization'] = self.body_localization_seq(common)
+            res['gt'] = x.get('gt')
+            return self.flow_module(res)
+
+    model = SecurityModule()
+    datasets = {
+        'train': train_dataset,
+        'valid': val_dataset,
+        'infer': val_dataset
+    }
+    return model, nested_loaders, datasets, project
---
+++ packages/dnn-cool/dnn_cool-0.1.0a0.tar.gz/dnn_cool-0.1.0a0/dnn_cool/task_flow.py
@@ -0,0 +1,350 @@
+from typing import Iterable, Optional, Callable, Tuple
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset
+
+from dnn_cool.activations import CompositeActivation
+from dnn_cool.datasets import FlowDataset, LeafTaskDataset
+from dnn_cool.decoders import BinaryDecoder, TaskFlowDecoder, Decoder, ClassificationDecoder, \
+    MultilabelClassificationDecoder
+from dnn_cool.evaluation import EvaluationCompositeVisitor, EvaluationVisitor
+from dnn_cool.filter import FilterCompositeVisitor, FilterVisitor
+from dnn_cool.losses import TaskFlowLoss, ReducedPerSample
+from dnn_cool.metrics import TorchMetric, BinaryAccuracy, ClassificationAccuracy, \
+    ClassificationF1Score, ClassificationPrecision, ClassificationRecall, BinaryF1Score, \
+    BinaryPrecision, BinaryRecall, MeanAbsoluteError, MultiLabelClassificationAccuracy
+from dnn_cool.missing_values import positive_values, positive_values_unsqueezed, has_no_missing_labels
+from dnn_cool.modules import SigmoidAndMSELoss, Identity, TaskFlowModule
+from dnn_cool.treelib import TreeExplainer
+
+
+class ITask:
+
+    def __init__(self, name: str, inputs, available_func=None, metrics: Tuple[str, TorchMetric] = ()):
+        self.name = name
+        self.inputs = inputs
+        self._available_func = available_func
+        self._metrics = metrics
+
+    def get_name(self):
+        return self.name
+
+    def get_activation(self) -> Optional[nn.Module]:
+        return None
+
+    def get_decoder(self):
+        return None
+
+    def get_filter(self):
+        return FilterVisitor(self, prefix='')
+
+    def get_evaluator(self):
+        return EvaluationVisitor(self, prefix='')
+
+    def has_children(self):
+        return False
+
+    def get_available_func(self):
+        return self._available_func
+
+    def get_loss(self):
+        raise NotImplementedError()
+
+    def get_per_sample_loss(self):
+        raise NotImplementedError()
+
+    def torch(self):
+        """
+        Creates a new instance of a Pytorch module, that has to be invoked for those parts of the input, for which
+        the precondition is satisfied.
+        :return:
+        """
+        raise NotImplementedError()
+
+    def get_inputs(self, *args, **kwargs):
+        return self.inputs
+
+    def get_labels(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def get_dataset(self, **kwargs):
+        raise NotImplementedError()
+
+    def get_metrics(self):
+        for i in range(len(self._metrics)):
+            metric_name, metric = self._metrics[i]
+            metric.bind_to_task(self)
+        return self._metrics
+
+
+class Task(ITask):
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss: Callable,
+                 per_sample_loss: Callable,
+                 available_func: Callable = None,
+                 inputs=None,
+                 activation: Optional[nn.Module] = None,
+                 decoder: Decoder = None,
+                 module: Optional[nn.Module] = Identity(),
+                 metrics: Tuple[str, TorchMetric] = ()):
+        super().__init__(name, inputs, available_func, metrics)
+        self._activation = activation
+        self._decoder = decoder
+        self._loss = loss
+        self._per_sample_loss = per_sample_loss
+        self._module = module
+        self._inputs = inputs
+        self._labels = labels
+
+    def get_activation(self) -> Optional[nn.Module]:
+        return self._activation
+
+    def get_decoder(self):
+        return self._decoder
+
+    def get_loss(self):
+        return self._loss
+
+    def get_per_sample_loss(self):
+        return self._per_sample_loss
+
+    def torch(self):
+        return self._module
+
+    def get_inputs(self, *args, **kwargs):
+        return self._inputs
+
+    def get_labels(self, *args, **kwargs):
+        return self._labels
+
+    def get_dataset(self, **kwargs):
+        return LeafTaskDataset(self._inputs, self._labels)
+
+
+class BinaryHardcodedTask(Task):
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss: Callable = None,
+                 per_sample_loss: Callable = None,
+                 available_func: Callable = positive_values,
+                 inputs=None,
+                 activation: Optional[nn.Module] = None,
+                 decoder: Decoder = None,
+                 module: nn.Module = Identity(),
+                 metrics: Tuple[str, TorchMetric] = ()):
+        super().__init__(name=name,
+                         labels=labels,
+                         loss=loss,
+                         per_sample_loss=per_sample_loss,
+                         available_func=available_func,
+                         inputs=inputs,
+                         activation=activation,
+                         decoder=decoder,
+                         module=module,
+                         metrics=metrics)
+
+
+class BoundedRegressionTask(Task):
+    """
+    Represents a regression task, where the labels are normalized between 0 and 1. Examples include bounding box top
+    left corners regression. Here are the defaults:
+    * activation - `nn.Sigmoid()` - so that the output is in `[0, 1]`
+    * loss - `SigmoidAndMSELoss` - sigmoid on the logits, then standard mean squared error loss.
+    """
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss=SigmoidAndMSELoss(reduction='mean'),
+                 per_sample_loss=ReducedPerSample(SigmoidAndMSELoss(reduction='none'), reduction=torch.sum),
+                 available_func: Callable = None,
+                 module=Identity(),
+                 activation: Optional[nn.Module] = nn.Sigmoid(),
+                 decoder: Decoder = None,
+                 inputs=None,
+                 metrics=(
+                         ('mean_absolute_error', MeanAbsoluteError()),
+                 )):
+        super().__init__(name=name,
+                         labels=labels,
+                         loss=loss,
+                         per_sample_loss=per_sample_loss,
+                         available_func=available_func,
+                         inputs=inputs,
+                         activation=activation,
+                         decoder=decoder,
+                         module=module,
+                         metrics=metrics)
+
+
+class BinaryClassificationTask(Task):
+    """
+    Represents a normal binary classification task. Labels should be between 0 and 1.
+    * activation - `nn.Sigmoid()`
+    * loss - ``nn.BCEWithLogitsLoss()`
+    """
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss=nn.BCEWithLogitsLoss(reduction='mean'),
+                 per_sample_loss=ReducedPerSample(nn.BCEWithLogitsLoss(reduction='none'), reduction=torch.mean),
+                 available_func=positive_values,
+                 inputs=None,
+                 activation: Optional[nn.Module] = nn.Sigmoid(),
+                 decoder: Decoder = BinaryDecoder(),
+                 module: nn.Module = Identity(),
+                 metrics: Tuple[str, TorchMetric] = (
+                         ('accuracy', BinaryAccuracy()),
+                         ('f1_score', BinaryF1Score()),
+                         ('precision', BinaryPrecision()),
+                         ('recall', BinaryRecall()),
+                 )):
+        super().__init__(name=name,
+                         labels=labels,
+                         loss=loss,
+                         per_sample_loss=per_sample_loss,
+                         available_func=available_func,
+                         inputs=inputs,
+                         activation=activation,
+                         decoder=decoder,
+                         module=module,
+                         metrics=metrics)
+
+
+class ClassificationTask(Task):
+    """
+    Represents a classification task. Labels should be integers from 0 to N-1, where N is the number of classes
+    * activation - `nn.Softmax(dim=-1)`
+    * loss - `nn.CrossEntropyLoss()`
+    """
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss=nn.CrossEntropyLoss(reduction='mean'),
+                 per_sample_loss=ReducedPerSample(nn.CrossEntropyLoss(reduction='none'), torch.mean),
+                 available_func=positive_values_unsqueezed,
+                 inputs=None,
+                 activation=nn.Softmax(dim=-1),
+                 decoder=ClassificationDecoder(),
+                 module: nn.Module = Identity(),
+                 metrics=(
+                         ('accuracy', ClassificationAccuracy()),
+                         ('f1_score', ClassificationF1Score()),
+                         ('precision', ClassificationPrecision()),
+                         ('recall', ClassificationRecall()),
+                 )):
+        super().__init__(name=name,
+                         labels=labels,
+                         loss=loss,
+                         per_sample_loss=per_sample_loss,
+                         available_func=available_func,
+                         inputs=inputs,
+                         activation=activation,
+                         decoder=decoder,
+                         module=module,
+                         metrics=metrics)
+
+
+class MultilabelClassificationTask(Task):
+
+    def __init__(self,
+                 name: str,
+                 labels,
+                 loss=nn.BCEWithLogitsLoss(reduction='mean'),
+                 per_sample_loss=ReducedPerSample(nn.BCEWithLogitsLoss(reduction='none'), torch.mean),
+                 available_func=has_no_missing_labels,
+                 inputs=None,
+                 activation=nn.Sigmoid(),
+                 decoder=MultilabelClassificationDecoder(),
+                 module: nn.Module = Identity(),
+                 metrics=(
+                         ('accuracy', MultiLabelClassificationAccuracy()),
+                 )):
+        super().__init__(name=name,
+                         labels=labels,
+                         loss=loss,
+                         per_sample_loss=per_sample_loss,
+                         available_func=available_func,
+                         inputs=inputs,
+                         activation=activation,
+                         decoder=decoder,
+                         module=module,
+                         metrics=metrics)
+
+
+class TaskFlow(ITask):
+
+    def __init__(self, name, tasks: Iterable[ITask], flow_func=None, inputs=None, available_func=None):
+        super().__init__(name, inputs, available_func)
+        self.tasks = {}
+        for task in tasks:
+            self.tasks[task.get_name()] = task
+        if flow_func is not None:
+            self._flow_func = flow_func
+
+    def get_loss(self):
+        return TaskFlowLoss(self, parent_reduction='mean', child_reduction='per_sample')
+
+    def get_per_sample_loss(self):
+        return TaskFlowLoss(self, parent_reduction='per_sample', child_reduction='per_sample')
+
+    def torch(self):
+        return TaskFlowModule(self)
+
+    def has_children(self):
+        return True
+
+    def get_dataset(self, **kwargs) -> Dataset:
+        return FlowDataset(self, **kwargs)
+
+    def flow(self, x, out):
+        raise NotImplementedError()
+
+    def get_flow_func(self):
+        if hasattr(self, '_flow_func') and self._flow_func is not None:
+            return self._flow_func
+        return self.__class__.flow
+
+    def get_metrics(self):
+        all_metrics = []
+        for task in self.tasks.values():
+            all_metrics += task.get_metrics()
+        return all_metrics
+
+    def get_treelib_explainer(self):
+        return TreeExplainer(self)
+
+    def get_labels(self, *args, **kwargs):
+        all_labels = []
+        for task in self.tasks.values():
+            all_labels += task.get_labels()
+        return all_labels
+
+    def get_decoder(self):
+        return TaskFlowDecoder(self)
+
+    def get_activation(self) -> Optional[nn.Module]:
+        return CompositeActivation(self)
+
+    def get_evaluator(self):
+        return EvaluationCompositeVisitor(self, prefix='')
+
+    def get_filter(self):
+        return FilterCompositeVisitor(self, prefix='')
+
+    def get_all_children(self, prefix=''):
+        tasks = {}
+        for task_name, task in self.tasks.items():
+            if task.has_children():
+                tasks.update(task.get_all_children(prefix=f'{prefix}{task.get_name()}.'))
+            else:
+                tasks[prefix + task_name] = task
+        return tasks
