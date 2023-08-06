dbdf016c7b0c75de0b426b2ae57f421917d68605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package portable-spreadsheet/1.1.3/portable-spreadsheet-1.1.3.tar.gz

---
+++ packages/portable-spreadsheet/portable-spreadsheet-1.1.3.tar.gz/portable-spreadsheet-1.1.3/portable_spreadsheet/__init__.py
@@ -0,0 +1,10 @@
+from .spreadsheet import Spreadsheet  # noqa
+from .cell import Cell  # noqa
+from .cell_indices import CellIndices  # noqa
+from .cell_slice import CellSlice  # noqa
+from .grammars import GRAMMARS  # noqa
+from .grammar_utils import GrammarUtils  # noqa
+from .skipped_label import SkippedLabel  # noqa
+
+__version__ = "1.1.3"
+__status__ = "Production"
