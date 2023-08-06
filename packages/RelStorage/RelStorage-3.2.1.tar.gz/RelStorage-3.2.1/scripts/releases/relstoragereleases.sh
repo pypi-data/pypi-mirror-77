131be548b8886865d7c2a61ce88919d55eacc6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:31 +0000

    Add package RelStorage/3.2.1/RelStorage-3.2.1.tar.gz

---
+++ packages/RelStorage/RelStorage-3.2.1.tar.gz/RelStorage-3.2.1/scripts/releases/relstoragereleases.sh
@@ -0,0 +1,26 @@
+#!/opt/local/bin/bash
+
+# Quick hack script to create many gevent releases.
+# Contains hardcoded paths. Probably only works on my (JAM) machine
+# (OS X 10.11)
+
+mkdir /tmp/RelStorage/
+
+
+# 2.7 is a python.org build, builds a 10_9_intel wheel
+./relstoragerel.sh /usr/local/bin/python2.7
+
+# 3.5 is a python.org build, builds a 10_6_intel wheel
+./relstoragerel.sh /usr/local/bin/python3.5
+
+# 3.6 is a python.org build, builds a 10_6_intel wheel
+./relstoragerel.sh /usr/local/bin/python3.6
+
+# 3.7 is a python.org build, builds a 10_9_intel wheel
+./relstoragerel.sh /usr/local/bin/python3.7
+
+# Likewise for 3.8
+./relstoragerel.sh /usr/local/bin/python3.8
+
+# PyPy 4.0
+./relstoragerel.sh `which pypy`
