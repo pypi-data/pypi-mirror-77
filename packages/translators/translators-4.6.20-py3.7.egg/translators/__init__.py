dcfe759000b1e8062775a7e0aefd67c1d25da2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:07 +0000

    Add package translators/4.6.20/translators-4.6.20-py3.7.egg

---
+++ packages/translators/translators-4.6.20-py3.7.egg/translators/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "4.6.20"
+__author__ = "UlionTse"
+
+from .apis import alibaba, baidu, bing, deepl, google, sogou, tencent, youdao
+from .apis import _alibaba, _baidu, _bing, _deepl, _google, _sogou, _tencent, _youdao
