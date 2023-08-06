3d3947347cd6ec756fcb644a1a2d320b3cdc447e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:29 +0000

    Add package pymanga-dl/0.1.10/pymanga_dl-0.1.10-py3-none-any.whl

---
+++ packages/pymanga-dl/pymanga_dl-0.1.10-py3-none-any.whl/manga_dl/api.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: muketong
+@file: api.py
+@time: 2020-08-15
+"""
+
+import requests
+from tenacity import retry, wait_fixed, wait_exponential, retry_if_exception_type, stop_after_attempt
+
+from . import config
+from .exceptions import RequestError, ResponseError, DataError
+
+
+class MangaApi:
+    # class property
+    # 子类修改时使用deepcopy
+    session = requests.Session()
+    session.headers.update(config.get("fake_headers"))
+    if config.get("proxies"):
+        session.proxies.update(config.get("proxies"))
+    session.headers.update({"referer": "http://www.google.com/"})
+
+    @classmethod
+    @retry(wait=wait_fixed(5), stop=stop_after_attempt(1000))
+    def request(cls, url, method="GET", data=None):
+        if method == "GET":
+            resp = cls.session.get(url, params=data, timeout=7)
+        else:
+            resp = cls.session.post(url, data=data, timeout=7)
+        if resp.status_code != requests.codes.ok:
+            raise RequestError(resp.text)
+        if not resp.text:
+            raise ResponseError("No response data.")
+        return resp
+
+    @classmethod
+    def auto_set_proxy(cls, source):
+        proxy_config = config.get("proxy_config")
+        if_proxy = proxy_config[source]
+
+        if if_proxy:
+            if config.get("proxies"):
+                cls.session.proxies.update(config.get("proxies"))
+        else:
+            cls.session.proxies.update({})
