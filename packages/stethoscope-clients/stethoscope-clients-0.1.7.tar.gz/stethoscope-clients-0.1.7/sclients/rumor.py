f0caef045529da7cc43260003056e13642735d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:17 +0000

    Add package stethoscope-clients/0.1.7/stethoscope-clients-0.1.7.tar.gz

---
+++ packages/stethoscope-clients/stethoscope-clients-0.1.7.tar.gz/stethoscope-clients-0.1.7/sclients/rumor.py
@@ -0,0 +1,17 @@
+import trio
+from tenacity import retry, stop_after_delay, wait_fixed
+
+from sclients.types import ENR
+
+
+async def connect_rumor(rumor, client, enr: ENR):
+    await rumor.host.start(security='noise')
+
+    enr_str = enr.enr_teku if client == 'teku' else enr.enr
+
+    return await peer_connect(rumor, enr_str)
+
+
+@retry(sleep=trio.sleep, wait=wait_fixed(0.5), stop=stop_after_delay(20))
+async def peer_connect(rumor, enr_str):
+    return await rumor.peer.connect(enr_str).peer_id()
