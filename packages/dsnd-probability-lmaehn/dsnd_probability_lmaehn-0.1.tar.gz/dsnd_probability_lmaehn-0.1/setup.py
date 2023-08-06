0bd874e9a8ac4937a4c89523cc5bfcf933a2efb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package dsnd-probability-lmaehn/0.1/dsnd_probability_lmaehn-0.1.tar.gz

---
+++ packages/dsnd-probability-lmaehn/dsnd_probability_lmaehn-0.1.tar.gz/dsnd_probability_lmaehn-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_lmaehn',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_lmaehn'],
+      author='Lucas Mähn',
+      author_email='lucasmaehn@gmail.com',
+      zip_safe=False
+     )
