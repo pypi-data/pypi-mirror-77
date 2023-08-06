163198b286bc1708156857b80638bc6a00bdbe13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:39 +0000

    Add package text2num/2.2.1/text2num-2.2.1.tar.gz

---
+++ packages/text2num/text2num-2.2.1.tar.gz/text2num-2.2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "2.2.1"
+
+
+def readme():
+    with open("README.rst", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="text2num",
+    version=VERSION,
+    description="Parse and convert numbers written in French, Spanish or English into their digit representation.",
+    long_description=readme(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: Filters",
+        "Natural Language :: French",
+        "Natural Language :: English",
+        "Natural Language :: Spanish"
+    ],
+    keywords="French, Spanish and English NLP words-to-numbers",
+    url="https://github.com/allo-media/text2num",
+    author="Allo-Media",
+    author_email="contact@allo-media.fr",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    test_suite="tests",
+    include_package_data=True,
+    zip_safe=False,
+)
