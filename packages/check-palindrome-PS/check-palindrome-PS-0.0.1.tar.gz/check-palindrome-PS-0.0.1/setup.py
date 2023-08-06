7b40ee1fbe3014923b0247022edd4a231bdff597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:23 +0000

    Add package check-palindrome-PS/0.0.1/check-palindrome-PS-0.0.1.tar.gz

---
+++ packages/check-palindrome-PS/check-palindrome-PS-0.0.1.tar.gz/check-palindrome-PS-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'check-palindrome-PS',
+    version = '0.0.1',
+    description = 'This is palindrome checking function',
+    long_description = open('Readme.txt').read(),
+    url = '',
+    author = 'Prakash Shelar',
+    keywords = ['palindrome','Python palindrome'],
+    license = 'MIT',
+    packages = ['is_palindrome_PS'],
+    install_requres = ['']
+)
