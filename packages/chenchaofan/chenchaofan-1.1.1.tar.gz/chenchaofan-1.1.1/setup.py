143def5c2fa4d686b0ccac0c4521c36be9ee09b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:05 +0000

    Add package chenchaofan/1.1.1/chenchaofan-1.1.1.tar.gz

---
+++ packages/chenchaofan/chenchaofan-1.1.1.tar.gz/chenchaofan-1.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "chenchaofan",
+    version = "1.1.1",
+    keywords = ("pip", "pathtool","timetool", "chenchaofan", "mage"),
+    description = "time and path tool",
+    long_description = "time and path tool",
+    license = "MIT Licence",
+
+    url = "https://github.com/a654721574/ccf123.git",
+    author = "a654721574",
+    author_email = "654721574@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
