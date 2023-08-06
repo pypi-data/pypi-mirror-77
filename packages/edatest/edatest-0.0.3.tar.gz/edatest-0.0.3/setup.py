9d505827ff1eeb18746bb6cdd5e3b6a02167a663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package edatest/0.0.3/edatest-0.0.3.tar.gz

---
+++ packages/edatest/edatest-0.0.3.tar.gz/edatest-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edatest",
+    version="0.0.3",
+    author="Nicholas Walker",
+    author_email="nicholas.walk@gmail.com",
+    description="This is a simple package used to run Edabit tests.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/nicholas.walk/edatest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
