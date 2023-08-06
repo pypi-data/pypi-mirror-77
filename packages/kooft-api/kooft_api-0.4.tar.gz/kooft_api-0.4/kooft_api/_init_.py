5adfa07bde35729e41e26b103e06dfb1b03dcbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:27 +0000

    Add package kooft-api/0.4/kooft_api-0.4.tar.gz

---
+++ packages/kooft-api/kooft_api-0.4.tar.gz/kooft_api-0.4/kooft_api/_init_.py
@@ -0,0 +1,32 @@
+import requests as rq
+
+def login(name, password):
+	r=rq.post('http://www.kooft.ml/login', data={'n':name, 'pwd':password})
+	if not r.text == 'uncpwd':
+		return 'auth:*'+name+'*'+password
+	else:
+		return False
+def login_simple(u):
+	r=rq.post('http://www.kooft.ml/login', data={'n':u.split('*')[1], 'pwd':u.split('*')[2]})
+	if not r.text == 'uncpwd':
+		return True
+	else:
+		return False
+
+def view(prov):
+
+	r=rq.get('http://www.kooft.ml/type/'+prov+'/')
+	return r.text
+def add(user, prov, ttl, content, ad):
+	if login_simple(user):
+		r=rq.post('http://www.kooft.ml/api_content/add/', data={'type':prov,'author':user.split('*')[1],'ttl':ttl,'txt':content,'ad':ad})
+		return r.text
+	else:
+		return False
+
+
+
+
+
+
+		
---
+++ packages/kooft-api/kooft_api-0.4.tar.gz/kooft_api-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kooft_api',
+      version='0.4',
+      description='Kooft API',
+      packages=['kooft_api'],
+      author_email='likel0vsky@gmail.com',
+      zip_safe=False)
