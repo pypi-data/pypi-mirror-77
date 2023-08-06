e5f6bc92c67c631e6e51059dbaa8d1ba5605c67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:37 +0000

    Add package pykir/0.1.0/pykir-0.1.0.tar.gz

---
+++ packages/pykir/pykir-0.1.0.tar.gz/pykir-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pykir']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pykir',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Kiran PS',
+    'author_email': 'kiran.ps@shuttl.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
