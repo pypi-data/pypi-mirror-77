bdbd2e29f5bf52af0ccb6675c06945606a61154c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:57 +0000

    Add package irreceiver/0.9.2/irreceiver-0.9.2.tar.gz

---
+++ packages/irreceiver/irreceiver-0.9.2.tar.gz/irreceiver-0.9.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='irreceiver',
+      version='0.9.2',
+      description='A module to parse the NEC IR remote control protocol in Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/computersarecool/irreceiver',
+      author='Willy Nolan',
+      author_email='contact@interactiondepartment.com',
+      license='MIT',
+      packages=['irreceiver'],
+      zip_safe=False)
