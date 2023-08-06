5990a0d7e5282ae09b4ca357babf7964c01de113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:39 +0000

    Add package ys-distributions/0.1/ys_distributions-0.1.tar.gz

---
+++ packages/ys-distributions/ys_distributions-0.1.tar.gz/ys_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ys_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ys_distributions'],
+      zip_safe=False)
