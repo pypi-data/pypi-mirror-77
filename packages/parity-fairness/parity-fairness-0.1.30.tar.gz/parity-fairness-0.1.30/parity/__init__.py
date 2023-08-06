b79dd84d8c6486df0a7ed60c28f97b740a93ca46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:27 +0000

    Add package parity-fairness/0.1.30/parity-fairness-0.1.30.tar.gz

---
+++ packages/parity-fairness/parity-fairness-0.1.30.tar.gz/parity-fairness-0.1.30/parity/__init__.py
@@ -0,0 +1,9 @@
+from parity.fairness_metrics import show_bias, prepare_data, get_fair_metrics_and_plot, plot_fair_metrics, get_fair_metrics
+from parity.fairness_metrics import add_to_df_algo_metrics, plot_model_performance, get_model_performance
+from parity.fair import fairness_dashboard, decode_dataset, get_attributes, convert_to_pd_dataframe, disparate_impact_remover, learning_fair_representation
+from parity.fair import reweight, structured_data_train_test_split, adversarial_debias, prejudice_remover, calibrate_equality_of_odds, reject_option
+from parity.explainer import feature_importances, shap_feature_explainer, plot_prediction_causes, dependence_plots
+from parity.counterfactual_explainer import get_data_object, get_explainer_object, generate_counterfactual
+from parity.reduction import gridSearch, show_comparison
+from parity.thresholdOptimizer import thresholdOptimizer, show_comparison
+from parity.transfer import dump_to_aws_s3
---
+++ packages/parity-fairness/parity-fairness-0.1.30.tar.gz/parity-fairness-0.1.30/setup.py
@@ -0,0 +1,60 @@
+import pathlib
+from setuptools import setup
+
+# from os import path
+# this_directory = path.abspath(path.dirname(__file__))
+# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+# The directory containing this script
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Call setup
+setup(
+  name = 'parity-fairness', # How you named your package folder (MyLib)
+  packages = ['parity'], # Chose the same as "name"
+  version = '0.1.30', # Start with a small number and increase it with every change you make
+  license='MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Measure bias from data and machine learning models.', # Give a short description about your library
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author = 'Xavier M. Puspus', # Type in your name
+  author_email = 'xpuspus@gmail.com', # Type in your E-Mail
+  url = 'https://github.com/xmpuspus/parity-fairness', # Provide either the link to your github or to your website
+  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.30.tar.gz', # I explain this later on
+  keywords = ['fairness', 'bias', 'explainability', 'AI', 'machine learning', 'data'], # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'matplotlib',
+          'joblib',
+          'numpy',
+          'altair',
+          'pandas',
+          'scikit_learn',
+          'sklearn', 
+          'aif360',
+          'plotly',
+          'Ipython',
+          'sklearn',
+          'fairlearn',
+          'dice_ml',
+          'interpret',
+          'BlackBoxAuditing',
+          'witwidget',
+          'boto3'],
+    
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
+ 
