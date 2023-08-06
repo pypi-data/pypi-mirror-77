83b8a38c09360baf5f20cff8116ea8dedf838176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:12 +0000

    Add package uptrace/0.1.1b0/uptrace-0.1.1b0.tar.gz

---
+++ packages/uptrace/uptrace-0.1.1b0.tar.gz/uptrace-0.1.1b0/setup.py
@@ -0,0 +1,10 @@
+import os
+
+import setuptools
+
+
+PKG_INFO = {}
+with open(os.path.join("src", "uptrace", "version.py")) as f:
+    exec(f.read(), PKG_INFO)
+
+setuptools.setup(version=PKG_INFO["__version__"])
