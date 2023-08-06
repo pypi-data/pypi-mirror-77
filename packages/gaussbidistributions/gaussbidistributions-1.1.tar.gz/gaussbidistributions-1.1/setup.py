36012b13b76061f7e8462c50eb440046b4a54704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:16 +0000

    Add package gaussbidistributions/1.1/gaussbidistributions-1.1.tar.gz

---
+++ packages/gaussbidistributions/gaussbidistributions-1.1.tar.gz/gaussbidistributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussbidistributions',
+      version='1.1',
+      description='Gaussian Binomial distributions',
+      packages=['gaussbidistributions'],
+      author='Hina Tariq',
+      author_email='hina.tarique@gmail.com',
+      zip_safe=False)
