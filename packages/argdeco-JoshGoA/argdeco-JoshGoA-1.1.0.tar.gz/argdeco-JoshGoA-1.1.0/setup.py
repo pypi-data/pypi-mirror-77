a96a350a140b2481ffe251ce6b4e6c1bb2524ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package argdeco-JoshGoA/1.1.0/argdeco-JoshGoA-1.1.0.tar.gz

---
+++ packages/argdeco-JoshGoA/argdeco-JoshGoA-1.1.0.tar.gz/argdeco-JoshGoA-1.1.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from os import path
+
+with open(path.join(path.dirname(__file__), "README.md"), encoding="utf8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="argdeco-JoshGoA",
+    version="1.1.0",
+    description="Unopinionated argparse wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="JoshGoA",
+    url="https://github.com/JoshGoA/argdeco",
+    py_modules=["argdeco"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Basque",
+        "Natural Language :: English",
+        "Natural Language :: Spanish",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    package_data={"argdeco": ["*.md", "*.txt"]},
+    license="MIT",
+    keywords="argparse wrapt",
+    project_urls={"Source": "https://github.com/pypa/argdeco"},
+    install_requires=["wrapt"],
+    python_requires=">=3.8"
+)
