da06401daf0f9b4e99ca11a9abe9164547dd61a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:09 +0000

    Add package oui/0.0.11/oui-0.0.11-py3-none-any.whl

---
+++ packages/oui/oui-0.0.11-py3-none-any.whl/oui/splatter/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './js');
+
+module.exports = {
+    mode: 'development',
+    context: sourcePath,
+    entry: {
+        main: './index.ts',
+    },
+    output: {
+        path: outPath,
+        publicPath: '/',
+        filename: 'splatter.js',
+    },
+    target: 'web',
+    resolve: {
+        modules: [sourcePath, 'node_modules'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    externals: {
+        fs: true
+    },
+    module: {
+        rules: [{
+        //     test: /\.tsx?$/,
+        //     enforce: 'pre',
+        //     use: ['tslint-loader'],
+        // }, {
+            test: /\.tsx?$/,
+            use: ['awesome-typescript-loader'],
+        // }, {
+        //     test: /\.s?css$/,
+        //     use: ['style-loader', 'css-loader', 'sass-loader'],
+        // }, {
+        //     test: /\.json$/,
+        //     loader: 'json-loader',
+        }],
+    },
+    plugins: [],
+    devServer: {
+        contentBase: sourcePath,
+        watchContentBase: true,
+        compress: true,
+        hot: false,
+        disableHostCheck: true,
+        stats: {
+            warnings: false,
+        },
+        publicPath: '/',
+    },
+};
