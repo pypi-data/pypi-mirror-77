8751966bc1a8937bef0dfbf95a47f559c88a3ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:04 +0000

    Add package Flask-Atomic/0.0.36/Flask-Atomic-0.0.36.tar.gz

---
+++ packages/Flask-Atomic/Flask-Atomic-0.0.36.tar.gz/Flask-Atomic-0.0.36/flask_atomic/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import Response
+from flask import jsonify
+from flask.json import JSONEncoder
+
+from sqlalchemy.orm.collections import InstrumentedList
+
+from sqlalchemy_plus import serialize
+from sqlalchemy_plus import iserialize
+
+
+class ModelEncoder(JSONEncoder):
+    def default(self, data):
+        if isinstance(data, InstrumentedList):
+            collection = serialize(data)
+        else:
+            data = iserialize(data)
+        return data
+        return JSONEncoder.default(self, data)
+
+
+class JSONResponse(Response):
+    @classmethod
+    def force_type(cls, rv, environ=None):
+        if isinstance(rv, dict):
+            rv = jsonify(rv)
+        elif isinstance(rv, tuple):
+            rv = jsonify(rv[0]), rv[1]
+        return super(JSONResponse, cls).force_type(rv, environ)
+
+
+class FlaskJSON(Flask):
+    response_class = JSONResponse
+    json_encoder = ModelEncoder
+
