11bb7be196e003dd9029beaf071136008c3f2799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:49 +0000

    Add package django-aid/0.1.2/django_aid-0.1.2-py3-none-any.whl

---
+++ packages/django-aid/django_aid-0.1.2-py3-none-any.whl/django_aid/drf/renderers.py
@@ -0,0 +1,18 @@
+from rest_framework.renderers import BrowsableAPIRenderer
+
+__all__ = ("BrowsableAPIRendererWithoutForms",)
+
+
+class BrowsableAPIRendererWithoutForms(BrowsableAPIRenderer):
+    def get_context(self, *args, **kwargs):
+        context = super(BrowsableAPIRendererWithoutForms, self).get_context(
+            *args, **kwargs
+        )
+        context["display_edit_forms"] = False
+        return context
+
+    def get_filter_form(self, data, view, request):
+        return None
+
+    def get_rendered_html_form(self, data, view, method, request):
+        return None
