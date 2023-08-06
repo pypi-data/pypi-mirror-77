206c1c289a494ecfa3cf571f9eda5a912935f1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package inference-gym/0.0.0/inference_gym-0.0.0.tar.gz

---
+++ packages/inference-gym/inference_gym-0.0.0.tar.gz/inference_gym-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="inference_gym",
+    version="0.0.0",
+    author='Google LLC',
+    author_email='no-reply@google.com',
+    url="https://github.com/tensorflow/probability/spinoffs/inference_gym",
+    license='Apache 2.0',
+    packages=setuptools.find_packages('.'),
+    python_requires='>=3.6',
+)
