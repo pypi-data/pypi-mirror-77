5213f786f31fc0018a7712a50eb0807f104b4670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:08 +0000

    Add package pathy/0.2.0/pathy-0.2.0.tar.gz

---
+++ packages/pathy/pathy-0.2.0.tar.gz/pathy-0.2.0/pathy/cli.py
@@ -0,0 +1,118 @@
+import typer
+from .api import Pathy, FluidPath
+
+app = typer.Typer(help="Pathy command line interface.")
+
+
+@app.command()
+def cp(from_location: str, to_location: str):
+    """
+    Copy a blob or folder of blobs from one bucket to another.
+    """
+    from_path: FluidPath = Pathy.fluid(from_location)
+    if not from_path.exists():
+        raise ValueError(f"from_path is not an existing Path or Pathy: {from_path}")
+    to_path: FluidPath = Pathy.fluid(to_location)
+    if from_path.is_dir():
+        to_path.mkdir(parents=True, exist_ok=True)
+        for blob in from_path.rglob("*"):
+            if not blob.is_file():
+                continue
+            to_blob = to_path / str(blob.relative_to(from_path))
+            to_blob.write_bytes(blob.read_bytes())
+    elif from_path.is_file():
+        # Copy prefix from the source if the to_path has none.
+        #
+        # e.g. "cp ./file.txt gs://bucket-name/" writes "gs://bucket-name/file.txt"
+        if isinstance(to_path, Pathy) and to_path.prefix == "":
+            to_path = to_path / from_path
+
+        to_path.parent.mkdir(parents=True, exist_ok=True)
+        to_path.write_bytes(from_path.read_bytes())
+
+
+@app.command()
+def mv(from_location: str, to_location: str):
+    """
+    Move a blob or folder of blobs from one path to another.
+    """
+    from_path: FluidPath = Pathy.fluid(from_location)
+    to_path: FluidPath = Pathy.fluid(to_location)
+
+    if from_path.is_file():
+        # Copy prefix from the source if the to_path has none.
+        #
+        # e.g. "cp ./file.txt gs://bucket-name/" writes "gs://bucket-name/file.txt"
+        if isinstance(to_path, Pathy) and to_path.prefix == "":
+            to_path = to_path / from_path
+        to_path.parent.mkdir(parents=True, exist_ok=True)
+        to_path.write_bytes(from_path.read_bytes())
+        from_path.unlink()
+        return
+
+    if from_path.is_dir():
+        to_path.mkdir(parents=True, exist_ok=True)
+        to_unlink = []
+        for blob in from_path.rglob("*"):
+            if not blob.is_file():
+                continue
+            to_blob = to_path / str(blob.relative_to(from_path))
+            to_blob.write_bytes(blob.read_bytes())
+            to_unlink.append(blob)
+        for blob in to_unlink:
+            blob.unlink()
+        if from_path.is_dir():
+            from_path.rmdir()
+
+
+@app.command()
+def rm(
+    location: str,
+    recursive: bool = typer.Option(
+        False, "--recursive", "-r", help="Recursively remove files and folders."
+    ),
+    verbose: bool = typer.Option(
+        False, "--verbose", "-v", help="Print removed files and folders."
+    ),
+):
+    """
+    Remove a blob or folder of blobs from a given location.
+    """
+    path: FluidPath = Pathy.fluid(location)
+    if not path.exists():
+        raise typer.Exit(f"rm: {path}: No such file or directory")
+
+    if path.is_dir():
+        if not recursive:
+            raise typer.Exit(f"rm: {path}: is a directory")
+        selector = path.rglob("*") if recursive else path.glob("*")
+        to_unlink = [b for b in selector if b.is_file()]
+        for blob in to_unlink:
+            if verbose:
+                typer.echo(str(blob))
+            blob.unlink()
+        if path.exists():
+            if verbose:
+                typer.echo(str(path))
+            path.rmdir()
+    elif path.is_file():
+        if verbose:
+            typer.echo(str(path))
+        path.unlink()
+
+
+@app.command()
+def ls(location: str):
+    """
+    List the blobs that exist at a given location.
+    """
+    path: FluidPath = Pathy.fluid(location)
+    if not path.exists() or path.is_file():
+        typer.echo(f"ls: {path}: No such file or directory")
+        return
+    for file in path.iterdir():
+        typer.echo(file)
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/pathy/pathy-0.2.0.tar.gz/pathy-0.2.0/pathy/file.py
@@ -0,0 +1,254 @@
+from dataclasses import dataclass, field
+from typing import Optional, List, Generator, cast
+from .client import BucketClient, ClientBucket, ClientBlob, ClientError, BucketEntry
+from .base import PurePathy
+import pathlib
+import shutil
+import os
+
+
+class BucketEntryFS(BucketEntry["ClientBucketFS", pathlib.Path]):
+    ...
+
+
+@dataclass
+class ClientBlobFS(ClientBlob["ClientBucketFS", pathlib.Path]):
+    raw: pathlib.Path
+    bucket: "ClientBucketFS"
+
+    def delete(self) -> None:
+        """Delete a file-based blob."""
+        file_folder: str = os.path.dirname(self.raw)
+        self.raw.unlink()
+        # NOTE: in buckets folders only exist if a file is contained in them. Mimic
+        # that behavior here by removing empty folders when the last file is removed.
+        if len(os.listdir(file_folder)) == 0:
+            shutil.rmtree(file_folder)
+
+    def exists(self) -> bool:
+        return self.raw.exists()
+
+
+@dataclass
+class ClientBucketFS(ClientBucket):
+    name: str
+    bucket: pathlib.Path
+
+    def get_blob(self, blob_name: str) -> Optional[ClientBlobFS]:
+        native_blob = self.bucket / blob_name
+        if not native_blob.exists() or native_blob.is_dir():
+            return None
+        stat = native_blob.stat()
+        # path.owner() raises KeyError if the owner's UID isn't known
+        #
+        # https://docs.python.org/3/library/pathlib.html#pathlib.Path.owner
+        try:
+            owner = native_blob.owner()
+        except KeyError:
+            owner = None
+        return ClientBlobFS(
+            bucket=self,
+            owner=owner,
+            name=blob_name,
+            raw=native_blob,
+            size=stat.st_size,
+            updated=int(round(stat.st_mtime_ns * 1000)),
+        )
+
+    def copy_blob(
+        self, blob: ClientBlobFS, target: "ClientBucketFS", name: str
+    ) -> Optional[ClientBlobFS]:
+        in_file = str(blob.bucket.bucket / blob.name)
+        out_file = str(target.bucket / name)
+        out_path = pathlib.Path(os.path.dirname(out_file))
+        if not out_path.exists():
+            out_path.mkdir(parents=True)
+        shutil.copy(in_file, out_file)
+        return None
+
+    def delete_blob(self, blob: ClientBlobFS) -> None:
+        blob.delete()
+
+    def delete_blobs(self, blobs: List[ClientBlobFS]) -> None:
+        for blob in blobs:
+            blob.delete()
+
+
+@dataclass
+class BucketClientFS(BucketClient):
+    # Root to store file-system buckets as children of
+    root: pathlib.Path
+
+    def make_uri(self, path: PurePathy):
+        uri = super().make_uri(path)
+        return uri.replace("gs://", "file:///")
+
+    def full_path(self, path: PurePathy) -> pathlib.Path:
+        if path.root is None:
+            raise ValueError(f"Invalid bucket name for path: {path}")
+        full_path = self.root.absolute() / path.root
+        if path.key is not None:
+            full_path = full_path / path.key
+        return full_path
+
+    def exists(self, path: PurePathy) -> bool:
+        """Return True if the path exists as a file or folder on disk"""
+        return self.full_path(path).exists()
+
+    def is_dir(self, path: PurePathy) -> bool:
+        return self.full_path(path).is_dir()
+
+    def rmdir(self, path: PurePathy) -> None:
+        full_path = self.full_path(path)
+        return shutil.rmtree(str(full_path))
+
+    def open(
+        self,
+        path: PurePathy,
+        *,
+        mode="r",
+        buffering=-1,
+        encoding=None,
+        errors=None,
+        newline=None,
+    ):
+        if self.lookup_bucket(path) is None:
+            raise ClientError(message=f'bucket "{path.root}" does not exist', code=404)
+
+        full_path = self.full_path(path)
+        if not full_path.exists():
+            if full_path.name != "":
+                full_path = full_path.parent
+            full_path.mkdir(parents=True, exist_ok=True)
+        return super().open(
+            path,
+            mode=mode,
+            buffering=buffering,
+            encoding=encoding,
+            errors=errors,
+            newline=newline,
+        )
+
+    def make_uri(self, path: PurePathy) -> str:
+        if not path.root:
+            raise ValueError(f"cannot make a URI to an invalid bucket: {path.root}")
+        result = f"file://{self.root.absolute() / path.root / path.key}"
+        return result
+
+    def create_bucket(self, path: PurePathy) -> ClientBucket:
+        if not path.root:
+            raise ValueError(f"Invalid bucket name: {path.root}")
+        bucket_path: pathlib.Path = self.root / path.root
+        if bucket_path.exists():
+            raise FileExistsError(f"Bucket already exists at: {bucket_path}")
+        bucket_path.mkdir(parents=True, exist_ok=True)
+        return ClientBucketFS(str(path.root), bucket=bucket_path)
+
+    def delete_bucket(self, path: PurePathy) -> None:
+        bucket_path: pathlib.Path = self.root / str(path.root)
+        if bucket_path.exists():
+            shutil.rmtree(bucket_path)
+
+    def lookup_bucket(self, path: PurePathy) -> Optional[ClientBucketFS]:
+        if path.root:
+            bucket_path: pathlib.Path = self.root / path.root
+            if bucket_path.exists():
+                return ClientBucketFS(str(path.root), bucket=bucket_path)
+        return None
+
+    def get_bucket(self, path: PurePathy) -> ClientBucketFS:
+        if not path.root:
+            raise ValueError(f"path has an invalid bucket_name: {path.root}")
+        bucket_path: pathlib.Path = self.root / path.root
+        if bucket_path.is_dir():
+            return ClientBucketFS(str(path.root), bucket=bucket_path)
+        raise FileNotFoundError(f"Bucket {path.root} does not exist!")
+
+    def list_buckets(self, **kwargs) -> Generator[ClientBucketFS, None, None]:
+        for f in self.root.glob("*"):
+            if f.is_dir():
+                yield ClientBucketFS(f.name, f)
+
+    def scandir(
+        self,
+        path: Optional[PurePathy] = None,
+        prefix: Optional[str] = None,
+        delimiter: Optional[str] = None,
+    ) -> Generator[BucketEntryFS, None, None]:
+        if path is None or not path.root:
+            for bucket in self.list_buckets():
+                yield BucketEntryFS(bucket.name, is_dir=True, raw=None)
+            return
+        assert path is not None
+        assert path.root is not None
+        scan_path = self.root / path.root
+        if prefix is not None:
+            scan_path = scan_path / prefix
+        for dir_entry in scan_path.glob("*"):
+            if dir_entry.is_dir():
+                yield BucketEntryFS(dir_entry.name, is_dir=True, raw=None)
+            else:
+                file_path = pathlib.Path(dir_entry)
+                stat = file_path.stat()
+                file_size = stat.st_size
+                updated = int(round(stat.st_mtime_ns * 1000))
+                blob: ClientBlob = ClientBlobFS(
+                    self.get_bucket(path),
+                    name=dir_entry.name,
+                    size=file_size,
+                    updated=updated,
+                    owner=None,
+                    raw=file_path,
+                )
+                yield BucketEntryFS(
+                    name=dir_entry.name,
+                    is_dir=False,
+                    size=file_size,
+                    last_modified=updated,
+                    raw=blob,
+                )
+
+    def list_blobs(
+        self,
+        path: PurePathy,
+        prefix: Optional[str] = None,
+        delimiter: Optional[str] = None,
+        include_dirs: bool = False,
+    ) -> Generator[ClientBlobFS, None, None]:
+        assert path.root is not None
+        bucket = self.get_bucket(path)
+        scan_path = self.root / path.root
+        if prefix is not None:
+            scan_path = scan_path / prefix
+        elif prefix is not None:
+            scan_path = scan_path / path.key
+
+        # Path to a file
+        if scan_path.exists() and not scan_path.is_dir():
+            stat = scan_path.stat()
+            file_size = stat.st_size
+            updated = int(round(stat.st_mtime_ns * 1000))
+            yield ClientBlobFS(
+                bucket,
+                name=scan_path.name,
+                size=file_size,
+                updated=updated,
+                owner=None,
+                raw=scan_path,
+            )
+
+        # Yield blobs for each file
+        for file_path in scan_path.rglob("*"):
+            if file_path.is_dir():
+                continue
+            stat = file_path.stat()
+            file_size = stat.st_size
+            updated = int(round(stat.st_mtime_ns * 1000))
+            yield ClientBlobFS(
+                bucket,
+                name=f"{prefix if prefix is not None else ''}{file_path.name}",
+                size=file_size,
+                updated=updated,
+                owner=None,
+                raw=file_path,
+            )
