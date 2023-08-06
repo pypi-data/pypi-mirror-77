40f238ca0ee6994aa1f2ffbbf511b6b5a31e807c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:35 +0000

    Add package pygqlc/2.0.1/pygqlc-2.0.1.tar.gz

---
+++ packages/pygqlc/pygqlc-2.0.1.tar.gz/pygqlc-2.0.1/pygqlc/__init__.py
@@ -0,0 +1,9 @@
+from .GraphQLClient import GraphQLClient
+from .QueryParser import QueryParser
+from .MutationParser import MutationParser
+from .SubscriptionParser import SubscriptionParser
+
+# * Package name:
+name = 'pygqlc'
+# * required here for pypi upload exceptions:
+__version__ = "2.0.1"
