a57019a604f3a928b39cdffd7d3e9e1c4689058d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:26 +0000

    Add package midr/1.3.8/midr-1.3.8.tar.gz

---
+++ packages/midr/midr-1.3.8.tar.gz/midr-1.3.8/cmake-build-debug/CMakeFiles/c_archimedian.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_archimedian.dir/cpp/archimedean.cpp.o"
+  "c_archimedian.cpython-38-darwin.so"
+  "c_archimedian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_archimedian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
