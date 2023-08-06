5e8da1d8fd6b4973c79c9e10d61178fa6013bd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:48 +0000

    Add package cloudsplaining/0.1.8/cloudsplaining-0.1.8.tar.gz

---
+++ packages/cloudsplaining/cloudsplaining-0.1.8.tar.gz/cloudsplaining-0.1.8/cloudsplaining/command/scan.py
@@ -0,0 +1,254 @@
+"""
+Scan a single account authorization file
+"""
+# Copyright (c) 2020, salesforce.com, inc.
+# All rights reserved.
+# Licensed under the BSD 3-Clause license.
+# For full license text, see the LICENSE file in the repo root
+# or https://opensource.org/licenses/BSD-3-Clause
+import logging
+import os
+import webbrowser
+import json
+from pathlib import Path
+import yaml
+import click
+import click_log
+from cloudsplaining.shared.constants import EXCLUSIONS_FILE
+from cloudsplaining.shared.validation import check_authorization_details_schema
+from cloudsplaining.shared.exclusions import Exclusions, DEFAULT_EXCLUSIONS
+from cloudsplaining.scan.authorization_details import AuthorizationDetails
+from cloudsplaining.output.triage_worksheet import create_triage_worksheet
+from cloudsplaining.output.data_file import write_results_data_file
+from cloudsplaining.output.report import HTMLReport
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+
+@click.command(
+    short_help="Scan a single file containing AWS IAM account authorization details and generate report on "
+    "IAM security posture. "
+)
+@click.option(
+    "--input-file",
+    type=click.Path(exists=True),
+    required=True,
+    help="Path of IAM account authorization details file",
+)
+@click.option(
+    "--exclusions-file",
+    help="A yaml file containing a list of policy names to exclude from the scan.",
+    type=click.Path(exists=True),
+    required=False,
+    default=EXCLUSIONS_FILE,
+)
+@click.option(
+    "--output",
+    required=False,
+    type=click.Path(exists=True),
+    default=os.getcwd(),
+    help="Output directory.",
+)
+@click.option(
+    "--skip-open-report",
+    required=False,
+    default=False,
+    is_flag=True,
+    help="Don't open the HTML report in the web browser after creating. "
+    "This helps when running the report in automation.",
+)
+@click_log.simple_verbosity_option()
+# pylint: disable=redefined-builtin
+def scan(
+    input_file, exclusions_file, output, skip_open_report
+):  # pragma: no cover
+    """
+    Given the path to account authorization details files and the exclusions config file, scan all inline and
+    managed policies in the account to identify actions that do not leverage resource constraints.
+    """
+    if exclusions_file:
+        # Get the exclusions configuration
+        with open(exclusions_file, "r") as yaml_file:
+            try:
+                exclusions_cfg = yaml.safe_load(yaml_file)
+            except yaml.YAMLError as exc:
+                logger.critical(exc)
+        exclusions = Exclusions(exclusions_cfg)
+    else:
+        exclusions = DEFAULT_EXCLUSIONS
+
+    if os.path.isfile(input_file):
+        account_name = Path(input_file).stem
+        with open(input_file) as f:
+            contents = f.read()
+            account_authorization_details_cfg = json.loads(contents)
+        rendered_html_report = scan_account_authorization_details(
+            account_authorization_details_cfg, exclusions, account_name, output, write_data_files=True
+        )
+        html_output_file = os.path.join(output, f"iam-report-{account_name}.html")
+        logger.info("Saving the report to %s", html_output_file)
+        if os.path.exists(html_output_file):
+            os.remove(html_output_file)
+
+        with open(html_output_file, "w") as f:
+            f.write(rendered_html_report)
+
+        print(f"Wrote HTML results to: {html_output_file}")
+
+        # Open the report by default
+        if not skip_open_report:
+            print("Opening the HTML report")
+            url = "file://%s" % os.path.abspath(html_output_file)
+            webbrowser.open(url, new=2)
+
+    if os.path.isdir(input_file):
+        logger.info(
+            "The path given is a directory. Scanning for account authorization files and generating report."
+        )
+        input_files = get_authorization_files_in_directory(input_file)
+        for file in input_files:
+            logger.info(f"Scanning file: {file}")
+            with open(file) as f:
+                contents = f.read()
+                account_authorization_details_cfg = json.loads(contents)
+
+            account_name = Path(file).stem
+            # Scan the Account Authorization Details config
+            rendered_html_report = scan_account_authorization_details(
+                account_authorization_details_cfg, exclusions, account_name, output, write_data_files=True
+            )
+            html_output_file = os.path.join(output, f"iam-report-{account_name}.html")
+            logger.info("Saving the report to %s", html_output_file)
+            if os.path.exists(html_output_file):
+                os.remove(html_output_file)
+
+            with open(html_output_file, "w") as f:
+                f.write(rendered_html_report)
+
+            print(f"Wrote HTML results to: {html_output_file}")
+
+            # Open the report by default
+            if not skip_open_report:
+                print("Opening the HTML report")
+                url = "file://%s" % os.path.abspath(html_output_file)
+                webbrowser.open(url, new=2)
+
+
+def scan_account_authorization_details(
+    account_authorization_details_cfg, exclusions, account_name="default", output_directory=os.getcwd(), write_data_files=False
+):  # pragma: no cover
+    """
+    Given the path to account authorization details files and the exclusions config file, scan all inline and
+    managed policies in the account to identify actions that do not leverage resource constraints.
+    """
+
+    logger.debug(
+        "Identifying modify-only actions that are not leveraging "
+        "resource constraints..."
+    )
+    check_authorization_details_schema(account_authorization_details_cfg)
+    authorization_details = AuthorizationDetails(account_authorization_details_cfg)
+    results = authorization_details.missing_resource_constraints(
+        exclusions, modify_only=True
+    )
+
+    principal_policy_mapping = authorization_details.principal_policy_mapping
+    # For the IAM Principals tab, add on risk stats per principal
+    for principal_policy_entry in principal_policy_mapping:
+        for finding in results:
+            if principal_policy_entry.get("PolicyName").lower() == finding.get("PolicyName").lower():
+                principal_policy_entry["Actions"] = len(finding["Actions"])
+                principal_policy_entry["PrivilegeEscalation"] = len(
+                    finding["PrivilegeEscalation"]
+                )
+                principal_policy_entry["DataExfiltrationActions"] = len(
+                    finding["DataExfiltrationActions"]
+                )
+                principal_policy_entry["PermissionsManagementActions"] = len(
+                    finding["PermissionsManagementActions"]
+                )
+                principal_name = principal_policy_entry["Principal"]
+                # Customer Managed Policies
+                if finding.get("Type") == "Policy" and finding.get("ManagedBy") == "Customer" and principal_policy_entry.get("Type") != "Policy":
+                    if "Principals" not in finding:
+                        finding["Principals"] = [principal_name]
+                    else:
+                        if principal_name not in finding["Principals"]:
+                            finding["Principals"].append(principal_name)
+
+                # AWS Managed Policies
+                if finding.get("Type") == "Policy" and finding.get("ManagedBy") == "AWS":
+                    if "Principals" not in finding:
+                        finding["Principals"] = [principal_name]
+                    else:
+                        if principal_name not in finding["Principals"]:
+                            finding["Principals"].append(principal_name)
+
+    # Lazy method to get an account ID
+    account_id = None
+    for item in results:
+        if item["ManagedBy"] == "Customer":
+            account_id = item["AccountID"]
+            break
+
+    html_report = HTMLReport(
+        account_id=account_id,
+        account_name=account_name,
+        results=results,
+        exclusions_cfg=exclusions,
+        principal_policy_mapping=principal_policy_mapping
+    )
+    rendered_report = html_report.get_html_report()
+
+    # Raw data file
+    if write_data_files:
+        if output_directory is None:
+            output_directory = os.getcwd()
+
+        # new_data = authorization_details.new_principal_policy_mapping
+        # new_raw_data_file = os.path.join(output_directory, f"iam-new-principal-policy-mapping-{account_name}.json")
+        # new_raw_data_filepath = write_results_data_file(new_data, new_raw_data_file)
+        # print(f"Raw data file saved: {str(new_raw_data_filepath)}")
+
+        raw_data_file = os.path.join(output_directory, f"iam-results-{account_name}.json")
+        raw_data_filepath = write_results_data_file(results, raw_data_file)
+        print(f"Raw data file saved: {str(raw_data_filepath)}")
+
+        # Principal policy mapping
+        principal_policy_mapping_file = os.path.join(
+            output_directory, f"iam-principals-{account_name}.json"
+        )
+        principal_policy_mapping_filepath = write_results_data_file(
+            principal_policy_mapping, principal_policy_mapping_file
+        )
+        print(f"Principals data file saved: {str(principal_policy_mapping_filepath)}")
+
+        # Create the CSV triage sheet
+        create_triage_worksheet(account_name, results, output_directory)
+
+    return rendered_report
+
+
+def get_authorization_files_in_directory(directory):  # pragma: no cover
+    """Get a list of download-account-authorization-files in a directory"""
+    file_list = [
+        f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))
+    ]
+    file_list_with_full_path = []
+    for file in file_list:
+        if file.endswith(".json"):
+            file_list_with_full_path.append(
+                os.path.abspath(os.path.join(directory, file))
+            )
+    new_file_list = []
+    for file in file_list_with_full_path:
+        with open(file) as f:
+            contents = f.read()
+        account_authorization_details_cfg = json.loads(contents)
+        valid_schema = check_authorization_details_schema(
+            account_authorization_details_cfg
+        )
+        if valid_schema:
+            new_file_list.append(file)
+    return new_file_list
---
+++ packages/cloudsplaining/cloudsplaining-0.1.8.tar.gz/cloudsplaining-0.1.8/cloudsplaining/scan/group_details.py
@@ -0,0 +1,222 @@
+"""Processes an entry under GroupDetailList"""
+from cloudsplaining.scan.inline_policy import InlinePolicy
+
+
+class GroupDetailList:
+    """Processes all entries under the GroupDetailList"""
+    def __init__(self, group_details, policy_details):
+        self.groups = []
+        for group_detail in group_details:
+            self.groups.append(GroupDetail(group_detail, policy_details))
+
+    def get_group_detail(self, name):
+        """Get a GroupDetail object by providing the Name of the group. This is useful to UserDetail objects"""
+        result = None
+        for group_detail in self.groups:
+            if group_detail.group_name == name:
+                result = group_detail
+                break
+        return result
+
+    def get_all_allowed_actions_for_group(self, name):
+        """Returns a list of all allowed actions by the group across all its policies"""
+        result = None
+        for group_detail in self.groups:
+            if group_detail.group_name == name:
+                result = group_detail.all_allowed_actions
+                break
+        return result
+
+    def get_all_iam_statements_for_group(self, name):
+        """Returns a list of all StatementDetail objects across all the policies assigned to the group"""
+        result = None
+        for group_detail in self.groups:
+            if group_detail.group_name == name:
+                result = group_detail.all_iam_statements
+                break
+        return result
+
+    @property
+    def group_names(self):
+        """Get a list of all group names in the account"""
+        results = []
+        for group_detail in self.groups:
+            results.append(group_detail.group_name)
+        results.sort()
+        return results
+
+    @property
+    def json(self):
+        """Get all JSON results"""
+        result = {}
+        for group in self.groups:
+            result[group.group_id] = group.json
+        return result
+
+
+class GroupDetail:
+    """Processes an entry under GroupDetailList"""
+    def __init__(self, group_detail, policy_details):
+        """
+        Initialize the GroupDetail object.
+
+        :param group_detail: Details about a particular group
+        :param policy_details: The ManagedPolicyDetails object - i.e., details about all managed policies in the account so the group can inherit those attributes
+        """
+        self.create_date = group_detail.get("CreateDate")
+        self.arn = group_detail.get("Arn")
+        self.path = group_detail.get("Path")
+        self.group_id = group_detail.get("GroupId")
+        self.group_name = group_detail.get("GroupName")
+
+        # Inline Policies
+        self.inline_policies = []
+        if group_detail.get("GroupPolicyList"):
+            self._inline_policies_details(
+                group_detail.get("GroupPolicyList")
+            )
+
+        # Managed Policies (either AWS-managed or Customer managed)
+        self.attached_managed_policies = []
+        if group_detail.get("AttachedManagedPolicies"):
+            self._attached_managed_policies_details(
+                group_detail.get("AttachedManagedPolicies"),
+                policy_details
+            )
+
+    def _attached_managed_policies_details(self, attached_managed_policies_list, policy_details):
+        for policy in attached_managed_policies_list:
+            arn = policy.get("PolicyArn")
+            attached_managed_policy_details = policy_details.get_policy_detail(arn)
+            self.attached_managed_policies.append(attached_managed_policy_details)
+
+    def _inline_policies_details(self, group_policies_list):
+        for policy in group_policies_list:
+            inline_policy = InlinePolicy(policy)
+            self.inline_policies.append(inline_policy)
+
+    @property
+    def all_allowed_actions(self):
+        """Return a list of which actions are allowed by the principal"""
+        privileges = []
+        for managed_policy in self.attached_managed_policies:
+            privileges.extend(managed_policy.policy_document.all_allowed_actions)
+        for inline_policy in self.inline_policies:
+            privileges.extend(inline_policy.policy_document.all_allowed_actions)
+        return privileges
+
+    @property
+    def all_iam_statements(self):
+        """Return a list of which actions are allowed by the principal"""
+        statements = []
+        for managed_policy in self.attached_managed_policies:
+            statements.extend(managed_policy.policy_document.statements)
+        for inline_policy in self.inline_policies:
+            statements.extend(inline_policy.policy_document.statements)
+        return statements
+
+    @property
+    def consolidated_risks(self):
+        """Return a dict containing the consolidated risks from all inline and managed policies for the group"""
+        privilege_escalation_results = {}
+        resource_exposure_results = []
+        data_exfiltration_results = []
+
+        # Get it from each inline policy
+        if self.inline_policies:
+            for inline_policy in self.inline_policies:
+                # Privilege Escalation
+                if inline_policy.policy_document.allows_privilege_escalation:
+                    for entry in inline_policy.policy_document.allows_privilege_escalation:
+                        if entry["type"] not in privilege_escalation_results.keys():
+                            privilege_escalation_results[entry["type"]] = entry["actions"]
+                # Resource Exposure
+                if inline_policy.policy_document.permissions_management_without_constraints:
+                    for action in inline_policy.policy_document.permissions_management_without_constraints:
+                        if action not in resource_exposure_results:
+                            resource_exposure_results.append(action)
+                # Data Exfiltration
+                if inline_policy.policy_document.allows_data_leak_actions:
+                    for action in inline_policy.policy_document.allows_data_leak_actions:
+                        if action not in data_exfiltration_results:
+                            data_exfiltration_results.append(action)
+
+        if self.attached_managed_policies:
+            for managed_policy in self.attached_managed_policies:
+                # Privilege Escalation
+                if managed_policy.policy_document.allows_privilege_escalation:
+                    for entry in managed_policy.policy_document.allows_privilege_escalation:
+                        if entry["type"] not in privilege_escalation_results.keys():
+                            privilege_escalation_results[entry["type"]] = entry["actions"]
+                # Resource Exposure
+                if managed_policy.policy_document.permissions_management_without_constraints:
+                    for action in managed_policy.policy_document.permissions_management_without_constraints:
+                        if action not in resource_exposure_results:
+                            resource_exposure_results.append(action)
+                # Data Exfiltration
+                if managed_policy.policy_document.allows_data_leak_actions:
+                    for action in managed_policy.policy_document.allows_data_leak_actions:
+                        if action not in data_exfiltration_results:
+                            data_exfiltration_results.append(action)
+
+        # turn it into a list because we want to be able to count the number of results
+        these_privilege_escalation_results = []
+
+        for key in privilege_escalation_results:
+            result = {
+                "type": key,
+                "actions": privilege_escalation_results[key]
+            }
+            these_privilege_escalation_results.append(result)
+
+        resource_exposure_results.sort()
+        data_exfiltration_results.sort()
+
+        results = {
+            "PrivilegeEscalation": these_privilege_escalation_results,
+            "ResourceExposure": resource_exposure_results,
+            "DataExfiltration": data_exfiltration_results,
+        }
+
+        return results
+
+    @property
+    def attached_managed_policies_json(self):
+        """Return JSON representation of attached managed policies"""
+        policies = {}
+        for policy in self.attached_managed_policies:
+            policies[policy.policy_id] = policy.json
+        return policies
+
+    @property
+    def attached_managed_policies_pointer_json(self):
+        """Return JSON representation of attached managed policies"""
+        policies = {}
+        for policy in self.attached_managed_policies:
+            policies[policy.policy_id] = policy.policy_name
+        return policies
+
+    @property
+    def inline_policies_json(self):
+        """Return JSON representation of attached inline policies"""
+        policies = {}
+        for policy in self.inline_policies:
+            policies[policy.policy_id] = policy.json
+        return policies
+
+    @property
+    def json(self):
+        """Return the JSON representation of the Group Detail"""
+
+        this_group_detail = dict(
+            arn=self.arn,
+            create_date=self.create_date,
+            id=self.group_id,
+            inline_policies=self.inline_policies_json,
+            inline_policies_count=len(self.inline_policies_json),
+            path=self.path,
+            managed_policies_count=len(self.attached_managed_policies),
+            managed_policies=self.attached_managed_policies_pointer_json,
+            risks=self.consolidated_risks
+        )
+        return this_group_detail
