38190de9fe2a4d69b24fdabc757ccab3651c5ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:42 +0000

    Add package ffdb/2.5.5/ffdb-2.5.5.tar.gz

---
+++ packages/ffdb/ffdb-2.5.5.tar.gz/ffdb-2.5.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README_brief.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ffdb",
+    version="2.5.5",
+    author="Dr Giuseppe Insana",
+    author_email="insana@insana.net",
+    description="ffdb",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=["scripts/indexer.py", "scripts/extractor.py",
+             "scripts/remover.py", "scripts/merger.py"],
+    url="https://github.com/g-insana/ffdb.py",
+    license="AGPL",
+    py_modules=["ffdb"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "tqdm",
+        "sortedcontainers",
+        "requests",
+        "pycryptodomex"
+    ],
+    python_requires=">=3.5",
+)
