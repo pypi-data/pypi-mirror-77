77d9cc32bd1bd5084042d5c54d7d7721424bd13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:48 +0000

    Add package wavespectra/3.7.1/wavespectra-3.7.1.tar.gz

---
+++ packages/wavespectra/wavespectra-3.7.1.tar.gz/wavespectra-3.7.1/wavespectra/core/attributes.py
@@ -0,0 +1,24 @@
+"""Standarise SpecArray attributes.
+
+attrs (dict): standarised names for spectral variables, standard_names and units
+"""
+# from collections import OrderedDict
+import os
+import yaml
+from attrdict import AttrDict
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(HERE, "attributes.yml")) as stream:
+    attrs = AttrDict(yaml.load(stream, yaml.SafeLoader))
+
+
+def set_spec_attributes(dset):
+    """
+    Standarise CF attributes in specarray variables
+    """
+    for varname, varattrs in attrs.ATTRS.items():
+        try:
+            dset[varname].attrs = varattrs
+        except Exception:
+            pass
