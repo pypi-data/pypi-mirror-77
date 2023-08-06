57ab70b3184e7777c3cce3da2d8da604ecac4a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:06 +0000

    Add package rdel-distributions/1.1/rdel_distributions-1.1.tar.gz

---
+++ packages/rdel-distributions/rdel_distributions-1.1.tar.gz/rdel_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rdel_distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['rdel_distributions'],
+      author = 'Ronda Lunn',
+      author_email = 'ronda@rondalunn.com',
+      zip_safe=False)
