fbfff71251af2846899c07bcf7be605b43f87f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:04 +0000

    Add package genie.libs.sdk/20.8.1/genie.libs.sdk-20.8.1-py3-none-any.whl

---
+++ packages/genie.libs.sdk/genie.libs.sdk-20.8.1-py3-none-any.whl/genie/libs/sdk/apis/junos/log/get.py
@@ -0,0 +1,43 @@
+"""Common get functions for log"""
+
+# Python
+import re
+import logging
+from datetime import datetime
+
+# Genie
+from genie.utils.timeout import Timeout
+from genie.libs.sdk.libs.utils.normalize import GroupKeys
+from genie.metaparser.util.exceptions import SchemaEmptyParserError
+
+log = logging.getLogger(__name__)
+
+def get_log_message_time(device, message):
+    """ Gets the timestamp of a log message
+
+    Args:
+        device (obj): Device object
+        message (str): Message
+
+    Returns:
+        (datetime): Timestamp object
+    """
+
+    try:
+        out = device.parse('show log messages')
+    except SchemaEmptyParserError:
+        return None
+
+    messages_ = out.q.contains('.*{}.*'.format(message), regex=True)
+    
+    if not messages_:
+        return None
+
+    time_group = re.match(r'^(?P<month>\S+) +(?P<day>\d+) +(?P<time>[\d:]+)', messages_[0][1]).groupdict()
+
+    time_text = f"{time_group['month']} {time_group['day']} {time_group['time']}"
+
+    time_obj = datetime.strptime(time_text, '%b %d %H:%M:%S')
+
+    return time_obj.replace(year=datetime.now().year)
+
