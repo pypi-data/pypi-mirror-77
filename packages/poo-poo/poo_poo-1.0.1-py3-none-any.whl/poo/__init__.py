e47ba442e477240eef81c0933d325d8eaf5f648e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:52 +0000

    Add package poo-poo/1.0.1/poo_poo-1.0.1-py3-none-any.whl

---
+++ packages/poo-poo/poo_poo-1.0.1-py3-none-any.whl/poo/__init__.py
@@ -0,0 +1,19 @@
+from random import*
+from tkinter import*
+
+__version__ = "1.0.0"
+window = Tk()
+size = 500
+c = Canvas(window, height=size, width=size, bg="brown")
+c.pack()
+window.title("poo poo poo poo evrywhere")
+photo = PhotoImage(file = "poo.png")
+window.iconphoto(False, photo)
+def poo():
+    while True:
+        x0 = randint(0, size)
+        y0 = randint(0, size)
+        d = randint(0, size/5)
+        c.create_oval(x0, y0, x0 + d, y0 + d, fill="yellow")
+        window.update()
+poo()
