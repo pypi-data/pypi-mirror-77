ac08bcfa6537beeca0abfde57c6e2d1e15656bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:02 +0000

    Add package inform/1.23.0/inform-1.23.0-py3-none-any.whl

---
+++ packages/inform/inform-1.23.0-py3-none-any.whl/inform/__init__.py
@@ -0,0 +1,31 @@
+__version__ = '1.23.0'
+__released__ = '2020-08-26'
+
+from .inform import (
+    # inform utility functions and classes
+    cull, indent, is_collection, is_iterable, is_mapping, is_str, join,
+    Color, Info,
+
+    # user utility functions and classes
+    columns, conjoin, did_you_mean, fmt, format_range, full_stop, os_error,
+    parse_range, plural, ProgressBar, render, render_bar, title_case,
+
+    # debug functions
+    aaa, ppp, ddd, vvv, sss,
+
+    # inform classes
+    InformantFactory, Inform, Error,
+
+    # inform functions
+    done, terminate, terminate_if_errors, errors_accrued, get_prog_name,
+
+    # built-in informants
+    log, comment, codicil, narrate, display, output,
+    notify, debug, warn, error, fatal, panic,
+
+    # the currently active informer
+    get_informer, set_informer,
+
+    # culprit functions
+    set_culprit, add_culprit, get_culprit, join_culprit
+)
