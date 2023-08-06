0711ed1526aaf5fbb274cee3af2e27b01f0c65c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:23 +0000

    Add package fpga-ethernet-control/0.0.2/fpga_ethernet_control-0.0.2.tar.gz

---
+++ packages/fpga-ethernet-control/fpga_ethernet_control-0.0.2.tar.gz/fpga_ethernet_control-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open ("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fpga_ethernet_control',
+    version='0.0.2',
+    description='Ethernet to FPGA API',
+    py_modules=["wiz_scpi"],
+    package_dir={'':'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'socket_control_ivie',
+    ],
+    extras_require={
+        "dev":[
+            "socket_control_ivie",
+        ],
+    },
+)
---
+++ packages/fpga-ethernet-control/fpga_ethernet_control-0.0.2.tar.gz/fpga_ethernet_control-0.0.2/src/wiz_scpi.py
@@ -0,0 +1,7 @@
+from socket_control import socket_control
+
+class wiz_scpi(socket_control):
+    def __init__(self):
+        super().__init__()
+
+
