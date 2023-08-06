a064693ceeddc0fe2e8395c4881d504db6f28e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:22 +0000

    Add package pytest-commander/2.0.0/pytest_commander-2.0.0.tar.gz

---
+++ packages/pytest-commander/pytest_commander-2.0.0.tar.gz/pytest_commander-2.0.0/pytest_commander/web_client/build/precache-manifest.4799a0fb66d4c08063896770b0ec4923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93eea0bc6652945c2760e04106b4556b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85a8f789749f7dd2b7f0",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "85a8f789749f7dd2b7f0",
+    "url": "/static/js/2.80dbfdb8.chunk.js"
+  },
+  {
+    "revision": "73228ee743bd43575fc7c2066895ba0d",
+    "url": "/static/js/2.80dbfdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723d99045151a500d449",
+    "url": "/static/js/main.cb8721d3.chunk.js"
+  },
+  {
+    "revision": "17d2f0c5770643c56aee",
+    "url": "/static/js/runtime-main.207135d3.js"
+  }
+]);
