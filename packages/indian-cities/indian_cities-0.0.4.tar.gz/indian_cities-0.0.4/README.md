b145a625e09ac1c49989b4b937e7a90d22fadfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:02 +0000

    Add package indian-cities/0.0.4/indian_cities-0.0.4.tar.gz

---
+++ packages/indian-cities/indian_cities-0.0.4.tar.gz/indian_cities-0.0.4/README.md
@@ -0,0 +1,33 @@
+[![PyPI version](https://badge.fury.io/py/indian-cities.svg)](https://badge.fury.io/py/indian-cities)
+![PyPI - License](https://img.shields.io/pypi/l/indian-cities)
+
+# django_indian_cities -- states & cities for india
+
+## How to use this
+
+> To import
+
+```
+from dj_city import CITIES
+
+class myModel(models.Model):
+
+    city = models.CharField(choices=CITIES, null=False, max_length=20)
+
+```
+
+Requirements:
+
+Any version of Python, Django
+
+## Installation
+
+To install this
+
+> pip install indian_cities
+
+---
+
+### issues
+
+- [ ] pipenv install indian_cities not working
---
+++ packages/indian-cities/indian_cities-0.0.4.tar.gz/indian_cities-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="indian_cities",
+    version="0.0.4",
+    description="Django Indian Cities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/chayandatta/django_indian_cities",
+    author="Chayan Datta",
+    author_email="chayan.datta1996@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+)
