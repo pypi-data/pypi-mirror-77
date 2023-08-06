76205c32b5539cd2a5fd8992d791b47b8ccf97fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:29 +0000

    Add package localspelling/0.5/localspelling-0.5.tar.gz

---
+++ packages/localspelling/localspelling-0.5.tar.gz/localspelling-0.5/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name = 'localspelling',
+  packages = ['localspelling'],
+  version = '0.5',
+  license='MIT', 
+  description = 'Translates input between UK and US spelling',
+  author = 'Thomas Wood',
+  author_email = 'thomas@fastdatascience.com',      
+  url = 'https://fastdatascience.com',   
+  keywords = ['spelling', 'localisation', 'localization', 'uk', 'us', 'british english', 'us english'],
+  install_requires=[
+          
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',   
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+  include_package_data=True,
+)
