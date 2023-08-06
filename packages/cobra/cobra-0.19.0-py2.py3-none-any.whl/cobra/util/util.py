beec1f6a79fa244a21ca89271e453ed32f07b0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:15 +0000

    Add package cobra/0.19.0/cobra-0.19.0-py2.py3-none-any.whl

---
+++ packages/cobra/cobra-0.19.0-py2.py3-none-any.whl/cobra/util/util.py
@@ -0,0 +1,27 @@
+"""General utilities."""
+
+from depinfo import print_dependencies
+
+
+def format_long_string(string, max_length=50):
+    if len(string) > max_length:
+        string = string[: max_length - 3]
+        string += "..."
+    return string
+
+
+class AutoVivification(dict):
+    """Implementation of perl's autovivification feature. Checkout
+    http://stackoverflow.com/a/652284/280182 """
+
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
+
+def show_versions():
+    """Print dependency information."""
+    print_dependencies("cobra")
