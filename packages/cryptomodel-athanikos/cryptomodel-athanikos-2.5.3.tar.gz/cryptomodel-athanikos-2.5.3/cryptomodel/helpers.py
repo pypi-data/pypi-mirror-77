9e304d2867473faf78b497a5a3f3f6de1b57302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:08 +0000

    Add package cryptomodel-athanikos/2.5.3/cryptomodel-athanikos-2.5.3.tar.gz

---
+++ packages/cryptomodel-athanikos/cryptomodel-athanikos-2.5.3.tar.gz/cryptomodel-athanikos-2.5.3/cryptomodel/helpers.py
@@ -0,0 +1,26 @@
+
+import mongoengine
+from mongoengine import connect, Q
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+def do_connect(configuration):
+    url = 'mongodb://' + configuration.USERNAME + ':' + configuration.PASSWORD + '@' \
+          + configuration.SERVERNAME + ':' + str(configuration.PORT) + '/?authSource=admin'
+    conn = connect(db=configuration.DATABASE, username=configuration.USERNAME, host=url)
+
+
+def get_url(configuration):
+    return 'mongodb://' + configuration.USERNAME + ':' + configuration.PASSWORD + '@' \
+           + configuration.SERVERNAME + ':' + str(configuration.PORT) + '/?authSource=admin'
+
+
+def server_time_out_wrapper(repository, method_to_call, *args):
+    try:
+        return method_to_call(*args)
+    except ServerSelectionTimeoutError as inst:
+        repository.log_error(exception=inst, web_method_name=str(method_to_call), cfg=repository.configuration)
+        raise
+    except mongoengine.connection.ConnectionFailure as cf:
+        repository.log_error(exception=cf, web_method_name=str(method_to_call), cfg=repository.configuration)
+        raise mongoengine.connection.ConnectionFailure from cf
