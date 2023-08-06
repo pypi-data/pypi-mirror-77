65db7479331c0897e2cf0897bd314d81ccaccfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package igem-wikisync/0.0.13/igem-wikisync-0.0.13.tar.gz

---
+++ packages/igem-wikisync/igem-wikisync-0.0.13.tar.gz/igem-wikisync-0.0.13/src/igem_wikisync/parsers.py
@@ -0,0 +1,113 @@
+import re
+
+from bs4 import BeautifulSoup
+
+from igem_wikisync.path import iGEM_URL
+
+# process HTML files
+
+
+def HTMLparser(config: dict, path, contents: str, upload_map: dict) -> str:
+    """
+    Parses a given HTML string using bs4 and
+    converts relative paths to absolute iGEM URLs.
+
+    Arguments:
+        config: dictionary containing 'src_dir', 'build_dir' and 'path'
+        path: path to the file
+        contents: contents of the file
+        upload_map: custom upload map
+
+    Return: Contents with replaced URLs
+    """
+
+    # TODO: Change Path to pathlib.Path (but do we import the module for this?)
+    # https://stackoverflow.com/questions/2725156/complete-list-of-html-tag-attributes-which-have-a-url-value
+
+    soup = BeautifulSoup(contents, 'html5lib')
+
+    # tag-attribute pairs we're goign to check
+    queries = [('link', 'href'), ('script', 'src'), ('a', 'href'),
+               ('applet', 'codebase'), ('area', 'href'), ('base', 'href'),
+               ('blockquote', 'cite'), ('body', 'background'), ('del', 'cite'),
+               ('form', 'action'), ('frame', 'longdesc'), ('frame', 'src'),
+               ('head', 'profile'), ('iframe', 'longdesc'), ('iframe', 'src'),
+               ('img', 'longdesc'), ('img', 'src'), ('img', 'usemap'),
+               ('input', 'src'), ('input', 'usemap'), ('ins', 'cite'),
+               ('object', 'classid'), ('object', 'codebase'), ('object', 'data'),
+               ('object', 'usemap'), ('q', 'cite'), ('audio', 'src'),
+               ('button', 'formaction'), ('command', 'icon'), ('embed', 'src'),
+               ('html', 'manifest'), ('input', 'formaction'), ('source', 'src'),
+               ('track', 'src'), ('video', 'poster'), ('video', 'src')]
+
+    # TODO: Replace URLs for AJAX loads as well
+    # TODO: img srcset
+
+    for (tag_name, attr) in queries:
+        # don't replace if data-nosub is set
+        query = soup.findAll(tag_name, attrs={attr: True, 'data-nosub': False})
+        for tag in query:
+            tag[attr] = iGEM_URL(config, path, upload_map, tag[attr])
+            # TODO: Add error handling
+
+    # pass inline_styles to CSSparser
+    inline_styles = soup.findAll('style')
+    for style in inline_styles:
+        style.string = CSSparser(config, path, style.string, upload_map)
+        pass
+
+    # return soup as string
+    contents = str(soup)
+    return contents
+
+
+def CSSparser(config: dict, path, contents: str, upload_map: dict) -> str:
+    """
+    Parses a given CSS string and
+    converts relative paths to absolute iGEM URLs.
+
+    Arguments:
+        config: dictionary containing 'src_dir', 'build_dir' and 'path'
+        path: path to the file
+        contents: contents of the file
+        upload_map: custom upload map
+
+    Return: Contents with replaced URLs
+    """
+
+    # TODO: Replace URLs in image(), image-set() and cross-fade()
+    css = contents
+
+    # 1) Find all css links
+    exp = r'url\([\'\"]?([^\)\'\"]*)[\'\"]?\)'
+    links = re.findall(exp, css)
+
+    for i in range(len(links)):
+        links[i] = links[i].split('?')[0]
+        links[i] = links[i].split('#')[0]
+
+    # 2) Clear all duplicates
+    links = list(dict.fromkeys(links))
+
+    # 3) Replace all links with the absolute path
+    for link in links:
+
+        css = css.replace(link, iGEM_URL(config, path, upload_map, link.strip()))
+
+    return css
+
+
+def JSparser(contents: str) -> str:
+    """
+    Is supposed to replace URLs in JS.
+
+    Arguments:
+        contents: JS string
+
+    Returns:
+        Input JS string
+
+    """
+
+    # TODO: URL replacement
+    return contents
---
+++ packages/igem-wikisync/igem-wikisync-0.0.13.tar.gz/igem-wikisync-0.0.13/tests/test_parsers.py
@@ -0,0 +1,115 @@
+import hashlib
+from datetime import date
+
+import pytest
+
+from igem_wikisync.parsers import CSSparser, HTMLparser, JSparser
+
+
+@pytest.fixture
+def config():
+    return {
+        'team': 'BITSPilani-Goa_India',
+        'src_dir': 'tests/data',
+        'build_dir': 'tests/build',
+        'year': str(date.today().year),
+        'silence_warnings': False
+    }
+
+
+def md5hash(text):
+    return hashlib.md5(text.encode('UTF-8')).hexdigest()
+
+
+@pytest.fixture
+def upload_map():
+    return {
+        'assets': {
+            'assets/img/test.jpg': {
+                'link_URL': 'somerandomURLthatiGEMsends'
+            },
+            'assets/img/test-test.jpg': {
+                'link_URL': 'anotherrandomURLthatiGEMsends'
+            }
+        }
+    }
+
+
+def test_HTMLparser(config, upload_map):
+    with open('tests/data/Test/index.html', 'r') as file:
+        contents = file.read()
+
+    parsed = HTMLparser(config, 'Test/index.html', contents, upload_map)
+
+    assert md5hash(parsed) == '3efd614407d7bfac36b2832ea47d2512'
+
+
+def test_HTMLparser_link(config, upload_map):
+    with open('tests/data/Test/html/link.html', 'r') as file:
+        contents = file.read()
+    parsed = HTMLparser(config, 'index.html', contents, upload_map)
+    assert md5hash(parsed) == '25ec984a0e9889dab0a0986b1eacee09'
+
+
+def test_HTMLparser_script(config, upload_map):
+    with open('tests/data/Test/html/script.html', 'r') as file:
+        contents = file.read()
+    parsed = HTMLparser(config, 'index.html', contents, upload_map)
+    assert md5hash(parsed) == '727d10aa1f48361ff0fb5236becc3f63'
+
+
+def test_HTMLparser_inline_styles(config, upload_map):
+    with open('tests/data/Test/html/inline-styles.html', 'r') as file:
+        contents = file.read()
+    parsed = HTMLparser(config, 'Test/html/inline-styles.html', contents, upload_map)
+    assert md5hash(parsed) == 'f1bf98174c3a74ceb670af98514f069f'
+
+
+def test_HTMLparser_img(config, upload_map):
+    with open('tests/data/Test/html/img.html', 'r') as file:
+        contents = file.read()
+    parsed = HTMLparser(config, 'Test/html/img.html', contents, upload_map)
+    assert md5hash(parsed) == 'ba2d3f459a58bb24113e481a169016b4'
+
+
+def test_CSSparser_without_quotes(config, upload_map):
+    with open('tests/data/Test/css/without_quotes.css', 'r') as file:
+        contents = file.read()
+
+    parsed = CSSparser(config, 'Test/css/without_quotes.css', contents, upload_map)
+    assert md5hash(parsed) == 'dcef7d4a8f51008157a019f3e65c10cc'
+
+
+def test_CSSparser_single_quotes(config, upload_map):
+    with open('tests/data/Test/css/single_quotes.css', 'r') as file:
+        contents = file.read()
+
+    parsed = CSSparser(config, 'Test/css/style.css', contents, upload_map)
+
+    # assert parsed == 'hello'
+    assert md5hash(parsed) == '3f01c1910d12a07e541ae56cd1c3fb35'
+
+
+def test_CSSparser_double_quotes(config, upload_map):
+    with open('tests/data/Test/css/double_quotes.css', 'r') as file:
+        contents = file.read()
+
+    parsed = CSSparser(config, 'Test/css/style.css', contents, upload_map)
+    assert md5hash(parsed) == 'a2aefc22412100e5b27e34028985a215'
+
+
+def test_CSSparser_hyphen(config, upload_map):
+    with open('tests/data/Test/css/hyphen.css', 'r') as file:
+        contents = file.read()
+
+    parsed = CSSparser(config, 'Test/css/style.css', contents, upload_map)
+    assert md5hash(parsed) == 'c1aa579595355b4cab71fbb5009f8ca2'
+
+
+def test_JSparser():
+    with open('tests/data/Test/js/index.js', 'r') as file:
+        contents = file.read()
+
+    parsed = JSparser(contents)
+
+    assert md5hash(parsed) == '98ceab3bcb66cfe2bc3c07a2bfd56503'
