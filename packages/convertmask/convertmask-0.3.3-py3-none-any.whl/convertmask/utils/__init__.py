266ead2625089cb55b83cb1caaa25a21ae62ad00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:00 +0000

    Add package convertmask/0.3.3/convertmask-0.3.3-py3-none-any.whl

---
+++ packages/convertmask/convertmask-0.3.3-py3-none-any.whl/convertmask/utils/__init__.py
@@ -0,0 +1,11 @@
+'''
+@lanhuage: python
+@Descripttion: 
+@version: beta
+@Author: xiaoshuyui
+@Date: 2020-07-10 10:09:56
+LastEditors: xiaoshuyui
+LastEditTime: 2020-08-21 15:49:22
+'''
+__version__ = '0.3.3'
+
