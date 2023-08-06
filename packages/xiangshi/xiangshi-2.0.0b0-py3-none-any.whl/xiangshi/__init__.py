ac02102d70a1c0ffeae7268e5ad63f4cd289b970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:10 +0000

    Add package xiangshi/2.0.0b0/xiangshi-2.0.0b0-py3-none-any.whl

---
+++ packages/xiangshi/xiangshi-2.0.0b0-py3-none-any.whl/xiangshi/__init__.py
@@ -0,0 +1,8 @@
+from xiangshi.main import calculator
+
+calculator = calculator()
+for x in dir(calculator):
+    if x[:2] != "__":
+        exec(x + "=" + "calculator." + x)
+
+cal = calculator.cossim
