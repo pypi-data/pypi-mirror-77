33b0263dee180229f1ba2c38467a8fba49d21a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:10 +0000

    Add package jupyterlab-scheduler/0.1.2/jupyterlab_scheduler-0.1.2.tar.gz

---
+++ packages/jupyterlab-scheduler/jupyterlab_scheduler-0.1.2.tar.gz/jupyterlab_scheduler-0.1.2/setup.py
@@ -0,0 +1,120 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2017, Project Jupyter
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Setup Module to setup Python Handlers for the jlab_ext_example extension.
+"""
+import os
+from os.path import join as pjoin
+
+from jupyter_packaging import (
+    create_cmdclass, install_npm, ensure_targets,
+    combine_commands, ensure_python, get_version    
+)
+import setuptools
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The name of the project
+name="jupyterlab_scheduler"
+
+# Ensure a valid python version
+ensure_python(">=3.5")
+
+# Get the version
+version = get_version(pjoin(name, "_version.py"))
+
+lab_path = pjoin(HERE, name, "labextension")
+
+# Representative files that should exist after a successful build
+jstargets = [
+    pjoin(HERE, "lib", "plugin.js"),
+]
+
+package_data_spec = {
+    name: [
+        "*"
+    ]
+}
+
+data_files_spec = [
+    ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
+    ("etc/jupyter/jupyter_notebook_config.d",
+     "jupyter-config", "jupyterlab_scheduler.json"),
+]
+
+cmdclass = create_cmdclass("jsdeps",
+    package_data_spec=package_data_spec,
+    data_files_spec=data_files_spec
+)
+
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(HERE, build_cmd="build:all", npm=["jlpm"]),
+    ensure_targets(jstargets),
+)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url="https://github.com/tiburon-security/jupyterlab_scheduler",
+    author="Jovanni Hernandez",
+    description="CRON scheduler for Jupyter Lab",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    cmdclass= cmdclass,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "jupyterlab~=2.0",
+        "python-crontab~=2.5",
+        "papermill~=2.1"
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    license="MIT",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "JupyterLab"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Jupyter",
+    ],
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_args)
---
+++ packages/jupyterlab-scheduler/jupyterlab_scheduler-0.1.2.tar.gz/jupyterlab_scheduler-0.1.2/src/ShowJobs.js
@@ -0,0 +1,146 @@
+import { ReactWidget, MainAreaWidget } from '@jupyterlab/apputils';
+import { requestAPI } from './api';
+import { ViewLog } from './ViewLog';
+import React from 'react';
+
+
+class Jobs extends React.Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      jobs: []
+    }
+  }
+
+
+  componentDidMount(){
+    this.getScheduledJobs()
+  }
+
+
+  openLog(schedule, command){
+
+        
+        // Create widget for displaying jobs & attach
+        const content = new ViewLog(command, schedule);
+        const widget = new MainAreaWidget({ content });
+        widget.title.label = `Log - ${command}`;
+        widget.title.closable = true;
+        widget.id = 'scheduled-job-log';
+
+        this.props.shell.add(widget, 'main');
+
+      
+
+  }
+
+  async getScheduledJobs() {
+    try {
+      const data = await requestAPI('list');
+
+      if("data" in data){
+        this.setState({jobs:data.data})
+      }
+
+      console.log("data", data);
+
+    } catch (reason) {
+      console.error(`Error fetching jobs: ${reason}`);
+    }
+  }
+
+  async deleteJob(schedule, command){
+
+    const dataToSend = { 
+      command,
+      schedule
+    };
+
+    try {
+      const reply = await requestAPI('delete', {
+        body: JSON.stringify(dataToSend),
+        method: 'POST'
+      });
+
+      // Delete the item the user selected from the state array
+      // Particulary complicated because I had to account for the fact that
+      // there may be duplicate jobs & the server is just going to remove the first instance
+      let jobIndexToRemove = null;
+      this.state.jobs.forEach((job, i)=> {
+        if(job.command === command && job.schedule === schedule && jobIndexToRemove === null){
+          jobIndexToRemove = i;
+        }
+      });
+
+      if(jobIndexToRemove !== null){
+        let updatedJobs = this.state.jobs.filter((job, i) => i !== jobIndexToRemove)
+
+        this.setState({
+          jobs:updatedJobs
+        })
+      }
+
+    } catch (reason) {
+      console.error(
+        `Error sending delete: ${reason}`
+      );
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <table style={{
+          "border" : "1px solid black",
+          "borderCollapse": "collapse"
+        }}>
+          <thead>
+            <tr style={{"border" : "1px solid black"}}>
+              <th style={{"border" : "1px solid black"}}>Schedule</th>
+              <th style={{"border" : "1px solid black"}}>Script</th>
+              <th style={{"border" : "1px solid black"}}>Command</th>
+              <th style={{"border" : "1px solid black"}}>Log Location</th>
+              <th style={{"border" : "1px solid black"}}>Log</th>
+              <th style={{"border" : "1px solid black"}}>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {this.state.jobs.map((job, i)=>(
+              <tr key={i} style={{"border" : "1px solid black"}}>
+                <td style={{"border" : "1px solid black"}}>{job.schedule}</td>
+                <td style={{"border" : "1px solid black"}}>{job.script}</td>
+                <td style={{"border" : "1px solid black"}}>{job.command}</td>
+                <td style={{"border" : "1px solid black"}}>{job.log_file}</td>
+                <td style={{"border" : "1px solid black"}}><button onClick={()=>{this.openLog(job.schedule, job.command)}}>View</button></td>
+                <td style={{"border" : "1px solid black"}}><button onClick={()=>{this.deleteJob(job.schedule, job.command)}}>X</button></td>
+
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+      </div >
+    )
+  }
+}
+
+/**
+ * A Lumino Widget that wraps a react component.
+ */
+export class ViewScheduledJobs extends ReactWidget {
+
+  constructor(shell) {
+    super();
+    this.addClass('ReactWidget');
+    console.log("yo", shell)
+    this.shell = shell;
+  }
+
+  render() {
+    return <Jobs dog="yo" shell={this.shell}/>;
+  }
+}
