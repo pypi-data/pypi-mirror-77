7eaf60d9a2da3586de153a6238caf767e4e806f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package uniplot/0.0.5/uniplot-0.0.5.tar.gz

---
+++ packages/uniplot/uniplot-0.0.5.tar.gz/uniplot-0.0.5/uniplot/pixel_matrix.py
@@ -0,0 +1,51 @@
+import numpy as np  # type: ignore
+from typing import Optional
+
+from uniplot.discretizer import discretize
+
+
+def render(
+    xs: np.array,
+    ys: np.array,
+    x_min: float,
+    x_max: float,
+    y_min: float,
+    y_max: float,
+    width: int,
+    height: int,
+) -> np.array:
+    """
+    Turn a list of 2D points into a raster matrix.
+
+    Returns the pixels as 2D array with 1 or 0 integer entries.
+
+    Note that the row order is optimized for drawing later, so the first row corresponds
+    to the highest line of pixels.
+    """
+    assert len(xs) == len(ys)
+    assert x_max > x_min
+    assert y_max > y_min
+    assert width > 0
+    assert height > 0
+
+    x_indices = discretize(np.array(xs), x_min, x_max, steps=width)
+    y_indices = discretize(np.array(ys), y_min, y_max, steps=height)
+
+    # Invert y direction to optimize for plotting later
+    y_indices = (height - 1) - y_indices
+
+    # Filter out of view pixels
+    xy_indices = np.stack((x_indices, y_indices)).T
+    xy_indices = xy_indices[
+        (xy_indices[:, 0] >= 0)
+        & (xy_indices[:, 0] < width)
+        & (xy_indices[:, 1] >= 0)
+        & (xy_indices[:, 1] < height)
+    ]
+    xy_indices = xy_indices.T
+
+    # Assemble pixel matrix
+    pixels = np.zeros((height, width), dtype=int)
+    pixels[xy_indices[1], xy_indices[0]] = 1
+
+    return pixels
---
+++ packages/uniplot/uniplot-0.0.5.tar.gz/uniplot-0.0.5/uniplot/uniplot.py
@@ -0,0 +1,114 @@
+import numpy as np  # type: ignore
+from typing import List, Optional
+
+from uniplot.options import Options
+import uniplot.layers as layers
+import uniplot.plot_elements as elements
+from uniplot.getch import getch
+
+
+def plot(ys: np.array, xs: Optional[np.array] = None, **kwargs) -> None:
+    """2D scatter dot plot on the terminal."""
+    ys = np.array(ys)
+    if xs is None:
+        xs = np.arange(1, len(ys) + 1, step=1, dtype=int)
+    else:
+        xs = np.array(xs)
+
+    options = Options(**kwargs)
+    options.initialize_view_to_show_all(xs=xs, ys=ys)
+
+    # Print title
+    if options.title is not None:
+        print(elements.plot_title(options.title, width=options.width))
+
+    # Main loop for interactive mode. Will only be executed once when not in interactive # mode.
+    continue_looping: bool = True
+    loop_iteration: int = 0
+    while continue_looping:
+        # Make sure we stop after first iteration when not in interactive mode
+        if not options.interactive:
+            continue_looping = False
+
+        # Prepare plot elements
+        y_axis_labels = elements.yaxis_ticks(
+            y_min=options.y_min, y_max=options.y_max, height=options.height
+        )
+        x_axis_labels = elements.xaxis_ticks(
+            x_min=options.x_min, x_max=options.x_max, width=options.width
+        )
+
+        # Prefare graph surface
+        gridline_layers = [
+            layers.render_y_gridline(y=y, options=options) for y in options.y_gridlines
+        ]
+        pixel_layer = layers.render_points(xs=xs, ys=ys, options=options)
+        all_layers = gridline_layers + [pixel_layer]
+        pixel_character_matrix = layers.merge_layers(all_layers, options=options)
+
+        # Delete plot before we re-draw
+        if loop_iteration > 0:
+            elements.erase_previous_lines(options.height + 4)
+
+        # Print plot (double resolution)
+        print(f"┌{'─'*options.width}┐")
+        for i in range(options.height):
+            row = pixel_character_matrix[i]
+            print(f"│{''.join(row)}│ {y_axis_labels[i]}")
+        print(f"└{'─'*options.width}┘")
+        print(x_axis_labels)
+
+        if options.interactive:
+            print(
+                "Interactive mode: Move viewport using h/j/k/l, zoom via u/n, or r to reset. Escape/q to quit"
+            )
+            key_pressed = getch().lower()
+
+            # TODO Move all of the below to the `Options` class
+            if key_pressed == "h":
+                # Left
+                step = 0.1 * (options.x_max - options.x_min)
+                options.x_min = options.x_min - step
+                options.x_max = options.x_max - step
+            elif key_pressed == "l":
+                # Right
+                step = 0.1 * (options.x_max - options.x_min)
+                options.x_min = options.x_min + step
+                options.x_max = options.x_max + step
+            elif key_pressed == "j":
+                # Up
+                step = 0.1 * (options.y_max - options.y_min)
+                options.y_min = options.y_min - step
+                options.y_max = options.y_max - step
+            elif key_pressed == "k":
+                # Down
+                step = 0.1 * (options.y_max - options.y_min)
+                options.y_min = options.y_min + step
+                options.y_max = options.y_max + step
+            elif key_pressed == "u":
+                # Zoom in
+                step = 0.1 * (options.x_max - options.x_min)
+                options.x_min = options.x_min + step
+                options.x_max = options.x_max - step
+                step = 0.1 * (options.y_max - options.y_min)
+                options.y_min = options.y_min + step
+                options.y_max = options.y_max - step
+            elif key_pressed == "n":
+                # Zoom out
+                step = 0.1 * (options.x_max - options.x_min)
+                options.x_min = options.x_min - step
+                options.x_max = options.x_max + step
+                step = 0.1 * (options.y_max - options.y_min)
+                options.y_min = options.y_min - step
+                options.y_max = options.y_max + step
+            elif key_pressed == "r":
+                # Reset view
+                options.x_min = xs.min()
+                options.x_max = xs.max()
+                options.y_min = ys.min()
+                options.y_max = ys.max()
+            elif key_pressed in ["q", "\x1b"]:
+                # q and Escape will end interactive mode
+                continue_looping = False
+
+            loop_iteration += 1
