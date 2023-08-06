25495e2ca8dcf47b0f140844b9d02a436dba98c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:37 +0000

    Add package switches.py/0.1.0/switches.py-0.1.0.tar.gz

---
+++ packages/switches.py/switches.py-0.1.0.tar.gz/switches.py-0.1.0/setup.py
@@ -0,0 +1,31 @@
+"""
+:copyright: Copyright (c) 2020 Jeremiah Ikosin (@ziord)
+:license: MIT, see LICENSE for more details
+"""
+
+from setuptools import setup, find_packages
+import info
+
+data = info.get_info()
+setup(
+    name=data["name"]+'.py',
+    version=data["version"],
+    description=data["description"],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url=data["project_urls"]["Source Code"],
+    author=data["author"],
+    license=data["license"],
+    keywords=data["keywords"],
+    project_urls=data["project_urls"],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'info': [*data['package_data'].values()]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
