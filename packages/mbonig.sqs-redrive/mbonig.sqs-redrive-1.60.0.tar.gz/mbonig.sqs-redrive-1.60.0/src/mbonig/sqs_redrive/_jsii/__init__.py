3f52a89b8a6378b62021401e217fe76321a850bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:18 +0000

    Add package mbonig.sqs-redrive/1.60.0/mbonig.sqs-redrive-1.60.0.tar.gz

---
+++ packages/mbonig.sqs-redrive/mbonig.sqs-redrive-1.60.0.tar.gz/mbonig.sqs-redrive-1.60.0/src/mbonig/sqs_redrive/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@matthewbonig/sqs-redrive",
+    "1.60.0",
+    __name__[0:-6],
+    "sqs-redrive@1.60.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
