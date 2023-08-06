3af424c50c55b90fc53ad73c2121a03e81d1a92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:26 +0000

    Add package deepnog/1.2.1/deepnog-1.2.1-py3-none-any.whl

---
+++ packages/deepnog/deepnog-1.2.1-py3-none-any.whl/deepnog/tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Description:
+
+    Helpers for deepnog tests.
+
+    Including:
+        - test data
+        - test network weights (parameters)
+        - some helper functions
+
+    Individual tests are located within the respective deepnog subpackages.
+"""
