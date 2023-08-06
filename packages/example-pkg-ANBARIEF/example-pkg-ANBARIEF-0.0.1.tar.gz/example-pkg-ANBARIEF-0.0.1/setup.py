bd471a8abab9d900201fd6f49dd634ba6097588c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package example-pkg-ANBARIEF/0.0.1/example-pkg-ANBARIEF-0.0.1.tar.gz

---
+++ packages/example-pkg-ANBARIEF/example-pkg-ANBARIEF-0.0.1.tar.gz/example-pkg-ANBARIEF-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(\
+    name='example-pkg-ANBARIEF',\
+    version='0.0.1',\
+    author='Arief Anbiya',\
+    author_email='anbarief@live.com',\
+    description='blablabla',\
+    url='https://github.com/pypa/sampleproject',\
+    packages=setuptools.find_packages(),\
+    classifiers=["Programming Language :: Python :: 3",\
+                 "License :: OSI Approved :: MIT License",\
+                 "Operating System :: OS Independent"],\
+    python_requires='>=3.6')
