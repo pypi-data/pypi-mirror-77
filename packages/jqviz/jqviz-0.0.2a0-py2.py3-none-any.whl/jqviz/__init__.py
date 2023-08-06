0e5c96eace69f50efc0f7d4a8363469b56c783b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:40 +0000

    Add package jqviz/0.0.2a0/jqviz-0.0.2a0-py2.py3-none-any.whl

---
+++ packages/jqviz/jqviz-0.0.2a0-py2.py3-none-any.whl/jqviz/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.2a0"
+
+__title__ = "jqviz"
+__description__ = "Visualize your jq commands"
+__url__ = "https://github.com/jkubicek/jkviz"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Jim Kubicek"
+__email__ = "jim@jimkubicek.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020 Jim Kubicek"
