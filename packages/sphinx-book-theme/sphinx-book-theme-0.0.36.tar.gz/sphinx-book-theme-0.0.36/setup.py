ff53cd7630191cf3a96f243b6e18acc35174a183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:23 +0000

    Add package sphinx-book-theme/0.0.36/sphinx-book-theme-0.0.36.tar.gz

---
+++ packages/sphinx-book-theme/sphinx-book-theme-0.0.36.tar.gz/sphinx-book-theme-0.0.36/setup.py
@@ -0,0 +1,79 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+lines = Path("sphinx_book_theme").joinpath("__init__.py")
+for line in lines.read_text().split("\n"):
+    if line.startswith("__version__ ="):
+        version = line.split(" = ")[-1].strip('"')
+        break
+
+setup(
+    name="sphinx-book-theme",
+    version=version,
+    python_requires=">=3.6",
+    author="Project Jupyter Contributors",
+    author_email="jupyter@googlegroups.com",
+    url="https://jupyterbook.org/",
+    project_urls={
+        "Documentation": "https://jupyterbook.org",
+        "Funding": "https://jupyter.org/about",
+        "Source": "https://github.com/jupyter/jupyter-book/",
+        "Tracker": "https://github.com/jupyter/jupyter-book/issues",
+    },
+    # this should be a whitespace separated string of keywords, not a list
+    keywords="reproducible science environments scholarship notebook",
+    description="Jupyter Book: Create an online book with Jupyter Notebooks",
+    long_description=Path("./README.md").read_text(),
+    long_description_content_type="text/markdown",
+    license="BSD",
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml",
+        "docutils>=0.15",
+        "sphinx",
+        "click",
+        "setuptools",
+        "libsass",
+        "pydata-sphinx-theme~=0.3.0",
+        "beautifulsoup4",
+    ],
+    extras_require={
+        "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
+        "sphinx": [
+            "folium",
+            "numpy",
+            "matplotlib",
+            "ipywidgets",
+            "pandas",
+            "nbclient",
+            "myst-nb~=0.9.1",
+            "sphinx-togglebutton>=0.2.1",
+            "sphinx-copybutton",
+            "plotly",
+            "sphinxcontrib-bibtex",
+            "sphinx-thebe",
+        ],
+        "testing": [
+            "coverage",
+            "pytest>=3.6,<4",
+            "pytest-cov",
+            "beautifulsoup4",
+            "pytest-regressions",
+        ],
+    },
+    entry_points={"sphinx.html_themes": ["sphinx_book_theme = sphinx_book_theme"]},
+    package_data={
+        "sphinx_book_theme": [
+            "theme.conf",
+            # Templates
+            "*.html",
+            "topbar/*.html",
+            # Stylesheets
+            "scss/*",
+            # Other static files
+            "static/*",
+            "static/images/*",
+        ]
+    },
+    include_package_data=True,
+)
