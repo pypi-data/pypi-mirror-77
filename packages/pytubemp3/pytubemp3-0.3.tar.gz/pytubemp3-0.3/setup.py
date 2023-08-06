82747d74d7817bfbe3657acfb36b60fd0ef44b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:00 +0000

    Add package pytubemp3/0.3/pytubemp3-0.3.tar.gz

---
+++ packages/pytubemp3/pytubemp3-0.3.tar.gz/pytubemp3-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pytubemp3',
+      version='0.3',
+      description='downloader',
+      packages=['pytubemp3'],
+      author='abdiwoli hassen',
+      author_email='abdiwolih@gmail.com',
+      zip_safe=False)
