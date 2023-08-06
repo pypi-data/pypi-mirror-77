25e4ee2680c7ca4ddb46b60f2d292051764096c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package amcatclient/3.4.13/amcatclient-3.4.13.tar.gz

---
+++ packages/amcatclient/amcatclient-3.4.13.tar.gz/amcatclient-3.4.13/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="amcatclient",
+    version='3.4.13',
+    description="Python bindings for the AmCAT API",
+    author="Wouter van Atteveldt",
+    author_email="wouter@vanatteveldt.com",
+    packages=["amcatclient"],
+    keywords = ["amcat", "client"],
+    download_url="https://github.com/amcat/amcatclient/tarball/3.4.1",
+    url="http://github.com/amcat/amcatclient",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing",
+    ],
+    install_requires=[
+        "requests",
+        "six==1.10.0"
+    ],
+)
