6f3ad04b589f23e08830f3bd3839dceb4e5958b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:35 +0000

    Add package pyplanet-agones/0.2.2/pyplanet_agones-0.2.2.tar.gz

---
+++ packages/pyplanet-agones/pyplanet_agones-0.2.2.tar.gz/pyplanet_agones-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+EXCLUDE_FROM_PACKAGES = [
+    'env*',
+]
+
+setup(name='pyplanet_agones',
+    version='0.2.2',
+    description='Connects PyPlanet with Agones',
+    url='https://github.com/LudusMatchMaking/pyplanet_agones',
+    author='Jonathan Hertz',
+    author_email='jonathan_hertz2@live.dk',
+    license='MIT',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    install_requires=[
+        'pyplanet',
+        'requests'
+    ],
+    zip_safe=False)
