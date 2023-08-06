d03eb5e8c1b1b19793b0d53a45733daa616999f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package preview-generator/0.15.4/preview_generator-0.15.4.tar.gz

---
+++ packages/preview-generator/preview_generator-0.15.4.tar.gz/preview_generator-0.15.4/setup.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# python setup.py sdist upload -r pypi
+
+
+import os
+import sys
+from typing import List
+
+from preview_generator import infos
+
+py_version = sys.version_info[:2]
+
+try:
+    from setuptools import find_packages
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+
+    use_setuptools()
+    from setuptools import find_packages
+    from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    documentation = open(os.path.join(here, "README.rst")).read()
+except IOError:
+    documentation = ""
+except UnicodeDecodeError:
+    documentation = ""
+
+testpkgs = []  # type: List[str]
+
+install_requires = [
+    "python-magic",
+    "Wand",
+    "PyPDF2",
+    "Pillow",
+    "pyexifinfo",
+    "xvfbwrapper",
+    "pathlib",
+    "pdf2image",
+    "cairosvg",
+    "ffmpeg-python",
+    "filelock",
+]
+tests_require = ["pytest"]
+
+devtools_require = ["flake8", "isort", "mypy", "pre-commit"]
+
+# add black for python 3.6+
+if sys.version_info.major == 3 and sys.version_info.minor >= 6:
+    devtools_require.append("black")
+
+if py_version <= (3, 4):
+    install_requires.append("typing")
+
+# TODO - G.M - 2019-11-05 - restore vtk as normal requirement, vtk is not compatible
+# with current version of vtk see https://gitlab.kitware.com/vtk/vtk/issues/17670,
+if py_version < (3, 8):
+    install_requires.append("vtk")
+
+setup(
+    name="preview_generator",
+    version=infos.__version__,
+    description=(
+        "A library for generating preview (thumbnails, text or json overview) "
+        "for file-based content"
+    ),
+    long_description=documentation,
+    author="Algoo",
+    author_email="contact@algoo.fr",
+    url="https://github.com/algoo/preview-generator",
+    download_url=(
+        "https://github.com/algoo/preview-generator/archive/release_{}.tar.gz".format(
+            infos.__version__
+        )
+    ),
+    keywords=["preview", "preview_generator", "thumbnail", "cache"],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(exclude=["ez_setup"]),
+    install_requires=install_requires,
+    python_requires=">= 3.5",
+    include_package_data=True,
+    extras_require={"testing": tests_require, "dev": tests_require + devtools_require},
+    test_suite="py.test",  # TODO : change test_suite
+    tests_require=testpkgs,
+    package_data={"preview_generator": ["i18n/*/LC_MESSAGES/*.mo", "templates/*/*", "public/*/*"]},
+    entry_points={"console_scripts": ["preview = preview_generator.__main__:main"]},
+)
---
+++ packages/preview-generator/preview_generator-0.15.4.tar.gz/preview_generator-0.15.4/tests/test_manager.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import typing
+
+from preview_generator.manager import PreviewManager
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+CACHE_DIR = "/tmp/preview-generator-tests/cache"
+
+
+def setup_function(function: typing.Callable) -> None:
+    shutil.rmtree(CACHE_DIR, ignore_errors=True)
+
+
+def test_cache_dir_not_created() -> None:
+    shutil.rmtree(CACHE_DIR, ignore_errors=True)
+    PreviewManager(cache_folder_path=CACHE_DIR, create_folder=False)
+    assert not os.path.exists(CACHE_DIR)
+
+
+def test_cache_dir_is_created() -> None:
+    shutil.rmtree(CACHE_DIR, ignore_errors=True)
+    PreviewManager(cache_folder_path=CACHE_DIR, create_folder=True)
+    assert os.path.exists(CACHE_DIR)
+
+
+def test_get_preview_name() -> None:
+    pm = PreviewManager(cache_folder_path=CACHE_DIR, create_folder=True)
+
+    filehash = pm.get_preview_context("/tmp/image.jpeg", file_ext=".jpeg").hash
+    hash = pm._get_preview_name(filehash)
+    assert hash == "7f8df7223d8be60a7ac8a9bf7bd1df2a"
+
+
+def test_get_preview_name_with_size() -> None:
+    pm = PreviewManager(cache_folder_path=CACHE_DIR, create_folder=True)
+    from preview_generator.utils import ImgDims
+
+    filehash = pm.get_preview_context("/tmp/image.jpeg", file_ext=".jpeg").hash
+    hash = pm._get_preview_name(filehash, ImgDims(width=512, height=256))
+    assert hash == "7f8df7223d8be60a7ac8a9bf7bd1df2a-512x256"
+
+
+def test_get_preview_name_with_size_and_page() -> None:
+    pm = PreviewManager(cache_folder_path=CACHE_DIR, create_folder=True)
+    from preview_generator.utils import ImgDims
+
+    filehash = pm.get_preview_context("/tmp/image.jpeg", file_ext=".jpeg").hash
+    hash = pm._get_preview_name(filehash, ImgDims(width=512, height=256), page=3)
+    assert hash == "7f8df7223d8be60a7ac8a9bf7bd1df2a-512x256-page3"
+
+
+def test_get_preview_name_with_page() -> None:
+    pm = PreviewManager(cache_folder_path=CACHE_DIR, create_folder=True)
+
+    filehash = pm.get_preview_context("/tmp/image.jpeg", file_ext=".jpeg").hash
+    hash = pm._get_preview_name(filehash, page=3)
+    assert hash == "7f8df7223d8be60a7ac8a9bf7bd1df2a-page3"
