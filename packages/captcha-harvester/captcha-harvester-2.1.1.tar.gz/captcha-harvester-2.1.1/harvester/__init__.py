06d5202ab8ebf6eb04614c5f05473a2917c593a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:50 +0000

    Add package captcha-harvester/2.1.1/captcha-harvester-2.1.1.tar.gz

---
+++ packages/captcha-harvester/captcha-harvester-2.1.1.tar.gz/captcha-harvester-2.1.1/harvester/__init__.py
@@ -0,0 +1,8 @@
+from harvester import browser, fetch, server
+from harvester.browser import BrowserEnum
+from harvester.server import CaptchaKindEnum, Harvester
+
+__version__ = '2.1.1'
+
+__all__ = ['browser', 'server', 'fetch',
+           'CaptchaKindEnum', 'BrowserEnum', 'Harvester']
