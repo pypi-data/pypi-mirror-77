a5e58573bd2c786699c934b0898822d106db8b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package jayellhohoho/0.1/jayellhohoho-0.1.tar.gz

---
+++ packages/jayellhohoho/jayellhohoho-0.1.tar.gz/jayellhohoho-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jayellhohoho',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jayellhohoho'],
+      zip_safe=False)
