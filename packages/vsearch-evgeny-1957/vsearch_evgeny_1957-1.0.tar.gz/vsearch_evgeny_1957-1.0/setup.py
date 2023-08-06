d43f71a59138521974c6d3c4a5d0ace430f2c2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:05 +0000

    Add package vsearch-evgeny-1957/1.0/vsearch_evgeny_1957-1.0.tar.gz

---
+++ packages/vsearch-evgeny-1957/vsearch_evgeny_1957-1.0.tar.gz/vsearch_evgeny_1957-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+    
+setup (
+    name ='vsearch_evgeny_1957',
+    version='1.0',
+    description = 'The Head First Python Search Tools',
+    author ='HF Python 2e',
+    author_email ='evyakush@gmail.com',
+    py_modules=['vsearch_evgeny_1957'],
+)
