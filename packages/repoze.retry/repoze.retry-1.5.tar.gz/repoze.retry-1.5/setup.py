27b475f705b918c5fb237ed29e7e9d718a1f60ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:15 +0000

    Add package repoze.retry/1.5/repoze.retry-1.5.tar.gz

---
+++ packages/repoze.retry/repoze.retry-1.5.tar.gz/repoze.retry-1.5/setup.py
@@ -0,0 +1,54 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+def _read_file(filename):
+    try:
+        with open(os.path.join(here, filename)) as f:
+            return f.read()
+    except IOError:  # Travis???
+        return ''
+README = _read_file('README.rst')
+CHANGES = _read_file('CHANGES.rst')
+
+testing_extras = ['nose', 'coverage']
+
+setup(name='repoze.retry',
+      version='1.5',
+      description='Middleware which implements a retryable exceptions',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        ],
+      keywords='wsgi middleware retry',
+      author="Agendaless Consulting",
+      author_email="repoze-dev@lists.repoze.org",
+      url="http://www.repoze.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['repoze'],
+      zip_safe=False,
+      test_suite = "repoze.retry.tests",
+      entry_points="""
+      [paste.filter_app_factory]
+      retry = repoze.retry:make_retry
+      """,
+      extras_require = {
+        'testing': testing_extras,
+      }
+)
