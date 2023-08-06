7b171349c3adf64e0d8429566ebc3c9de68d6bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package toolsbyerazhan/0.1.1/toolsbyerazhan-0.1.1.tar.gz

---
+++ packages/toolsbyerazhan/toolsbyerazhan-0.1.1.tar.gz/toolsbyerazhan-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "toolsbyerazhan",
+    version = "0.1.1",
+    keywords = ( "toolsbyerazhan","timetools","gpu4tftools"),
+    description = "tools tutorial",
+    long_description = "python tools tutorial",
+    license = "MIT Licence",
+
+    url = "https://github.com/erazhan/CommonTools",
+    author = "erazhan",
+    author_email = "erazhan@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []#['tensorflow']
+)
