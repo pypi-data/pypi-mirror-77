a7c2014f5473ffb9d3834a92f3b3d0ece4df00c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:22 +0000

    Add package nats-python-client/0.0.2/nats_python_client-0.0.2.tar.gz

---
+++ packages/nats-python-client/nats_python_client-0.0.2.tar.gz/nats_python_client-0.0.2/README.md
@@ -0,0 +1,10 @@
+# nats-python-client
+
+## create enviroment
+- python3 -m venv .venv
+- touch ./src/.env
+- source .venv/bin/activate
+
+## example
+
+- run `python3 example.py`
---
+++ packages/nats-python-client/nats_python_client-0.0.2.tar.gz/nats_python_client-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nats_python_client",
+    version="0.0.2",
+    author="jp-vargas",
+    author_email="vargasm.jp@gmail.com",
+    description="Nats client abstraction to use inside python projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Routte/nats-python-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
