db12b906c0090c10d310223c325bc7fab5090f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:29 +0000

    Add package aertb/0.3.4/aertb-0.3.4.tar.gz

---
+++ packages/aertb/aertb-0.3.4.tar.gz/aertb-0.3.4/aertb/core/types.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+
+__author__      = "Rafael Mosca"
+__email__       = "rafael.mosca@mail.polimi.it"
+__copyright__   = "Copyright 2020 - Rafael Mosca"
+__license__     = "MIT"
+__version__     = "1.0"
+
+# =============================================================================
+import numpy as np
+from collections import namedtuple
+# =============================================================================
+
+event_dtype = np.dtype([('x', np.uint16), ('y', np.uint16),
+                        ('ts', np.float32), ('p', np.int8)])
+
+event_dtype_ts64 = np.dtype([('x', np.uint16), ('y', np.uint16),
+                             ('ts', np.float64), ('p', np.int8)])
+
+stereo_event_dtype = np.dtype([('x', np.uint16), ('y', np.uint16),
+                               ('ts', np.float32), ('p', np.int8),
+                               ('x_', np.float32), ('y_', np.float32),
+                               ('z_', np.float32), ('d_', np.float32)])
+
+Sample = namedtuple('Sample', ['group', 'name'])
+EvSample = namedtuple('EvSample', ['label', 'name', 'events'])
+# =============================================================================
