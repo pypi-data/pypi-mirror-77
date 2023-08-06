39cccadc5908ef0f65be923dd64333c8a36af3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package lonny-flask-auth/1.6/lonny_flask_auth-1.6.tar.gz

---
+++ packages/lonny-flask-auth/lonny_flask_auth-1.6.tar.gz/lonny_flask_auth-1.6/lonny_flask_auth/password.py
@@ -0,0 +1,31 @@
+from .cfg import Configuration
+from flask import request
+from .base import Strategy, get_user
+import bcrypt
+
+class PasswordStrategy(Strategy):
+    def get_credentials_str(self):
+        return request.headers.get("x-lonny-password")
+
+    def get_credentials(self):
+        header = self.get_credentials_str()
+        if header is None:
+            return None
+        split = header.split(":")
+        if len(split) != 2:
+            return None
+        return split
+
+    def get_hash(self, user):
+        return user["password"]
+
+    def authenticate(self, state):
+        pair = self.get_credentials()
+        if pair is None:
+            return
+        state.attempted = True
+        email, password = pair
+        user = self.get_user(email)
+        if user is None or not bcrypt.checkpw(password, self.get_hash(user)):
+            return
+        state.user = self.get_user(data)
