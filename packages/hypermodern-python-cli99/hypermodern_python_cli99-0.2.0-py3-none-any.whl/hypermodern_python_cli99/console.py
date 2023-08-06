d7233a29fd372b4e8dc45d802fa6a62ef0454bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package hypermodern-python-cli99/0.2.0/hypermodern_python_cli99-0.2.0-py3-none-any.whl

---
+++ packages/hypermodern-python-cli99/hypermodern_python_cli99-0.2.0-py3-none-any.whl/hypermodern_python_cli99/console.py
@@ -0,0 +1,30 @@
+"""Command-line interface."""
+
+# standard library
+import textwrap
+
+
+# third-party packages
+import click
+
+
+# local packages
+from . import __version__, wikipedia
+
+
+@click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
+@click.version_option(version=__version__)
+def main(language: str) -> None:
+    """The hyermodern Python project."""
+    page = wikipedia.random_page(language=language)
+
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
