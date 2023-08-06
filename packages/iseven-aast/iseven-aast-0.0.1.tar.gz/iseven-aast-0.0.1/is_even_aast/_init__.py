80f1e3ffbe259c83a5e6451fdf597313d30ed009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package iseven-aast/0.0.1/iseven-aast-0.0.1.tar.gz

---
+++ packages/iseven-aast/iseven-aast-0.0.1.tar.gz/iseven-aast-0.0.1/is_even_aast/_init__.py
@@ -0,0 +1,5 @@
+def is_even_aast(n):
+  if n%2 ==0 :
+    print("even") 
+  else : print("odd") 
+is_even_aast(9)
---
+++ packages/iseven-aast/iseven-aast-0.0.1.tar.gz/iseven-aast-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'iseven-aast',
+    version = '0.0.1',
+    description = 'This is a even-odd checking function.',
+    long_description = open('Readme.txt').read(),
+    author = 'Aastha Singh',
+    keywords = ['even','odd','even_odd'],
+    license = 'MIT',
+    packages = ['is_even_aast'],
+    install_requires = ['']
+
+)
