c913c9f9087bfc8c3fe3e29b5c8291404596ea29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:59 +0000

    Add package dagit/0.9.3rc0/dagit-0.9.3rc0.tar.gz

---
+++ packages/dagit/dagit-0.9.3rc0.tar.gz/dagit-0.9.3rc0/dagit/webapp/build/precache-manifest.a1ab52c8b574a5fe2d07fb227c292ea2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82c8782a0e63ea1d7e0b43222d77be6",
+    "url": "/435cbb63dc0ee5c641b2.worker.js"
+  },
+  {
+    "revision": "b8714132b55319d9985b269752700df3",
+    "url": "/435cbb63dc0ee5c641b2.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7c93f0e226140950dcb14da1c6b67f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3dd13add66a8c1945d1",
+    "url": "/static/css/2.d3cd5de9.chunk.css"
+  },
+  {
+    "revision": "e3dd13add66a8c1945d1",
+    "url": "/static/js/2.35a9575a.chunk.js"
+  },
+  {
+    "revision": "50fbb10c2f10877dfae25577c57ea2a6",
+    "url": "/static/js/2.35a9575a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae57d9a137e8c19a219",
+    "url": "/static/js/main.dd7f8bfe.chunk.js"
+  },
+  {
+    "revision": "1ce0accb9006b5881ad3",
+    "url": "/static/js/runtime-main.cffdbaad.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
