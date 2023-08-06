535fc23c006e2c104deccda41a779a78bcfdff06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:08 +0000

    Add package easy-nlp/0.1.5/easy%20nlp-0.1.5.tar.gz

---
+++ packages/easy-nlp/easy%20nlp-0.1.5.tar.gz/easy nlp-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(name='easy nlp',
+version = '0.1.5',
+description = "NLP made easy.",
+long_description = "NLP made easy. Built on the top of spacy." ,
+url = "https://github.com/RajeshNathani/easy-nlp/tree/master",
+author = "Rajesh Nathani" ,
+author_email = "rajeshnathani055@gmail.com",
+license = "MIT" ,
+install_requires =['spacy==2.2'],
+packages = ['easy_nlp'] , zip_safe = False,
+
+)
