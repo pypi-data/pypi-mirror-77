18b7c467bf4a41a6d3548a7f6fc4e50ef35a951e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:01 +0000

    Add package pooraka/0.1.3/pooraka-0.1.3.tar.gz

---
+++ packages/pooraka/pooraka-0.1.3.tar.gz/pooraka-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pooraka',
+    version='0.1.3',
+    author='Chakkrit Termritthikun',
+    author_email='chakkritt60@nu.ac.th',
+    packages=['pooraka'],
+    url='https://github.com/chakkritte/pooraka',
+    scripts=[],
+    description='Pytorch wrapper',
+    install_requires=['torch >= 1.0', 'torchvision'],
+    python_requires='>=3.6',
+)
