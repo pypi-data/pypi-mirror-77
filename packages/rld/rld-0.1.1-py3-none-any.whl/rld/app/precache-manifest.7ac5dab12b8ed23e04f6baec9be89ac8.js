6d1c951e6facce0d15f3c6eae02588507d7f135e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:41 +0000

    Add package rld/0.1.1/rld-0.1.1-py3-none-any.whl

---
+++ packages/rld/rld-0.1.1-py3-none-any.whl/rld/app/precache-manifest.7ac5dab12b8ed23e04f6baec9be89ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01124275f4f026b86da1654300dc57a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25efc6fe6c7142d2d8da",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7c8dbbe970bb14fe66e8",
+    "url": "/static/css/main.fc910167.chunk.css"
+  },
+  {
+    "revision": "25efc6fe6c7142d2d8da",
+    "url": "/static/js/2.c53c3bcb.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.c53c3bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8dbbe970bb14fe66e8",
+    "url": "/static/js/main.284f6faf.chunk.js"
+  },
+  {
+    "revision": "accd022bdeba3d112601",
+    "url": "/static/js/runtime-main.e6726076.js"
+  }
+]);
