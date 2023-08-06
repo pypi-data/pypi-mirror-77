631c62d0fdbf4f682969ec50b4ce5038f0992126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:11 +0000

    Add package pytwanalysis/0.0.2/pytwanalysis-0.0.2.tar.gz

---
+++ packages/pytwanalysis/pytwanalysis-0.0.2.tar.gz/pytwanalysis-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+    
+setuptools.setup(
+    name="pytwanalysis",
+    version="0.0.2",
+    author="Lia Nogueira",    
+    description="A tool to gather, discover, and analyze Twitter data using a combinations of graph-clustering and topic modeling techniques with the goal of semantically grouping tweet messages together.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lianogueira/pytwanalysis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research"        
+    ],
+)
