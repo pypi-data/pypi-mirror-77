bc84900ea5c9c53546a394ab8eee05e198fbee19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:13 +0000

    Add package pywrangle/0.2.3/pywrangle-0.2.3.tar.gz

---
+++ packages/pywrangle/pywrangle-0.2.3.tar.gz/pywrangle-0.2.3/pywrangle/__init__.py
@@ -0,0 +1,40 @@
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-08-20 16:07:15
+ * @modify date 2020-08-20 16:07:15
+ * @desc [
+    Init file for pywrangle. Imports functions from:
+        - missing_data
+        - string_cleaning
+ ]
+ */
+"""
+
+
+##########
+# Standard
+##########
+
+
+##########
+# 3rd party
+##########
+
+import pandas as pd
+import numpy as np
+
+
+##########
+# PyWrangle functions
+##########
+
+try:
+    from pywrangle.string_cleaning import clean_str_columns
+    from pywrangle.missing_data import print_nulls_per_col
+    
+except ModuleNotFoundError:
+    from string_cleaning import clean_str_columns
+    from missing_data import print_nulls_per_col
+
+
