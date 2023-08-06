70f659c6689c3e4a36855cad9a3982e6b18b8567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:14 +0000

    Add package wsmocky/0.1.3/wsmocky-0.1.3.tar.gz

---
+++ packages/wsmocky/wsmocky-0.1.3.tar.gz/wsmocky-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+version = '0.1.3'
+
+
+setup(name='wsmocky',
+      version=version,
+      description="Create dynamic mock server with wsdl file.",
+      long_description="""Create dynamic mock server with wsdl file.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='webservice mock',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      url='https://github.com/ipconfiger/wsmocky',
+      license='MIT',
+      packages=['wsmocky'],
+      zip_safe=True,
+      install_requires=[
+          "tornado",
+          "lxml",
+          "click",
+          "requests"
+      ],
+      entry_points={
+        'console_scripts': ['wsmocky=wsmocky.wsmocky:main'],
+      },
+      )
