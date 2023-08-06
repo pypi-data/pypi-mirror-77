add1b63e3ff52ff33af981ec202407552b31c9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:37 +0000

    Add package telekinesis/0.1.0a1/telekinesis-0.1.0a1.tar.gz

---
+++ packages/telekinesis/telekinesis-0.1.0a1.tar.gz/telekinesis-0.1.0a1/telekinesis/__init__.py
@@ -0,0 +1,6 @@
+from .portal import Portal, RemoteController
+from .hub import Hub
+from .common import verify, sign, extend_role, check_min_role#, encrypt, decrypt
+import warnings
+
+# warnings.warn('EXPERIMENTAL package. API may change. Use only in non-critical, sand-boxed applications. Submit issues at https://github.com/eneuman/camarere')
