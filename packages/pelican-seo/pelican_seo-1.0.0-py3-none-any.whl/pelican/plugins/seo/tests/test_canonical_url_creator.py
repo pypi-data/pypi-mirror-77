7bd97563074ba79042581e8a0b5c44b6ac7f0fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:09 +0000

    Add package pelican-seo/1.0.0/pelican_seo-1.0.0-py3-none-any.whl

---
+++ packages/pelican-seo/pelican_seo-1.0.0-py3-none-any.whl/pelican/plugins/seo/tests/test_canonical_url_creator.py
@@ -0,0 +1,17 @@
+""" Units tests for Canonical URL Creator. """
+
+from seo.seo_enhancer.html_enhancer import CanonicalURLCreator
+
+
+class TestCanonicalURLCreator:
+    """ Unit tests for CanonicalURLCreator. """
+
+    def test_create_url(self, fake_article):
+        """ Test if create_url() returns the join of site URL and article URL. """
+
+        canonical = CanonicalURLCreator(
+            fileurl=fake_article.url, siteurl=fake_article.settings["SITEURL"]
+        )
+        canonical_link = canonical.create_url()
+
+        assert canonical_link == "fakesite.com/fake-title.html"
