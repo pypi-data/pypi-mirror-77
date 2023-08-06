ee6cee8a419118da40961712be743b5fd38c2a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:00 +0000

    Add package cmake/3.18.2/cmake-3.18.2.tar.gz

---
+++ packages/cmake/cmake-3.18.2.tar.gz/cmake-3.18.2/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2.tar.gz")
+set(unix_source_sha256    "5d4e40fc775d3d828c72e5c45906b4d9b59003c9433ff1b36a1cb552bbd51d7e")
+
+set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2.zip")
+set(windows_source_sha256 "ccffbd2e983a604ac1b95433e751da8c11e552bbc1e138a953f23fd3e6c594d1")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "7b73ef901eb9fe615977a4f2254521cf9ee9da7efcd20c621a61ead51a870948")
+
+set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "7cf5525169b723e6b7544a44c391c36d855df3b51355d60e3aed53d640b8569c")
+
+set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win32-x86.zip")
+set(win32_binary_sha256 "209c7e3cf0eb7e83801858397e48322479eaacbf296b8c02d4d7d4614b1ce5e3")
+
+set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win64-x64.zip")
+set(win64_binary_sha256 "5f4ec834fbd9b62fbf73bc48ed459fa2ea6a86c403106c90fedc2ac76d51612d")
