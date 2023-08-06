c608e6055bc5a61933e1da797e3c7bc3f107c008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:43 +0000

    Add package python-dto/0.2.3/python-dto-0.2.3.tar.gz

---
+++ packages/python-dto/python-dto-0.2.3.tar.gz/python-dto-0.2.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md",encoding="utf-8") as f:
+    long_description=f.read()
+setup(
+    name="python-dto",
+    packages=["PyDto"],
+    version="0.2.3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Symoon",
+    author_email="symoon.gao@gmail.com",
+    url="https://github.com/Gaoshengyue/PyDto",
+    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.2.3",
+    keywords=["dto,DTO,Dto,PyDto,pydto,PyDto"],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent"
+
+    ]
+)
