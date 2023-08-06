1d21deb14e86d695efa00ec54dab52ee3a551db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package ocrd/2.14.0/ocrd-2.14.0.tar.gz

---
+++ packages/ocrd/ocrd-2.14.0.tar.gz/ocrd-2.14.0/ocrd/cli/__init__.py
@@ -0,0 +1,26 @@
+import click
+
+from ocrd.cli.ocrd_tool import ocrd_tool_cli
+from ocrd.cli.workspace import workspace_cli
+from ocrd.cli.process import process_cli
+from ocrd.cli.bashlib import bashlib_cli
+from ocrd.cli.validate import validate_cli
+from ocrd.decorators import ocrd_loglevel
+from .zip import zip_cli
+from .log import log_cli
+
+@click.group()
+@click.version_option()
+@ocrd_loglevel
+def cli(**kwargs): # pylint: disable=unused-argument
+    """
+    CLI to OCR-D
+    """
+
+cli.add_command(ocrd_tool_cli)
+cli.add_command(workspace_cli)
+cli.add_command(process_cli)
+cli.add_command(bashlib_cli)
+cli.add_command(zip_cli)
+cli.add_command(validate_cli)
+cli.add_command(log_cli)
