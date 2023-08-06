a64b7b6821d423902eb4a5be1f3b98d5fa0c361b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:45 +0000

    Add package BannerQuery/0.0.2/BannerQuery-0.0.2.tar.gz

---
+++ packages/BannerQuery/BannerQuery-0.0.2.tar.gz/BannerQuery-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BannerQuery", # Replace with your own username
+    version="0.0.2",
+    author="Sergio A. Mora Pardo",
+    author_email="sergiomora823@gmail.com",
+    description="Easy tool to do query from Banner by Ellucian to solve problems and decision making!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sergiomora03/BannerQuery",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',
+  ],
+    python_requires='>=3.6',
+)
