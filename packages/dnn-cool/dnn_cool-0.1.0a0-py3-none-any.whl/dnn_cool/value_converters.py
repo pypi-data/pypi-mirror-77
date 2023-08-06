ab3a6c8a7f7ab3a60750f39d8891eeb42f209581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:48 +0000

    Add package dnn-cool/0.1.0a0/dnn_cool-0.1.0a0-py3-none-any.whl

---
+++ packages/dnn-cool/dnn_cool-0.1.0a0-py3-none-any.whl/dnn_cool/value_converters.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import torch
+
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+def binary_value_converter(values):
+    values[np.isnan(values.astype(float))] = -1
+    return torch.tensor(values.astype(float)).float().unsqueeze(dim=-1)
+
+
+def classification_converter(values):
+    values[np.isnan(values)] = -1
+    return torch.tensor(values).long()
+
+
+def multilabel_converter(values):
+    available_labels = values[~pd.isna(values)]
+    labels = []
+    for i in range(len(available_labels)):
+        split = available_labels.iloc[i].split(',')
+        labels.append(list(filter(lambda x: len(x) > 0, split)))
+    binarizer = MultiLabelBinarizer()
+    one_hot_labels = binarizer.fit_transform(labels)
+    res = np.ones((len(values), one_hot_labels.shape[1]), dtype=np.float32) * -1.
+    res[~pd.isna(values)] = one_hot_labels
+    return res
