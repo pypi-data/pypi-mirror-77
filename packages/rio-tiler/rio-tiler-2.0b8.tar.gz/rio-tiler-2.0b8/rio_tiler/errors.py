acc700d5915a6df9adab5d1fdfd39cf8fb4fa1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:56 +0000

    Add package rio-tiler/2.0b8/rio-tiler-2.0b8.tar.gz

---
+++ packages/rio-tiler/rio-tiler-2.0b8.tar.gz/rio-tiler-2.0b8/rio_tiler/errors.py
@@ -0,0 +1,49 @@
+"""Errors and warnings."""
+
+
+class RioTilerError(Exception):
+    """Base exception class."""
+
+
+class InvalidFormat(RioTilerError):
+    """Invalid image format."""
+
+
+class TileOutsideBounds(RioTilerError):
+    """Z-X-Y Tile is outside image bounds."""
+
+
+class PointOutsideBounds(RioTilerError):
+    """Point is outside image bounds."""
+
+
+class InvalidBandName(RioTilerError):
+    """Invalid band name."""
+
+
+class InvalidColorMapName(Exception):
+    """Invalid colormap name."""
+
+
+class DeprecationWarning(UserWarning):
+    """Rio-tiler module deprecations warning."""
+
+
+class AlphaBandWarning(UserWarning):
+    """Automaticaly removed Alpha band from output array."""
+
+
+class NoOverviewWarning(UserWarning):
+    """Dataset has no overviews."""
+
+
+class InvalidAssetName(RioTilerError):
+    """Invalid Asset name."""
+
+
+class MissingAssets(RioTilerError):
+    """Missing Assets."""
+
+
+class ExpressionMixingWarning(UserWarning):
+    """Expression and assets/indexes mixing."""
---
+++ packages/rio-tiler/rio-tiler-2.0b8.tar.gz/rio-tiler-2.0b8/rio_tiler/mosaic/__init__.py
@@ -0,0 +1,3 @@
+"""rio-tiler.mosaic."""
+
+from .reader import mosaic_reader, mosaic_tiler  # noqa
