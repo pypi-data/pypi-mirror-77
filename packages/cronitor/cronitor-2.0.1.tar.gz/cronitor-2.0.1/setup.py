716c88a79bbb261cc6fe90c55ab06b5ba5e46755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:22 +0000

    Add package cronitor/2.0.1/cronitor-2.0.1.tar.gz

---
+++ packages/cronitor/cronitor-2.0.1.tar.gz/cronitor-2.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronitor',
+    version='2.0.1',
+    packages=find_packages(),
+    url='https://github.com/cronitorio/cronitor-python',
+    license='MIT License',
+    author='aflanagan',
+    author_email='',
+    description='Python wrapper for Cronitor\'s Monitor and Ping APIs.',
+    install_requires=[
+        'requests',
+    ],
+    entry_points=dict(console_scripts=['cronitor = cronitor.__main__:main'])
+)
