96f2e130786a4aaa6a2545fe427f806bfe3c2888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:44 +0000

    Add package sylajone/0.3.1/sylajone-0.3.1.tar.gz

---
+++ packages/sylajone/sylajone-0.3.1.tar.gz/sylajone-0.3.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+from setuptools import setup
+
+# to install type:
+# python setup.py install --root=/
+from io import open
+def readme():
+    with open('README.rst', encoding="utf8") as f:
+        return f.read()
+
+setup (name='sylajone', version='0.3.1',
+      description="Sylajone: Arabic syntax Analyzer library",
+      long_description = readme(),      
+
+      author='Taha Zerrouki',
+      author_email='taha.zerrouki@gmail.com',
+      url='http://sylajone.sourceforge.net/',
+      license='GPL',
+      package_dir={'sylajone': 'sylajone'},
+      packages=['sylajone'],
+      install_requires=[ 'pyarabic>=0.6.2',
+			"pickledb>=0.9.0",
+			"libqutrub>=1.2.4.1",
+			"naftawayh>=0.4",
+			"qalsadi>=0.3.5",
+      ],         
+      include_package_data=True,
+      package_data = {
+        'aranasyn': ['doc/*.*','doc/html/*', 'data/*.sqlite', 'data/*.sql'],
+        },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+    );
+
