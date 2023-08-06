19ec3464c2ef539a94dfc527f036f2bb8336b29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:33 +0000

    Add package draw-with-percentage/0.0.1/draw%20with%20percentage-0.0.1.tar.gz

---
+++ packages/draw-with-percentage/draw%20with%20percentage-0.0.1.tar.gz/draw with percentage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='draw with percentage',
+      version='0.0.1',
+      description='Python Library to draw with percentage',
+      author='Mateusz Kołodziejczyk',
+      author_email='mateusz.j.kolodziejczyk@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      python_requires='>=3.5'
+      )
