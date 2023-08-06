8b56ba0ccdadcab466cef19104cd604f49483921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:13 +0000

    Add package Avatar-Utils/1.6.11/Avatar-Utils-1.6.11.tar.gz

---
+++ packages/Avatar-Utils/Avatar-Utils-1.6.11.tar.gz/Avatar-Utils-1.6.11/avatar_utils/db/mixins/__init__.py
@@ -0,0 +1,9 @@
+from avatar_utils.db.mixins.crud import CRUDMixin
+from avatar_utils.db.mixins.json_cast import JsonCastMixin
+from avatar_utils.db.mixins.time_cast import TimeCastMixin
+
+__all__ = [
+    'CRUDMixin',
+    'TimeCastMixin',
+    'JsonCastMixin',
+]
