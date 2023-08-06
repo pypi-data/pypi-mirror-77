09e05598ef4663b91738a2d54851ec31efdb2c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:27 +0000

    Add package kodi-remotecontrol/0.7.0/kodi_remotecontrol-0.7.0.tar.gz

---
+++ packages/kodi-remotecontrol/kodi_remotecontrol-0.7.0.tar.gz/kodi_remotecontrol-0.7.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import setuptools
+from kodi_remotecontrol import __version__
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    
+KEYWORDS = ('kodi api remote control')
+
+setuptools.setup(
+    name="kodi_remotecontrol",
+    version=__version__,
+    author="Denis MACHARD",
+    author_email="d.machard@gmail.com",
+    description="Python remote control gateway for Kodi server",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmachard/kodi_remotecontrol",
+    packages=['kodi_remotecontrol'],
+    include_package_data=True,
+    platforms='any',
+    keywords=KEYWORDS,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+    entry_points={'console_scripts': ['kodi_remotecontrol = kodi_remotecontrol.gateway:start_remotecontrol']},
+    install_requires=[
+        "websockets"
+    ]
+)
