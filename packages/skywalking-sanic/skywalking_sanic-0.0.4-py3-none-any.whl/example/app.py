eff0497a118c0d8f1358e4e400dc1f84836ebf79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:28 +0000

    Add package skywalking-sanic/0.0.4/skywalking_sanic-0.0.4-py3-none-any.whl

---
+++ packages/skywalking-sanic/skywalking_sanic-0.0.4-py3-none-any.whl/example/app.py
@@ -0,0 +1,19 @@
+from sanic import Sanic
+from sanic.response import text
+
+from skywalking_sanic import SkywalkingSanic, sw_requests
+
+sw_requests.install()
+
+app = Sanic(__name__)
+
+
+@app.route('/')
+def hello_world(request):
+    return text("Hello World")
+
+
+SkywalkingSanic(app, service='My Service', collector='127.0.0.1:11800')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
