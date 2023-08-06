68ac67e5fe610ffeff171581552a20496eaedc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:37 +0000

    Add package switches.py/0.1.0/switches.py-0.1.0-py3-none-any.whl

---
+++ packages/switches.py/switches.py-0.1.0-py3-none-any.whl/switches/__init__.py
@@ -0,0 +1,9 @@
+"""
+:copyright: Copyright (c) 2020 Jeremiah Ikosin (@ziord)
+:license: MIT, see LICENSE for more details
+"""
+
+__all__ = ('switch',)
+
+version = (0, 1, 0)
+__version__ = '.'.join(map(str, version))
