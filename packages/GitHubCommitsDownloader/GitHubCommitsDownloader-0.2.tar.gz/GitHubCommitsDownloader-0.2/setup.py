7a2552c08c23380f8c4e1d35b8731bbd40622bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:11 +0000

    Add package GitHubCommitsDownloader/0.2/GitHubCommitsDownloader-0.2.tar.gz

---
+++ packages/GitHubCommitsDownloader/GitHubCommitsDownloader-0.2.tar.gz/GitHubCommitsDownloader-0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+  name = 'GitHubCommitsDownloader',
+  packages = ['GitHubCommitsDownloader', 'GHCD'],
+  version = '0.2',
+  license = 'MIT',
+  description = 'Github Commits Downloader App',
+  author = 'MDReal',
+  author_email = 'vminecrafter2015@gmail.com',
+  url = 'https://github.com/user/reponame',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+  install_requires=[
+		'requests',
+		'termcolor'
+	],
+	entry_points = {
+		'console_scripts': [
+			'ghcd=exec:run',
+			'GitHubCommitDownloader=exec:run'
+		]
+	},
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
