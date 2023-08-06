794584b3de57f2292763b5fcc529eeb59f7306a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:10 +0000

    Add package Google-Colab-Transfer/0.1.5/Google%20Colab%20Transfer-0.1.5.tar.gz

---
+++ packages/Google-Colab-Transfer/Google%20Colab%20Transfer-0.1.5.tar.gz/Google Colab Transfer-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+# noinspection PyUnresolvedReferences
+import setuptools
+
+setup(
+    name='Google Colab Transfer',
+    packages=['colab_transfer'],
+    install_requires=[
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    version='0.1.5',
+    description='Transfer data between Colab and Drive.',
+    long_description='Transfer data between Colab and Drive.',
+    long_description_content_type='text/x-rst',
+    author='Wok',
+    author_email='wok@tuta.io',
+    url='https://github.com/woctezuma/google-colab-transfer',
+    download_url='https://github.com/woctezuma/google-colab-transfer/archive/0.1.5.tar.gz',
+    keywords=['google-colab', 'google-colaboratory', 'google-drive'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
