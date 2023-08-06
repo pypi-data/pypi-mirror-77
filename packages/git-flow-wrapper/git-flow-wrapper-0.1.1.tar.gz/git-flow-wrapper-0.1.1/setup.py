3e0865964e3e88d7cf71f2d5120a9a5df5171e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:47 +0000

    Add package git-flow-wrapper/0.1.1/git-flow-wrapper-0.1.1.tar.gz

---
+++ packages/git-flow-wrapper/git-flow-wrapper-0.1.1.tar.gz/git-flow-wrapper-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['git_flow_wrapper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer>=0.1.0,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['flow = git_flow_wrapper.cli:main']}
+
+setup_kwargs = {
+    'name': 'git-flow-wrapper',
+    'version': '0.1.1',
+    'description': 'Wrapper to run common git flow commands with remote repositories',
+    'long_description': None,
+    'author': 'Patrick',
+    'author_email': 'patrick.pwall@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
