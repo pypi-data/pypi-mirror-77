96949cb909ffb5ecabe7c0fe1535dd839a4c9385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package oui/0.0.4/oui-0.0.4.tar.gz

---
+++ packages/oui/oui-0.0.4.tar.gz/oui-0.0.4/oui/__init__.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+pkg_root_dir = os.path.dirname(__file__)
+pjoin = lambda *p: os.path.join(pkg_root_dir, *p)
+
+data_dirpath = pjoin('data')
+djoin = lambda *p: os.path.join(data_dirpath, *p)
+
+
+def get_pkg_data(filename):
+    path = djoin(filename)
+    if filename.endswith('.json'):
+        return json.load(open(path, 'r'))
+    else:
+        raise ValueError(f"Unrecognized extension in {path}")
+
+
+from oui.splatter import splatter
+# import oui.multi_time_vis
+# import oui.confusion_chords
