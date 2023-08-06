3d3619d76739947d9b64ea0cdf3c628b67b0431b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:57 +0000

    Add package archivy/0.0.2/archivy-0.0.2-py3-none-any.whl

---
+++ packages/archivy/archivy-0.0.2-py3-none-any.whl/archivy/extensions.py
@@ -0,0 +1,20 @@
+from tinydb import TinyDB, Query, operations
+from elasticsearch import Elasticsearch
+from archivy.config import Config
+
+
+DB = TinyDB(Config.APP_PATH + "/db.json")
+
+def get_max_id():
+    max_id = DB.search(Query().name == "max_id")
+    if not max_id:
+        DB.insert({"name": "max_id", "val": 0})
+        return 0
+    return max_id[0]["val"]
+
+def set_max_id(val):
+    DB.update(operations.set("val", val), Query().name == "max_id")
+
+
+def elastic_client():
+    return Elasticsearch([Config.ELASTICSEARCH_URL]) if Config.ELASTICSEARCH_ENABLED else None
