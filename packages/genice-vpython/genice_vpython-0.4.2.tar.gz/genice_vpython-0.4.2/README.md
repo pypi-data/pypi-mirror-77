8680d65023858ad6f6567a3563ff238f0e6f8a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:14 +0000

    Add package genice-vpython/0.4.2/genice_vpython-0.4.2.tar.gz

---
+++ packages/genice-vpython/genice_vpython-0.4.2.tar.gz/genice_vpython-0.4.2/README.md
@@ -0,0 +1,40 @@
+# [genice-vpython](https://github.com/vitroid/genice-vpython/)
+
+A [GenIce](https://github.com/vitroid/GenIce) plugin to visualize the structure with [VPython](http://vpython.org).
+
+version 0.4.2
+
+## Requirements
+
+* vpython
+* genice>=0.25
+
+## Installation from PyPI
+
+    % pip install genice_vpython
+
+## Manual Installation
+
+### System-wide installation
+
+    % make install
+
+### Private installation
+
+Copy the files in genice_svg/formats/ into your local formats/ folder.
+
+## Usage
+
+    
+    Usage:
+        genice III -f vpython
+    
+        opens a window in the web browser to show the image.
+    
+    Options:
+    
+        No options available.
+
+## Test in place
+
+    % make test
---
+++ packages/genice-vpython/genice_vpython-0.4.2.tar.gz/genice_vpython-0.4.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from numpy.distutils.misc_util import get_numpy_include_dirs
+import os
+import codecs
+import re
+
+#Copied from wheel package
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'genice_vpython', '__init__.py'),
+                 encoding='utf8') as version_file:
+    metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", version_file.read()))
+    
+long_desc = "".join(open("README.md").readlines())
+
+setup(
+    name='genice_vpython',
+    version=metadata['version'],
+    description='VPYTHON format plugin for GenIce.',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ],
+    author='Masakazu Matsumoto',
+    author_email='vitroid@gmail.com',
+    url='https://github.com/vitroid/genice-vpython/',
+    keywords=['genice', 'VPYTHON'],
+
+    packages=['genice_vpython',
+              'genice_vpython.formats',
+    ],
+    
+    entry_points = {
+        'genice_format': [
+            'vpython      = genice_vpython.formats.vpython',
+        ],
+    },
+    install_requires=['vpython', 'genice>=0.25'],
+
+    license='MIT',
+)
