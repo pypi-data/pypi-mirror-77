db3e9bc15dffac1ecc2dedcffc28e751ed422a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:06 +0000

    Add package poo-poo/1.0.3/poo-poo-1.0.3.tar.gz

---
+++ packages/poo-poo/poo-poo-1.0.3.tar.gz/poo-poo-1.0.3/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# This call to setup() does all the work
+setup(
+    name="poo-poo",
+    version="1.0.3",
+    description="see a poopy animation use python -m poo to activate import poo-poo",
+    url="https://pypi.com",
+    author="nathblade16",
+    author_email="nathblade16@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires='>=3.6',
+)
