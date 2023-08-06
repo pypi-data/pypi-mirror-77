fb2dc84759c990d0a9ad85a15c64b1f0ada0ba99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:16 +0000

    Add package musicbot/0.3.0/musicbot-0.3.0.tar.gz

---
+++ packages/musicbot/musicbot-0.3.0.tar.gz/musicbot-0.3.0/musicbot/admin.py
@@ -0,0 +1,38 @@
+import click
+from . import helpers
+from .graphql import GraphQL
+from .helpers import config_string
+
+
+DEFAULT_GRAPHQL_ADMIN = 'http://127.0.0.1:5001/graphql'
+graphql_admin_option = [
+    click.option(
+        '--graphql-admin',
+        help='GraphQL endpoint',
+        default=DEFAULT_GRAPHQL_ADMIN,
+        callback=config_string,
+        show_default=True
+    )
+]
+
+
+class Admin(GraphQL):
+    @helpers.timeit
+    def __init__(self, graphql=None):
+        graphql = graphql if graphql is not None else DEFAULT_GRAPHQL_ADMIN
+        GraphQL.__init__(self, graphql=graphql)
+
+    @helpers.timeit
+    def users(self):
+        query = """{
+            accountsList {
+                user {
+                  lastName
+                  createdAt
+                  updatedAt
+                  firstName
+                }
+                email
+            }
+        }"""
+        return self._post(query)['data']['accountsList']
