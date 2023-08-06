63251c543b874dde71e7cd8c6ccadc1d5930e096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:09 +0000

    Add package omnilogic/0.3.6/omnilogic-0.3.6.tar.gz

---
+++ packages/omnilogic/omnilogic-0.3.6.tar.gz/omnilogic-0.3.6/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+  name = 'omnilogic',
+  packages = ['omnilogic'],
+  version = '0.3.6',
+  license='apache-2.0',
+  description = 'Integration for the Hayward OmniLogic pool control system',
+  author = 'Tim Empringham',
+  author_email = 'tim.empringham@live.ca',
+  url = 'https://github.com/djtimca/omnilogic',
+  download_url = 'https://github.com/djtimca/omnilogic/archive/v_036.tar.gz',
+  keywords = ['OmniLogic', 'Hayward', 'Pool', 'Spa'],
+  install_requires=[
+          'xmltodict',
+          'config',
+          'aiohttp',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
