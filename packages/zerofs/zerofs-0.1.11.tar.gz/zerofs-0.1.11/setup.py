7316c0c73771788d42f475b96b2024f1354fd396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:48 +0000

    Add package zerofs/0.1.11/zerofs-0.1.11.tar.gz

---
+++ packages/zerofs/zerofs-0.1.11.tar.gz/zerofs-0.1.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def read(file_name):
+  with open(file_name) as f:
+    return f.read()
+
+
+setup(name='zerofs',
+      version='0.1.11',
+      description='Transparant filesystem backed by Backblaze B2 object store',
+      long_description=read('README.md'),
+      url='https://github.com/picklelo/zerofs',
+      author='Nikhil Rao',
+      author_email='nikhil@nikhilrao.me',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+      keywords='backblaze b2 zero fs filesystem',
+      packages=['zerofs'],
+      scripts=['bin/zerofs'],
+      install_requires=['backblazeb2', 'fusepy'])
