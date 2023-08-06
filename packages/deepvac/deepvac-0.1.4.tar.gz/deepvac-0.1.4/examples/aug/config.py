1e978fce0be0ac65120ef4889228f83fc88ce676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package deepvac/0.1.4/deepvac-0.1.4.tar.gz

---
+++ packages/deepvac/deepvac-0.1.4.tar.gz/deepvac-0.1.4/examples/aug/config.py
@@ -0,0 +1,5 @@
+import os
+from syszux_config import *
+##########text synthesis########
+config.text.input_dir = '/gemfield/hostpv/gemfield/input/'
+config.text.output_dir = '/gemfield/hostpv/gemfield/output/'
