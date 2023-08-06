1483b4b2fd74e69d532ea78898c9fb0e2b54ccc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package classy-json/1.3.2/classy-json-1.3.2.tar.gz

---
+++ packages/classy-json/classy-json-1.3.2.tar.gz/classy-json-1.3.2/classyjson/CustomTypes.py
@@ -0,0 +1,32 @@
+
+class CustomList(list):
+    def __init__(self, _list):
+        for i, val in enumerate(_list):
+            if isinstance(val, list):
+                _list[i] = CustomList(val)
+            elif isinstance(val, dict):
+                _list[i] = CustomDict(val)
+
+        list.__init__(self, _list)
+
+
+class CustomDict(dict):
+    def __init__(self, _dict):
+        for key in list(_dict):
+            if isinstance(_dict[key], list):
+                _dict[key] = CustomList(_dict[key])
+            elif isinstance(_dict[key], dict):
+                _dict[key] = CustomDict(_dict[key])
+
+        dict.__init__(self, _dict)
+
+    def __getattr__(self, name):  # CustomDict.a
+        return dict.__getitem__(self, name)
+
+    def __setattr__(self, name, value):  # CustomDict.a = 'something'
+        return dict.__setitem__(self, name, value)
+
+    def __delattr__(self, name):  # del CustomDict.a
+        popped = dict.__getitem__(name)
+        dict.__delitem__(self, name)
+        return popped
