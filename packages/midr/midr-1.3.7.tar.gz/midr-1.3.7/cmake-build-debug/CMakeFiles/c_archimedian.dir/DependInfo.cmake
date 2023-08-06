0d0442f07f8fc4aab70e590eccf370572131b9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:17 +0000

    Add package midr/1.3.7/midr-1.3.7.tar.gz

---
+++ packages/midr/midr-1.3.7.tar.gz/midr-1.3.7/cmake-build-debug/CMakeFiles/c_archimedian.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/laurent/work/projects/gandrillon/midr/src/cpp/archimedean.cpp" "/Users/laurent/work/projects/gandrillon/midr/src/cmake-build-debug/CMakeFiles/c_archimedian.dir/cpp/archimedean.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "c_archimedian_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/eigen"
+  "../include/predef/include"
+  "../include/integer/include"
+  "../include/array/include"
+  "../include/container/include"
+  "../include/move/include"
+  "../include/smart_ptr/include"
+  "../include/config/include"
+  "../include/numeric_conversion/include"
+  "../include/utility/include"
+  "../include/concept_check/include"
+  "../include/detail/include"
+  "../include/core/include"
+  "../include/iterator/include"
+  "../include/range/include"
+  "../include/lexical_cast/include"
+  "../include/throw_exception/include"
+  "../include/static_assert/include"
+  "../include/assert/include"
+  "../include/preprocessor/include"
+  "../include/mpl/include"
+  "../include/type_traits/include"
+  "../include/math/include"
+  "../include/pybind11/include"
+  "/usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/include/python3.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
