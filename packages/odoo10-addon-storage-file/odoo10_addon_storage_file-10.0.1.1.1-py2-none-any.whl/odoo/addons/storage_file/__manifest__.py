188dba687d57768c1b92f3bbf4704eb31007f7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:53 +0000

    Add package odoo10-addon-storage-file/10.0.1.1.1/odoo10_addon_storage_file-10.0.1.1.1-py2-none-any.whl

---
+++ packages/odoo10-addon-storage-file/odoo10_addon_storage_file-10.0.1.1.1-py2-none-any.whl/odoo/addons/storage_file/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage File",
+    "summary": "Storage file in storage backend",
+    "version": "10.0.1.1.1",
+    "category": "Storage",
+    "website": "https://www.github.com/OCA/storage",
+    "author": " Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": ["slugify"], "bin": []},
+    "depends": ["storage_backend"],
+    "data": [
+        "views/storage_file_view.xml",
+        "views/storage_backend_view.xml",
+        "security/ir.model.access.csv",
+        "security/storage_file.xml",
+        "data/ir_cron.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
