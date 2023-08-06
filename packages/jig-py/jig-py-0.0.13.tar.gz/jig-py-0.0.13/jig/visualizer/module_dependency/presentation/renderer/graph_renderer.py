d3d57a671a3285bb6a4d3bbd64c7283593d6756a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:52 +0000

    Add package jig-py/0.0.13/jig-py-0.0.13.tar.gz

---
+++ packages/jig-py/jig-py-0.0.13.tar.gz/jig-py-0.0.13/jig/visualizer/module_dependency/presentation/renderer/graph_renderer.py
@@ -0,0 +1,30 @@
+import dataclasses
+
+from graphviz import Digraph
+
+from jig.visualizer.module_dependency.domain.model.graph import Graph
+from .cluster_renderer import ClusterRenderer
+
+
+@dataclasses.dataclass
+class GraphRenderer:
+    graph: Graph
+
+    def render(self) -> Digraph:
+        d = Digraph()
+
+        for node in sorted(self.graph.nodes):
+            node_options = node.style.to_dict()
+            # 描画する node の配下に位置する node が存在する場合には、 shape=record として表示する
+            if self.graph.child_node_exists(node):
+                node_options["shape"] = "record"
+            d.node(name=node.path.name, **node_options)
+
+        for edge in sorted(self.graph.edges):
+            d.edge(edge.tail.name, edge.head.name, **edge.style.to_dict())
+
+        for cluster in self.graph.clusters.values():
+            cluster_renderer = ClusterRenderer(cluster)
+            d.subgraph(cluster_renderer.render())
+
+        return d
