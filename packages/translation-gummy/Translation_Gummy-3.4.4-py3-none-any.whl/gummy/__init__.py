f8767b338ade0093930854ec6a73ae70f234a0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:51 +0000

    Add package translation-gummy/3.4.4/Translation_Gummy-3.4.4-py3-none-any.whl

---
+++ packages/translation-gummy/Translation_Gummy-3.4.4-py3-none-any.whl/gummy/__init__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+__copyright__       = "Copyright (C) 2020 Shuto Iwasaki"
+__version__         = "3.4.4"
+try:
+    __VERSION_INFO__
+except NameError:
+    print(f"Translation-Gummy ver.{__version__}")
+    __VERSION_INFO__ = "Output Completed"
+
+__license__         = "MIT"
+__author__          = "Shuto Iwasaki"
+__author_twitter__  = "https://twitter.com/cabernet_rock"
+__author_email__    = "cabernet.rock@gmail.com"
+__url__             = "https://github.com/iwasakishuto/Translation-Gummy"
+
+from . import gateways
+from . import journals
+from . import models
+from . import translators
+from .models import TranslationGummy
