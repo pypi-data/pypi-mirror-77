232b6f13c2c54825c0585a99d480f0ec08860de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:28 +0000

    Add package django-help-me/0.1.5/django_help_me-0.1.5-py3-none-any.whl

---
+++ packages/django-help-me/django_help_me-0.1.5-py3-none-any.whl/helpme/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from helpme import views
+
+app_name = "helpme"
+
+urlpatterns = [
+    path("", views.SupportDashboardView.as_view(), name="dashboard"),
+    path("request/", views.SupportRequestView.as_view(), name="submit-request"),
+    path("success/", views.SupportRequestSuccessView.as_view(), name="success"),
+    path("ticket/<uuid:uuid>/", views.TicketDetailView.as_view(), name="ticket-detail"),
+    path("teams/", views.TeamCreateView.as_view(), name="team-list"),
+    path("teams/<uuid:uuid>/", views.TeamDetailView.as_view(), name="team-detail"),
+]
