07d97b180de5f00dc1e3e9eafeb278d2ab660e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package politico-civic-race-ratings/1.0a7.dev2/politico-civic-race-ratings-1.0a7.dev2.tar.gz

---
+++ packages/politico-civic-race-ratings/politico-civic-race-ratings-1.0a7.dev2.tar.gz/politico-civic-race-ratings-1.0a7.dev2/raceratings/admin/__init__.py
@@ -0,0 +1,25 @@
+# Imports from Django.
+from django.contrib import admin
+
+
+# Imports from other dependencies.
+from election.models import Race
+
+
+# Imports from race_ratings.
+from raceratings.admin.page_content import PageContentAdmin
+from raceratings.admin.race_rating import RaceRatingAdmin
+from raceratings.models import Author
+from raceratings.models import BodyRating
+from raceratings.models import Category
+from raceratings.models import DataProfile
+from raceratings.models import RaceRating
+from raceratings.models import RatingPageContent
+
+
+admin.site.register(Author)
+admin.site.register(BodyRating)
+admin.site.register(Category)
+admin.site.register(DataProfile)
+admin.site.register(RatingPageContent, PageContentAdmin)
+admin.site.register(RaceRating, RaceRatingAdmin)
