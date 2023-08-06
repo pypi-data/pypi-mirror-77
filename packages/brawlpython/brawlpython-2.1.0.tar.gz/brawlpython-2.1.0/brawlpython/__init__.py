9aef193a5a4d63f25465b7b9ee0823d708c2f1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:54 +0000

    Add package brawlpython/2.1.0/brawlpython-2.1.0.tar.gz

---
+++ packages/brawlpython/brawlpython-2.1.0.tar.gz/brawlpython-2.1.0/brawlpython/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "2.1.0"
+__name__ = "brawlpython"
+
+
+from .clients import AsyncClient, SyncClient
+
+
+__all__ = (
+    "AsyncClient",
+    "SyncClient",
+)
