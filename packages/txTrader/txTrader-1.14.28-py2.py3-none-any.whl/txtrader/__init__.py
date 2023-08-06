c6c964467921f808e075add8b40321ffed689021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:16 +0000

    Add package txTrader/1.14.28/txTrader-1.14.28-py2.py3-none-any.whl

---
+++ packages/txTrader/txTrader-1.14.28-py2.py3-none-any.whl/txtrader/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+  __init__.py
+  -----------
+
+  TxTrader module init
+
+  Copyright (c) 2015 Reliance Systems Inc. <mkrueger@rstms.net>
+  Licensed under the MIT license.  See LICENSE for details.
+
+"""
+
+__all__ = ['version', 'tcpserver', 'webserver', 'rtx', 'client', 'monitor']
+from .version import VERSION, DATE, TIME
+LABEL = 'TxTrader Securities Trading API Controller'
+HEADER = f"{LABEL} {VERSION} {DATE} {TIME}"
