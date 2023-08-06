2a620058a744c19c8d37360b2b879ed46fc42cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:47 +0000

    Add package jsontextmc/2.0.3/jsontextmc-2.0.3.tar.gz

---
+++ packages/jsontextmc/jsontextmc-2.0.3.tar.gz/jsontextmc-2.0.3/jsontextmc/constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Constant variables like version numbers."""
+PROJECT: str = "JSONTextMC"
+VERSION: str = "2.0.3"
+DESCRIPTION: str = "JSONTextMC converts legacy color codes to the modern JSON format."
+LONG_DOC: str = """Translate format-text to list, using ``sep`` as the format code separator.
+
+To use 1.16 hex colors, use ``&#RRGGBB``, where R, G, B are red, green, blue, respectively.
+
+If strict mode is on:
+  * codes inherited from previous entries in the string will not carry over
+  * formatting will not work after a color char
+  * color will not work after a formatting char
+If you have issues with not being able to combine character codes, turn ``strict`` off."""
