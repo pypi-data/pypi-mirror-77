6b561bb7ad2d92c94a8724d0df6a2ecdea3a2b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:35 +0000

    Add package py-abac/0.4.0/py_abac-0.4.0.tar.gz

---
+++ packages/py-abac/py_abac-0.4.0.tar.gz/py_abac-0.4.0/py_abac/policy/conditions/others/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Other conditions
+"""
+
+from .any import Any
+from .cidr import CIDR
+from .equals_attribute import EqualsAttribute
+from .exists import Exists
+from .not_exists import NotExists
