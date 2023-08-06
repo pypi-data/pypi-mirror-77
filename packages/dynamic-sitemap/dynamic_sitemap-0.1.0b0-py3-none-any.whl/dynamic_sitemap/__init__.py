3f203f667181b820aa7fe7cbf7cb3a7284dbf7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package dynamic-sitemap/0.1.0b0/dynamic_sitemap-0.1.0b0-py3-none-any.whl

---
+++ packages/dynamic-sitemap/dynamic_sitemap-0.1.0b0-py3-none-any.whl/dynamic_sitemap/__init__.py
@@ -0,0 +1,17 @@
+from .main import SitemapConfig, SitemapMeta
+from .flask_map import FlaskSitemap
+
+
+__author__ = 'Denis Kazakov'
+__about__ = dict(
+    __title__='dynamic-sitemap',
+    __module__=__package__,
+    __version__='0.1.0b',
+    __url__='https://github.com/KazakovDenis/dynamic-sitemap',
+
+    __author__=__author__,
+    __email__='denis@kazakov.ru.net',
+
+    __license__='MIT',
+    __copyright__=f'Copyright 2020 {__author__}',
+)
