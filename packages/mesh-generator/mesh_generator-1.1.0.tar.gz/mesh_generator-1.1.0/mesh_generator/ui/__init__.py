1c7d09c6ed9a5e5e7fd2a9548e39225bce9359bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:32 +0000

    Add package mesh-generator/1.1.0/mesh_generator-1.1.0.tar.gz

---
+++ packages/mesh-generator/mesh_generator-1.1.0.tar.gz/mesh_generator-1.1.0/mesh_generator/ui/__init__.py
@@ -0,0 +1,11 @@
+""" UI Mesh python modules.
+The MainWindow module: tkinter_main_app.py
+The FirstWindow: upload_magnetogram.py
+
+To start the Main UI:
+
+from mesh_generator.ui.upload_magnetogram import MeshGeneratorUI
+>>> MeshGeneratorUI()
+"""
+
+from mesh_generator.ui.upload_magnetogram import MeshGeneratorUI
