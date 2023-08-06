cfe108a1ab2afadaca90c57680b33b3508a809db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:55 +0000

    Add package json-e/4.3.0/json-e-4.3.0.tar.gz

---
+++ packages/json-e/json-e-4.3.0.tar.gz/json-e-4.3.0/setup.py
@@ -0,0 +1,31 @@
+import json
+import os
+from setuptools import setup, find_packages
+
+package_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'package.json')
+with open(package_json) as f:
+    version = json.load(f)['version']
+
+description='A data-structure parameterization system written for embedding context in JSON objects'
+
+long_description = '''\
+{description}.
+
+See https://taskcluster.github.io/json-e/ for usage information.
+'''.format(description=description)
+
+setup(name='json-e',
+    version=version,
+    description=description,
+    long_description=long_description,
+    author='Dustin J. Mitchell',
+    url='https://taskcluster.github.io/json-e/',
+    author_email='dustin@mozilla.com',
+    packages=['jsone'],
+    license='MPL2',
+    extras_require={
+        'release': [
+            'towncrier',
+        ],
+    }
+)
