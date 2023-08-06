1be358a1ca8aafd462fb11cdc3954aa777e856dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package fluyt-nightly/0.1.0.dev20200823220548/fluyt_nightly-0.1.0.dev20200823220548-cp38-cp38-win_amd64.whl

---
+++ packages/fluyt-nightly/fluyt_nightly-0.1.0.dev20200823220548-cp38-cp38-win_amd64.whl/fluyt_nightly-0.1.0.dev20200823220548.data/purelib/fluyt/core/python/ops/basic_ops.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Yong Tang. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""fluyt.ops"""
+
+import functools
+import operator
+
+import tensorflow as tf
+
+from fluyt.core.python.ops.core_ops import Transform
+
+
+@Transform
+def flatten(input, *, name=None):
+    return tf.reshape(input, [tf.shape(input)[0], -1])
+
+
+@Transform
+def reshape(input, *, shape, name=None):
+    shape = [-1 if e is None else e for e in shape]
+    return tf.reshape(input, shape)
