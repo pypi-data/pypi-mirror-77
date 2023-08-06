c10f7c1d271f35c1489fc03c6c29815abe201414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:29 +0000

    Add package mys/0.20.0/mys-0.20.0.tar.gz

---
+++ packages/mys/mys-0.20.0.tar.gz/mys-0.20.0/mys/iter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// https://github.com/ryanhaining/cppitertools
+
+#include "iter/enumerate.hpp"
+#include "iter/range.hpp"
+#include "iter/reversed.hpp"
+#include "iter/sorted.hpp"
+#include "iter/zip.hpp"
+
+using iter::range;
+using iter::enumerate;
