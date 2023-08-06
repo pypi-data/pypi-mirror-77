05c1aa067efcf699e70f11b6063c0e2e356c5d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:18 +0000

    Add package pygrank/0.1.16/pygrank-0.1.16-py3-none-any.whl

---
+++ packages/pygrank/pygrank-0.1.16-py3-none-any.whl/pygrank/algorithms/pagerank.py
@@ -0,0 +1,183 @@
+import scipy
+import numpy as np
+import warnings
+import pygrank.algorithms.utils
+
+
+class PageRank:
+    """A Personalized PageRank power method algorithm. Supports warm start."""
+
+    def __init__(self, alpha=0.85, to_scipy=None, convergence=None, use_quotient=True, converge_to_eigenvectors=False, **kwargs):
+        """ Initializes the PageRank scheme parameters.
+
+        Attributes:
+            alpha: Optional. 1-alpha is the bias towards the personalization. Default value is 0.85.
+            to_scipy: Optional. Method to extract a scipy sparse matrix from a networkx graph.
+                If None (default), pygrank.algorithms.utils.to_scipy_sparse_matrix with default arguments is used.
+            convergence: Optional. The ConvergenceManager that determines when iterations stop. If None (default),
+                a ConvergenceManager with the additional keyword arguments is constructed.
+            use_quotient: Optional. If True (default) performs a L1 re-normalization of ranks after each iteration.
+                This significantly speeds ups the convergence speed of symmetric normalization (col normalization
+                preserves the L1 norm during computations on its own).
+            converge_to_eigenvectors: Optional. If True (default is False) the outcome of ranking does not depend on
+                the alpha parameters and only weakly on potential personalization. Instead ranking strongly biased
+                towards the principal eigenvector. If more than one near-max eigenvalues of to_scipy(graph) exist
+                then this scheme selects one based on the personalization scheme, otherwise the principal eigenvector
+                is outputted. When the graph is bipartite, using this and the argument use_quotient=False
+                effectively toggles the behavior described in
+                `VenueRank: Identifying Venues that Contribute to Artist Popularity`.
+
+        Example:
+            >>> from pygrank.algorithms import pagerank
+            >>> algorithm = pagerank.PageRank(alpha=0.99, tol=1.E-9) # tol passed to the ConvergenceManager
+        """
+        self.alpha = float(alpha) # typecast to make sure that a graph is not accidentally the first argument
+        self.to_scipy = pygrank.algorithms.utils.to_scipy_sparse_matrix if to_scipy is None else to_scipy
+        self.convergence = pygrank.algorithms.utils.ConvergenceManager(**kwargs) if convergence is None else convergence
+        self.use_quotient = use_quotient
+        self.converge_to_eigenvectors = converge_to_eigenvectors
+
+    def rank(self, G, personalization=None, warm_start=None):
+        M = self.to_scipy(G)
+        degrees = scipy.array(M.sum(axis=1)).flatten()
+
+        personalization = scipy.repeat(1.0, len(G)) if personalization is None else scipy.array([personalization.get(n, 0) for n in G], dtype=float)
+        if personalization.sum() == 0:
+            raise Exception("The personalization vector should contain at least one non-zero entity")
+        personalization = personalization / personalization.sum()
+        ranks = personalization if warm_start is None else scipy.array([warm_start.get(n, 0) for n in G], dtype=float)
+
+        is_dangling = scipy.where(degrees == 0)[0]
+        self.convergence.start()
+        while not self.convergence.has_converged(ranks):
+            ranks = self.alpha * (ranks * M + sum(ranks[is_dangling]) * personalization) + (1 - self.alpha) * personalization
+            if self.use_quotient:
+                ranks = ranks/ranks.sum()
+            if self.converge_to_eigenvectors:
+                personalization = ranks
+
+        ranks = dict(zip(G.nodes(), map(float, ranks)))
+        return ranks
+
+
+class HeatKernel:
+    """ Heat kernel filter."""
+
+    def __init__(self, t=5, to_scipy=None, convergence=None, **kwargs):
+        """ Initializes the HearKernel filter parameters.
+
+        Attributes:
+            t: Optional. How many hops until the importance of new nodes starts decreasing. Default value is 5.
+            to_scipy: Optional. Method to extract a scipy sparse matrix from a networkx graph.
+                If None (default), pygrank.algorithms.utils.to_scipy_sparse_matrix with default arguments is used.
+            convergence: Optional. The ConvergenceManager that determines when iterations stop. If None (default),
+                a ConvergenceManager with the additional keyword arguments is constructed.
+
+        Example:
+            >>> from pygrank.algorithms import pagerank
+            >>> algorithm = pagerank.HeatKernel(t=5, tol=1.E-9) # tol passed to the ConvergenceManager
+        """
+        self.t = t
+        self.to_scipy = pygrank.algorithms.utils.to_scipy_sparse_matrix if to_scipy is None else to_scipy
+        self.convergence = pygrank.algorithms.utils.ConvergenceManager(**kwargs) if convergence is None else convergence
+
+    def rank(self, G, personalization=None):
+        M = self.to_scipy(G)
+
+        personalization = scipy.repeat(1.0, len(G)) if personalization is None else scipy.array([personalization.get(n, 0) for n in G], dtype=float)
+        if personalization.sum() == 0:
+            raise Exception("The personalization vector should contain at least one non-zero entity")
+        personalization = personalization / personalization.sum()
+
+        coefficient = np.exp(-self.t)
+        ranks = personalization*coefficient
+
+        self.convergence.start()
+        Mpower = M
+        while not self.convergence.has_converged(ranks):
+            coefficient *= self.t/(self.convergence.iteration+1)
+            Mpower *= M
+            ranks += personalization*Mpower
+
+        ranks = dict(zip(G.nodes(), map(float, ranks)))
+        return ranks
+
+
+class AbsorbingRank:
+    """ Implementation of partial absorbing random walks for Lambda = aI
+    Wu, Xiao-Ming, et al. "Learning with partially absorbing random walks." Advances in neural information processing systems. 2012.
+    """
+
+    def __init__(self, alpha=1-1.E-6, to_scipy=None, use_quotient=True, convergence=None, **kwargs):
+        """ Initializes the AbsorbingRank filter parameters.
+
+        Attributes:
+            alpha: Optional. (1-alpha)/alpha is the absorbsion rate of the random walk. This is chosen to yield the
+                same underlying meaning as PageRank (for which Lambda = a Diag(degrees) instead of aI)
+            to_scipy: Optional. Method to extract a scipy sparse matrix from a networkx graph.
+                If None (default), pygrank.algorithms.utils.to_scipy_sparse_matrix with default arguments is used.
+            convergence: Optional. The ConvergenceManager that determines when iterations stop. If None (default),
+                a ConvergenceManager with the additional keyword arguments is constructed.
+            use_quotient: Optional. If True (default) performs a L1 re-normalization of ranks after each iteration.
+                This significantly speeds ups the convergence speed.
+
+        Example:
+            >>> from pygrank.algorithms import pagerank
+            >>> algorithm = pagerank.HeatKernel(t=5, tol=1.E-9) # tol passed to the ConvergenceManager
+        """
+        self.alpha = float(alpha) # typecast to make sure that a graph is not accidentally the first argument
+        self.to_scipy = pygrank.algorithms.utils.to_scipy_sparse_matrix if to_scipy is None else to_scipy
+        self.convergence = pygrank.algorithms.utils.ConvergenceManager(**kwargs) if convergence is None else convergence
+        self.use_quotient = use_quotient
+
+    def rank(self, G, personalization=None, absorbtion=None, warm_start=None):
+        M = self.to_scipy(G)
+        degrees = scipy.array(M.sum(axis=1)).flatten()
+
+        personalization = scipy.repeat(1.0, len(G)) if personalization is None else scipy.array([personalization.get(n, 0) for n in G], dtype=float)
+        if personalization.sum() == 0:
+            raise Exception("The personalization vector should contain at least one non-zero entity")
+        personalization = personalization / personalization.sum()
+        ranks = personalization if warm_start is None else scipy.array([warm_start.get(n, 0) for n in G], dtype=float)
+
+        is_dangling = scipy.where(degrees == 0)[0]
+        self.convergence.start()
+        diag_of_lamda = (1-self.alpha)/self.alpha * (scipy.repeat(1.0, len(G)) if absorbtion is None else scipy.array([absorbtion.get(n, 0) for n in G], dtype=float))
+        while not self.convergence.has_converged(ranks):
+            ranks = (ranks * M + sum(ranks[is_dangling]) * personalization)*degrees/(diag_of_lamda+degrees) + personalization*diag_of_lamda/(diag_of_lamda+degrees)
+            if self.use_quotient:
+                ranks = ranks/ranks.sum()
+
+        ranks = dict(zip(G.nodes(), map(float, ranks)))
+        return ranks
+
+
+class BiasedKernel:
+    """ Heuristic kernel-like method that places emphasis on shorter random walks."""
+
+    def __init__(self, alpha=0.85, t=1, to_scipy=None, convergence=None, **kwargs):
+        self.alpha = alpha
+        self.t = t
+        self.to_scipy = pygrank.algorithms.utils.to_scipy_sparse_matrix if to_scipy is None else to_scipy
+        self.convergence = pygrank.algorithms.utils.ConvergenceManager(**kwargs) if convergence is None else convergence
+        warnings.warn("BiasedKernel is a low-quality heuristic", stacklevel=2)
+
+    def rank(self, G, personalization=None, warm_start=None):
+        M = self.to_scipy(G)
+        degrees = scipy.array(M.sum(axis=1)).flatten()
+
+        personalization = scipy.repeat(1.0, len(G)) if personalization is None else scipy.array([personalization.get(n, 0) for n in G], dtype=float)
+        if personalization.sum() == 0:
+            raise Exception("The personalization vector should contain at least one non-zero entity")
+        personalization = personalization / personalization.sum()
+        ranks = personalization if warm_start is None else scipy.array([warm_start.get(n, 0) for n in G], dtype=float)
+
+        is_dangling = scipy.where(degrees == 0)[0]
+        self.convergence.start()
+        while not self.convergence.has_converged(ranks):
+            a = self.alpha*self.t/self.convergence.iteration
+            ranks = personalization + a * ((ranks * M + sum(ranks[is_dangling]) * personalization) - ranks)
+            ranks = ranks/ranks.sum()
+
+        ranks = dict(zip(G.nodes(), map(float, ranks)))
+        return ranks
---
+++ packages/pygrank/pygrank-0.1.16-py3-none-any.whl/pygrank/metrics/supervised.py
@@ -0,0 +1,38 @@
+import numpy as np
+import sklearn.metrics
+
+
+class NDCG:
+    """Provides evaluation of NDCG@k score between given and known ranks"""
+
+    def __init__(self, known_ranks, k=None):
+        """ Initializes the PageRank scheme parameters.
+
+        Attributes:
+            known_ranks: A dict of known ranks, where higher ranks correspond to more related elements.
+            k: Optional. Calculates NDCG@k. If None (default), len(known_ranks) is used.
+        """
+        self.known_ranks = known_ranks
+        if not k is None and k > len(known_ranks):
+            raise Exception("NDCG@k cannot be computed for k greater than the number of known ranks")
+        self.k = len(known_ranks) if k is None else k
+
+    def evaluate(self, ranks):
+        DCG = 0
+        IDCG = 0
+        for i, v in enumerate(list(sorted(ranks, key=ranks.get, reverse=True))[:self.k]):
+            DCG += self.known_ranks.get(v, 0) / np.log2(i + 2)
+        for i, v in enumerate(list(sorted(self.known_ranks, key=self.known_ranks.get, reverse=True))[:self.k]):
+            IDCG += self.known_ranks[v] / np.log2(i + 2)
+        return DCG / IDCG
+
+
+class AUC:
+    """Wrapper for sklearn.metrics.auc evaluation"""
+
+    def __init__(self, known_ranks):
+        self.known_ranks = known_ranks
+
+    def evaluate(self, ranks):
+        fpr, tpr, _ = sklearn.metrics.roc_curve([self.known_ranks.get(v, 0) for v in ranks], list(ranks.values()))
+        return sklearn.metrics.auc(fpr, tpr)
