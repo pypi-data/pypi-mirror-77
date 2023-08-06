99c6ee03412c3efbc8baa84ad7f1bd21d7a2506e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:22 +0000

    Add package pytest-commander/2.0.0/pytest_commander-2.0.0-py3-none-any.whl

---
+++ packages/pytest-commander/pytest_commander-2.0.0-py3-none-any.whl/pytest_commander/web_client/build/precache-manifest.035d14c6513fd666d4b3e318317a084a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef31f024835a04c0d509ba2101f36386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf32188c13e1d960808",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "baf32188c13e1d960808",
+    "url": "/static/js/2.7a2c6aa0.chunk.js"
+  },
+  {
+    "revision": "73228ee743bd43575fc7c2066895ba0d",
+    "url": "/static/js/2.7a2c6aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0377c1952f0ffd0acb09",
+    "url": "/static/js/main.240227f0.chunk.js"
+  },
+  {
+    "revision": "3f88458ca36b08e28528",
+    "url": "/static/js/runtime-main.0b95b564.js"
+  }
+]);
---
+++ packages/pytest-commander/pytest_commander-2.0.0-py3-none-any.whl/pytest_commander/web_client/build/precache-manifest.ed2300bf54a8646f527e3286c81e9fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9371f7d46d8ebead5e2f93e8dd82e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05784448bea6e68dd4ea",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "05784448bea6e68dd4ea",
+    "url": "/static/js/2.3f95a9b4.chunk.js"
+  },
+  {
+    "revision": "73228ee743bd43575fc7c2066895ba0d",
+    "url": "/static/js/2.3f95a9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa13dd032a87fdb838a",
+    "url": "/static/js/main.ed84a826.chunk.js"
+  },
+  {
+    "revision": "3f88458ca36b08e28528",
+    "url": "/static/js/runtime-main.0b95b564.js"
+  }
+]);
