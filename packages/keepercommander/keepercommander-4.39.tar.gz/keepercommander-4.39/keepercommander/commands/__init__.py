bf55ee8ba701a9da5c46e0897e9e17aa60a6b8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:51 +0000

    Add package keepercommander/4.39/keepercommander-4.39.tar.gz

---
+++ packages/keepercommander/keepercommander-4.39.tar.gz/keepercommander-4.39/keepercommander/commands/__init__.py
@@ -0,0 +1,14 @@
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Commander
+# Copyright 2018 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
+from .base import register_commands, register_enterprise_commands, aliases, commands, enterprise_commands
+
+__all__ = ['register_commands', 'register_enterprise_commands', 'aliases', 'commands', 'enterprise_commands']
