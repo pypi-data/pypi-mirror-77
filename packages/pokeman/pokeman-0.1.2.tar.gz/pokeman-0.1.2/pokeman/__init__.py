e907ca726db451b5f9da1899ce545452a81911b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:14 +0000

    Add package pokeman/0.1.2/pokeman-0.1.2.tar.gz

---
+++ packages/pokeman/pokeman-0.1.2.tar.gz/pokeman-0.1.2/pokeman/__init__.py
@@ -0,0 +1,25 @@
+from pokeman.pokeman import Pokeman
+from pokeman.amqp_resources.globals import Exchange, RoutingKey, Queue
+from pokeman.composite import ConnectionParameters, Connection, BasicConfig
+from pokeman import coatings
+
+
+__version__ = '0.1.2'
+
+
+__all__ = [
+    # Globals
+    '__version__', 'Pokeman',
+    'Exchange', 'RoutingKey', 'Queue',
+
+    # Connection
+    'ConnectionParameters', 'Connection', 'BasicConfig',
+
+    # EIP
+    'coatings']
+
+import platform
+
+
+_current_os = platform.system()
+
