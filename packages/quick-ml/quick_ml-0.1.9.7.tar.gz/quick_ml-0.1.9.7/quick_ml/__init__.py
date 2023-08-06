7944a7325ec53fc456f39113bcc2c6eec1b28354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:30 +0000

    Add package quick-ml/0.1.9.7/quick_ml-0.1.9.7.tar.gz

---
+++ packages/quick-ml/quick_ml-0.1.9.7.tar.gz/quick_ml-0.1.9.7/quick_ml/__init__.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+if tf.__version__ == '2.2.0':
+	print(f"Tensorflow imported successfully. Tensorflow version -> {tf.__version__}")
+else:
+	print("Tensorflow version mismatch!. Please check tensorflow isn't imported before installing quick_ml. Restart the session to fix the error.")
+
+from quick_ml import tfrecords_maker
+from quick_ml import begin_tpu
+from quick_ml import visualize_and_check_data
+from quick_ml import load_models_quick
+from quick_ml import training_predictions
