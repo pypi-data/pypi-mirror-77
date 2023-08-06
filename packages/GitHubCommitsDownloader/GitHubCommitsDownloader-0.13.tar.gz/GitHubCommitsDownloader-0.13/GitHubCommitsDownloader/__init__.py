5c3de9ef7a0c4017bd9e90ed0728523c934fb085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:28 +0000

    Add package GitHubCommitsDownloader/0.13/GitHubCommitsDownloader-0.13.tar.gz

---
+++ packages/GitHubCommitsDownloader/GitHubCommitsDownloader-0.13.tar.gz/GitHubCommitsDownloader-0.13/GitHubCommitsDownloader/__init__.py
@@ -0,0 +1,38 @@
+import os
+from .clear import clear
+from .parse import parse
+from .GitHub import GitHub
+import argparse
+
+clear()
+
+class GitHubCommitsDownloader():
+	def __init__(self, options):
+		try:
+			self.root = options.workingdir
+		except:
+			self.root = os.getcwd()
+
+		if type(options) == argparse.Namespace:
+			if not options.user:
+				raise Exception("User is required; use -u/--user UserName")
+			self.user = options.user
+			if not options.repo:
+				raise Exception("Repo is required; use -r/--repo repo")
+			self.repo = options.repo
+		else:
+			if not options.get('user'):
+				raise Exception("User is required; use { \"user\": \"UserName\" }")
+			self.user = options.get('user')
+			if not options.get('repo'):
+				raise Exception("Repo is required; use { \"repo\": \"User's reposetory name\" }")
+			self.repo = options.get('repo')
+
+		try:
+			self.branch = options.branch
+		except:
+			self.branch = options.get('branch') or "all"
+
+	def parse(self):
+		user, repo, branch = parse(self.user, self.repo, self.branch)
+		GitHub(user, repo, branch, self.root)
