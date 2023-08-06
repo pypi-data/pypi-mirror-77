0a72c909fb89f74ad0c7b793540398df70bbd993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package django-inlines-actions/0.4/django_inlines_actions-0.4-py3-none-any.whl

---
+++ packages/django-inlines-actions/django_inlines_actions-0.4-py3-none-any.whl/inlines_actions/__init__.py
@@ -0,0 +1,8 @@
+from .mixins import InlineActionsMixin, InlineActionsModelAdminMixin
+from .views import InlineActionView
+
+__all__ = (
+    InlineActionsModelAdminMixin,
+    InlineActionsMixin,
+    InlineActionView,
+)
