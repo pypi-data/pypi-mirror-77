18ed26ee719a2530440ca8f52aac5f02f28ce791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package cryptomodel-athanikos/2.5.5/cryptomodel_athanikos-2.5.5-py3-none-any.whl

---
+++ packages/cryptomodel-athanikos/cryptomodel_athanikos-2.5.5-py3-none-any.whl/cryptomodel/sent_notification.py
@@ -0,0 +1,19 @@
+import jsonpickle
+from mongoengine import *
+
+from calculator.helpers import if_none_raise
+from cryptomodel import computed_notification
+from cryptomodel.notification_type import NOTIFICATION_TYPE
+
+'''
+Represents a sent notification  
+Holds a computed_notifications 
+Extends with sent flag and sent_date
+'''
+
+
+class sent_notification(computed_notification):
+    meta = {'strict': False}
+
+    sent = BooleanField(default=False)
+    sent_date = DateField()
