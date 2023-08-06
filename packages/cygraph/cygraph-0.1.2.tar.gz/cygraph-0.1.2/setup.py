58a6f2c96afcd0111f03621168607f6b5bb4d810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:21 +0000

    Add package cygraph/0.1.2/cygraph-0.1.2.tar.gz

---
+++ packages/cygraph/cygraph-0.1.2.tar.gz/cygraph-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import sys
+from setuptools import setup, find_packages
+
+import numpy as np
+from Cython.Build import cythonize
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='cygraph',
+    version='0.1.2',
+    author='Arin Khare',
+    author_email='arinmkhare@gmail.com',
+    description='A graph theory library written in Cython',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lol-cubes/cygraph',
+    packages=find_packages(),
+    ext_modules=cythonize(
+        ['cygraph/*.pyx', 'cygraph/algorithms/*.pyx',
+            'cygraph/graph_/*.pyx']
+    ),
+    include_dirs=[np.get_include()]
+)
