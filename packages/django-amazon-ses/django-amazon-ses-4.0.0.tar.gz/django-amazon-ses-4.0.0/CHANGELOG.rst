10cfd11f6353cb94d0915dac62d54144a56c8fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package django-amazon-ses/4.0.0/django-amazon-ses-4.0.0.tar.gz

---
+++ packages/django-amazon-ses/django-amazon-ses-4.0.0.tar.gz/django-amazon-ses-4.0.0/CHANGELOG.rst
@@ -0,0 +1,127 @@
+4.0.0 - 2020-08-25
+==================
+
+- Add support for Django 3.1.
+- Add support for Black source code formatting
+- Remove support for end-of-life Django 1.11, 2.0 and 2.1.
+- Remove support for Python 3.5
+- Migrate from Travis to GitHub Actions.
+
+3.0.2 - 2020-01-19
+==================
+
+- Make verious improvements to ``tox`` test suite configuration.
+- Fix ``send_messages`` so that it returns ``0`` when no messages are supplied.
+- Move all module configuration to ``setup.cfg`` from ``setup.py``.
+- Upgrade ``moto``; stop making use of ``mock_ses_deprecated``.
+
+3.0.1 - 2020-01-05
+==================
+
+- Bump version in ``setup.py`` to match Git tags. No actual code changes
+  were made.
+
+3.0.0 - 2020-01-05
+==================
+
+- Add support for Django 3.x and Python 3.8. No actual code changes were
+  required.
+- Add ``AWS_SES_CONFIGURATION_SET_NAME`` option to use a specific SES
+  configuration set.
+- Drop support for Python 2.7 and 3.4.
+- Fix double invocation of post message send signal.
+
+2.1.1 - 2019-06-08
+==================
+
+- Add ``BotoCoreError`` to set of exceptions for ``fail_silently``.
+- Reformat source code with `black`.
+
+2.1.0 - 2019-04-03
+==================
+
+- Add support and testing for Django 2.1, 2.2 and Python 3.7. No actual code
+  changes were required.
+
+2.0.0 - 2018-04-10
+==================
+
+- Remove support for Django < 1.11.
+- Add ``AWS_SES_*`` settings to configure AWS credentials through
+  ``settings.py``.
+- Add ``EmailBackend`` constructor arguments to override AWS credentials.
+
+1.0.0 - 2017-12-07
+==================
+
+- Drop support for Python 3.3.
+- Add testing and support for Django 2.0 (no actual code changes were
+  required).
+- Add settings ``AWS_ACCESS_KEY_ID`` and ``AWS_SECRET_ACCESS_KEY`` to configure
+  credentials through ``settings.py``.
+- Rename setting ``DJANGO_AMAZON_SES_REGION`` to ``AWS_DEFAULT_REGION`` (to
+  match the Boto 3 environment variable).
+- ``django_amazon_ses`` is now a module instead of a package. To upgrade,
+  change the ``EMAIL_BACKEND`` setting in ``settings.py``:
+
+  .. code:: python
+
+    EMAIL_BACKEND = 'django_amazon_ses.EmailBackend'
+
+  Importing signals should now be imported from ``django_amazon_ses`` instead
+  of ``django_amazon_ses.backends.boto``.
+
+0.3.2 - 2017-11-15
+==================
+
+- Fix Travis CI deployment process to account for wheels.
+- Ensure that only one element of the build matrix publishes to PyPI.
+
+0.3.1 - 2017-11-15
+==================
+
+- Fix ``tox`` installation of test dependencies.
+- Add ``pip`` cache to Travis CI configuration.
+- Include license file in wheel package.
+
+0.3.0 - 2017-03-30
+==================
+
+- Officially support Python 3.x.
+- Use a more sophisticated matrix build process to test Django compatibility.
+- Add support for ``DJANGO_AMAZON_SES_REGION`` setting.
+
+0.2.2 - 2017-01-05
+==================
+
+- Functionally identical to ``0.2.0-0.2.1``, but includes a reStructuredText formatting change to the ``README`` for PyPI compatibility.
+
+0.2.1 - 2017-01-05
+==================
+
+- Functionally identical to ``0.2.0``, but includes a reStructuredText formatting change to the ``README`` for PyPI compatibility.
+
+0.2.0 - 2017-01-05
+==================
+
+- Add support for ``pre_send`` and ``post_send`` signals for email messages.
+
+0.1.3 - 2016-03-23
+==================
+
+- Update PyPI credentials; functionally identical to ``0.1.0-0.1.2``.
+
+0.1.2 - 2016-03-23
+==================
+
+- Functionally identical to ``0.1.0-0.1.1``, but actually updates ``setup.py``.
+
+0.1.1 - 2016-03-23
+==================
+
+- Ensure that manifest accounts for ``CHANGELOG.rst``.
+
+0.1.0 - 2016-03-23
+==================
+
+- Initial release.
---
+++ packages/django-amazon-ses/django-amazon-ses-4.0.0.tar.gz/django-amazon-ses-4.0.0/tests/test_boto.py
@@ -0,0 +1,149 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+from moto import mock_ses
+
+from unittest import mock
+
+from django.conf import settings
+from django.core import mail
+from django.core.mail import EmailMessage
+
+from django.test import SimpleTestCase
+
+from django_amazon_ses import pre_send
+
+settings.configure()
+
+
+class MailTests(SimpleTestCase):
+    @mock_ses
+    def test_custom_backend(self):
+        """Test Amazon SES backend."""
+        client = boto3.client("ses", region_name="us-east-1")
+        client.verify_email_identity(EmailAddress="bounce@example.com")
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+        self.assertGreater(conn.send_messages([email]), 0)
+
+    @mock_ses
+    @mock.patch("django_amazon_ses.pre_send.send")
+    def test_signal_pre(self, mock_signal):
+        client = boto3.client("ses", region_name="us-east-1")
+        client.verify_email_identity(EmailAddress="bounce@example.com")
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+        conn.send_messages([email])
+        called_args, called_kwargs = mock_signal.call_args
+        self.assertIn("message", called_kwargs)
+        self.assertEqual(email, called_kwargs["message"])
+
+    @mock_ses
+    @mock.patch("django_amazon_ses.post_send.send")
+    def test_signal_post(self, mock_signal):
+        client = boto3.client("ses", region_name="us-east-1")
+        client.verify_email_identity(EmailAddress="bounce@example.com")
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+        conn.send_messages([email])
+        called_args, called_kwargs = mock_signal.call_args
+        self.assertIn("message", called_kwargs)
+        self.assertEqual(email, called_kwargs["message"])
+        self.assertIn("message_id", called_kwargs)
+        self.assertRegex(
+            called_kwargs["message_id"],
+            r"\w{16,16}-\w{8,8}-\w{4,4}-\w{4,4}-\w{4,4}-\w{12,12}-\w{6,6}",
+        )
+
+    @mock_ses
+    def test_pre_change_recipients(self):
+        new_email_address = "changed@example.com"
+
+        def change_recipients(sender, message=None, **kwargs):
+            message.to = [new_email_address]
+
+        pre_send.connect(change_recipients)
+
+        client = boto3.client("ses", region_name="us-east-1")
+        client.verify_email_identity(EmailAddress="bounce@example.com")
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+        self.assertNotEqual(email.to, [new_email_address])
+        conn.send_messages([email])
+        self.assertEqual(email.to, [new_email_address])
+
+    @mock_ses
+    def test_pre_remove_recipients(self):
+        def remove_recipients(sender, message=None, **kwargs):
+            message.to = []
+
+        pre_send.connect(remove_recipients)
+
+        client = boto3.client("ses", region_name="us-east-1")
+        client.verify_email_identity(EmailAddress="bounce@example.com")
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+        conn.send_messages([email])
+        self.assertEqual(conn.send_messages([email]), 0)
+
+    @mock.patch("botocore.client.BaseClient._make_api_call")
+    def test_suppress_botocore_error(self, mock_make_api_call):
+        """Ensure that fail_silently works when botocore raises an error."""
+        mock_make_api_call.side_effect = NoCredentialsError
+
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        email = EmailMessage(
+            "Subject",
+            "Content",
+            "bounce@example.com",
+            ["to@example.com"],
+            headers={"From": "from@example.com"},
+        )
+
+        self.assertFalse(conn.fail_silently)
+        with self.assertRaises(NoCredentialsError):
+            conn.send_messages([email])
+
+        conn.fail_silently = True
+        message_sent = conn.send_messages([email])
+        self.assertFalse(message_sent)
+
+    @mock_ses
+    def test_send_messages_empty_list(self):
+        conn = mail.get_connection("django_amazon_ses.EmailBackend")
+        self.assertEqual(conn.send_messages([]), 0)
