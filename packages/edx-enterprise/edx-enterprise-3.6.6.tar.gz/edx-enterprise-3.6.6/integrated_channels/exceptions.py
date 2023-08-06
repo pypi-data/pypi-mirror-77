5d92942ecc1376dbfab34fdb2a4c7d470c560a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:58 +0000

    Add package edx-enterprise/3.6.6/edx-enterprise-3.6.6.tar.gz

---
+++ packages/edx-enterprise/edx-enterprise-3.6.6.tar.gz/edx-enterprise-3.6.6/integrated_channels/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Integrated channel custom exceptions.
+"""
+
+
+class ClientError(Exception):
+    """
+    Indicate a problem when interacting with an integrated channel.
+    """
