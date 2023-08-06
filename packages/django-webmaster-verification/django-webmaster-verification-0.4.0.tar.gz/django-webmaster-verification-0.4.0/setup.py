4c8b1e19a09e2bb37ec9d4336763cf0af22ae280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package django-webmaster-verification/0.4.0/django-webmaster-verification-0.4.0.tar.gz

---
+++ packages/django-webmaster-verification/django-webmaster-verification-0.4.0.tar.gz/django-webmaster-verification-0.4.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-webmaster-verification",
+    version="0.4.0",
+    packages=find_packages(),
+    author="Nicolas Kuttler",
+    author_email="pypi@kuttler.eu",
+    description="Webmaster tools verification for Django",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/nkuttler/django-webmaster-verification",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=["Django >= 3.0", ],
+    zip_safe=True,
+)
