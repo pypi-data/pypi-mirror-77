5ec5d5614445bf93cc3835b04676bcca204ab94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:33 +0000

    Add package detext/0.0.84.dev0/detext-0.0.84.dev0.tar.gz

---
+++ packages/detext/detext-0.0.84.dev0.tar.gz/detext-0.0.84.dev0/src/detext/_version.py
@@ -0,0 +1,6 @@
+# Update (increment) the version number before releasing. Checkout published versions at
+# https://pypi.org/manage/project/detext/releases/
+# Please follow the following best practices for versioning: Breaking changes are indicated by increasing the
+# major number (high risk), new non-breaking features increment the minor number (medium risk) and all other
+# non-breaking changes increment the patch number (lowest risk).
+__version__ = "0.0.84.dev0"
