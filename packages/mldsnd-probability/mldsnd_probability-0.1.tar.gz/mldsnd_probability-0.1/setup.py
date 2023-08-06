e8a8126ccc746153bde921524ad85b4b07a79342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:11 +0000

    Add package mldsnd-probability/0.1/mldsnd_probability-0.1.tar.gz

---
+++ packages/mldsnd-probability/mldsnd_probability-0.1.tar.gz/mldsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mldsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['mldsnd_probability'],
+      author = 'Juan Arroyo-Miranda',
+      author_email = 'something@nothing.com',
+      zip_safe=False)
