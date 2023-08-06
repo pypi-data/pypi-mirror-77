6ae3495e526c001f8b0691a4245c474611bbeee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:55 +0000

    Add package ticketgenerator/0.1/ticketgenerator-0.1.tar.gz

---
+++ packages/ticketgenerator/ticketgenerator-0.1.tar.gz/ticketgenerator-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ticketgenerator',
+  packages = ['ticketgenerator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Housie Ticket Generation',
+  author = 'ksgsarma',
+  author_email = 'ksgsarma5@gmail.com',
+  url = 'https://github.com/ksgsarma/Housie_PyPi.git',
+  keywords = ['ticket'],
+  classifiers = [],
+)
