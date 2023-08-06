db839f6bd6887ffb3550122bbf85c5d98592052e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:19 +0000

    Add package check-palindrome-aast/0.0.1/check-palindrome-aast-0.0.1.tar.gz

---
+++ packages/check-palindrome-aast/check-palindrome-aast-0.0.1.tar.gz/check-palindrome-aast-0.0.1/is_plaindrome_aast/_init__.py
@@ -0,0 +1,6 @@
+def plaindrome ( string ):
+  string2 = string[::-1]
+  if string2 == string :
+     return True
+  else:
+     return False
---
+++ packages/check-palindrome-aast/check-palindrome-aast-0.0.1.tar.gz/check-palindrome-aast-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'check-palindrome-aast',
+    version = '0.0.1',
+    description = 'This is a palindrome checking function.',
+    long_description = open('ReadMe.txt').read(),
+    url = 'https://asin1203.github.io',
+    author = 'Aastha Singh',
+    keywords = ['palindrome','python palindrome','palindrome evaluator'],
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = ['']
+
+)
