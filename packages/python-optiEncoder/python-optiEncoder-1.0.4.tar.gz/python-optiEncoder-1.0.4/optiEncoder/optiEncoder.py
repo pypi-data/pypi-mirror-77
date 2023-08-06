7b85d6ac2f14fb5cb13aefb5db195ba3d01dad0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:06 +0000

    Add package python-optiEncoder/1.0.4/python-optiEncoder-1.0.4.tar.gz

---
+++ packages/python-optiEncoder/python-optiEncoder-1.0.4.tar.gz/python-optiEncoder-1.0.4/optiEncoder/optiEncoder.py
@@ -0,0 +1,38 @@
+import copy
+import math
+
+class optiEncoder:
+    def __init__(self, lst):
+        self.lst = list(lst)
+        self.categories = set(self.lst)
+        self.categCount = len(self.categories)
+        self.bits = math.ceil(math.log(self.categCount)/math.log(2))
+        self.bitstrings = []
+        self.counter = self.categCount
+        self.generateBitStrings(self.bits, [])
+        self.generateDictionary()
+
+    def generateBitStrings(self, bitlen, payload):
+        if self.counter<=0:
+            return
+        if bitlen== 0:
+            self.bitstrings.append(payload)
+            self.counter = self.counter-1
+        else:
+            case1 = copy.deepcopy(payload)
+            case2 = copy.deepcopy(payload)
+            case1.append(0)
+            case2.append(1)
+            self.generateBitStrings(bitlen-1, case1)
+            self.generateBitStrings(bitlen-1, case2)
+    def generateDictionary(self):
+        self.mappings = {}
+        for i,j in enumerate(list(self.categories)):
+            self.mappings[j] = self.bitstrings[i] 
+    def getMappings(self):
+        return self.mappings
+    def getEncodedList(self):
+        self.encodedList = []
+        for i in self.lst:
+            self.encodedList.append(self.mappings[i])
+        return self.encodedList
