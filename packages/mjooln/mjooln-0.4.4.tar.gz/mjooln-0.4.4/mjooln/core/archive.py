8b227e4ccbf618549fa526c213b20308d3dc33db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:26 +0000

    Add package mjooln/0.4.4/mjooln-0.4.4.tar.gz

---
+++ packages/mjooln/mjooln-0.4.4.tar.gz/mjooln-0.4.4/mjooln/core/archive.py
@@ -0,0 +1,47 @@
+import shutil
+import zipfile
+import gzip
+
+from mjooln.core.file import File, Folder
+
+
+class ArchiveError(Exception):
+    pass
+
+
+class Archive:
+    # TODO: Add gz to zip
+    # TODO: Add handling of multiple files and folders in archive
+
+    @classmethod
+    def zip_to_gz(cls,
+                  file: File,
+                  delete_source_file: bool = True):
+        extensions = file.extensions()
+
+        if len(extensions) == 0 or extensions[-1] != 'zip':
+            raise ArchiveError(f'File is not zip-file: {file}')
+
+        with zipfile.ZipFile(file, 'r') as zr:
+            file_info = zr.filelist
+            if len(file_info) > 1:
+                raise ArchiveError(f'Multiple files in archive: {file}')
+            elif len(file_info) == 0:
+                raise ArchiveError(f'No files in archive: {file}')
+            file_info = file_info[0]
+            file_name = file_info.filename
+            if '/' in file_name:
+                file_name = file_name.split('/')[-1]
+            if '\\' in file_name:
+                file_name = file_name.split('\\')[-1]
+            gz_file = File.join(file.folder(), file_name + '.gz')
+            with zr.open(file_info, 'r') as zf:
+                with gzip.open(gz_file, 'w') as gf:
+                    shutil.copyfileobj(zf, gf)
+
+        if delete_source_file:
+            file.delete()
+
+        return gz_file
+
+
