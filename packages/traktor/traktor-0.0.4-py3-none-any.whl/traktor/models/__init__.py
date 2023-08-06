86d99958327de0a0ff29498494bd9a18c220269b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:44 +0000

    Add package traktor/0.0.4/traktor-0.0.4-py3-none-any.whl

---
+++ packages/traktor/traktor-0.0.4-py3-none-any.whl/traktor/models/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["User", "Project", "Task", "Entry", "Report"]
+
+from traktor.models.user import User
+from traktor.models.project import Project
+from traktor.models.task import Task
+from traktor.models.entry import Entry
+from traktor.models.report import Report
