645fb87aad107753470fef236a2ef70150587fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:41 +0000

    Add package moban-jinja2-github/0.0.1/moban_jinja2_github-0.0.1-py2.py3-none-any.whl

---
+++ packages/moban-jinja2-github/moban_jinja2_github-0.0.1-py2.py3-none-any.whl/moban_jinja2_github/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from moban_jinja2_github._version import __author__, __version__
+from moban.plugins.jinja2.extensions import jinja_global
+from moban_jinja2_github.contributors import get_contributors
+
+jinja_global("moban_jinja2_contributors", get_contributors)
