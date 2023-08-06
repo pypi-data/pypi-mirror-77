92056b50d71105fb1c0607f3d752f23e8350f622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package deepvac/0.1.2/deepvac-0.1.2.tar.gz

---
+++ packages/deepvac/deepvac-0.1.2.tar.gz/deepvac-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import sys
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+version="0.1.2"
+setup(name='deepvac',
+    version=version,
+    description='PyTorch python project standard',
+    long_description='PyTorch python project standard',
+    keywords='PyTorch python project standard',
+    url='https://github.com/DeepVAC/deepvac',
+    download_url="",
+    author='Gemfield',
+    author_email='gemfield@civilnet.cn',
+
+    install_requires=[
+        'opencv_python',
+        'numpy',
+        'Pillow',
+        'scipy',
+        'tensorboard'
+    ],
+    classifiers  = [
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries"
+     ],
+    include_package_data=True,
+    zip_safe=False)
