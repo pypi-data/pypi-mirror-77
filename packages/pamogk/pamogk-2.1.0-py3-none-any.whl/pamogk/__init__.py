a167a2d46ba78da057fac1d81cde815545735c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:30 +0000

    Add package pamogk/2.1.0/pamogk-2.1.0-py3-none-any.whl

---
+++ packages/pamogk/pamogk-2.1.0-py3-none-any.whl/pamogk/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'config',
+    'label_mapper',
+    'lib',
+    'data_processor',
+    'gene_mapper',
+    'kernels',
+    'pathway_reader',
+]
+
+__version__ = '2.1.0'
