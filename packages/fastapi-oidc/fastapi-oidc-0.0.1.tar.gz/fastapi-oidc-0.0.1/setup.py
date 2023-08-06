f2019531fd2d8c75ac130123445564c732fc5600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:40 +0000

    Add package fastapi-oidc/0.0.1/fastapi-oidc-0.0.1.tar.gz

---
+++ packages/fastapi-oidc/fastapi-oidc-0.0.1.tar.gz/fastapi-oidc-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_oidc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.61.0,<0.62.0']
+
+setup_kwargs = {
+    'name': 'fastapi-oidc',
+    'version': '0.0.1',
+    'description': 'A simple library for parsing and verifying externally issued OIDC ID tokens in fastapi.',
+    'long_description': None,
+    'author': 'HarryMWinters',
+    'author_email': 'harrymcwinters@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
