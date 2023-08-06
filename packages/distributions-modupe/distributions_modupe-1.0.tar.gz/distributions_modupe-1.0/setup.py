86016eea385c86ab309292730202756fb487c181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package distributions-modupe/1.0/distributions_modupe-1.0.tar.gz

---
+++ packages/distributions-modupe/distributions_modupe-1.0.tar.gz/distributions_modupe-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_modupe',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_modupe'],
+      zip_safe=False)
