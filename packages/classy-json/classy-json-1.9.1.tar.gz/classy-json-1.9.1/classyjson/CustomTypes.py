29132be678314aba4db5c73e4428d8485afe25d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:13 +0000

    Add package classy-json/1.9.1/classy-json-1.9.1.tar.gz

---
+++ packages/classy-json/classy-json-1.9.1.tar.gz/classy-json-1.9.1/classyjson/CustomTypes.py
@@ -0,0 +1,26 @@
+
+def nice(val):
+    if isinstance(val, list):
+        return NiceList(val)
+
+    if isinstance(val, dict):
+        return NiceDict(val)
+
+    return val
+
+
+class NiceList(list):
+    def __init__(self, _list):
+        map(nice, _list)
+        list.__init__(self, _list)
+
+
+class NiceDict(dict):
+    def __init__(self, _dict):
+        dict.__init__(self, {k:nice(v) for (k, v) in _dict.items()})
+
+    __getattr__ = dict.__getitem__
+    __delattr__ = dict.__delitem__
+
+    def __setattr__(self, name, value):  # NiceDict.a = 'something'
+        return dict.__setitem__(self, name, nice(value))
