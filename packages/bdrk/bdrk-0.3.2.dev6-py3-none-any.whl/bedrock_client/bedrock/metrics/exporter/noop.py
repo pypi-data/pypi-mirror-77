2699602c842157b0f84761a81d45dc17c3b6d820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package bdrk/0.3.2.dev6/bdrk-0.3.2.dev6-py3-none-any.whl

---
+++ packages/bdrk/bdrk-0.3.2.dev6-py3-none-any.whl/bedrock_client/bedrock/metrics/exporter/noop.py
@@ -0,0 +1,10 @@
+from ..context import PredictionContext
+from .type import LogExporter
+
+
+class NoopExporter(LogExporter):
+    """Exporter that can be used to disable logging.
+    """
+
+    def emit(self, prediction: PredictionContext):
+        pass
