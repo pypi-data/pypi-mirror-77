7000043facdcf9f5ef2b83cecf83fb98559550c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:47 +0000

    Add package viztracer/0.3.4/viztracer-0.3.4.tar.gz

---
+++ packages/viztracer/viztracer-0.3.4.tar.gz/viztracer-0.3.4/src/viztracer/__init__.py
@@ -0,0 +1,21 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+from .viztracer import VizTracer
+from .flamegraph import FlameGraph
+from .decorator import ignore_function
+from .vizcounter import VizCounter
+from .vizobject import VizObject
+from .main import main
+
+__version__ = "0.3.4"
+
+__all__ = [
+    "__version__",
+    "main",
+    "VizTracer",
+    "FlameGraph",
+    "ignore_function",
+    "VizCounter",
+    "VizObject"
+]
