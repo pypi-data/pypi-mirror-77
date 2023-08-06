8521e44329efafb1ff715fe7a6c68bab07114a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:12 +0000

    Add package drc-cmis/1.0.0/drc-cmis-1.0.0.tar.gz

---
+++ packages/drc-cmis/drc-cmis-1.0.0.tar.gz/drc-cmis-1.0.0/drc_cmis/settings.py
@@ -0,0 +1,17 @@
+import sys
+
+from django.conf import settings
+
+
+class _Settings(object):
+    @property
+    def BASE_FOLDER_LOCATION(self):
+        return getattr(settings, "BASE_FOLDER_LOCATION", "DRC")
+
+    def __getattr__(self, name):
+        return globals()[name]
+
+
+# other parts of itun that you WANT to code in
+# module-ish ways
+sys.modules[__name__] = _Settings()
