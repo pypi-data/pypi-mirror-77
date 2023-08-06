7aa857924c50d23a04088d027c52bbb92e32c443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:18 +0000

    Add package papyru/0.0.1.dev51/papyru-0.0.1.dev51.tar.gz

---
+++ packages/papyru/papyru-0.0.1.dev51.tar.gz/papyru-0.0.1.dev51/papyru/utils.py
@@ -0,0 +1,15 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+
+@contextmanager
+def limited_runtime(timeout):
+    start_time = datetime.now()
+
+    def has_runtime_left():
+        return (datetime.now() - start_time) < timeout
+
+    try:
+        yield has_runtime_left
+    finally:
+        pass
