8a728a880e0dde5809e64e99f6f91c7e9788da07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:13 +0000

    Add package fairmotion/0.0.4/fairmotion-0.0.4.tar.gz

---
+++ packages/fairmotion/fairmotion-0.0.4.tar.gz/fairmotion-0.0.4/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fairmotion",
+    version="0.0.4",
+    description="fairmotion is FAIR's library for human motion research",
+    url="https://github.com/fairinternal/fairmotion",
+    author="FAIR Pittsburgh",
+    author_email="dgopinath@fb.com",
+    install_requires=[
+        "black",
+        "dataclasses", # py3.6 backport required by human_body_prior
+        "human_body_prior",
+        "matplotlib",
+        "numpy",
+        "pillow",
+        "pyrender",
+        "scikit-learn",
+        "scipy",
+        "torch==1.4.0",
+        "tqdm",
+        "PyOpenGL==3.1.0",
+    ],
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
