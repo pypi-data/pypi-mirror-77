3f295bd2ebbac1fd8cefd0de96c926478419e1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:59 +0000

    Add package PyTCID/1.0.0/PyTCID-1.0.0-py3-none-any.whl

---
+++ packages/PyTCID/PyTCID-1.0.0-py3-none-any.whl/tcid/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package carries functions that can be used
+to Government-issued ID cards of the Republic of Turkey.
+"""
+__all__ = ['_verification_functions']
+__version__ = "1.0.0"
+from ._verification_functions import verify_national_id
