74b8706e6908b00c669c0891049c1f404b2c853f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package eks-spot-blocks/0.2.13/eks_spot_blocks-0.2.13-py3-none-any.whl

---
+++ packages/eks-spot-blocks/eks_spot_blocks-0.2.13-py3-none-any.whl/eks_spot_blocks/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_eks._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_ssm._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-serverless-api",
+    "0.2.13",
+    __name__[0:-6],
+    "cdk-serverless-api@0.2.13.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
