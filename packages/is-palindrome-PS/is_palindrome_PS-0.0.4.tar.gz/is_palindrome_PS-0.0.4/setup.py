e802494f6736a9e7dc62e03846cef736244d1f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:26 +0000

    Add package is-palindrome-PS/0.0.4/is_palindrome_PS-0.0.4.tar.gz

---
+++ packages/is-palindrome-PS/is_palindrome_PS-0.0.4.tar.gz/is_palindrome_PS-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'is_palindrome_PS',
+    version = '0.0.4',
+    description = 'This is palindrome checking function',
+    long_description = open('Readme.txt').read(),
+    url = 'https://balajibetadur.github.io',
+    author = 'Prakash Shelar',
+    keywords = ['palindrome','Python palindrome'],
+    license = 'MIT',
+    packages = ['is_palindrome_PS'],
+    install_requres = ['']
+)
