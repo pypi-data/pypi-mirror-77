2932f0f403ef631d21f1201d1d447ac95ca58b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:13 +0000

    Add package ataiat-probability/0.1/ataiat_probability-0.1.tar.gz

---
+++ packages/ataiat-probability/ataiat_probability-0.1.tar.gz/ataiat_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ataiat_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ataiat_probability'],
+      author = 'Ashutosh A. Patwardhan',
+      author_email = 'ashutosh.patwardhan@iitbombay.org',
+      zip_safe=False)
