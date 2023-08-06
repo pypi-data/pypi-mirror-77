b23f72cd60410afe1f7f2441a0d994144d71b9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:59 +0000

    Add package streamer/1.1/streamer-1.1.tar.gz

---
+++ packages/streamer/streamer-1.1.tar.gz/streamer-1.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+          name="streamer",
+	  version="1.1",
+          author="Udit Namdev",
+	  author_email="unamdev0@gmail.com",
+	  description="Stream movies directly from torrent without downloading",
+          long_description = long_description,
+          long_description_content_type="text/markdown",
+	  url="https://github.com/unamdev0/streamer",
+	  license='MIT',
+	  packages=['streamer'],
+        keywords = ['Torrent', 'Stream', 'Movies','Series','Streamer','Magnet link','utorrent','piratebay','yify'], 
+          classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+          ],
+	  install_requires=[
+		  'BeautifulSoup4',
+                 
+		  'requests'],
+          entry_points = {
+        'console_scripts': [
+            'streamer = streamer.__main__:main'
+        ]
+    },
+	  zip_safe=False)
