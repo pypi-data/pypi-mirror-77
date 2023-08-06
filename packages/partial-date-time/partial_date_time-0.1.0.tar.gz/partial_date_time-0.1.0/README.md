e8b44ad1ccfc6609575395d3a03da44f0351703e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:15 +0000

    Add package partial-date-time/0.1.0/partial_date_time-0.1.0.tar.gz

---
+++ packages/partial-date-time/partial_date_time-0.1.0.tar.gz/partial_date_time-0.1.0/README.md
@@ -0,0 +1,8 @@
+# partial_date_time
+
+Released under the LGPL-3.0 License.
+
+A modified copy of iris.time.PartialDateTime from https://github.com/SciTools/iris
+to allow PartialDateTime to be imported without the need for Conda.
+
+(C) PRIMAVERA 2020
---
+++ packages/partial-date-time/partial_date_time-0.1.0.tar.gz/partial_date_time-0.1.0/setup.py
@@ -0,0 +1,13 @@
+# Copyright PRIMAVERA
+#
+# This file is released under the LGPL license.
+# See LICENSE in the root of the repository for full licensing details.
+import setuptools
+from distutils.core import setup
+
+
+setup(name='partial_date_time',
+      version='0.1.0',
+      description='Iris PartialDateTime functionality',
+      url='https://github.com/PRIMAVERA-H2020/partial_date_time',
+      packages=['partial_date_time'])
