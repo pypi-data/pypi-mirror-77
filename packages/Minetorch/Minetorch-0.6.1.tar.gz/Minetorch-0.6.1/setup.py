86a0d5268950891d864604d2bdf10e002f8f5e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:10 +0000

    Add package Minetorch/0.6.1/Minetorch-0.6.1.tar.gz

---
+++ packages/Minetorch/Minetorch-0.6.1.tar.gz/Minetorch-0.6.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Minetorch',
+    description='A tools collection for pytorch users',
+    version='0.6.1',
+    packages=['minetorch'],
+    url="https://github.com/louis-she/minetorch",
+    author='louis',
+    author_email='chenglu.she@gmail.com',
+    keywords='pytorch minecraft',
+    install_requires=[
+        'tensorboardX',
+        'torch',
+        'matplotlib',
+        'albumentations',
+        'seaborn',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib'
+    ],
+    entry_points = {
+        'console_scripts': ['minetorch=minetorch.command_line:main'],
+    }
+)
