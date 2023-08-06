2a7f5dc82348333922aa3fdf88fb8b2775878aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:58 +0000

    Add package pytest-mock/3.3.0/pytest-mock-3.3.0.tar.gz

---
+++ packages/pytest-mock/pytest-mock-3.3.0.tar.gz/pytest-mock-3.3.0/src/pytest_mock/__init__.py
@@ -0,0 +1,12 @@
+from pytest_mock.plugin import *
+
+__all__ = [
+    "MockerFixture",
+    "pytest_addoption",
+    "pytest_configure",
+    "session_mocker",
+    "package_mocker",
+    "module_mocker",
+    "class_mocker",
+    "mocker",
+]
