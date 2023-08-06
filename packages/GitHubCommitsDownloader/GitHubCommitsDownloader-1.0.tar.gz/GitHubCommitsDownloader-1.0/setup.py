73cab01a34e4bd4a42375b820e52b274f53af467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:29 +0000

    Add package GitHubCommitsDownloader/1.0/GitHubCommitsDownloader-1.0.tar.gz

---
+++ packages/GitHubCommitsDownloader/GitHubCommitsDownloader-1.0.tar.gz/GitHubCommitsDownloader-1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+  name = 'GitHubCommitsDownloader',
+  packages = ['GitHubCommitsDownloader'],
+  version = '1.0',
+  license = 'MIT',
+  description = 'Github Commits Downloader App',
+  author = 'MDReal',
+  author_email = 'vminecrafter2015@gmail.com',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+  url = 'https://github.com/MDReal32/GitHubCommitsDownloader',
+  download_url = 'https://github.com/MDReal32/GitHubCommitsDownloader/archive/master.zip',
+  keywords = ['ss', 'github', 'commits', 'downloader', 'github commits downloader', 'githubcommitsdownloader'],
+  install_requires=[
+		'requests',
+		'termcolor'
+	],
+	entry_points = {
+		'console_scripts': [
+			'ghcd=GitHubCommitsDownloader.exec:run',
+			'GitHubCommitDownloader=GitHubCommitsDownloader.exec:run'
+		]
+	},
+  classifiers = [
+    # 'Development Status :: 1 - Alpha',
+    # 'Intended Audience :: Developers',
+    # 'Topic :: Software Development :: Build Tools',
+    # 'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+  ],
+	python_requires='>=3.8',
+)
