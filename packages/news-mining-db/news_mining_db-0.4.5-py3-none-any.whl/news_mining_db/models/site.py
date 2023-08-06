0bdd58923adc15338180815b5c762f45881ef635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package news-mining-db/0.4.5/news_mining_db-0.4.5-py3-none-any.whl

---
+++ packages/news-mining-db/news_mining_db-0.4.5-py3-none-any.whl/news_mining_db/models/site.py
@@ -0,0 +1,13 @@
+from news_mining_db.models.base_model import Base
+from sqlalchemy import Column, Integer, String
+
+
+class Site(Base):
+    __tablename__ = 'site'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(32))
+
+    rss_link = Column(String(64))
+    link = Column(String(64))
