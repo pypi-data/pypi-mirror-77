a4d1342a226a91435a7b52e2729decae247ab0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package abdoTheBest/0.7.6/abdoTheBest-0.7.6.tar.gz

---
+++ packages/abdoTheBest/abdoTheBest-0.7.6.tar.gz/abdoTheBest-0.7.6/genn/__init__.py
@@ -0,0 +1,6 @@
+from genn import importers
+from genn import controllers
+from genn.lstm import LSTMGenerator
+from genn.gru import GRUGenerator
+from genn.Document import Document
+from genn.preprocessing import Preprocessing
