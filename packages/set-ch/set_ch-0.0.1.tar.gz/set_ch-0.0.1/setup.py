cedd6ce7f8eda7b3a2e302b61f053f0c02f91657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package set-ch/0.0.1/set_ch-0.0.1.tar.gz

---
+++ packages/set-ch/set_ch-0.0.1.tar.gz/set_ch-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="set_ch",
+    version="0.0.1",
+    author="gmj021",
+    author_email="13002152679@163.com",
+    description="a package set chinese",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gmj021/set_ch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+)
