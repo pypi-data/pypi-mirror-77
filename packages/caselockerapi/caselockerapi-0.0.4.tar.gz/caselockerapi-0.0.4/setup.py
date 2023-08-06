dcf393fc123b627a0f838dd3ffe762f1e10a2e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package caselockerapi/0.0.4/caselockerapi-0.0.4.tar.gz

---
+++ packages/caselockerapi/caselockerapi-0.0.4.tar.gz/caselockerapi-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="caselockerapi",
+    version="0.0.4",
+    author="Litigation Locker",
+    author_email="trennon@litigationlocker.com",
+    description="Python API to interact with CaseLocker installation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://litigationlocker.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
