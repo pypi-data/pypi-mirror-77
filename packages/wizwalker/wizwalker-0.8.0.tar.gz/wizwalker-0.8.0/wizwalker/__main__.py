6eebc6dbfc6102843cfdf902cad4a247c858140f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package wizwalker/0.8.0/wizwalker-0.8.0.tar.gz

---
+++ packages/wizwalker/wizwalker-0.8.0.tar.gz/wizwalker-0.8.0/wizwalker/__main__.py
@@ -0,0 +1,40 @@
+import sys
+import asyncio
+from pathlib import Path
+
+from wizwalker import WizWalker
+from wizwalker.cli import WizWalkerConsole
+
+# https://pyinstaller.readthedocs.io/en/stable/runtime-information.html
+if getattr(sys, "frozen", False):
+    root = Path(sys.executable).parent
+else:
+    root = Path(__file__).parent.parent
+
+cache_dir = root / "cache"
+if not cache_dir.exists():
+    cache_dir.mkdir()
+
+
+async def main():
+    if sys.platform != "win32":
+        raise RuntimeError(f"This program is windows only, not {sys.platform}")
+
+    walker = WizWalker()
+    console = WizWalkerConsole(walker)
+    await console.interact()
+    await walker.close()
+
+    # app = WizWalker()
+    #
+    # # close WizWalker when app is closed
+    # atexit.register(app.close)
+    #
+    # app.run()
+
+
+def sync_main():
+    asyncio.run(main())
+
+if __name__ == "__main__":
+    sync_main()
