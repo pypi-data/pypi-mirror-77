f1c2cdc42970d42e1d7ef7eca4414d268dac1235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:05 +0000

    Add package gease/0.0.6/gease-0.0.6.tar.gz

---
+++ packages/gease/gease-0.0.6.tar.gz/gease-0.0.6/gease/exceptions.py
@@ -0,0 +1,22 @@
+class ReleaseExistException(Exception):
+    pass
+
+
+class RepoNotFoundError(Exception):
+    pass
+
+
+class GitHubException(Exception):
+    pass
+
+
+class NoGeaseConfigFound(Exception):
+    pass
+
+
+class AbnormalGithubResponse(Exception):
+    pass
+
+
+class UnhandledException(Exception):
+    pass
