ac656bc58e52052a68e1d2b9a99e36adc9ed8d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:44 +0000

    Add package torrent-tracker-scraper/3.1.2/torrent-tracker-scraper-3.1.2.tar.gz

---
+++ packages/torrent-tracker-scraper/torrent-tracker-scraper-3.1.2.tar.gz/torrent-tracker-scraper-3.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torrent-tracker-scraper",
+    version="3.1.2",
+    author="azerty",
+    packages=["torrent_tracker_scraper"],
+    author_email="kenokech94@gmail.com",
+    description="A UDP torrent tracker scraper written in Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/49e94b8f256530dc0d41f740dfe8a4c1/torrent-tracker-scraper",
+    # packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=["requests"],
+)
