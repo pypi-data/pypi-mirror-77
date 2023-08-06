c668ab8d0e1fd903c0a8cf6d36f08d767106f3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:05 +0000

    Add package libpymath/0.0.3/libpymath-0.0.3.tar.gz

---
+++ packages/libpymath/libpymath-0.0.3.tar.gz/libpymath-0.0.3/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils import setup, Extension
+
+ext_modules = [
+    Extension(
+        "libpymath.testModule",
+        ["LibPyMath/LibPyMathModules/testModule.c"],
+    )
+]
+
+setup(
+    name="libpymath",
+    version="0.0.3",
+    description="A general purpose Python math module",
+    long_description="",
+    author="Toby Davis",
+    author_email="pencilcaseman@gmail.com",
+    url="https://www.github.com/pencilcaseman/gpc",
+    ext_modules=ext_modules,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
