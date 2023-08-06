6a888992f44dce522a1fc61ae1f1306ddb57f8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package webspace/2.7.173/webspace-2.7.173.tar.gz

---
+++ packages/webspace/webspace-2.7.173.tar.gz/webspace-2.7.173/src/webspace/cms/static/webspace/assets/js/smc.js
@@ -0,0 +1,15 @@
+import Cms from './helpers/Cms';
+
+
+(function initCMS() {
+  Cms.SmoothScroll.init();
+  Cms.Carousel.init();
+  Cms.Clicker.init();
+  Cms.Aside.mobile();
+  Cms.Lazy.init();
+  Cms.Collect.init();
+  Cms.Accordion.init();
+  Cms.CounterUp.init();
+  Cms.Animation.init();
+  Cms.BgSlide.init();
+}());
