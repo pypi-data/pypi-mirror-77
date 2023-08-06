ec9a5f7d24bdab65c001b560b7f5108d15ba0aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package web-ctf-analyser/0.0.3/web_ctf_analyser-0.0.3-py3-none-any.whl

---
+++ packages/web-ctf-analyser/web_ctf_analyser-0.0.3-py3-none-any.whl/analyser/helpers/analysis.py
@@ -0,0 +1,28 @@
+from analyser.utils.utils import url_regex, resource_regex, file_regex
+import analyser.utils.log as log
+
+from urllib.parse import unquote
+
+def redirect(url):
+    if '?' not in url:
+        log.fail('Redirect contains no GET parameters', indent=2)
+        return
+    
+
+    # URL Decode
+    params = unquote(url.split('?')[1]).split('&')
+
+    for p in params:
+        name, value = p.split('=')
+
+        if re.match(url_regex, value):
+            log.success('Redirect appears to contain a URL - possible RFI or use of another URL scheme?', indent=2)
+            continue
+        
+        if re.match(resource_regex, value):
+            log.success('Redirect appears to contain a resource - possible LFI?', indent=2)
+            continue
+
+        if re.match(file_regex, value):
+            log.success('Redirect appears to contain a filename - possible LFI?', indent=2)
+            continue
