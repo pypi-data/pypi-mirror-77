2ded11429461d08f29b28e0bd0a972ebcd940a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:22 +0000

    Add package python-seed/1.0.0.post1/python-seed-1.0.0.post1.tar.gz

---
+++ packages/python-seed/python-seed-1.0.0.post1.tar.gz/python-seed-1.0.0.post1/setup.py
@@ -0,0 +1,42 @@
+"""Setup python-seed"""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+# Runtime requirements.
+inst_reqs = ["click"]
+
+extra_reqs = {
+    "test": ["pytest", "pytest-cov"],
+    "dev": ["pytest", "pytest-cov", "pre-commit"],
+}
+
+setup(
+    name="python-seed",
+    version="1.0.0.post1",
+    description="Create skeleton of python project",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author=u"Vincent Sarago",
+    author_email="vincent@developmentseed.com",
+    url="https://github.com/developementseed/python-seed",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="Python Generator tox pre-commit",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+    entry_points={"console_scripts": ["pyseed = python_seed.scripts.cli:pyseed"]},
+)
