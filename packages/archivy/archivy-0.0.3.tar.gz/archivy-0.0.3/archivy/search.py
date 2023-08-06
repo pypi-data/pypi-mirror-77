78c3f32f2b68815e98840d0cc829608f186128f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:23 +0000

    Add package archivy/0.0.3/archivy-0.0.3.tar.gz

---
+++ packages/archivy/archivy-0.0.3.tar.gz/archivy-0.0.3/archivy/search.py
@@ -0,0 +1,27 @@
+from archivy.extensions import elastic_client
+
+ELASTICSEARCH = elastic_client()
+
+def add_to_index(index, model):
+    if not ELASTICSEARCH:
+        return
+    payload = {}
+    for field in model.__searchable__:
+        payload[field] = getattr(model, field)
+    ELASTICSEARCH.index(index=index, id=model.id, body=payload)
+
+def remove_from_index(index, dataobj_id):
+    if not ELASTICSEARCH:
+        return
+    ELASTICSEARCH.delete(index=index, id=dataobj_id)
+
+def query_index(index, query):
+    if not ELASTICSEARCH:
+        return []
+    search = ELASTICSEARCH.search(
+        index=index,
+        body={
+            "query": {"multi_match": {"query": query, "fields": ["*"], "analyzer": "rebuilt_standard"}}},
+        )
+
+    return search["hits"]["hits"]
