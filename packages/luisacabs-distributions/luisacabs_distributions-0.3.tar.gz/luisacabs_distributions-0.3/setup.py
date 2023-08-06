b3a7baaf5145dabef07ff812ec58d36702cc2f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package luisacabs-distributions/0.3/luisacabs_distributions-0.3.tar.gz

---
+++ packages/luisacabs-distributions/luisacabs_distributions-0.3.tar.gz/luisacabs_distributions-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='luisacabs_distributions',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['luisacabs_distributions'],
+      zip_safe=False)
