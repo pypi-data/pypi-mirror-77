7dd57ec8f323e549ee6e3fa976aadba6c2d7559d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:42 +0000

    Add package epispot/1.0.0/epispot-1.0.0-py3-none-any.whl

---
+++ packages/epispot/epispot-1.0.0-py3-none-any.whl/epispot/__init__.py
@@ -0,0 +1,20 @@
+"""
+Doc strings provide additional documentation on a certain function.
+They can be accessed by the built-in Python `help()` command.
+These strings are formatted in Github-flavored markdown.
+Additionally, all files will have a 'STRUCTURE' label.
+"""
+
+# imports
+import warnings
+import random
+from matplotlib import pyplot as plt
+from matplotlib import colors
+
+# local
+from . import comps
+from . import models
+from . import plots
+
+# version info
+__version__ = 'v1.0.0'
