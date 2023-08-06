167ca38cb9c5ee3eb818d9c84734ce6b067b4f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:33 +0000

    Add package droit/1.0.3/droit-1.0.3-py3-none-any.whl

---
+++ packages/droit/droit-1.0.3-py3-none-any.whl/droit/loader.py
@@ -0,0 +1,87 @@
+# loader.py - parse Droit Databases
+# Copyright 2020 - Jakob Stolze
+#
+# This file is part of python-droit (https://github.com/jarinox/python-droit)
+
+
+import xml.etree.cElementTree as _ET
+import droit.models as _models
+import droit.legacy as _legacy
+import droit.tools as _tools
+
+
+def parseDroitXML(filename):
+	"""Parse a Droit XML Database"""
+	tree = _ET.parse(filename)
+	root = tree.getroot()
+	rules = []
+	for child in root:
+		if(child.tag == "droitxml"):
+			for rule in child:
+				if(rule.tag == "rule"):
+					inputrules = []
+					outputrules = []
+					for inrules in rule:
+						if(inrules.tag == "input"):
+							for inrule in inrules:
+								children = []
+								for inchild in inrule:
+									if(inchild.text != None):
+										children.append(inchild.text)
+								dri = _models.DroitRuleInOut(inrule.tag.upper(), inrule.attrib, children, "input")
+								inputrules.append(dri)
+					for outrules in rule:
+						if(outrules.tag == "output"):
+							for outrule in outrules:
+								children = []
+								for outchild in outrule:
+									if(outchild.text != None):
+										children.append(outchild.text)
+								dro = _models.DroitRuleInOut(outrule.tag.upper(), outrule.attrib, children, "output")
+								outputrules.append(dro)
+					dr = _models.DroitRule(inputrules, outputrules)
+					rules.append(dr)
+	return rules
+
+
+def parseLegacy(filename):
+	"""Parse a legacy Droit Database (.dda)"""
+	dda = _legacy.parseDDA(filename)
+	rules = []
+	pinfos = _tools.loadPluginInfos()
+	for rule in dda:
+		inputrules = []
+		outputrules = []
+		for inrule in rule[0]:
+			attr = {}
+			inrule[1] = inrule[1].replace("&arz;", "!").replace("&dpp;", ":")
+			if("*" in inrule[0]):
+				for info in pinfos:
+					if(info.name.lower() == inrule[0].split("*")[0].lower()):
+						ipkey = list(info.attrib.keys())[0]
+						attr[ipkey] = inrule[0].split("*")[1]
+				inrule[0] = inrule[0].split("*")[0]
+
+			if("NOTX" == inrule[0]):
+				inrule[0] = "TEXT"
+				attr["not"] = "true"
+			children = inrule[1].split(",")
+			if(inrule[1] == ""):
+				children = []
+			dri = _models.DroitRuleInOut(inrule[0], attr, children, "input")
+			inputrules.append(dri)
+		for outrule in rule[1]:
+			attr = {}
+			outrule[1] = outrule[1].replace("&arz;", "!").replace("&dpp;", ":")
+			if(outrule[0].upper() == "EVAL"):
+				dro = _models.DroitRuleInOut(outrule[0].upper(), attr, [outrule[1]], "output")
+			else:
+				children = outrule[1].split(",")
+				if(outrule[1] == ""):
+					children = []
+				dro = _models.DroitRuleInOut(outrule[0].upper(), attr, children, "output")
+			outputrules.append(dro)
+		dr = _models.DroitRule(inputrules, outputrules)
+		rules.append(dr)
+	return rules
+		
