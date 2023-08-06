ece9180ca04d2276b65b8c2819544eca88b05c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:09 +0000

    Add package kollavarsham/2.1.6/kollavarsham-2.1.6.tar.gz

---
+++ packages/kollavarsham/kollavarsham-2.1.6.tar.gz/kollavarsham-2.1.6/src/kollavarsham/_jsii/__init__.py
@@ -0,0 +1,19 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "kollavarsham", "2.1.6", __name__[0:-6], "kollavarsham@2.1.6.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
