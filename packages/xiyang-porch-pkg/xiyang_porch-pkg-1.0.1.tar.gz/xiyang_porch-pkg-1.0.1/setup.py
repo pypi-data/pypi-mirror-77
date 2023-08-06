21212a12cbf79f9fdcaecf5ee8507c2341b4c881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:52 +0000

    Add package xiyang-porch-pkg/1.0.1/xiyang_porch-pkg-1.0.1.tar.gz

---
+++ packages/xiyang-porch-pkg/xiyang_porch-pkg-1.0.1.tar.gz/xiyang_porch-pkg-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xiyang_porch-pkg",  # 名称为以后pip安装包的名字，后面最好加上用户名，避免名称冲突
+    version="1.0.1",
+    author="Xiyang",
+    author_email="838568501@qq.com",
+    description="morph package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
