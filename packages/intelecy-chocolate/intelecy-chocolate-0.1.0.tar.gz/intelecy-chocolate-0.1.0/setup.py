b68d1230f28a68064de84b169ab94cce5af72613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:25 +0000

    Add package intelecy-chocolate/0.1.0/intelecy-chocolate-0.1.0.tar.gz

---
+++ packages/intelecy-chocolate/intelecy-chocolate-0.1.0.tar.gz/intelecy-chocolate-0.1.0/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chocolate',
+ 'chocolate.conditional',
+ 'chocolate.connection',
+ 'chocolate.crossvalidation',
+ 'chocolate.mo',
+ 'chocolate.sample',
+ 'chocolate.search']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataset>=0.8,<0.9',
+ 'filelock>=2.0,<3.0',
+ 'ghalton>=0.6,<0.7',
+ 'numpy==1.16.4',
+ 'pandas>=0.19,<0.20',
+ 'psycopg2-binary>=2.8.5,<3.0.0',
+ 'pymongo>=3.4,<4.0',
+ 'scikit-learn>=0.18,<0.19',
+ 'scipy>=0.18,<0.19',
+ 'sphinx>=1.5,<2.0']
+
+setup_kwargs = {
+    'name': 'intelecy-chocolate',
+    'version': '0.1.0',
+    'description': 'Intelecy fork of Chocolate',
+    'long_description': None,
+    'author': 'areeh',
+    'author_email': 'are.haartveit@intelecy.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
