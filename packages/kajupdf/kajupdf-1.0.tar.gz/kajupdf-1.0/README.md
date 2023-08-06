b9d6d7deb3a1700cb5842cc55f286bed2b57ab07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:44 +0000

    Add package kajupdf/1.0/kajupdf-1.0.tar.gz

---
+++ packages/kajupdf/kajupdf-1.0.tar.gz/kajupdf-1.0/README.md
@@ -0,0 +1,1 @@
+this is tghe homepage of our project.
---
+++ packages/kajupdf/kajupdf-1.0.tar.gz/kajupdf-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="kajupdf",
+    version=1.0,
+    long_description=Path("README.MD").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
