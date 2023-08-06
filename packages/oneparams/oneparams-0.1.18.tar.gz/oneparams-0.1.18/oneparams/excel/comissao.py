7af028060d9a6e7d5e54cfd9f745a1507e8d2b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:11 +0000

    Add package oneparams/0.1.18/oneparams-0.1.18.tar.gz

---
+++ packages/oneparams/oneparams-0.1.18.tar.gz/oneparams-0.1.18/oneparams/excel/comissao.py
@@ -0,0 +1,29 @@
+from oneparams.api.commission import Commission
+from oneparams.excel.excel import Excel
+from oneparams.utils import get_names
+
+
+def comissao(book):
+    ex = Excel(book, "servico")
+
+    ex.add_column(key="servico", name="nome")
+    ex.add_column(key="cols", name="profissionais")
+
+    one = Commission()
+
+    for row in range(2, ex.nrows):
+        data = ex.data_row(row)
+        if data["cols"] is None:
+            continue
+
+        data["cols"] = get_names(data["cols"])
+
+        proc_data = []
+        for i in data["cols"]:
+            proc_data.append({
+                "cols": i,
+                "servico": data["servico"],
+                "ativo": True
+            })
+        for i in proc_data:
+            one.comissao(i)
