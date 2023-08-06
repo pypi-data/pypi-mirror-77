68d63defdefceafdc896a81fe3809529da4565a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:43 +0000

    Add package pytf2/3.8.0/pytf2-3.8.0.tar.gz

---
+++ packages/pytf2/pytf2-3.8.0.tar.gz/pytf2-3.8.0/pytf2/item_data.py
@@ -0,0 +1,33 @@
+effects = {}
+
+qualities = {"Normal": 0,
+             "Genuine": 1,
+             "Vintage": 3,
+             "rarity3": 4,
+             "Unusual": 5,
+             "Unique": 6,
+             "Community": 7,
+             "Valve": 8,
+             "Self-Made": 9,
+             "Customized": 10,
+             "Strange": 11,
+             "Completed": 12,
+             "Haunted": 13,
+             "Collector's": 14,
+             "Decorated Weapon": 15}
+
+killstreaks = {"None": 0,
+               "Killstreak": 1,
+               "Specialized Killstreak": 2,
+               "Professional Killstreak": 3}
+
+wear = ["Factory New",
+        "Minimal Wear",
+        "Field-Tested",
+        "Well-Worn",
+        "Battle Scarred"]
+wear_brackets = ["(Factory New)",
+                 "(Minimal Wear)",
+                 "(Field-Tested)",
+                 "(Well-Worn)",
+                 "(Battle Scarred)"]
