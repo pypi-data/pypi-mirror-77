c57197393acbc415c8dc09ef1432f385314d5ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:29 +0000

    Add package supermath101010/1.0/supermath101010-1.0.tar.gz

---
+++ packages/supermath101010/supermath101010-1.0.tar.gz/supermath101010-1.0/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name='supermath101010',
+    version='1.0',
+    description='第一个对外发布模块，测试',
+    author='qi',
+    author_email='1143041058@qq.com',
+    py_modules=['supermath101010.demo1','supermath101010.demo2']
+)
