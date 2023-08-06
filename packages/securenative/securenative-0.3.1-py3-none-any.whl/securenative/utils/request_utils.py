0a151daa4dabc8a74e408f003ce2f85d77bcb6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package securenative/0.3.1/securenative-0.3.1-py3-none-any.whl

---
+++ packages/securenative/securenative-0.3.1-py3-none-any.whl/securenative/utils/request_utils.py
@@ -0,0 +1,29 @@
+class RequestUtils(object):
+    SECURENATIVE_COOKIE = "_sn"
+    SECURENATIVE_HEADER = "x-securenative"
+
+    @staticmethod
+    def get_secure_header_from_request(headers):
+        try:
+            return headers[RequestUtils.SECURENATIVE_HEADER]
+        except Exception:
+            return ""
+
+    @staticmethod
+    def get_client_ip_from_request(request):
+        try:
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[-1].strip()
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+            return ip
+        except Exception:
+            return request.environ.get('HTTP_X_FORWARDED_FOR', request.environ.get('REMOTE_ADDR', ""))
+
+    @staticmethod
+    def get_remote_ip_from_request(request):
+        try:
+            return request.raw._original_response.fp.raw._sock.getpeername()[0]
+        except Exception:
+            return ""
