13480816addaf5b8e207c70bf770e7150deb0f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:06 +0000

    Add package py2pdf/0.0/py2pdf-0.0.tar.gz

---
+++ packages/py2pdf/py2pdf-0.0.tar.gz/py2pdf-0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(name='py2pdf',
+      version='0.0',
+      description='Python source to pdf conversion library.',
+      author='C. Ecker',
+      author_email='textmodelview@gmail.com',
+      url='https://pypi.python.org/pypi/py2pdf',
+      python_requires="<3.0",
+      license='BSD',
+      py_modules = ['py2pdf'],
+      platforms = ['any'],
+     )
