5f6394a70867a9deadf8b62d66e3d42f7b0698c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:34 +0000

    Add package climetlab/0.0.58/climetlab-0.0.58.tar.gz

---
+++ packages/climetlab/climetlab-0.0.58.tar.gz/climetlab-0.0.58/climetlab/helpers/pandas.py
@@ -0,0 +1,78 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
+#
+
+
+class PandasHelper:
+    def __init__(self, frame, margins=0, column=None, **kwargs):
+
+        self.frame = frame
+        self.kwargs = kwargs
+        self.margins = margins
+        self.column = column
+
+        if "lat@hdr" in self.frame:
+            self.lat = "lat@hdr"
+            self.lon = "lon@hdr"
+        elif "lat" in self.frame:
+            self.lat = "lat"
+            self.lon = "lon"
+        else:
+            self.lat = "latitude"
+            self.lon = "longitude"
+
+    def plot_map(self, driver):
+
+        north = self.frame[self.lat].max()
+        south = self.frame[self.lat].min()
+
+        lons1 = self.frame[self.lon]
+        east1 = lons1.max()
+        west1 = lons1.min()
+
+        lons2 = self.frame[self.lon] % 360
+        east2 = lons2.max()
+        west2 = lons2.min()
+
+        if abs(east1 - west1) <= abs(east2 - west2):
+            east, west = east1, west1
+        else:
+            east, west = east2, west2
+
+        driver.bounding_box(
+            north=north + self.margins,
+            south=south - self.margins,
+            west=west - self.margins,
+            east=east + self.margins,
+        )
+
+        if self.column is None:
+            column = self.lat
+        else:
+            column = self.column
+
+        driver.plot_pandas(self.frame, self.lat, self.lon, column)
+        driver.apply_kwargs(self.kwargs)
+
+    def bounding_box(self):
+
+        north, east = self.frame[[self.lat, self.lon]].max()
+        south, west = self.frame[[self.lat, self.lon]].min()
+
+        return [
+            north + self.margins,
+            west - self.margins,
+            south - self.margins,
+            east + self.margins,
+        ]
+
+    def dates(self):
+        return sorted(set([str(x).split("T")[0] for x in self.frame["time"].values]))
+
+
+helper = PandasHelper
