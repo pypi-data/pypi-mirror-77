3b3a982ad765ceb021277cdf06b510099564a60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:41 +0000

    Add package streamlit-cropper/0.1.0/streamlit_cropper-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-cropper/streamlit_cropper-0.1.0-py3-none-any.whl/streamlit_cropper/frontend/build/precache-manifest.642783f2aff575ebdb6f76014d13da4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca407cc0a9da5963e595db23f464cab7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b307e3626cd63564ea52",
+    "url": "./static/js/2.13ce121a.chunk.js"
+  },
+  {
+    "revision": "d6226ead92df551047503e21c60dd32c",
+    "url": "./static/js/2.13ce121a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d4d3e2ad7585678790",
+    "url": "./static/js/main.9ad33065.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.9ad33065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
---
+++ packages/streamlit-cropper/streamlit_cropper-0.1.0-py3-none-any.whl/streamlit_cropper/frontend/build/precache-manifest.91bf67f244703ec63e379e3c823188c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b9b0939e83b1e35c3bbbf27820065",
+    "url": "./index.html"
+  },
+  {
+    "revision": "516636697141b36da5cb",
+    "url": "./static/js/2.2b7c2927.chunk.js"
+  },
+  {
+    "revision": "d6226ead92df551047503e21c60dd32c",
+    "url": "./static/js/2.2b7c2927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4a83ea2e3ede6d60c9",
+    "url": "./static/js/main.f23bc6ca.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.f23bc6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
