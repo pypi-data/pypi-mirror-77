3e5960c2a68f91a0a8b2c3f9572124d8551c4f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package brambox/3.0.1/brambox-3.0.1-cp38-cp38-win32.whl

---
+++ packages/brambox/brambox-3.0.1-cp38-cp38-win32.whl/brambox/__init__.py
@@ -0,0 +1,10 @@
+#
+# BRAMBOX: Basic Recipes for Annotation Munching toolBOX
+# Copyright EAVISE
+#
+from ._log import *
+from ._version import __version__
+
+from . import io
+from . import stat
+from . import util
